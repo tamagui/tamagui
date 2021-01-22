d8027e7c95f054de2a4921ef94958bfa64500f60
natew <natewienert@gmail.com>
2021-01-22 10:12:18 -0700

    [snackui] media query keep in layout effect

--- packages/snackui/src/hooks/useMedia.ts
+++ packages/snackui/src/hooks/useMedia.ts
@@ -130,21 +130,23 @@ export const useMedia = () => {
   state.current.isRendering = true
 
   // track usage
-  const st = state.current
-  st.isRendering = false
-  // delete old
-  for (const key in st.selections) {
-    if (!(key in st.nextSelections)) {
-      mediaQueryListeners[key].delete(forceUpdate)
+  useLayoutEffect(() => {
+    const st = state.current
+    st.isRendering = false
+    // delete old
+    for (const key in st.selections) {
+      if (!(key in st.nextSelections)) {
+        mediaQueryListeners[key].delete(forceUpdate)
+      }
     }
-  }
-  // add new
-  for (const key in st.nextSelections) {
-    if (!(key in st.selections)) {
-      mediaQueryListeners[key] = mediaQueryListeners[key] || new Set()
-      mediaQueryListeners[key].add(forceUpdate)
+    // add new
+    for (const key in st.nextSelections) {
+      if (!(key in st.selections)) {
+        mediaQueryListeners[key] = mediaQueryListeners[key] || new Set()
+        mediaQueryListeners[key].add(forceUpdate)
+      }
     }
-  }
+  })
 
   // unmount
   useLayoutEffect(() => {
