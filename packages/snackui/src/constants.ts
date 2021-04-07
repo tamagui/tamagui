e953adc3dfd6562ec32a294578f6caf5cf4e77b5
natew <natewienert@gmail.com>
2021-04-06 17:20:06 -0700

    [snackui] web mode only when specifically set

--- packages/snackui/src/constants.ts
+++ packages/snackui/src/constants.ts
@@ -1,7 +1,8 @@
 // dont import things
 
-export const isWeb = !process.env.TARGET || process.env.TARGET === 'web'
+export const isWeb = process.env.TARGET === 'web'
 export const isWebIOS =
+  isWeb &&
   typeof window !== 'undefined' &&
   (/iPad|iPhone|iPod/.test(navigator.platform) ||
     (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1)) &&
