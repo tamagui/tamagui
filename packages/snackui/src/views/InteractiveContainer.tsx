ebc1860e3a88c23c1415c7608f4cb397970d9fff
natew <natewienert@gmail.com>
2021-08-11 09:30:40 -0700

    [app] various design, breadcrumbs

--- packages/snackui/src/views/InteractiveContainer.tsx
+++ packages/snackui/src/views/InteractiveContainer.tsx
@@ -8,7 +8,7 @@ export const InteractiveContainer = (props: StackProps) => {
   const theme = useTheme()
   return (
     <HStack
-      borderRadius={10}
+      borderRadius={100}
       borderWidth={1}
       borderColor={theme.borderColor}
       hoverStyle={{
