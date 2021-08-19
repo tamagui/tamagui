08522e8b9c6be781766b09e404121a9498fdd754
natew <natewienert@gmail.com>
2021-08-18 17:06:58 -0700

    [snackui] UnorderedList use Paragraph

--- packages/snackui/src/views/UnorderedList.tsx
+++ packages/snackui/src/views/UnorderedList.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Paragraph } from 'snackui/src'
 import { StackProps } from '../StackProps'
 import { getSizedTextProps } from './getSizedTextProps'
 import { SizableTextProps } from './SizableTextProps'
 import { HStack, VStack } from './Stacks'
-import { Text } from './Text'
+
 
 export const UnorderedList = (props: StackProps) => {
   return <VStack paddingLeft={20} {...props} />
@@ -14,8 +14,8 @@ export const UnorderedListItem = (props: SizableTextProps) => {
   const { fontSize = 14, lineHeight, ...rest } = getSizedTextProps(props)
   return (
     <HStack marginVertical={4}>
-      <Text {...props} fontSize={fontSize * 2} lineHeight={fontSize * 1.333}>{`\u2022`}</Text>
-      <Text
+      <Paragraph {...props} fontSize={fontSize * 2} lineHeight={fontSize * 1.333}>{`\u2022`}</Paragraph>
+      <Paragraph
         flex={1}
         paddingLeft={fontSize * 0.5}
         fontSize={fontSize}
