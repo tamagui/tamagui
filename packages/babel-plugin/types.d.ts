316d013fe7c3fa84a13d5610f71edbf85473fb6a
natew <natewienert@gmail.com>
2021-09-23 11:40:57 -0700

    [snackui] move to simpler types setup

--- packages/babel-plugin/types.d.ts
+++
@@ -1,5 +0,0 @@
-declare module "@snackui/babel-plugin" {
-    const _default: any;
-    export default _default;
-}
-//# sourceMappingURL=types.d.ts.map
--- packages/snackui-loader/types.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "snackui-loader" {
-    const _default: any;
-    export default _default;
-}
-
-declare module "snackui-loader" {
-    export function loader(this: any, source: string): any;
-}
-//# sourceMappingURL=types.d.ts.map
