66677628511b91954591c7adda4e0606e16208f4
natew <natewienert@gmail.com>
2021-09-27 15:23:45 -0700

    [snackui] Suspense SnackUIProvider

--- packages/snackui/src/views/SnackUIProvider.tsx
+++ packages/snackui/src/views/SnackUIProvider.tsx
@@ -1,5 +1,5 @@
 import { OverlayProvider } from '@react-native-aria/overlays'
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Platform } from 'react-native'
 import {
   Metrics,
@@ -11,10 +11,10 @@ import { ThemeProvider, ThemeProviderProps } from '../hooks/useTheme'
 
 export type SnackUIProviderProps = ThemeProviderProps & {
   initialWindowMetrics?: any
+  fallback?: any
 }
 
-// https://github.com/th3rdwave/react-native-safe-area-context/issues/132
-const defaultInitialWindowMetricsBasedOnPlatform: Metrics | null = Platform.select({
+const defaultMetrics: Metrics | null = Platform.select({
   web: {
     frame: { x: 0, y: 0, width: 0, height: 0 },
     insets: { top: 0, left: 0, right: 0, bottom: 0 },
@@ -26,15 +26,18 @@ export const SnackUIProvider = ({
   defaultTheme,
   themes,
   initialWindowMetrics,
+  fallback,
   children,
 }: SnackUIProviderProps) => {
   return (
     <OverlayProvider>
       <SafeAreaProvider
-        initialMetrics={initialWindowMetrics ?? defaultInitialWindowMetricsBasedOnPlatform}
+        initialMetrics={initialWindowMetrics ?? defaultMetrics}
       >
         <ThemeProvider defaultTheme={defaultTheme} themes={themes}>
-          {children}
+          <Suspense fallback={fallback || null}>
+            {children}
+          </Suspense>
         </ThemeProvider>
       </SafeAreaProvider>
     </OverlayProvider>
