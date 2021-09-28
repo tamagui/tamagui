4ef58f75882d52feec5dd0d4d4ac1dce40742ba2
natew <natewienert@gmail.com>
2021-09-27 19:59:31 -0700

    update deps

--- packages/snackui-static/tests/webpack-test.tsx
+++ packages/snackui-static/tests/webpack-test.tsx
@@ -45,7 +45,7 @@ test('2. extracts className for complex views but keeps other props', () => {
   expect(outFalse).toMatchSnapshot()
 })
 
-test('3. places className correctly given a single spread', () => {
+test('3. places className correctly given a single spread', async () => {
   const {
     test3: { Element },
   } = context
@@ -54,7 +54,7 @@ test('3. places className correctly given a single spread', () => {
   expect(list).toMatchSnapshot()
 })
 
-test('4. leaves dynamic variables', () => {
+test('4. leaves dynamic variables', async () => {
   const {
     test4: { renderer, Element },
   } = context
@@ -66,44 +66,44 @@ test('4. leaves dynamic variables', () => {
   expect(r).toMatchSnapshot()
 })
 
-test('5. spread conditional', () => {
+test('5. spread conditional', async () => {
   const { test5 } = context
   const out = test5.renderer.toJSON()
   expect(out).toMatchSnapshot()
 })
 
-test('6. spread ternary', () => {
+test('6. spread ternary', async () => {
   const { test6 } = context
   expect(test6.renderer.toJSON()).toMatchSnapshot()
   expect(test6.rendererFalse.toJSON()).toMatchSnapshot()
 })
 
-test('7. ternary + data-is', () => {
+test('7. ternary + data-is', async () => {
   const { test7 } = context
   const out = test7.renderer.toJSON()
   expect(out).toMatchSnapshot()
 })
 
-test('8. styleExpansions', () => {
+test('8. styleExpansions', async () => {
   const { test8 } = context
   const out = test8.renderer.toJSON()
   expect(out).toMatchSnapshot()
   // TODO test constant folding
 })
 
-test('9. combines with classname', () => {
+test('9. combines with classname', async () => {
   const { test9 } = context
   const out = test9.renderer.toJSON()
   expect(out).toMatchSnapshot()
 })
 
-test('10. extracts Text', () => {
+test('10. extracts Text', async () => {
   const { test10 } = context
   const out = test10.renderer.toJSON()
   expect(out).toMatchSnapshot()
 })
 
-test('11. combines everything', () => {
+test('11. combines everything', async () => {
   const {
     test11: { Element },
   } = context
@@ -113,18 +113,18 @@ test('11. combines everything', () => {
   expect(classList).toMatchSnapshot()
 })
 
-test('12. ternary multiple on same key', () => {
+test('12. ternary multiple on same key', async () => {
   const { test12 } = context
   expect(test12.renderer.toJSON()).toMatchSnapshot()
 })
 
-// test('13. text with complex conditional and local vars', () => {
+// test('13. text with complex conditional and local vars', async () => {
 //   const { test13 } = context
 //   // console.log('test13', test13.renderer!.toTree())
 //   t.is(1, 1)
 // })
 
-test('14. extracts psuedo styles and evaluates constants', () => {
+test('14. extracts psuedo styles and evaluates constants', async () => {
   const { test14 } = context
   const out = test14.renderer.toJSON()
   expect(out).toMatchSnapshot()
