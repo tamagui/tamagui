7c9f418b62ff14d086bfa9851689542cb180bbce
natew <natewienert@gmail.com>
2021-11-18 23:07:07 -0800

    site: improve buttons demo code and add ThemeInverse documentation

--- packages/site/components/demos/index.tsx
+++ packages/site/components/demos/index.tsx
@@ -1,5 +1,6 @@
 export { StacksDemo } from './StacksDemo'
 export { TextDemo } from './TextDemo'
 export { ButtonDemo } from './ButtonDemo'
+export { ThemeInverseDemo } from './ThemeInverseDemo'
 export { FormsDemo } from './FormsDemo'
 export { LinearGradientDemo } from './LinearGradientDemo'
