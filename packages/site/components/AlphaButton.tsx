648569525f5f851da30cb2c085f8d83f4670a0b8
natew <natewienert@gmail.com>
2021-11-19 22:43:12 -0800

    site: header design

---
+++ packages/site/components/AlphaButton.tsx
@@ -0,0 +1,11 @@
+import NextLink from 'next/link'
+import React from 'react'
+import { Button, YStack } from 'tamagui'
+
+export const AlphaButton = () => (
+  <NextLink href="/blog/introducing-tamagui" passHref>
+    <YStack tag="a" mx={-5} $sm={{ display: 'none' }}>
+      <Button br="$6">Alpha</Button>
+    </YStack>
+  </NextLink>
+)
