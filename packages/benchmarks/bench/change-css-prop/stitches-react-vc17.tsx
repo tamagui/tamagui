fb39c1c19ad122939825c1b92db4d6484b2b2aad
natew <natewienert@gmail.com>
2021-11-22 14:50:10 -0800

    site: update benchmarks

--- packages/benchmarks/bench/change-css-prop/stitches-react-vc17.tsx
+++ packages/benchmarks/bench/change-css-prop/stitches-react-vc17.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import { TestComponentProps, TestRunner } from '../TestRunner';
-import { buttonStyles } from '../utils/buttonStyles';
-import { styled } from '../utils/stitches-react-v025.config';
+import React from 'react'
+
+import { TestComponentProps, TestRunner } from '../TestRunner'
+import { buttonStyles } from '../utils/buttonStyles'
+import { styled } from '../utils/stitches-react-v025.config'
 
 const Button = styled('button', {
   ...(buttonStyles as any),
-});
+})
 
 const Test = ({ testIndex }: TestComponentProps) => {
+  const val = Math.random()
   return (
-    <Button
-      css={{
-        '--test-index': testIndex,
-        backgroundColor: `hsl(${Math.floor(Math.random() * 360)} 80% 80%)`,
-        padding: '20px',
-      }}
-    >
-      testing
-    </Button>
-  );
-};
+    <>
+      <Button
+        css={{
+          backgroundColor: val > 0.5 ? 'red' : 'green',
+          padding: '20px',
+          borderRadius: 10,
+          margin: 2,
+        }}
+      >
+        testing
+      </Button>
+    </>
+  )
+}
 
 const StitchesTest = () => {
   return (
@@ -27,10 +32,12 @@ const StitchesTest = () => {
       <TestRunner numberOfRuns={3} iterationN={1000} TestComponent={Test} />
 
       <div style={{ opacity: 0, pointerEvents: 'none' }}>
-        <Button>we mount the button outside the test to make sure we're not clocking any mount time</Button>
+        <Button>
+          we mount the button outside the test to make sure we're not clocking any mount time
+        </Button>
       </div>
     </>
-  );
-};
+  )
+}
 
-export default StitchesTest;
+export default StitchesTest
--- packages/core/src/helpers/addStylesUsingClassname.tsx
+++ packages/core/src/helpers/addStylesUsingClassname.tsx
@@ -1,7 +1,12 @@
+// @ts-ignore
+import { useInsertionEffect } from 'react'
+
 import { getStylesAtomic } from './getStylesAtomic'
 import { insertStyleRule } from './insertStyleRule'
 
-let added = {}
+// can be cleared occasionally
+let added = new Set<string>()
+let cached = new Map()
 
 // TODO use upcoming react method for inserting
 
@@ -15,14 +20,47 @@ export function addStylesUsingClassname(styles: any[]) {
     }
     for (const { identifier, rules } of getStylesAtomic(style)) {
       className += ` ${identifier}`
-      if (added[identifier]) {
+      if (added.has(identifier)) {
         continue
       }
-      added[identifier] = true
+      added.add(identifier)
       for (const rule of rules) {
         insertStyleRule(rule)
       }
     }
   }
+  return className
+}
+
+export function useStylesAsClassname(styles: any[]) {
+  let className = ''
+  let insertions: any[] = []
+
+  for (const style of styles.flat(1)) {
+    if (!style) continue
+    for (const key in style) {
+      const cacheKey = `${key}${style[key]}`
+      if (cached.has(cacheKey)) {
+        className += ` ${cached.get(cacheKey)}`
+        continue
+      }
+      let cns = ``
+      for (const { identifier, rules } of getStylesAtomic({ [key]: style[key] })) {
+        cns += ` ${identifier}`
+        for (const rule of rules) {
+          insertions.push(rule)
+        }
+      }
+      cached.set(cacheKey, cns)
+      className += cns
+    }
+  }
+
+  useInsertionEffect(() => {
+    for (const ruleset of insertions) {
+      insertStyleRule(ruleset)
+    }
+  })
+
   return className
 }
