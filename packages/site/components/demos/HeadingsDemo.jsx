efe77ead0fbf64d6539df51b07cc068af20e8950
natew <natewienert@gmail.com>
2021-11-25 00:52:48 -0800

    site: headings demo hero

---
+++ packages/site/components/demos/HeadingsDemo.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { H1, H2, H3, H4, H5, H6, YStack } from 'tamagui'
+
+export function HeadingsDemo() {
+  return (
+    <YStack als="center" space>
+      <H1>Heading 1</H1>
+      <H2>Heading 2</H2>
+      <H3>Heading 3</H3>
+      <H4>Heading 4</H4>
+      <H5>Heading 5</H5>
+      <H6>Heading 6</H6>
+    </YStack>
+  )
+}
