54011dbf709e97cc4655f79ecdb157876a068723
natew <natewienert@gmail.com>
2021-12-02 12:11:18 -0800

    fix(core): 🐛 `createTheme` now allows `string | Variable` on all values for better theme construction

--- packages/core/src/createTheme.ts
+++ packages/core/src/createTheme.ts
@@ -2,4 +2,8 @@ import { Variable } from './createVariable'
 
 type GenericTheme = { [key: string]: string | Variable }
 
-export const createTheme = <Theme extends GenericTheme>(theme: Theme): Theme => theme
+export const createTheme = <Theme extends GenericTheme>(
+  theme: Theme
+): {
+  [K in keyof Theme]: Theme[K] | string | Variable
+} => theme
--- packages/core/types/createTheme.d.ts
+++ packages/core/types/createTheme.d.ts
@@ -2,6 +2,6 @@ import { Variable } from './createVariable';
 declare type GenericTheme = {
     [key: string]: string | Variable;
 };
-export declare const createTheme: <Theme extends GenericTheme>(theme: Theme) => Theme;
+export declare const createTheme: <Theme extends GenericTheme>(theme: Theme) => { [K in keyof Theme]: string | Variable | Theme[K]; };
 export {};
 //# sourceMappingURL=createTheme.d.ts.map
