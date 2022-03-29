03b5d9ae47fcefcdc433030457be681bab30c860
natew <natewienert@gmail.com>
2022-03-28 16:26:31 -1000

    docs(Text): Improve Text documentation and prop docs

--- packages/site/components/demos/TextDemo.tsx
+++ packages/site/components/demos/TextDemo.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
-import { Paragraph, SizableText, Text, YStack } from 'tamagui'
+import { Paragraph, SizableText, Text, XStack, YStack } from 'tamagui'
 
 export default function TextDemo() {
   return (
     <YStack space="$2" ai="center">
-      <Text>Hello world</Text>
-      <SizableText size="$3">Hello world</SizableText>
-      <Paragraph size="$2">Hello world</Paragraph>
+      <Text>Text</Text>
+      <SizableText size="$3">SizableText</SizableText>
+      <XStack space>
+        <SizableText theme="alt1" size="$3">
+          alt1
+        </SizableText>
+        <SizableText theme="alt2" size="$3">
+          alt2
+        </SizableText>
+        <SizableText theme="alt3" size="$3">
+          alt3
+        </SizableText>
+      </XStack>
+      <Paragraph size="$2" fow="800">
+        Paragraph
+      </Paragraph>
     </YStack>
   )
 }
