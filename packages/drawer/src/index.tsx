874fa1afbcbf93fbf3267db4d5557861a2d0e9cb
natew <natewienert@gmail.com>
2022-04-01 21:13:03 -1000

    chore: fix production build

--- packages/drawer/src/index.tsx
+++ packages/drawer/src/index.tsx
@@ -1,2 +1,3 @@
+import '@tamagui/polyfill-dev'
 export { Drawer } from './Drawer'
 export { DrawerProvider } from './DrawerProvider'
--- packages/drawer/types/index.d.ts
+++ packages/drawer/types/index.d.ts
@@ -1,3 +1,4 @@
+import '@tamagui/polyfill-dev';
 export { Drawer } from './Drawer';
 export { DrawerProvider } from './DrawerProvider';
 //# sourceMappingURL=index.d.ts.map
