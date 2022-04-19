e243b35d38e03a17c18b9571ada8d10ab4563cd9
natew <natewienert@gmail.com>
2022-04-18 18:00:35 -1000

    feat(tamagui): Add A component

---
+++ packages/tamagui/types/views/A.d.ts
@@ -0,0 +1,10 @@
+import { ReactComponentWithRef } from '@tamagui/core';
+import { View } from 'react-native';
+import { SizableTextProps } from './SizableText';
+export declare type AProps = SizableTextProps & {
+    href?: string;
+    target?: string;
+    rel?: string;
+};
+export declare const A: ReactComponentWithRef<AProps, HTMLAnchorElement | View>;
+//# sourceMappingURL=A.d.ts.map
