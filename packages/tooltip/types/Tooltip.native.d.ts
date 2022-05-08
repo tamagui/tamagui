b86743309584bf44455b506ba619e367ece3132d
natew <natewienert@gmail.com>
2022-05-07 21:14:14 -1000

    fix(tooltip): render to null on native

---
+++ packages/tooltip/types/Tooltip.native.d.ts
@@ -0,0 +1,8 @@
+export declare const Tooltip: ((props: any) => any) & {
+    Anchor: (props: any) => any;
+    Arrow: (props: any) => null;
+    Close: (props: any) => null;
+    Content: (props: any) => null;
+    Trigger: (props: any) => any;
+};
+//# sourceMappingURL=Tooltip.native.d.ts.map
