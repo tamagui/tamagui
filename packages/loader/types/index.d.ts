557c9a714d1d922278e8562fb8b56ac2c627b34d
natew <natewienert@gmail.com>
2022-05-19 21:14:56 -1000

    feat(next-plugin): Allow configuration shouldExtract to extend default extraction lookups

--- packages/loader/types/index.d.ts
+++ packages/loader/types/index.d.ts
@@ -1,4 +1,4 @@
 declare const _default: any;
 export default _default;
-export declare const shouldExclude: (path: string, projectRoot: string, tamaguiOptions: any) => boolean;
+export declare const shouldExclude: (path: string, projectRoot: string) => boolean;
 //# sourceMappingURL=index.d.ts.map
