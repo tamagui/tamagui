5ba3c576fba41779cadb31de25c0f09059d3a016
natew <natewienert@gmail.com>
2022-06-10 18:42:46 -0700

    fix(tamagui): Implement autoselect on focus when labels are tapped that target Input elements

--- packages/tamagui/src/helpers/focusableInputHOC.tsx
+++ packages/tamagui/src/helpers/focusableInputHOC.tsx
@@ -1,28 +1,65 @@
 import { composeRefs } from '@tamagui/compose-refs'
-import React from 'react'
+import { isTamaguiComponent } from '@tamagui/core'
+import React, { useRef } from 'react'
 import { forwardRef, useCallback, useEffect } from 'react'
 
 import { registerFocusable, unregisterFocusable } from '../lib/registerFocusable'
 
 export function focusableInputHOC<A extends Function>(Component: A): A {
-  return forwardRef((props: { id?: string }, ref) => {
-    const inputRef = useCallback(
-      (input) => {
-        if (!props.id) return
-        registerFocusable(props.id, input)
+  return forwardRef(
+    (
+      props: {
+        id?: string
+        onChangeText?: (val: string) => void
+        value?: string
+        defaultValue?: string
       },
-      [props.id]
-    )
+      ref
+    ) => {
+      const isInput = isTamaguiComponent(Component) && Component.staticConfig.isInput
+      const inputValue = useRef(props.value || props.defaultValue || '')
 
-    const combinedRefs = composeRefs(ref, inputRef)
+      const inputRef = useCallback(
+        (input) => {
+          if (!props.id) return
+          if (!input) return
+          registerFocusable(props.id, {
+            focus: input.focus,
 
-    useEffect(() => {
-      return () => {
-        if (!props.id) return
-        unregisterFocusable(props.id)
-      }
-    }, [props.id])
+            ...(isInput && {
+              // react-native doesn't support programmatic .select()
+              focusAndSelect() {
+                input.focus()
+                if (input.setSelection && typeof inputValue.current === 'string') {
+                  input.setSelection(0, inputValue.current.length)
+                }
+              },
+            }),
+          })
+        },
+        [props.id]
+      )
 
-    return <Component ref={combinedRefs} {...props} />
-  }) as any
+      const combinedRefs = composeRefs(ref, inputRef)
+
+      useEffect(() => {
+        return () => {
+          if (!props.id) return
+          unregisterFocusable(props.id)
+        }
+      }, [props.id])
+
+      const finalProps = isInput
+        ? {
+            ...props,
+            onChangeText(value) {
+              inputValue.current = value
+              props.onChangeText?.(value)
+            },
+          }
+        : props
+
+      return <Component ref={combinedRefs} {...finalProps} />
+    }
+  ) as any
 }
