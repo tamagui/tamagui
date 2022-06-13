1695c5502dfe04b70a41b0a7ca64fbc547218649
natew <natewienert@gmail.com>
2022-06-12 19:15:46 -1000

    fix(Group): Improve sizing and border passing logic

--- packages/tamagui/src/views/Group.tsx
+++ packages/tamagui/src/views/Group.tsx
@@ -1,5 +1,6 @@
 import {
   GetProps,
+  getExpandedShorthands,
   getTokens,
   getVariableValue,
   mergeProps,
@@ -39,109 +40,109 @@ export type GroupProps = GetProps<typeof GroupFrame> & {
 }
 
 export const Group = GroupFrame.extractable(
-  forwardRef(
-    (
-      {
-        children: childrenProp,
-        space,
-        spaceDirection,
-        separator,
-        size: sizeProp = '$4',
-        scrollable,
-        vertical,
-        disabled: disabledProp,
-        disablePassBorderRadius: disablePassBorderRadiusProp,
-        disablePassSize: disablePassSizeProp,
-        ...props
-      }: GroupProps,
-      ref
-    ) => {
-      const radius = sizeProp ? getVariableValue(getTokens().radius[sizeProp]) - 1 : undefined
-      const hasRadius = typeof radius !== 'number'
-      const disablePassBorderRadius = disablePassBorderRadiusProp ?? hasRadius
-      const childrens = Children.toArray(childrenProp)
-      const disablePassSize = disablePassSizeProp ?? sizeProp === undefined
-      const children = childrens.map((child, i) => {
-        if (!isValidElement(child)) return child
-        const disabled = child.props.disabled ?? disabledProp
-        const size = child.props.size ?? sizeProp
+  forwardRef((propsIn: GroupProps, ref) => {
+    const {
+      children: childrenProp,
+      space,
+      spaceDirection,
+      separator,
+      size: sizeProp = '$4',
+      scrollable,
+      vertical,
+      disabled: disabledProp,
+      disablePassBorderRadius: disablePassBorderRadiusProp,
+      disablePassSize: disablePassSizeProp,
+      borderRadius,
+      ...props
+    } = getExpandedShorthands(propsIn)
 
-        const isFirst = i === 0
-        const isLast = i === childrens.length - 1
-        const props = {
-          disabled,
-          ...(!disablePassSize && {
-            size,
-          }),
-          ...(!disablePassBorderRadius && {
-            borderTopLeftRadius: 0,
-            borderTopRightRadius: 0,
-            borderBottomLeftRadius: 0,
-            borderBottomRightRadius: 0,
-            ...(hasRadius && {
-              ...(isFirst &&
-                !vertical && {
-                  borderTopLeftRadius: radius,
-                  borderBottomLeftRadius: radius,
-                }),
-              ...(isFirst &&
-                vertical && {
-                  borderTopLeftRadius: radius,
-                  borderTopRightRadius: radius,
-                }),
-              ...(isLast &&
-                !vertical && {
-                  borderTopRightRadius: radius,
-                  borderBottomRightRadius: radius,
-                }),
-              ...(isLast &&
-                vertical && {
-                  borderBottomLeftRadius: radius,
-                  borderBottomRightRadius: radius,
-                }),
-            }),
-          }),
-        }
-        return cloneElementWithPropOrder(child, props)
-      })
+    const radius =
+      borderRadius ?? (sizeProp ? getVariableValue(getTokens().radius[sizeProp]) - 1 : undefined)
+    const hasRadius = radius !== undefined
+    const disablePassBorderRadius = disablePassBorderRadiusProp ?? !hasRadius
+    const childrens = Children.toArray(childrenProp)
+    const disablePassSize = disablePassSizeProp ?? sizeProp === undefined
+    const children = childrens.map((child, i) => {
+      if (!isValidElement(child)) return child
+      const disabled = child.props.disabled ?? disabledProp
+      const size = child.props.size ?? sizeProp
 
-      const wrapScroll = (children: any) => {
-        if (scrollable)
-          return (
-            <ScrollView
-              {...(vertical && {
-                showsVerticalScrollIndicator: false,
-              })}
-              {...(!vertical && {
-                horizontal: true,
-                showsHorizontalScrollIndicator: false,
-              })}
-            >
-              {children}
-            </ScrollView>
-          )
-        return children
+      const isFirst = i === 0
+      const isLast = i === childrens.length - 1
+      const props = {
+        disabled,
+        ...(!disablePassSize && {
+          size,
+        }),
+        ...(!disablePassBorderRadius && {
+          borderTopLeftRadius: 0,
+          borderTopRightRadius: 0,
+          borderBottomLeftRadius: 0,
+          borderBottomRightRadius: 0,
+          ...(hasRadius && {
+            ...(isFirst &&
+              !vertical && {
+                borderTopLeftRadius: radius,
+                borderBottomLeftRadius: radius,
+              }),
+            ...(isFirst &&
+              vertical && {
+                borderTopLeftRadius: radius,
+                borderTopRightRadius: radius,
+              }),
+            ...(isLast &&
+              !vertical && {
+                borderTopRightRadius: radius,
+                borderBottomRightRadius: radius,
+              }),
+            ...(isLast &&
+              vertical && {
+                borderBottomLeftRadius: radius,
+                borderBottomRightRadius: radius,
+              }),
+          }),
+        }),
       }
+      return cloneElementWithPropOrder(child, props)
+    })
 
-      return (
-        <GroupFrame
-          ref={ref}
-          size={sizeProp}
-          flexDirection={!vertical ? 'row' : 'column'}
-          {...props}
-        >
-          {wrapScroll(
-            spacedChildren({
-              direction: spaceDirection,
-              separator,
-              space,
-              children,
-            })
-          )}
-        </GroupFrame>
-      )
+    const wrapScroll = (children: any) => {
+      if (scrollable)
+        return (
+          <ScrollView
+            {...(vertical && {
+              showsVerticalScrollIndicator: false,
+            })}
+            {...(!vertical && {
+              horizontal: true,
+              showsHorizontalScrollIndicator: false,
+            })}
+          >
+            {children}
+          </ScrollView>
+        )
+      return children
     }
-  )
+
+    return (
+      <GroupFrame
+        ref={ref}
+        size={sizeProp}
+        flexDirection={!vertical ? 'row' : 'column'}
+        borderRadius={borderRadius}
+        {...props}
+      >
+        {wrapScroll(
+          spacedChildren({
+            direction: spaceDirection,
+            separator,
+            space,
+            children,
+          })
+        )}
+      </GroupFrame>
+    )
+  })
 )
 
 const cloneElementWithPropOrder = (child: any, props: Object) => {
