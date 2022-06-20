40e9fdb6d9addb555a04de17ab1316fac2128bd4
natew <natewienert@gmail.com>
2022-06-19 20:07:13 -1000

    repo: fix build and dep ordering

--- packages/core/types/helpers/prefixStyles.d.ts
+++
@@ -1,5 +0,0 @@
-declare type StyleModifier = (style: Object) => Object;
-export declare const prefixInlineStyles: StyleModifier;
-export declare const prefixStyles: StyleModifier;
-export {};
-//# sourceMappingURL=prefixStyles.d.ts.map
