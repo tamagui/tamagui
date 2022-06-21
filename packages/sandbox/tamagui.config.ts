ac39d3c950ec5bfea095bd08e46b923075ebc6f2
natew <natewienert@gmail.com>
2022-06-20 15:22:27 -1000

    site: start on theme blog post

--- packages/sandbox/tamagui.config.ts
+++ packages/sandbox/tamagui.config.ts
@@ -1,7 +1,10 @@
 import { config } from '@tamagui/config-base'
 import { createTamagui } from 'tamagui'
 
-const tamaConf = createTamagui(config)
+const tamaConf = createTamagui({
+  ...config,
+  themeClassNameOnRoot: false,
+})
 
 export type Conf = typeof tamaConf
 
