3a987c88fb170e132fd0c3deb3ebc3847c4212b8
natew <natewienert@gmail.com>
2022-07-07 22:31:32 -1000

    fix(helpers): revert compose event handlers dont use hooks

--- packages/helpers/src/composeEventHandlers.ts
+++ packages/helpers/src/composeEventHandlers.ts
@@ -1,4 +1,4 @@
-import { FocusEvent, useCallback, useRef } from 'react'
+import { FocusEvent } from 'react'
 import { GestureResponderEvent } from 'react-native'
 
 export type EventHandler<E extends Event | GestureResponderEvent | FocusEvent> = (event: E) => void
@@ -8,16 +8,10 @@ export function composeEventHandlers<E extends Event | GestureResponderEvent | F
   next?: EventHandler<E>,
   { checkDefaultPrevented = true } = {}
 ) {
-  const handlers = useRef<(EventHandler<E> | undefined)[]>([])
-  handlers.current[0] = og
-  handlers.current[1] = next
-
-  // mimic upcoming react useEvent and keep it stable
-  return useCallback(function composedEventHandler(event: E) {
-    const [og, next] = handlers.current
+  return function composedEventHandler(event: E) {
     og?.(event)
     if (!checkDefaultPrevented || !event.defaultPrevented) {
       return next?.(event)
     }
-  }, [])
+  }
 }
