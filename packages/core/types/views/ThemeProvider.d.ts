188579d023967dde870b6162d08b35db1a0d1c38
natew <natewienert@gmail.com>
2022-07-11 01:01:26 -1000

    fix messed up press on animated icon

--- packages/core/types/views/ThemeProvider.d.ts
+++ packages/core/types/views/ThemeProvider.d.ts
@@ -1,4 +1,3 @@
-/// <reference types="react" />
 export declare type ThemeProviderProps = {
     className?: string;
     themes: any;
--- packages/tamagui/types/views/Input.d.ts
+++ packages/tamagui/types/views/Input.d.ts
@@ -9,5 +9,13 @@ export declare const InputFrame: import("@tamagui/core").TamaguiComponent<import
     size?: import("@tamagui/core").SizeTokens | undefined;
 }>;
 export declare type InputProps = GetProps<typeof InputFrame>;
-export declare const Input: any;
+export declare const Input: import("@tamagui/core").TamaguiComponent<import("react-native").TextInputProps & Omit<import("@tamagui/core").TextProps, keyof import("react-native").TextInputProps> & Omit<{}, "size"> & {
+    size?: import("@tamagui/core").SizeTokens | undefined;
+} & import("@tamagui/core").MediaProps<Partial<import("react-native").TextInputProps & Omit<import("@tamagui/core").TextProps, keyof import("react-native").TextInputProps> & Omit<{}, "size"> & {
+    size?: import("@tamagui/core").SizeTokens | undefined;
+}>> & import("@tamagui/core").PseudoProps<Partial<import("react-native").TextInputProps & Omit<import("@tamagui/core").TextProps, keyof import("react-native").TextInputProps> & Omit<{}, "size"> & {
+    size?: import("@tamagui/core").SizeTokens | undefined;
+}>>, any, import("react-native").TextInputProps & Omit<import("@tamagui/core").TextProps, keyof import("react-native").TextInputProps>, {
+    size?: import("@tamagui/core").SizeTokens | undefined;
+}>;
 //# sourceMappingURL=Input.d.ts.map
