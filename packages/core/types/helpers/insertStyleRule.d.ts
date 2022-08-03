bb8f86226ab73150c742df2d13788e85008f2860
natew <natewienert@gmail.com>
2022-08-02 21:31:30 -1000

    fix(core): fix bug in concurrent mode - if a component re-renders before useEffect called it wouldnt insert, cache only after effect runs

--- packages/core/types/helpers/insertStyleRule.d.ts
+++ packages/core/types/helpers/insertStyleRule.d.ts
@@ -1,11 +1,12 @@
 import { StyleObject } from '@tamagui/helpers';
 export declare const insertedTransforms: {};
 export declare const getAllSelectors: () => {};
-export declare const getInsertedRules: () => unknown[];
+export declare const getAllRules: () => unknown[];
 export declare const getAllTransforms: () => {};
 export declare function updateInserted(): void;
-export declare function updateInsertedCache(identifier: string, rules: string[]): boolean;
+export declare function updateRules(identifier: string, rules: string[]): boolean;
 export declare type PartialStyleObject = Pick<StyleObject, 'identifier' | 'property' | 'rules'>;
 export declare type RulesToInsert = PartialStyleObject[];
 export declare function insertStyleRules(rulesToInsert: RulesToInsert): void;
+export declare function shouldInsertStyleRules(styleObject: PartialStyleObject): boolean;
 //# sourceMappingURL=insertStyleRule.d.ts.map
