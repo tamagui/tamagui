876397a5a9a862da2e066a7b4da11be8e75eed23
natew <natewienert@gmail.com>
2022-08-09 18:09:13 -1000

    fix(core): simplify mergeProps, account for shorthands merge more consistently

--- packages/core/types/helpers/mergeProps.d.ts
+++ packages/core/types/helpers/mergeProps.d.ts
@@ -1,2 +1,4 @@
-export declare const mergeProps: (a: Object, b: Object, leaveOutClassNames?: boolean, inverseShorthands?: Record<string, string>) => readonly [Record<string, string>, Record<string, string>];
+declare type StringRecord = Record<string, string>;
+export declare const mergeProps: (a: Object, b: Object, leaveOutClassNames?: boolean, inverseShorthands?: StringRecord) => readonly [StringRecord, StringRecord];
+export {};
 //# sourceMappingURL=mergeProps.d.ts.map
