d4126045294b93de964194a5c8f16402c0b67fa5
natew <natewienert@gmail.com>
2022-08-11 22:38:41 -1000

    feat(react-native-web-lite): slimmed down react-native-web and prep for RSC, vite, esmodules

---
+++ react-native-web-lite/src/exports/TextInput/types.tsx
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) Nicolas Gallagher.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import { ColorValue, GenericStyleProp } from '../../types'
+import { TextStyle } from '../Text/types'
+import { ViewProps } from '../View/types'
+
+export type TextInputStyle = {
+  caretColor?: ColorValue
+  resize?: 'none' | 'vertical' | 'horizontal' | 'both'
+} & TextStyle
+
+export type TextInputProps = {
+  autoCapitalize?: 'characters' | 'none' | 'sentences' | 'words'
+  autoComplete?: string | null
+  autoCompleteType?: string | null // Compat with React Native (Bug react-native#26003),
+  autoCorrect?: boolean | null
+  autoFocus?: boolean | null
+  blurOnSubmit?: boolean | null
+  clearTextOnFocus?: boolean | null
+  defaultValue?: string | null
+  dir?: 'auto' | 'ltr' | 'rtl' | null
+  disabled?: boolean | null
+  editable?: boolean | null
+  inputAccessoryViewID?: string | null
+  keyboardType?:
+    | 'default'
+    | 'email-address'
+    | 'number-pad'
+    | 'numbers-and-punctuation'
+    | 'numeric'
+    | 'phone-pad'
+    | 'search'
+    | 'url'
+    | 'web-search'
+  maxLength?: number | null
+  multiline?: boolean | null
+  numberOfLines?: number | null
+  onChange?: (e: any) => void
+  onChangeText?: (e: string) => void
+  onContentSizeChange?: (e: any) => void
+  onEndEditing?: (e: any) => void
+  onKeyPress?: (e: any) => void
+  onSelectionChange?: (e: any) => void
+  onScroll?: (e: any) => void
+  onSubmitEditing?: (e: any) => void
+  placeholder?: string | null
+  placeholderTextColor?: ColorValue | null
+  returnKeyType?: 'enter' | 'done' | 'go' | 'next' | 'previous' | 'search' | 'send'
+  secureTextEntry?: boolean | null
+  selectTextOnFocus?: boolean | null
+  selection?: {
+    start: number
+    end?: number
+  }
+  selectionColor?: ColorValue | null
+  spellCheck?: boolean | null
+  style?: GenericStyleProp<TextInputStyle> | null
+  value?: string | null
+} & ViewProps
---
+++ react-native-web-lite/src/modules/useEvent/index.tsx
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *
+ * @flow
+ */
+
+import createEventHandle from '../createEventHandle'
+import useLayoutEffect from '../useLayoutEffect'
+import useStable from '../useStable'
+
+type Callback = null | ((arg0: any) => void)
+type AddListener = (target: EventTarget, listener: null | ((arg0: any) => void)) => () => void
+
+/**
+ * This can be used with any event type include custom events.
+ *
+ * const click = useEvent('click', options);
+ * useEffect(() => {
+ *   click.setListener(target, onClick);
+ *   return () => click.clear();
+ * }).
+ */
+export default function useEvent(
+  event: string,
+  options?: {
+    capture?: boolean
+    passive?: boolean
+  } | null
+): AddListener {
+  const targetListeners = useStable(() => new Map())
+
+  const addListener = useStable(() => {
+    const addEventListener = createEventHandle(event, options)
+    return (target: EventTarget, callback: Callback) => {
+      const removeTargetListener = targetListeners.get(target)
+      if (removeTargetListener != null) {
+        removeTargetListener()
+      }
+      if (callback == null) {
+        targetListeners.delete(target)
+      }
+      const removeEventListener = addEventListener(target, callback)
+      targetListeners.set(target, removeEventListener)
+      return removeEventListener
+    }
+  })
+
+  useLayoutEffect(() => {
+    return () => {
+      targetListeners.forEach((removeListener) => {
+        removeListener()
+      })
+      targetListeners.clear()
+    }
+  }, [targetListeners])
+
+  return addListener
+}
