f94cc8cc3cd9551834be4cf0fd9b9da29618afdc
natew <natewienert@gmail.com>
2022-08-15 01:48:33 -1000

    fix(animations-css): Accept array-like animation values

--- packages/animations-css/src/createAnimations.tsx
+++ packages/animations-css/src/createAnimations.tsx
@@ -8,7 +8,8 @@ export function createAnimations<A extends Object>(animations: A): AnimationDriv
     Text: 'span',
     animations,
     useAnimations: (props, { getStyle }) => {
-      const animation = animations[props.animation as any]
+      const animationKey = Array.isArray(props.animation) ? props.animation[0] : props.animation
+      const animation = animations[animationKey as any]
       if (!animation) {
         throw new Error(`no animation found: ${props.animation}`)
       }
