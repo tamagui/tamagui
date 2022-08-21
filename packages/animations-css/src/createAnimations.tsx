9c780e15ddc70be3ac97da4fecc43bbc68e56742
natew <natewienert@gmail.com>
2022-08-20 16:15:48 -1000

    fix(animations-css): Fully support AnimatePresence, fix issues with enter and exit styles not applying sometimes

--- packages/animations-css/src/createAnimations.tsx
+++ packages/animations-css/src/createAnimations.tsx
@@ -1,5 +1,6 @@
-import { AnimationDriver, Stack, Text } from '@tamagui/core'
-import { useMemo, useRef } from 'react'
+import { PresenceContext, usePresence } from '@tamagui/animate-presence'
+import { AnimationDriver, Stack, Text, useIsomorphicLayoutEffect } from '@tamagui/core'
+import { useContext, useEffect, useLayoutEffect, useMemo, useRef } from 'react'
 
 export function createAnimations<A extends Object>(animations: A): AnimationDriver<A> {
   return {
@@ -32,24 +33,49 @@ export function createAnimations<A extends Object>(animations: A): AnimationDriv
       return getStyle(val.getValue())
     },
 
-    useAnimations: (props, { getStyle }) => {
+    useAnimations: (props, { getStyle, state, hostRef }) => {
+      const [isPresent, sendExitComplete] = usePresence()
+      const presence = useContext(PresenceContext)
+      const isExiting = isPresent === false
+      const isEntering = !state.mounted
       const animationKey = Array.isArray(props.animation) ? props.animation[0] : props.animation
       const animation = animations[animationKey as any]
       if (!animation) {
-        throw new Error(`no animation found: ${props.animation}`)
+        return null
       }
 
       const keys = props.animateOnly ? props.animateOnly.join(' ') : 'all'
 
+      useIsomorphicLayoutEffect(() => {
+        if (!sendExitComplete || !isExiting || !hostRef.current) return
+        const node = hostRef.current as HTMLElement
+        const onFinishAnimation = () => {
+          sendExitComplete?.()
+        }
+        node.addEventListener('transitionend', onFinishAnimation)
+        node.addEventListener('transitioncancel', onFinishAnimation)
+        return () => {
+          node.removeEventListener('transitionend', onFinishAnimation)
+          node.addEventListener('transitioncancel', onFinishAnimation)
+        }
+      }, [sendExitComplete, isExiting])
+
       const style = {
-        style: {
-          transition: `${keys} ${animation}`,
-          ...getStyle(),
-        },
+        transition: `${keys} ${animation}`,
+        ...getStyle({
+          isEntering,
+          isExiting,
+          exitVariant: presence?.exitVariant,
+          enterVariant: presence?.enterVariant,
+        }),
+      }
+
+      if (process.env.NODE_ENV === 'development' && props['debug']) {
+        console.log('CSS animation', style, { isEntering, isExiting })
       }
 
       return useMemo(() => {
-        return style
+        return { style }
         // eslint-disable-next-line react-hooks/exhaustive-deps
       }, [JSON.stringify(style)])
     },
