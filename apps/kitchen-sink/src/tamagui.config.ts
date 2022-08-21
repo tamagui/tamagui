0e50c21454d25c958d89d5702997477c4fea1578
natew <natewienert@gmail.com>
2022-08-20 19:03:20 -1000

    remove tamagram private example repo to redo it later

--- apps/kitchen-sink/src/tamagui.config.ts
+++ apps/kitchen-sink/src/tamagui.config.ts
@@ -1,92 +1,12 @@
-import { createInterFont } from '@tamagui/font-inter'
-import { shorthands } from '@tamagui/shorthands'
-import { themes, tokens } from '@tamagui/theme-base'
+import { config } from '@tamagui/config-base'
 import { createTamagui } from 'tamagui'
 
-import { animations } from './constants/animations'
+const tamaConf = createTamagui(config)
 
-const headingFont = createInterFont({
-  size: {
-    6: 15,
-  },
-  transform: {
-    6: 'uppercase',
-    7: 'none',
-  },
-  weight: {
-    6: '400',
-    7: '700',
-  },
-  color: {
-    6: '$colorFocus',
-    7: '$color',
-  },
-  letterSpacing: {
-    5: 2,
-    6: 1,
-    7: 0,
-    8: -1,
-    9: -2,
-    10: -3,
-    12: -4,
-    14: -5,
-    15: -6,
-  },
-})
-
-const bodyFont = createInterFont(
-  {},
-  {
-    sizeSize: (size) => Math.round(size * 1.1),
-    sizeLineHeight: (size) => Math.round(size * 1.1 + (size > 20 ? 10 : 10)),
-  }
-)
-
-// testing
-const body_cn = createInterFont(
-  {},
-  {
-    sizeSize: (size) => Math.round(size * 10),
-    sizeLineHeight: (size) => Math.round(size * 10),
-  }
-)
-
-const config = createTamagui({
-  animations,
-  defaultTheme: 'light',
-  shouldAddPrefersColorThemes: true,
-  themeClassNameOnRoot: true,
-  shorthands,
-  fonts: {
-    heading: headingFont,
-    heading_cn: body_cn,
-    body: bodyFont,
-    body_cn,
-  },
-  themes,
-  tokens,
-  media: {
-    xs: { maxWidth: 660 },
-    sm: { maxWidth: 800 },
-    md: { maxWidth: 1020 },
-    lg: { maxWidth: 1280 },
-    xl: { maxWidth: 1420 },
-    xxl: { maxWidth: 1600 },
-    gtXs: { minWidth: 660 + 1 },
-    gtSm: { minWidth: 800 + 1 },
-    gtMd: { minWidth: 1020 + 1 },
-    gtLg: { minWidth: 1280 + 1 },
-    short: { maxHeight: 820 },
-    tall: { minHeight: 820 },
-    hoverNone: { hover: 'none' },
-    pointerCoarse: { pointer: 'coarse' },
-  },
-})
-
-export type Conf = typeof config
+export type Conf = typeof tamaConf
 
 declare module 'tamagui' {
   interface TamaguiCustomConfig extends Conf {}
 }
 
-export default config
+export default tamaConf
