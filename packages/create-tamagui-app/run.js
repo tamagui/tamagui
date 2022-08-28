0a42d1deb433e97ad54ed0d329577e1db854d50f
natew <natewienert@gmail.com>
2022-08-28 12:11:39 -1000

    fix(create-tamagui-app): Remove unnecessary esbuild-register that isnt resolving properly

--- packages/create-tamagui-app/run.js
+++ packages/create-tamagui-app/run.js
@@ -3,6 +3,6 @@
 const command = require.resolve('create-tamagui-app')
 const args = process.argv.slice(2)
 
-require('child_process').execSync(`node -r esbuild-register ${command} ${args.join(' ')}`, {
+require('child_process').execSync(`node ${command} ${args.join(' ')}`, {
   stdio: 'inherit',
 })
