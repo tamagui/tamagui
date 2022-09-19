64cd502a9641eb10254bf5a976e82df0e823bcc8
natew <natewienert@gmail.com>
2022-09-18 15:19:10 -1000

    fix(feather-icons): Support string token values as well as theme values

--- packages/feather-icons/src/themed.tsx
+++ packages/feather-icons/src/themed.tsx
@@ -1,11 +1,17 @@
-import { getVariable, getVariableValue, useTheme } from '@tamagui/core'
+import { getVariable, useTheme } from '@tamagui/core'
 import React from 'react'
 
 export function themed<A extends React.FC>(Component: A) {
   const wrapped = (props: any) => {
     const theme = useTheme()
-    const color = props.color || (!props.disableTheme ? theme.color : null) || '#000'
-    return <Component {...props} color={getVariable(color)} />
+    return (
+      <Component
+        {...props}
+        color={getVariable(
+          theme[props.color] || props.color || (!props.disableTheme ? theme.color : null) || '#000'
+        )}
+      />
+    )
   }
   return wrapped as unknown as A
 }
