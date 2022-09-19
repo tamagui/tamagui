c280398687c26032b366cf5fc8c8d4faf677b189
natew <natewienert@gmail.com>
2022-09-18 14:30:59 -1000

    fix(linear-gradient): Fix colors types to work with Intellisense autocomplete for Color and Theme tokens
    Fixes #206

--- packages/demos/src/LinearGradientDemo.tsx
+++ packages/demos/src/LinearGradientDemo.tsx
@@ -1,16 +1,28 @@
 import React from 'react'
-import { LinearGradient } from 'tamagui'
+import { LinearGradient, XStack, YStack } from 'tamagui'
 
 export function LinearGradientDemo() {
   return (
-    <LinearGradient
-      als="center"
-      width={100}
-      height={100}
-      colors={['$red10', '$yellow10']}
-      start={[0, 1]}
-      br="$4"
-      end={[0, 0]}
-    />
+    <XStack space>
+      {/* tokens */}
+      <LinearGradient
+        width="$6"
+        height="$6"
+        br="$4"
+        colors={['$red10', '$yellow10']}
+        start={[0, 1]}
+        end={[0, 0]}
+      />
+
+      {/* theme values */}
+      <LinearGradient
+        width="$6"
+        height="$6"
+        br="$4"
+        colors={['$background', '$color']}
+        start={[1, 1]}
+        end={[0, 0]}
+      />
+    </XStack>
   )
 }
--- packages/tamagui/types/views/LinearGradient.d.ts
+++ packages/tamagui/types/views/LinearGradient.d.ts
@@ -4,7 +4,7 @@ import * as React from 'react';
 import { View } from 'react-native';
 import { LinearGradientProps as ExpoLinearGradientProps } from '../lib/linear-gradient';
 export declare type LinearGradientProps = Omit<ExpoLinearGradientProps, 'colors'> & Omit<YStackProps, 'children' | keyof ExpoLinearGradientProps> & {
-    colors?: (ColorTokens | string)[];
+    colors?: (ColorTokens | (string & {}))[];
 };
 export declare const LinearGradient: React.ForwardRefExoticComponent<LinearGradientProps & React.RefAttributes<HTMLElement | View>>;
 //# sourceMappingURL=LinearGradient.d.ts.map
