tamagui/tamagui

commit d3cdcc28f08f17310193cec57938c860dd29a8ee

feat(core): styled() supports any component, falling back to passing just style unless acceptsClassName is used

packages/core/src/createComponent.tsx
@@ -149,7 +149,7 @@ export function createComponent<
     const states = useServerState<TamaguiComponentState>(defaultComponentState)
     const state = propsIn.forceStyle ? { ...states[0], [propsIn.forceStyle]: true } : states[0]
 
-    const shouldAvoidClasses = !!(props.animation && avoidClasses)
+    const shouldAvoidClasses = !!(props.animation && avoidClasses) || !staticConfig.acceptsClassName
     const shouldForcePseudo = !!propsIn.forceStyle
     const hasTextAncestor = !!(isWeb && isText ? useContext(TextAncestorContext) : false)
     const splitStyleState =
@@ -213,8 +213,13 @@ export function createComponent<
 
     const { viewProps: viewPropsIn, pseudos, medias, style, classNames, mediaKeys } = splitStyles
 
+    if (debugProp) console.log('mediaKeys', mediaKeys, splitStyles, mediaState)
+
     // media queries
     useIsomorphicLayoutEffect(() => {
+      if (!mediaKeys.length) {
+        return
+      }
       for (const key of mediaKeys) {
         addMediaQueryListener(key, forceUpdate)
       }
@@ -1105,6 +1110,7 @@ export function createComponent<
         pressIn: false,
         focus: false,
         resolveVariablesAs: 'both',
+        noClassNames: !staticConfig.acceptsClassName,
         keepVariantsAsProps: true,
       },
       undefined,

packages/core/src/helpers/getSplitStyles.tsx
@@ -346,8 +346,9 @@ export const getSplitStyles: StyleSplitter = (
       // media
       if (isMedia) {
         const mediaKey = key
-        mediaKeys.push(key)
         const mediaKeyShort = mediaKey.slice(1)
+
+        mediaKeys.push(mediaKeyShort)
 
         if (!mediaQueryConfig[mediaKeyShort]) {
           if (!usedKeys.has(key)) {
@@ -376,7 +377,7 @@ export const getSplitStyles: StyleSplitter = (
         if (process.env.NODE_ENV === 'development' && debug === 'verbose') {
           // prettier-ignore
           // eslint-disable-next-line no-console
-          console.log('  🔹 mediaStyle', { mediaKey, mediaStyle, props, shouldDoClasses })
+          console.log(`  🔹 ${mediaKey}`, mediaState[mediaKeyShort], { mediaKey, mediaStyle, props, shouldDoClasses, mediaState: { ...mediaState } })
         }
 
         if (shouldDoClasses) {
@@ -391,7 +392,7 @@ export const getSplitStyles: StyleSplitter = (
             }
           }
         } else {
-          if (mediaState[mediaKey]) {
+          if (mediaState[mediaKeyShort]) {
             if (process.env.NODE_ENV === 'development' && debug) {
               // eslint-disable-next-line no-console
               console.log('apply media style', mediaKey, mediaState)

packages/core/src/hooks/useMedia.tsx
@@ -3,11 +3,12 @@ import { useMemo, useState } from 'react'
 import { useIsomorphicLayoutEffect } from '../constants/platform'
 import { matchMedia } from '../helpers/matchMedia'
 import {
-  ConfigureMediaQueryOptions,
+  CreateTamaguiProps,
   MediaQueries,
   MediaQueryKey,
   MediaQueryObject,
   MediaQueryState,
+  TamaguiInternalConfig,
 } from '../types'
 import { useSafeRef } from './useSafeRef'
 
@@ -35,29 +36,17 @@ const dispose = new Set<Function>()
 // for SSR capture it at time of startup
 let initialMediaState: MediaQueryState | null
 
-export const configureMedia = ({
-  queries,
-  defaultActive = {},
-}: ConfigureMediaQueryOptions = {}) => {
-  if (!queries) return
-
-  // support hot reload
-  if (initialMediaState) {
-    if (process.env.NODE_ENV === 'development') {
-      if (JSON.stringify(queries) === JSON.stringify(mediaQueryConfig)) {
-        // hmr avoid update
-        return
-      }
-    }
-    setupMediaListeners()
+export const configureMedia = (config: TamaguiInternalConfig) => {
+  const { media, mediaQueryDefaultActive } = config
+  if (!media) return
+  for (const key in media) {
+    mediaState[key] = mediaQueryDefaultActive?.[key] || false
   }
-
-  Object.assign(mediaQueryConfig, queries)
-  // start in the initial state
-  for (const key in queries) {
-    mediaState[key] = defaultActive[key] || false
-  }
+  Object.assign(mediaQueryConfig, media)
   initialMediaState = { ...mediaState }
+  if (config.disableSSR) {
+    setupMediaListeners(config)
+  }
 }
 
 function unlisten() {
@@ -65,7 +54,20 @@ function unlisten() {
   dispose.clear()
 }
 
-function setupMediaListeners() {
+/**
+ * Note: This should *not* set the state on the first render!
+ * Because to avoid hydration issues SSR must match the server
+ * *and then* re-render with the actual media query state.
+ */
+let configuredKey = ''
+function setupMediaListeners({ disableSSR }: TamaguiInternalConfig) {
+  // avoid setting up more than once per config
+  const nextKey = JSON.stringify(mediaQueryConfig)
+  if (nextKey === configuredKey) {
+    return
+  }
+  configuredKey = nextKey
+
   // hmr, undo existing before re-binding
   unlisten()
 
@@ -79,6 +81,7 @@ function setupMediaListeners() {
     // react native needs these deprecated apis for now
     match.addListener(update)
     dispose.add(() => match.removeListener(update))
+
     update()
 
     function update() {
@@ -87,17 +90,15 @@ function setupMediaListeners() {
       mediaState[key] = next
       const listeners = mediaQueryListeners[key]
       if (listeners?.size) {
-        for (const cb of [...listeners]) {
-          cb()
-        }
+        listeners.forEach((cb) => cb())
       }
     }
   }
 }
 
-export function useMediaQueryListeners() {
+export function useMediaQueryListeners(config: TamaguiInternalConfig) {
   useIsomorphicLayoutEffect(() => {
-    setupMediaListeners()
+    setupMediaListeners(config)
     return unlisten
   }, [])
 }
@@ -135,7 +136,7 @@ export function useMedia(): {
       new Proxy(state, {
         get(_, key: string) {
           if (!keys.current[key]) {
-            keys.current = { ...keys.current, [key]: true }
+            keys.current[key] = true
           }
           return Reflect.get(state, key)
         },

packages/core-node/types/hooks/useMedia.d.ts
@@ -1,11 +1,11 @@
-import { ConfigureMediaQueryOptions, MediaQueries, MediaQueryKey, MediaQueryObject, MediaQueryState } from '../types';
+import { MediaQueries, MediaQueryKey, MediaQueryObject, MediaQueryState, TamaguiInternalConfig } from '../types';
 export declare const mediaState: MediaQueryState;
 export declare function addMediaQueryListener(key: MediaQueryKey, cb: any): () => void;
 export declare function removeMediaQueryListener(key: MediaQueryKey, cb: any): void;
 export declare const mediaQueryConfig: MediaQueries;
 export declare const getMedia: () => MediaQueryState;
-export declare const configureMedia: ({ queries, defaultActive, }?: ConfigureMediaQueryOptions) => void;
-export declare function useMediaQueryListeners(): void;
+export declare const configureMedia: (config: TamaguiInternalConfig) => void;
+export declare function useMediaQueryListeners(config: TamaguiInternalConfig): void;
 export declare function useMedia(): {
     [key in MediaQueryKey]: boolean;
 };

packages/core-node/types/styled.d.ts
@@ -14,6 +14,7 @@ export declare function styled<ParentComponent extends StylableComponent, Varian
     defaultVariants?: {
         [key: string]: any;
     };
+    acceptsClassName?: boolean;
 }, staticExtractionOptions?: Partial<StaticConfig>): TamaguiComponent<Variants extends void ? GetProps<ParentComponent> : GetBaseProps<ParentComponent> & Omit<GetVariantProps<ParentComponent>, keyof (Variants extends void ? {} : { [Key in keyof Variants]?: (Variants[Key] extends VariantSpreadFunction<any, infer Val> ? Val : GetVariantValues<keyof Variants[Key]>) | undefined; })> & (Variants extends void ? {} : { [Key in keyof Variants]?: (Variants[Key] extends VariantSpreadFunction<any, infer Val> ? Val : GetVariantValues<keyof Variants[Key]>) | undefined; }) & MediaProps<Partial<GetBaseProps<ParentComponent> & Omit<GetVariantProps<ParentComponent>, keyof (Variants extends void ? {} : { [Key in keyof Variants]?: (Variants[Key] extends VariantSpreadFunction<any, infer Val> ? Val : GetVariantValues<keyof Variants[Key]>) | undefined; })> & (Variants extends void ? {} : { [Key in keyof Variants]?: (Variants[Key] extends VariantSpreadFunction<any, infer Val> ? Val : GetVariantValues<keyof Variants[Key]>) | undefined; })>> & PseudoProps<Partial<GetBaseProps<ParentComponent> & Omit<GetVariantProps<ParentComponent>, keyof (Variants extends void ? {} : { [Key in keyof Variants]?: (Variants[Key] extends VariantSpreadFunction<any, infer Val> ? Val : GetVariantValues<keyof Variants[Key]>) | undefined; })> & (Variants extends void ? {} : { [Key in keyof Variants]?: (Variants[Key] extends VariantSpreadFunction<any, infer Val> ? Val : GetVariantValues<keyof Variants[Key]>) | undefined; })>>, TamaguiElement, GetBaseProps<ParentComponent>, GetVariantProps<ParentComponent> & (Variants extends void ? {} : { [Key in keyof Variants]?: (Variants[Key] extends VariantSpreadFunction<any, infer Val> ? Val : GetVariantValues<keyof Variants[Key]>) | undefined; })>;
 export {};
 //# sourceMappingURL=styled.d.ts.map

packages/core-node/types/types.d.ts
@@ -158,6 +158,7 @@ export declare type CreateTamaguiProps = {
         };
     };
     defaultTheme?: string;
+    disableSSR?: boolean;
     disableRootThemeClass?: boolean;
     defaultProps?: Record<string, any> & {
         Stack?: StackProps;
@@ -226,10 +227,6 @@ export declare type MediaProps<A> = {
 };
 export declare type MediaQueries = {
     [key in MediaQueryKey]: MediaQueryObject;
-};
-export declare type ConfigureMediaQueryOptions = {
-    queries?: MediaQueries;
-    defaultActive?: Record<MediaQueryKey, boolean>;
 };
 export declare type TransformStyleProps = {
     x?: number;
@@ -393,6 +390,7 @@ export declare type StaticConfig = {
     isTamagui?: boolean;
     isHOC?: boolean;
     parentNames?: string[];
+    acceptsClassName?: boolean;
 };
 export declare type StylableComponent = TamaguiComponent | React.Component | React.ForwardRefExoticComponent<any> | ReactComponentWithRef<any, any> | (new (props: any) => any) | typeof View | typeof Text | typeof TextInput | typeof Image;
 export declare type GetStyledVariants<A extends TamaguiComponent> = A extends TamaguiComponent<any, any, any, infer Variants> ? Variants : never;

packages/core/src/createTamagui.ts
@@ -36,11 +36,6 @@ export function createTamagui<Conf extends CreateTamaguiProps>(
     }
   }
 
-  configureMedia({
-    queries: configIn.media as any,
-    defaultActive: configIn.mediaQueryDefaultActive,
-  })
-
   const fontTokens = Object.fromEntries(
     Object.entries(configIn.fonts!).map(([k, v]) => {
       return [k, createVariables(v, 'f', true)]
@@ -202,6 +197,7 @@ export function createTamagui<Conf extends CreateTamaguiProps>(
   }
 
   setConfig(config)
+  configureMedia(config)
 
   if (configListeners.size) {
     configListeners.forEach((cb) => cb(config))

packages/core/src/styled.tsx
@@ -48,6 +48,7 @@ export function styled<
     variants?: Variants | undefined
     // thought i had this typed, but can't get it linked
     defaultVariants?: { [key: string]: any }
+    acceptsClassName?: boolean
   },
   staticExtractionOptions?: Partial<StaticConfig>
 ) {
@@ -73,7 +74,13 @@ export function styled<
     }
 
     if (options) {
-      const { variants, name, defaultVariants, ...defaultProps } = options
+      const {
+        variants,
+        name,
+        defaultVariants,
+        acceptsClassName: acceptsClassNameProp,
+        ...defaultProps
+      } = options
       if (defaultVariants) {
         Object.assign(defaultProps, defaultVariants)
       }
@@ -84,12 +91,7 @@ export function styled<
           RNComponents.add(Component)
           isReactNativeWeb = true
         } else {
-          if (process.env.NODE_ENV === 'development') {
-            // eslint-disable-next-line no-console
-            console.warn(
-              `Invalid styled() component: Tamagui only accepts react-native Text, View, Image, TextInput, or another styled Tamagui view`
-            )
-          }
+          // assuming some sort of non-tamagui non-rn view, will always use style={}
         }
       }
       const reactNativeWebComponent = isReactNativeWeb
@@ -106,6 +108,8 @@ export function styled<
       const isText = Boolean(
         isInput || staticExtractionOptions?.isText || parentStaticConfig?.isText || Comp === Text
       )
+
+      const acceptsClassName = acceptsClassNameProp ?? (isTamagui || isReactNativeWeb)
 
       const conf: Partial<StaticConfig> = {
         ...staticExtractionOptions,
@@ -125,10 +129,12 @@ export function styled<
         isInput,
         isText,
         isImage,
+        acceptsClassName,
       }
 
       // TODO compiler doesn't have logic to include children, de-opt (see EnsureFlexed for test usage)
-      if (defaultProps.children) {
+      // bail on non className views as well
+      if (defaultProps.children || !acceptsClassName) {
         conf.neverFlatten = true
       }
 

commit 57f36930d9bb4de611ff4000990c65be68710e07

fix(core): Fix media queries not refreshing at runtime occasionally (fix small regression from unreleased change in d3cdcc28f08f17310193cec57938c860dd29a8ee

apps/sandbox/Sandbox.tsx
@@ -50,8 +50,6 @@ export const Sandbox = () => {
   //   }, 1000)
   // }, [])
 
-  console.log(getTokens())
-
   return (
     <TamaguiProvider config={config} defaultTheme={theme}>
       <link href="/fonts/inter.css" rel="stylesheet" />

packages/core/src/createComponent.tsx
@@ -151,7 +151,7 @@ export function createComponent<
     const states = useServerState<TamaguiComponentState>(defaultComponentState)
     const state = propsIn.forceStyle ? { ...states[0], [propsIn.forceStyle]: true } : states[0]
 
-    const shouldAvoidClasses = !!(props.animation && avoidClasses) // || !staticConfig.acceptsClassName
+    const shouldAvoidClasses = !!(props.animation && avoidClasses) || !staticConfig.acceptsClassName
     const shouldForcePseudo = !!propsIn.forceStyle
     const hasTextAncestor = !!(isWeb && isText ? useContext(TextAncestorContext) : false)
     const splitStyleState =
@@ -1112,7 +1112,7 @@ export function createComponent<
         pressIn: false,
         focus: false,
         resolveVariablesAs: 'both',
-        // noClassNames: !staticConfig.acceptsClassName,
+        noClassNames: !staticConfig.acceptsClassName,
         keepVariantsAsProps: true,
       },
       undefined,

packages/core/src/helpers/getSplitStyles.tsx
@@ -118,7 +118,10 @@ export const getSplitStyles: StyleSplitter = (
   const usedKeys = new Set<string>()
   const propKeys = Object.keys(props)
   const shouldDoClasses =
-    (isWeb || process.env.IS_STATIC === 'is_static') && !state.noClassNames && !props.animation
+    staticConfig.acceptsClassName &&
+    (isWeb || process.env.IS_STATIC === 'is_static') &&
+    !state.noClassNames &&
+    !props.animation
   const len = propKeys.length
   const rulesToInsert: RulesToInsert = []
   const style: ViewStyle = {}
@@ -385,7 +388,7 @@ export const getSplitStyles: StyleSplitter = (
         if (process.env.NODE_ENV === 'development' && debug === 'verbose') {
           // prettier-ignore
           // eslint-disable-next-line no-console
-          console.log(`  🔹 ${mediaKey}`, mediaState[mediaKey], { mediaKey, mediaStyle, props, shouldDoClasses, mediaState: { ...mediaState } })
+          console.log(`  📺 ${mediaKey}`, mediaState[mediaKeyShort], { mediaKey, mediaStyle, props, shouldDoClasses, mediaState: { ...mediaState } })
         }
 
         if (shouldDoClasses) {
@@ -400,7 +403,7 @@ export const getSplitStyles: StyleSplitter = (
             }
           }
         } else {
-          if (mediaState[mediaKey]) {
+          if (mediaState[mediaKeyShort]) {
             if (process.env.NODE_ENV === 'development' && debug) {
               // eslint-disable-next-line no-console
               console.log('apply media style', mediaKey, mediaState)

packages/core/src/hooks/useMedia.tsx
@@ -11,7 +11,22 @@ import {
 } from '../types'
 import { useSafeRef } from './useSafeRef'
 
-export const mediaState: MediaQueryState = {} as any
+export const mediaState: MediaQueryState =
+  // development time safeguard
+  process.env.NODE_ENV === 'development'
+    ? new Proxy(
+        {},
+        {
+          get(target, key) {
+            if (typeof key === 'string' && key[0] === '$') {
+              throw new Error(`Access of mediaState should never include $`)
+            }
+            return Reflect.get(target, key)
+          },
+        }
+      )
+    : ({} as any)
+
 const mediaQueryListeners: { [key: string]: Set<Function> } = {}
 
 export function addMediaQueryListener(key: MediaQueryKey, cb: any) {