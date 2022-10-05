446e0c7b152b40dac6682b0eb0a271e5f315e13f
natew <natewienert@gmail.com>
2022-10-04 15:47:39 -1000

    feat(tamagui): Add createTamagui override that validates tokens

--- packages/tamagui/src/createTamagui.ts
+++ packages/tamagui/src/createTamagui.ts
@@ -1,54 +1,55 @@
 import { createTamagui as createTamaguiCore } from '@tamagui/core'
 
 /**
- * Adds some helpful validation at dev time
- * */
+ * Adds some helpful validation at dev time only for `tamagui` specifically
+ **/
 
-export const createTamagui = createTamaguiCore
+export const createTamagui: typeof createTamaguiCore =
+  process.env.NODE_ENV !== 'development'
+    ? createTamaguiCore
+    : (conf) => {
+        const base = ['$0', '$1', '$2', '$3', '$4', '$5', '$6', '$7', '$8', '$9', '$10']
+        const sizeTokenKeys = [...base, '$true']
 
-// : typeof createTamaguiCore =
-//   process.env.NODE_ENV !== 'development'
-//     ? createTamaguiCore
-//     : (conf) => {
-//         const base = ['$0', '$1', '$2', '$3', '$4', '$5', '$6', '$7', '$8', '$9', '$10']
-//         const sizeTokenKeys = [...base, '$true']
+        const hasKeys = (expectedKeys: string[], obj: Record<any, any>) => {
+          return expectedKeys.every((k) => typeof obj[k] !== 'undefined')
+        }
 
-//         const hasKeys = (expectedKeys: string[], obj: Record<any, any>) => {
-//           return expectedKeys.every((k) => typeof obj[k] !== 'undefined')
-//         }
+        const parsed = createTamaguiCore(conf)
 
-//         const parsed = createTamaguiCore(conf)
+        // size and space should be fully defined
+        for (const name of ['size', 'space'] as const) {
+          const tokenSet = parsed.tokensParsed[name]
+          if (!hasKeys(sizeTokenKeys, tokenSet)) {
+            throw new Error(`
+createTamagui() missing definition for expected tokens.${name}:
 
-//         // size and space should be fully defined
-//         for (const name of ['size', 'space'] as const) {
-//           const tokenSet = parsed.tokensParsed[name]
-//           if (!hasKeys(sizeTokenKeys, tokenSet)) {
-//             throw new Error(`
-// createTamagui() missing definition for expected tokens.${name}:
+Expected keys for: ${sizeTokenKeys.join(', ')}
 
-// Expected keys for: ${sizeTokenKeys.join(', ')}
+Received: ${Object.keys(tokenSet).join(', ')}
 
-// Received: ${Object.keys(tokenSet).join(', ')}
+`)
+          }
+        }
 
-// `)
-//           }
-//         }
+        // others can be partially defined
+        for (const name of ['radius', 'zIndex'] as const) {
+          const tokenSet = parsed.tokensParsed[name]
+          const givenKeys = Object.keys(tokenSet)
+          const missing = givenKeys.find((k) => !parsed.tokensParsed.size[k])
+          if (missing?.length) {
+            throw new Error(`
+createTamagui() invalid tokens.${name}:
 
-//         // others can be partially defined
-//         const sizeSupersetKeys = Object.keys(parsed.tokensParsed.size)
-//         for (const name of ['radius', 'zIndex'] as const) {
-//           const tokenSet = parsed.tokensParsed[name]
-//           if (Object.keys(tokenSet).some((k) => !(k in sizeSupersetKeys))) {
-//             throw new Error(`
-// createTamagui() invalid tokens.${name}:
+Expected subset of: ${Object.keys(parsed.tokensParsed.size).join(', ')}
 
-// Expected subset of: ${sizeSupersetKeys.join(', ')}
+Received: ${givenKeys.join(', ')}
 
-// Received: ${Object.keys(tokenSet).join(', ')}
+Missing: ${missing}
 
-// `)
-//           }
-//         }
+`)
+          }
+        }
 
-//         return parsed
-//       }
+        return parsed
+      }
