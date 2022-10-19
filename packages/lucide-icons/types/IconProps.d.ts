34353afe7e711440faac0e35f4b145bd1ef2857e
natew <natewienert@gmail.com>
2022-10-19 11:35:53 -1000

    feat(lucide-icons): Accept color types + accept size tokens and types

--- packages/lucide-icons/types/IconProps.d.ts
+++ packages/lucide-icons/types/IconProps.d.ts
@@ -1,7 +1,8 @@
+import { ColorTokens, SizeTokens, ThemeTokens } from '@tamagui/core';
 import { SvgProps } from 'react-native-svg';
 export declare type IconProps = SvgProps & {
-    size?: number;
-    color?: string;
+    size?: number | SizeTokens;
+    color?: (ColorTokens | ThemeTokens | (string & {})) | null;
     style?: any;
 };
 //# sourceMappingURL=IconProps.d.ts.map
