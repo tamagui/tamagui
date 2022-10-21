215b0965c78d70c500b901c5840f8f4b2fd175b5
natew <natewienert@gmail.com>
2022-10-21 13:31:51 -1000

    fix(core): Position relative all views

--- packages/core-node/types/constants/constants.d.ts
+++ packages/core-node/types/constants/constants.d.ts
@@ -1,9 +1,20 @@
 export declare const THEME_NAME_SEPARATOR = "_";
 export declare const THEME_CLASSNAME_PREFIX = "t_";
+export declare const webOnlyDefaultStyles: {
+    display: string;
+    flexBasis: string;
+    boxSizing: string;
+    position: string;
+    minHeight: number;
+    minWidth: number;
+};
 export declare const stackDefaultStyles: {
-    display?: string | undefined;
-    flexBasis?: string | undefined;
-    boxSizing?: string | undefined;
+    display: string;
+    flexBasis: string;
+    boxSizing: string;
+    position: string;
+    minHeight: number;
+    minWidth: number;
     alignItems: string;
     flexShrink: number;
 };
--- packages/core/src/constants/constants.ts
+++ packages/core/src/constants/constants.ts
@@ -3,13 +3,17 @@ import { isWeb } from '@tamagui/constants'
 export const THEME_NAME_SEPARATOR = '_'
 export const THEME_CLASSNAME_PREFIX = `t_`
 
+export const webOnlyDefaultStyles = {
+  display: 'flex',
+  flexBasis: 'auto',
+  boxSizing: 'border-box',
+  position: 'relative',
+  minHeight: 0,
+  minWidth: 0,
+}
+
 export const stackDefaultStyles = {
   alignItems: 'stretch',
   flexShrink: 0,
-  // more accurately match react-native defaults on web
-  ...(isWeb && {
-    display: 'flex',
-    flexBasis: 'auto',
-    boxSizing: 'border-box',
-  }),
+  ...webOnlyDefaultStyles,
 }
