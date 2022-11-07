dd6ed2dcbff4c01308dcc1d72e841e0f32323de7
natew <natewienert@gmail.com>
2022-11-06 15:17:04 -1000

    fix(focusable): Wrap with extractable HOC to properly allow re-wrapping Input and TextArea with styled()

--- packages/focusable/src/focusableInputHOC.tsx
+++ packages/focusable/src/focusableInputHOC.tsx
@@ -1,67 +1,72 @@
 import { composeRefs } from '@tamagui/compose-refs'
-import { isTamaguiComponent, useEvent } from '@tamagui/core'
+import { TamaguiComponent, isTamaguiComponent, useEvent } from '@tamagui/core'
 import React, { useRef } from 'react'
 import { forwardRef, useCallback, useEffect } from 'react'
 
 import { registerFocusable, unregisterFocusable } from './registerFocusable'
 
-export function focusableInputHOC<A extends Function>(Component: A): A {
-  return forwardRef(
-    (
-      props: {
-        id?: string
-        onChangeText?: (val: string) => void
-        value?: string
-        defaultValue?: string
-      },
-      ref
-    ) => {
-      const isInput = isTamaguiComponent(Component) && Component.staticConfig.isInput
-      const inputValue = useRef(props.value || props.defaultValue || '')
-
-      const inputRef = useCallback(
-        (input) => {
-          if (!props.id) return
-          if (!input) return
-          registerFocusable(props.id, {
-            focus: input.focus,
-
-            ...(isInput && {
-              // react-native doesn't support programmatic .select()
-              focusAndSelect() {
-                input.focus()
-                if (input.setSelection && typeof inputValue.current === 'string') {
-                  input.setSelection(0, inputValue.current.length)
-                }
-              },
-            }),
-          })
+export function focusableInputHOC<A extends TamaguiComponent>(Component: A): A {
+  const component = Component.extractable(
+    forwardRef(
+      (
+        props: {
+          id?: string
+          onChangeText?: (val: string) => void
+          value?: string
+          defaultValue?: string
         },
-        [isInput, props.id]
-      )
+        ref
+      ) => {
+        const isInput = isTamaguiComponent(Component) && Component.staticConfig.isInput
+        const inputValue = useRef(props.value || props.defaultValue || '')
 
-      const combinedRefs = composeRefs(ref, inputRef)
+        const inputRef = useCallback(
+          (input) => {
+            if (!props.id) return
+            if (!input) return
+            registerFocusable(props.id, {
+              focus: input.focus,
 
-      useEffect(() => {
-        if (!props.id) return
-        return () => {
-          unregisterFocusable(props.id!)
-        }
-      }, [props.id])
+              ...(isInput && {
+                // react-native doesn't support programmatic .select()
+                focusAndSelect() {
+                  input.focus()
+                  if (input.setSelection && typeof inputValue.current === 'string') {
+                    input.setSelection(0, inputValue.current.length)
+                  }
+                },
+              }),
+            })
+          },
+          [isInput, props.id]
+        )
 
-      const onChangeText = useEvent((value) => {
-        inputValue.current = value
-        props.onChangeText?.(value)
-      })
+        const combinedRefs = composeRefs(ref, inputRef)
 
-      const finalProps = isInput
-        ? {
-            ...props,
-            onChangeText,
+        useEffect(() => {
+          if (!props.id) return
+          return () => {
+            unregisterFocusable(props.id!)
           }
-        : props
+        }, [props.id])
 
-      return <Component ref={combinedRefs} {...finalProps} />
-    }
+        const onChangeText = useEvent((value) => {
+          inputValue.current = value
+          props.onChangeText?.(value)
+        })
+
+        const finalProps = isInput
+          ? {
+              ...props,
+              onChangeText,
+            }
+          : props
+
+        // @ts-expect-error
+        return <Component ref={combinedRefs} {...finalProps} />
+      }
+    )
   ) as any
+
+  return component
 }
