7ef343674a208baf70260aff958a19e49436a5b3
natew <natewienert@gmail.com>
2022-11-10 04:13:22 -1000

    v1.0.1-beta.214

--- packages/core-node/types/index.d.ts
+++ packages/core-node/types/index.d.ts
@@ -21,7 +21,6 @@ export * from './helpers/getVariantExtras';
 export * from './helpers/getAnimationDriver';
 export * from './helpers/getExpandedShorthands';
 export * from './helpers/getSize';
-export * from './helpers/getTokens';
 export * from './helpers/getSplitStyles';
 export * from './helpers/getStylesAtomic';
 export * from './helpers/pseudoDescriptors';
