6d1ad48c38bf3cd14babfc0d5865dc2c9a044a0e
natew <natewienert@gmail.com>
2022-11-13 15:26:09 -1000

    fix(core): Fix web extensions not supporting isDevTools check

--- packages/core/src/constants/isDevTools.ts
+++ packages/core/src/constants/isDevTools.ts
@@ -1,6 +1,12 @@
 // for verbose logging outside node/cli
 
-export const isDevTools =
-  process.env.NODE_ENV === 'development'
-    ? new Function('try {return this===window;}catch(e){ return false;}')()
-    : false
+export const isDevTools = (() => {
+  if (process.env.NODE_ENV === 'development') {
+    try {
+      return new Function('try {return this===window;}catch(e){ return false;}')()
+    } catch {
+      //
+    }
+  }
+  return false
+})()
