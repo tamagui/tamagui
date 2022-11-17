59b114ac684b4df84324cca3ef3d232fdad5f8b7
natew <natewienert@gmail.com>
2022-11-16 23:25:00 -1000

    fix use consistent react-native-svg version

--- apps/site/pages/index.tsx
+++ apps/site/pages/index.tsx
@@ -23,8 +23,6 @@ import { getCompilationExamples } from '../lib/getCompilationExamples'
 export default function Home({ animationCode, compilationExamples }) {
   const [top, setTop] = useState(0)
 
-  return <Community />
-
   return (
     <>
       <HomeGlow top={top} />
