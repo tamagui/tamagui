046bf27a6b67c738c251e0b3656a57d66f1df845
natew <natewienert@gmail.com>
2022-11-20 11:08:12 -1000

    site: write

--- packages/core/src/helpers/ThemeManager.tsx
+++ packages/core/src/helpers/ThemeManager.tsx
@@ -224,4 +224,4 @@ export class ThemeManager {
 export const ThemeManagerContext = createContext<ThemeManager | null>(null)
 export const emptyManager = new ThemeManager()
 
-const withoutComponentName = (name: string) => name.replace(/(\_[A-Z][a-zA-Z]+)+$/g, '')
+const withoutComponentName = (name: string) => name.replace(/(_[A-Z][a-zA-Z]+)+$/g, '')
