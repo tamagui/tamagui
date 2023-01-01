ce1115eafd84e4cd5f49ad1b1b96996a77a69f58
natew <natewienert@gmail.com>
2023-01-01 13:22:17 -1000

    update-starters-v1.0.2

--- starters/next-expo-solito/apps/next/next.config.js
+++ starters/next-expo-solito/apps/next/next.config.js
@@ -23,14 +23,12 @@ You can update this monorepo to the latest Tamagui release just by running:
 
 yarn upgrade:tamagui
 
-We've set up a few things for you. We set the "excludeReactNativeWebExports"
-setting in next.config.js, which omits these from the bundle:
+We've set up a few things for you. See the "excludeReactNativeWebExports"
+setting in next.config.js, which omits these from the bundle: Switch, ProgressBar
+Picker, CheckBox, Touchable.
 
-- Switch, ProgressBar, Picker, CheckBox, Touchable
-
-To save more, you can add more that you may not need, like:
-
-- AnimatedFlatList, FlatList, SectionList, VirtualizedList, VirtualizedSectionList
+To save more, you can add ones you don't need like: AnimatedFlatList, FlatList,
+SectionList, VirtualizedList, VirtualizedSectionList.
 
 Even better, enable "useReactNativeWebLite" and you can remove the
 excludeReactNativeWebExports setting altogether, and get tree-shaking and
