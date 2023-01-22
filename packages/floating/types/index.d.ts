1c3e87a22f22cd082a853c3264170e052f945382
Thomas Stevens <thomas.stevens89@icloud.com>
2023-01-22 15:56:05 -0500

    feat(floating): create tamagui-floating package

---
+++ packages/floating/types/index.d.ts
@@ -0,0 +1,3 @@
+export * from './Floating';
+export { useFloating, FloatingOverrideContext, type UseFloatingProps, type UseFloatingReturn, type UseFloatingFn } from './useFloating';
+//# sourceMappingURL=index.d.ts.map
