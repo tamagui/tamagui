a8f60d8f39d3b7ed3613f36f2eb89b00279a49f9
natew <natewienert@gmail.com>
2023-01-22 01:53:48 -1000

    v1.0.19

--- packages/get-button-sized/types/index.d.ts
+++ packages/get-button-sized/types/index.d.ts
@@ -1,7 +1,11 @@
 import type { SizeTokens, VariantSpreadExtras } from '@tamagui/core';
 export declare const getButtonSized: (val: SizeTokens | number, { tokens }: VariantSpreadExtras<any>) => {
-    paddingHorizontal: import("@tamagui/core").VariableVal;
-    height: import("@tamagui/core").VariableVal;
+    paddingHorizontal: number;
+    height: number;
+    borderRadius: number;
+} | {
+    paddingHorizontal: import("@tamagui/core").Variable<number>;
+    height: import("@tamagui/core").Variable<number>;
     borderRadius: import("@tamagui/core").VariableVal;
 };
 //# sourceMappingURL=index.d.ts.map
