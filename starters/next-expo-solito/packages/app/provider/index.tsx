6d077f65d6af6ca86a6b3ca219a69e15c9ae3436
Jonathan <jone.bala@gmail.com>
2023-01-22 19:20:18 -0600

    feat(native): preferred color scheme

--- starters/next-expo-solito/packages/app/provider/index.tsx
+++ starters/next-expo-solito/packages/app/provider/index.tsx
@@ -1,10 +1,12 @@
 import config from '../tamagui.config'
 import { NavigationProvider } from './navigation'
 import { TamaguiProvider, TamaguiProviderProps } from '@my/ui'
+import { useColorScheme } from 'react-native';
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
+  const scheme = useColorScheme();
   return (
-    <TamaguiProvider config={config} disableInjectCSS defaultTheme="light" {...rest}>
+    <TamaguiProvider config={config} disableInjectCSS defaultTheme={scheme === 'dark' ? 'dark' : 'light'} {...rest}>
       <NavigationProvider>{children}</NavigationProvider>
     </TamaguiProvider>
   )
