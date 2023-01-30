a670180038e9b493cb31dceb402085bee781a217
natew <natewienert@gmail.com>
2023-01-30 09:51:00 -1000

    fix(next-themes): set color-scheme on root if dark

--- packages/next-theme/src/NextThemeProvider.tsx
+++ packages/next-theme/src/NextThemeProvider.tsx
@@ -140,7 +140,7 @@ export const NextThemeProvider: React.FC<ThemeProviderProps> = ({
     const wePrefer = colorScheme || 'light'
 
     // avoid running this because it causes full page reflow
-    if (userPrefers !== wePrefer) {
+    if (userPrefers !== wePrefer || wePrefer === 'dark') {
       document.documentElement.style.setProperty('color-scheme', colorScheme)
     }
   }, [enableColorScheme, theme, resolvedTheme, forcedTheme])
