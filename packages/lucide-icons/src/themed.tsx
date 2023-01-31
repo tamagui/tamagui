fca643ccc175c21c36c4f4989b4e57d86845daaa
natew <natewienert@gmail.com>
2023-01-31 01:05:29 -1000

    fix(lucide-icons): Fix sizing not getting the right theme size token values

--- packages/lucide-icons/src/themed.tsx
+++ packages/lucide-icons/src/themed.tsx
@@ -1,4 +1,4 @@
-import { getVariable, useTheme } from '@tamagui/core'
+import { getTokens, getVariable, getVariableValue, useTheme } from '@tamagui/core'
 import React from 'react'
 
 export function themed<A extends React.FC>(Component: A) {
@@ -8,19 +8,14 @@ export function themed<A extends React.FC>(Component: A) {
       (props.color in theme ? theme[props.color] : undefined) ||
         props.color ||
         (!props.disableTheme ? theme.color : null) ||
-        '#000',
+        '#000'
     )
-    return (
-      <Component
-        {...props}
-        color={color}
-        size={
-          typeof props.size === 'string'
-            ? getVariable(theme[props.size] || props.size)
-            : props.size
-        }
-      />
-    )
+    const size =
+      typeof props.size === 'string'
+        ? getVariableValue(getTokens().size[props.size] || props.size)
+        : props.size
+
+    return <Component {...props} color={color} size={size} />
   }
   return wrapped as unknown as A
 }
