d5d136d808d2f74d07675ff828b34ada239e46b4
Lionel Bertrand <lionel.bertrand@paymium.com>
2023-02-05 17:03:04 +0100

    fix(site): fix color text 404 page #624

---
+++ apps/site/pages/404.tsx
@@ -0,0 +1,9 @@
+import { AlertTriangle } from "@tamagui/lucide-icons";
+import { H2, YStack } from "tamagui";
+
+export default function Custom404() {
+  return <YStack flex={1} alignItems="center" justifyContent="center" space="$4">
+    <AlertTriangle size="$7" />
+    <H2>404 - Page Not Found</H2>
+  </YStack>
+}
