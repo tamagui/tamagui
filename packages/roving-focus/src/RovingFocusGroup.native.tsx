50f1802435fdb71ea22938ac4315b27161adb393
Ali <alirezazamani2922@gmail.com>
2023-02-06 10:27:57 +0330

    fix: roving focus native wrapper

--- packages/roving-focus/src/RovingFocusGroup.native.tsx
+++ packages/roving-focus/src/RovingFocusGroup.native.tsx
@@ -1,4 +1,4 @@
-import { withStaticProperties } from '@tamagui/core'
+import { Slot, Stack, withStaticProperties } from '@tamagui/core'
 import { createContextScope } from '@tamagui/create-context'
 import React from 'react'
 
@@ -7,14 +7,16 @@ import { RovingFocusGroupProps, RovingFocusItemProps } from './RovingFocusGroup'
 // noop native doesn't need keyboard accessibility
 
 const ITEM_NAME = 'RovingFocusGroupItem'
-const RovingFocusGroupItem = React.forwardRef(
-  (props: RovingFocusItemProps, _ref) => props.children as any
-)
+const RovingFocusGroupItem = React.forwardRef(({children,...props}: RovingFocusItemProps, _ref) => (
+  <Stack {...props}>{children}</Stack>
+))
 RovingFocusGroupItem.displayName = ITEM_NAME
 const GROUP_NAME = 'RovingFocusGroup'
 
 const RovingFocusGroup = withStaticProperties(
-  React.forwardRef((props: RovingFocusGroupProps, _ref) => props.children as any),
+  React.forwardRef(({children,...props}: RovingFocusGroupProps, _ref) => (
+    <Stack {...props}>{children}</Stack>
+  )),
   {
     Item: RovingFocusGroupItem,
   }
