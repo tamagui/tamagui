4e9f33533f254a5b4fc919124bfd84ab05e6c3d3
Fernando Rojo <frojo@sas.upenn.edu>
2023-02-06 09:59:09 -0500

    fix: native double render
    There's no reason to double render here on web

--- packages/use-window-dimensions/src/index.ts
+++ packages/use-window-dimensions/src/index.ts
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import {
   ScaledSize,
   useWindowDimensions as useWindowDimensionsRN,
+  Platform
 } from 'react-native'
 
 /**
@@ -17,8 +18,11 @@ const initialValue: ScaledSize = {
 }
 
 export function useWindowDimensions() {
-  const [state, setState] = useState(initialValue)
   const current = useWindowDimensionsRN()
+  
+  if (process.env.TAMAGUI_TARGET != 'web') return current
+  
+  const [state, setState] = useState(initialValue)
 
   useIsomorphicLayoutEffect(() => {
     setState(current)
