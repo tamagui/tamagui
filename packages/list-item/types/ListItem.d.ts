96139971396066af2bdaaa7fe55e1cb23ccc9220
Jovi De Croock <decroockjovi@gmail.com>
2023-02-07 03:21:23 +0100

    fix(all): all packages now output both .mjs and .js and use them in their exports (#669)
    In this PR we offer both a `.mjs` as well as a js build, this so that we
    can put mjs in the export-maps so we can adhere to strict node
    resolution for all node-versions which need mjs to show them that we are
    using import/export. The reason for also supplying a js here is because
    tamagui uses dist/esm in the module property. This will make it so that
    older webpack/.. versions can still automatically pick what they can
    resolve according to their configuration.

--- packages/list-item/types/ListItem.d.ts
+++ packages/list-item/types/ListItem.d.ts
@@ -306,7 +306,9 @@ export declare const useListItem: (props: ListItemProps, { Text, Subtitle, Title
 }) => {
     props: {
         children: JSX.Element;
-        hitSlop?: import("react-native").Insets | (import("react-native").Insets & number) | undefined;
+        hitSlop?: import("react-native").Insets | (import("react-native").Insets & number) | undefined; /**
+         * adjust icon relative to size
+         */
         pointerEvents?: "box-none" | "none" | "box-only" | "auto" | undefined;
         removeClippedSubviews?: boolean | undefined;
         style?: import("react-native").StyleProp<import("react-native").ViewStyle>;
@@ -363,25 +365,22 @@ export declare const useListItem: (props: ListItemProps, { Text, Subtitle, Title
         onMagicTap?: (() => void) | undefined;
         accessibilityIgnoresInvertColors?: boolean | undefined;
         target?: string | undefined;
-        asChild?: boolean | undefined;
-        spaceDirection?: import("@tamagui/core").SpaceDirection | undefined; /**
-         * make the spacing elements flex
+        /**
+         * default: -1
          */
+        asChild?: boolean | undefined;
+        spaceDirection?: import("@tamagui/core").SpaceDirection | undefined;
         separator?: React.ReactNode;
         dangerouslySetInnerHTML?: {
             __html: string;
-        } | undefined; /**
-         * adjust internal space relative to icon size
-         */
+        } | undefined;
         animation?: import("@tamagui/core").AnimationProp | null | undefined;
         animateOnly?: string[] | undefined;
         debug?: boolean | "verbose" | undefined;
         disabled?: boolean | undefined;
         className?: string | undefined;
         themeShallow?: boolean | undefined;
-        id?: string | undefined; /**
-         * will not wrap text around `children` only, "all" will not wrap title or subTitle
-         */
+        id?: string | undefined;
         tag?: string | undefined;
         componentName?: string | undefined;
         forceStyle?: "focus" | "hover" | "press" | undefined;
