6bf8bd84d3e606686a0453d1824f1b3090507273
Alireza Zamani <alirezazamani2922@gmail.com>
2023-02-08 08:38:25 +0330

    fix: sync types

--- packages/roving-focus/types/RovingFocusGroup.d.ts
+++ packages/roving-focus/types/RovingFocusGroup.d.ts
@@ -1,11 +1,5 @@
 import { Stack } from '@tamagui/core';
 import * as React from 'react';
-/**
- * A custom hook that converts a callback to a ref to avoid triggering re-renders when passed as a
- * prop or avoid re-executing effects when passed as a dependency
- */
-declare function useCallbackRef<T extends (...args: any[]) => any>(callback: T | undefined): T;
-export { useCallbackRef };
 type PrimitiveDivProps = React.ComponentPropsWithoutRef<typeof Stack>;
 interface RovingFocusGroupImplProps extends Omit<PrimitiveDivProps, 'dir'>, RovingFocusGroupOptions {
     currentTabStopId?: string | null;
