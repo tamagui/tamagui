4472b138cd51ae9d405321925cbab7b26e3bb2fb
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-02-11 22:10:13 +0200

    fix(popover): Avoid bringing in extraneous packages on native (#689)
    * remove unused imports from Popover.tsx
    
    * fix(popover): #668 - @tamagui/popover - platform specific files
    
    * fix(popover): rename files for better gif diffs
    
    * fix(popover): refactor - useFloatingContext to platform specific file

---
+++ packages/popover/types/useFloatingContext.d.ts
@@ -0,0 +1,7 @@
+import type { UseFloatingProps } from '@floating-ui/react-dom-interactions';
+export declare const useFloatingContext: ({ open, setOpen, breakpointActive }: {
+    open: any;
+    setOpen: any;
+    breakpointActive: any;
+}) => (props: UseFloatingProps) => any;
+//# sourceMappingURL=useFloatingContext.d.ts.map
