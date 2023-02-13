2ae3c02698b3e4e104712cd6abc97aa5553debcb
natew <natewienert@gmail.com>
2023-02-12 21:24:08 -1000

    feat(config): add @tamagui/config to replace @tamagui/config-base with new themes

---
+++ packages/config/types/tamagui.config.d.ts
@@ -0,0 +1,154 @@
+export * from './animations';
+export declare const config: {
+    shouldAddPrefersColorThemes: boolean;
+    themeClassNameOnRoot: boolean;
+    animations: import("@tamagui/web").AnimationDriver<{
+        '100ms': {
+            type: "timing";
+            duration: number;
+        };
+        bouncy: {
+            damping: number;
+            mass: number;
+            stiffness: number;
+        };
+        lazy: {
+            damping: number;
+            stiffness: number;
+        };
+        slow: {
+            damping: number;
+            stiffness: number;
+        };
+        quick: {
+            damping: number;
+            mass: number;
+            stiffness: number;
+        };
+        tooltip: {
+            damping: number;
+            mass: number;
+            stiffness: number;
+        };
+    }>;
+    media: {
+        xl: {
+            maxWidth: number;
+        };
+        lg: {
+            maxWidth: number;
+        };
+        md: {
+            maxWidth: number;
+        };
+        sm: {
+            maxWidth: number;
+        };
+        xs: {
+            maxWidth: number;
+        };
+        xxs: {
+            maxWidth: number;
+        };
+        gtXs: {
+            minWidth: number;
+        };
+        gtSm: {
+            minWidth: number;
+        };
+        gtMd: {
+            minWidth: number;
+        };
+        gtLg: {
+            minWidth: number;
+        };
+        gtXl: {
+            minWidth: number;
+        };
+    };
+    shorthands: {
+        readonly ussel: "userSelect";
+        readonly cur: "cursor";
+        readonly pe: "pointerEvents";
+        readonly col: "color";
+        readonly ff: "fontFamily";
+        readonly fos: "fontSize";
+        readonly fost: "fontStyle";
+        readonly fow: "fontWeight";
+        readonly ls: "letterSpacing";
+        readonly lh: "lineHeight";
+        readonly ta: "textAlign";
+        readonly tt: "textTransform";
+        readonly ww: "wordWrap";
+        readonly ac: "alignContent";
+        readonly ai: "alignItems";
+        readonly als: "alignSelf";
+        readonly b: "bottom";
+        readonly bc: "backgroundColor";
+        readonly bg: "backgroundColor";
+        readonly bbc: "borderBottomColor";
+        readonly bblr: "borderBottomLeftRadius";
+        readonly bbrr: "borderBottomRightRadius";
+        readonly bbw: "borderBottomWidth";
+        readonly blc: "borderLeftColor";
+        readonly blw: "borderLeftWidth";
+        readonly boc: "borderColor";
+        readonly br: "borderRadius";
+        readonly bs: "borderStyle";
+        readonly brw: "borderRightWidth";
+        readonly brc: "borderRightColor";
+        readonly btc: "borderTopColor";
+        readonly btlr: "borderTopLeftRadius";
+        readonly btrr: "borderTopRightRadius";
+        readonly btw: "borderTopWidth";
+        readonly bw: "borderWidth";
+        readonly dsp: "display";
+        readonly f: "flex";
+        readonly fb: "flexBasis";
+        readonly fd: "flexDirection";
+        readonly fg: "flexGrow";
+        readonly fs: "flexShrink";
+        readonly fw: "flexWrap";
+        readonly h: "height";
+        readonly jc: "justifyContent";
+        readonly l: "left";
+        readonly m: "margin";
+        readonly mah: "maxHeight";
+        readonly maw: "maxWidth";
+        readonly mb: "marginBottom";
+        readonly mih: "minHeight";
+        readonly miw: "minWidth";
+        readonly ml: "marginLeft";
+        readonly mr: "marginRight";
+        readonly mt: "marginTop";
+        readonly mx: "marginHorizontal";
+        readonly my: "marginVertical";
+        readonly o: "opacity";
+        readonly ov: "overflow";
+        readonly p: "padding";
+        readonly pb: "paddingBottom";
+        readonly pl: "paddingLeft";
+        readonly pos: "position";
+        readonly pr: "paddingRight";
+        readonly pt: "paddingTop";
+        readonly px: "paddingHorizontal";
+        readonly py: "paddingVertical";
+        readonly r: "right";
+        readonly shac: "shadowColor";
+        readonly shar: "shadowRadius";
+        readonly shof: "shadowOffset";
+        readonly shop: "shadowOpacity";
+        readonly t: "top";
+        readonly w: "width";
+        readonly zi: "zIndex";
+    };
+    themes: any;
+    tokens: any;
+    fonts: {
+        heading: import("@tamagui/web").GenericFont<9 | 15 | 10 | 11 | 12 | 14 | 16 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 13 | "true">;
+        body: import("@tamagui/web").GenericFont<9 | 15 | 10 | 11 | 12 | 14 | 16 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 13 | "true">;
+        mono: import("@tamagui/web").GenericFont<9 | 15 | 10 | 11 | 12 | 14 | 16 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 13>;
+        silkscreen: import("@tamagui/web").GenericFont<9 | 15 | 10 | 11 | 12 | 14 | 16 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 13>;
+    };
+};
+//# sourceMappingURL=tamagui.config.d.ts.map
