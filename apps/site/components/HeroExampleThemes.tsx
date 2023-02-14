7cc86b1422f4edaceb7728aa8b90e03c3eff38a3
natew <natewienert@gmail.com>
2023-02-13 16:57:42 -1000

    site various cleanup and light mode fixes

--- apps/site/components/HeroExampleThemes.tsx
+++ apps/site/components/HeroExampleThemes.tsx
@@ -210,7 +210,7 @@ export const HeroExampleThemes = memo(function HeroExampleThemes() {
                     <ActiveCircle
                       onPress={() => updateActiveI([i, curShadeI])}
                       isActive={isActive}
-                      backgroundColor="$color5"
+                      backgroundColor="$color8"
                     />
                   </Theme>
                 )
