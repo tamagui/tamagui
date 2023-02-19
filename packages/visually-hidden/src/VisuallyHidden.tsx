779c6ff92322edd82b9862b41df7b69066cc9e00
natew <natewienert@gmail.com>
2023-02-18 17:30:03 -1000

    fix(static): fix resolve web to core to avoid duplicate tamagui core
    save
    
    save

--- packages/visually-hidden/src/VisuallyHidden.tsx
+++ packages/visually-hidden/src/VisuallyHidden.tsx
@@ -17,7 +17,7 @@ export const VisuallyHidden = styled(Stack, {
         width: 'auto',
         height: 'auto',
       },
-    } as const,
+    },
 
     visible: {
       true: {
@@ -30,8 +30,8 @@ export const VisuallyHidden = styled(Stack, {
         opacity: 1,
         pointerEvents: 'auto',
       },
-    } as const,
-  },
+    },
+  } as const,
 })
 
 // @tamgui/core checks for this in spacing
--- packages/visually-hidden/types/VisuallyHidden.d.ts
+++ packages/visually-hidden/types/VisuallyHidden.d.ts
@@ -1,14 +1,14 @@
 export declare const VisuallyHidden: import("@tamagui/web").TamaguiComponent<Omit<import("react-native").ViewProps, "display" | "children" | ("onLayout" | keyof import("react-native").GestureResponderHandlers)> & import("@tamagui/web").ExtendBaseStackProps & import("@tamagui/web").TamaguiComponentPropsBase & import("@tamagui/web").WithThemeValues<import("@tamagui/web").StackStylePropsBase> & import("@tamagui/web").WithShorthands<import("@tamagui/web").WithThemeValues<import("@tamagui/web").StackStylePropsBase>> & Omit<{}, "visible" | "preserveDimensions"> & {
-    preserveDimensions?: boolean | undefined;
-    visible?: boolean | undefined;
+    readonly preserveDimensions?: boolean | undefined;
+    readonly visible?: boolean | undefined;
 } & import("@tamagui/web").MediaProps<Partial<Omit<import("react-native").ViewProps, "display" | "children" | ("onLayout" | keyof import("react-native").GestureResponderHandlers)> & import("@tamagui/web").ExtendBaseStackProps & import("@tamagui/web").TamaguiComponentPropsBase & import("@tamagui/web").WithThemeValues<import("@tamagui/web").StackStylePropsBase> & import("@tamagui/web").WithShorthands<import("@tamagui/web").WithThemeValues<import("@tamagui/web").StackStylePropsBase>> & Omit<{}, "visible" | "preserveDimensions"> & {
-    preserveDimensions?: boolean | undefined;
-    visible?: boolean | undefined;
+    readonly preserveDimensions?: boolean | undefined;
+    readonly visible?: boolean | undefined;
 }>> & import("@tamagui/web").PseudoProps<Partial<Omit<import("react-native").ViewProps, "display" | "children" | ("onLayout" | keyof import("react-native").GestureResponderHandlers)> & import("@tamagui/web").ExtendBaseStackProps & import("@tamagui/web").TamaguiComponentPropsBase & import("@tamagui/web").WithThemeValues<import("@tamagui/web").StackStylePropsBase> & import("@tamagui/web").WithShorthands<import("@tamagui/web").WithThemeValues<import("@tamagui/web").StackStylePropsBase>> & Omit<{}, "visible" | "preserveDimensions"> & {
-    preserveDimensions?: boolean | undefined;
-    visible?: boolean | undefined;
+    readonly preserveDimensions?: boolean | undefined;
+    readonly visible?: boolean | undefined;
 }>>, import("@tamagui/web").TamaguiElement, import("@tamagui/web").StackPropsBase, {
-    preserveDimensions?: boolean | undefined;
-    visible?: boolean | undefined;
+    readonly preserveDimensions?: boolean | undefined;
+    readonly visible?: boolean | undefined;
 }>;
 //# sourceMappingURL=VisuallyHidden.d.ts.map
