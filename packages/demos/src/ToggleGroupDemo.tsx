fa009af8dd344941bc5fcb81e889127402f65fef
azeez1776 <aboud3am81@gmail.com>
2023-03-04 19:17:51 +0300

    modified the togglegroup demo

--- packages/demos/src/ToggleGroupDemo.tsx
+++ packages/demos/src/ToggleGroupDemo.tsx
@@ -1,11 +1,25 @@
 import { AlignCenter, AlignLeft, AlignRight } from '@tamagui/lucide-icons'
-import { Label, Separator, SizeTokens, ToggleGroup, XStack, YStack, XGroup} from 'tamagui'
+import {
+  Label,
+  Separator,
+  SizeTokens,
+  ToggleGroup,
+  XGroup,
+  XStack,
+  YStack,
+} from 'tamagui'
 
 export function ToggleGroupDemo() {
   return (
-    <YStack w={200} ai="center" space="$3">
-      <ToggleGroupComponent type="single" size="$6" />
-      <ToggleGroupComponent type="multiple" size="$6" />
+    <YStack w={500} ai="center" space="$3">
+      <XStack w={400} ai="center" space="$4">
+        <ToggleGroupComponent type="single" size="$4" />
+        <ToggleGroupComponent type="single" size="$3" />
+      </XStack>
+      <XStack w={400} ai="center" space="$4">
+        <ToggleGroupComponent type="multiple" size="$4" />
+        <ToggleGroupComponent type="multiple" size="$3" />
+      </XStack>
     </YStack>
   )
 }
@@ -15,22 +29,22 @@ function ToggleGroupComponent(props: { size: SizeTokens; type: 'single' | 'multi
   return (
     <XStack w={200} ai="center" space="$4">
       <Label pr="$0" miw={90} jc="flex-end" size={props.size} htmlFor={id}>
-        {props.type === 'single'?'Single':'Multiple'}
+        {props.type === 'single' ? 'Single' : 'Multiple'}
       </Label>
       <Separator mih={20} vertical />
-      <XGroup>
       <ToggleGroup id={id} type={props.type}>
-        <ToggleGroup.Item size={props.size} value="left" aria-label="Left aligned">
-          <AlignLeft />
-        </ToggleGroup.Item>
-        <ToggleGroup.Item size={props.size} value="center" aria-label="Center aligned">
-          <AlignCenter />
-        </ToggleGroup.Item>
-        <ToggleGroup.Item size={props.size} value="right" aria-label="Right aligned">
-          <AlignRight />
-        </ToggleGroup.Item>
+        <XGroup elevation={10} size="$2">
+          <ToggleGroup.Item size={props.size} value="left" aria-label="Left aligned">
+            <AlignLeft />
+          </ToggleGroup.Item>
+          <ToggleGroup.Item size={props.size} value="center" aria-label="Center aligned">
+            <AlignCenter />
+          </ToggleGroup.Item>
+          <ToggleGroup.Item size={props.size} value="right" aria-label="Right aligned">
+            <AlignRight />
+          </ToggleGroup.Item>
+        </XGroup>
       </ToggleGroup>
-      </XGroup>
     </XStack>
   )
 }
