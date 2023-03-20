0d268ea3d24e5800968fffdea069b0f1f10a1db1
natew <natewienert@gmail.com>
2023-03-19 14:02:03 -1000

    update burnt and add some docs on native deps, lazy load Burnt to avoid immedaite errors, document install

--- packages/toast/src/createNativeToast.native.tsx
+++ packages/toast/src/createNativeToast.native.tsx
@@ -1,11 +1,12 @@
-import * as Burnt from 'burnt'
-
 import { CreateNativeToastsOptions, CreateNativeToastsOptionsFn } from './types'
 
 export const createNativeToast: CreateNativeToastsOptionsFn = (
   title,
   { message, duration, preset = 'done' }: CreateNativeToastsOptions
 ) => {
+  // import inline to allow lazy usage of native dependecy:
+  const Burnt = require('burnt')
+
   Burnt.toast({
     title,
     message: message ?? '',
