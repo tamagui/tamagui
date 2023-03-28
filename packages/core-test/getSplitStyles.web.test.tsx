b009cfcd9753064b5f65855d570b072a96adce7f
Alireza Zamani <alirezazamani2922@gmail.com>
2023-03-29 01:38:43 +0430

    test(core): more getSplitStyles test for fonts

--- packages/core-test/getSplitStyles.web.test.tsx
+++ packages/core-test/getSplitStyles.web.test.tsx
@@ -42,11 +42,30 @@ describe('getSplitStyles', () => {
     expect(out.rulesToInsert[0]?.value).toEqual('10px')
   })
 
-  test(`fonts get merged properly`, () => {
-    const CustomText = styled(Text, {
-      name: 'Heading',
-      color: '$color',
+  test(`font props get the font family, regardless of the order`, () => {
+    expect(
+      simplifiedGetSplitStyles(Text, {
+        fontSize: '$1',
+      }).rulesToInsert.find((rule) => rule.property === 'fontSize')?.value
+    ).toEqual('$1') // no family provided - this is expected
 
+    expect(
+      simplifiedGetSplitStyles(Text, {
+        fontSize: '$1',
+        fontFamily: '$body',
+      }).rulesToInsert.find((rule) => rule.property === 'fontSize')?.value
+    ).toEqual('var(--f-si-1)')
+
+    expect(
+      simplifiedGetSplitStyles(Text, {
+        fontFamily: '$body',
+        fontSize: '$1',
+      }).rulesToInsert.find((rule) => rule.property === 'fontSize')?.value
+    ).toEqual('var(--f-si-1)')
+  })
+
+  test(`font props get the font family from a variant, regardless of the order`, () => {
+    const CustomText = styled(Text, {
       variants: {
         type: {
           myValue: {
@@ -55,17 +74,53 @@ describe('getSplitStyles', () => {
         },
       } as const,
     })
-    const splitStyles = simplifiedGetSplitStyles(CustomText, {
-      fontSize: '$1', // should be able to resolve $body from the variant and find the css value
-      type: 'myValue',
-    })
 
     expect(
-      splitStyles.rulesToInsert
-        .find((rule) => rule.property === 'fontSize')
-        ?.value.startsWith('var(--')
-    ).toBeTruthy()
+      simplifiedGetSplitStyles(CustomText, {
+        fontSize: '$1',
+        type: 'myValue',
+      }).rulesToInsert.find((rule) => rule.property === 'fontSize')?.value
+    ).toEqual('var(--f-si-1)')
+
+    expect(
+      simplifiedGetSplitStyles(CustomText, {
+        type: 'myValue',
+        fontSize: '$1',
+      }).rulesToInsert.find((rule) => rule.property === 'fontSize')?.value
+    ).toEqual('var(--f-si-1)')
   })
+
+  // this test is failing:
+  // TODO: support this - might need the getSplitStyles refactor (unifying getSubStyle)
+  // + write another similar test for pseudos
+  // test(`fonts get merged correctly if fontSize is media activates font family`, () => {
+  //   const CustomText = styled(Text, {
+  //     variants: {
+  //       type: {
+  //         myValue: {
+  //           fontFamily: '$body',
+  //         },
+  //       },
+  //     } as const,
+  //   })
+  //   const splitStyles = simplifiedGetSplitStyles(
+  //     CustomText,
+  //     {
+  //       type: 'myValue',
+  //       $xs: {
+  //         fontSize: '$1',
+  //       },
+  //     },
+  //     'p',
+  //     { xs: true }
+  //   )
+
+  //   const fontSizeRule = splitStyles.rulesToInsert.find(
+  //     (rule) => rule.property === 'fontSize'
+  //   )
+
+  //   expect(fontSizeRule?.rules[0].includes('font-size:var(--f-si-1)')).toBeTruthy()
+  // })
 
   // test(`prop "tabIndex" defaults to "0", overrides to "-1" when tag = button`, () => {
   //   expect(
@@ -91,7 +146,8 @@ describe('getSplitStyles', () => {
 function simplifiedGetSplitStyles(
   component: TamaguiComponent,
   props: Record<string, any>,
-  tag?: string
+  tag?: string,
+  mediaState?: Record<string, any>
 ) {
   return getSplitStyles(
     props,
@@ -103,6 +159,7 @@ function simplifiedGetSplitStyles(
       pressIn: false,
       focus: false,
       unmounted: true,
+      mediaState,
     },
     undefined,
     undefined,
