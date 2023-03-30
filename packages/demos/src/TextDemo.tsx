f8b86017d97ee6655fe2b57f823f092f74b75d9a
Peter Piekarczyk <peterpme@users.noreply.github.com>
2023-03-30 14:17:39 -0500

    Update TextDemo.tsx

--- packages/demos/src/TextDemo.tsx
+++ packages/demos/src/TextDemo.tsx
@@ -5,7 +5,7 @@ export function TextDemo() {
   return (
     <YStack space="$2" ai="center">
       <SizableText size="$3">SizableText</SizableText>
-      <Text>Basic Unstyled Font</Text>
+      <Text>Unstyled Text</Text>
       <XStack space>
         <SizableText theme="alt1" size="$3">
           alt1
