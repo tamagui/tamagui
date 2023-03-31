51416574488e9ec23b1f1c7a8f31f36a1789670a
Alireza Zamani <alirezazamani2922@gmail.com>
2023-03-31 20:31:31 +0330

    fix(animations): add .js to imports

--- packages/web/src/hooks/useAnimationDriver.tsx
+++ packages/web/src/hooks/useAnimationDriver.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react'
 
-import { AnimationDriverContext } from '../contexts/AnimationDriverContext'
-import { getAnimationDriver } from '../helpers/getAnimationDriver'
+import { AnimationDriverContext } from '../contexts/AnimationDriverContext.js'
+import { getAnimationDriver } from '../helpers/getAnimationDriver.js'
 
 export const useAnimationDriver = () => {
   return useContext(AnimationDriverContext) ?? getAnimationDriver()
