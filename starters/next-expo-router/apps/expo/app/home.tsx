1804adda3f2ac28dbf80de99eda0944f03f980a6
Ehsan Sarshar <ehsan.herai@gmail.com>
2023-04-01 23:25:58 +0430

    add expo-router

---
+++ starters/next-expo-router/apps/expo/app/home.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native'
+
+export default function Home() {
+  return <Text>Home</Text>
+}
---
+++ starters/next-expo-router/apps/expo/app/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'tamagui'
+
+export default function App() {
+  return <Text>This is another index.ts</Text>
+}
