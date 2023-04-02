88f899d9f39ccb06fbb9fc87bf2fcebfffac50ab
natew <natewienert@gmail.com>
2023-04-02 10:39:26 -1000

    save

--- packages/themes/src/index.tsx
+++ packages/themes/src/index.tsx
@@ -1,3 +1,8 @@
-export * from './themes'
+import generatedThemes from './generated'
+import { themes as ThemesType } from './themes'
+
+export const themes = generatedThemes as typeof ThemesType
+
+// export * from './themes'
 export * from './tokens'
 export * from '@tamagui/colors'
