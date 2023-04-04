a4b7dd22c6fa7e90dd9f35f46906038d15df9b26
Alireza Zamani <alirezazamani2922@gmail.com>
2023-04-04 19:42:16 +0330

    feat(studio): remote mode basic impl

--- packages/cli/src/studio.ts
+++ packages/cli/src/studio.ts
@@ -16,70 +16,79 @@ import { ResolvedOptions } from './types.js'
 const resolve =
   'url' in import.meta ? createRequire(import.meta.url).resolve : require.resolve
 
-export const studio = async (options: ResolvedOptions) => {
+export const studio = async (options: ResolvedOptions, isRemote = true) => {
   process.env.TAMAGUI_TARGET = 'web'
 
-  process.stdout.on('error', function (err) {
-    if (err.code == 'EPIPE') {
-      process.exit(0)
-    }
-  })
-
-  const { default: getPort } = await import('get-port')
-  const { paths } = options
-  const root = dirname(resolve('@takeout/studio/entry'))
-
-  const [serverPort, vitePort] = await Promise.all([
-    getPort({
-      port: 1421,
-    }),
-    getPort({
-      port: 1422,
-    }),
-  ])
-
-  const server = await createServer({
-    root,
-    server: {
-      host: options.host,
-      port: vitePort,
-      hmr: true,
-      cors: true,
-    },
-    plugins: [
-      tamaguiPlugin({
-        components: ['tamagui'],
-      }),
-      viteReactPlugin(),
-    ],
-  })
-
-  // these can be lazy loaded (eventually should put in own process)
-  await server.listen()
-
   const configWatchPromise = watchTamaguiConfig(options)
+  let localServerPromise = new Promise(() => {})
+  if (!isRemote) {
+    process.env.VITE_IS_LOCAL = '1'
 
-  const info = server.httpServer?.address() as AddressInfo
+    process.stdout.on('error', function (err) {
+      if (err.code == 'EPIPE') {
+        process.exit(0)
+      }
+    })
+    const { default: getPort } = await import('get-port')
+    const { paths } = options
+    const root = dirname(resolve('@takeout/studio/entry'))
 
-  const app = express()
+    const [serverPort, vitePort] = await Promise.all([
+      getPort({
+        port: 1421,
+      }),
+      getPort({
+        port: 1422,
+      }),
+    ])
 
-  app.disable('x-powered-by')
-  app.use(express.static(paths.dotDir, { maxAge: '2h' }))
-  // app.use(morgan('tiny'))
+    const server = await createServer({
+      root,
+      server: {
+        host: options.host,
+        port: vitePort,
+        hmr: true,
+        cors: true,
+      },
+      envPrefix: "VITE_",
+      plugins: [
+        tamaguiPlugin({
+          components: ['tamagui'],
+        }),
+        viteReactPlugin(),
+      ],
+    })
 
-  app.get('/conf.json', async (req, res) => {
-    const conf = await fs.readJSON(paths.conf)
-    res.status(200).json(conf)
-  })
+    // these can be lazy loaded (eventually should put in own process)
+    await server.listen()
 
-  app.use('/', proxy(`${info.address}:${vitePort}`))
+    const info = server.httpServer?.address() as AddressInfo
 
-  const appServer = app.listen(serverPort)
+    const app = express()
 
-  console.log(`Listening on`, chalk.green(`http://localhost:${serverPort}`))
+    app.disable('x-powered-by')
+    app.use(express.static(paths.dotDir, { maxAge: '2h' }))
+    // app.use(morgan('tiny'))
 
-  await Promise.allSettled([
-    configWatchPromise,
-    new Promise((res) => appServer.on('close', res)),
-  ])
+    app.get('/conf.json', async (req, res) => {
+      const conf = await fs.readJSON(paths.conf)
+      res.status(200).json(conf)
+    })
+
+    app.use('/', proxy(`${info.address}:${vitePort}`))
+
+    const appServer = app.listen(serverPort)
+
+    localServerPromise = new Promise((res) => appServer.on('close', res))
+
+    console.log(`Listening on`, chalk.green(`http://localhost:${serverPort}`))
+  } else {
+    console.log(
+      `Open `,
+      chalk.green(`https://studio.tamagui.dev/load`),
+      ` to load studio`
+    )
+  }
+
+  await Promise.allSettled([configWatchPromise, localServerPromise])
 }
--- packages/cli/types/studio.d.ts
+++ packages/cli/types/studio.d.ts
@@ -1,3 +1,3 @@
 import { ResolvedOptions } from './types.js';
-export declare const studio: (options: ResolvedOptions) => Promise<void>;
+export declare const studio: (options: ResolvedOptions, isRemote?: boolean) => Promise<void>;
 //# sourceMappingURL=studio.d.ts.map
