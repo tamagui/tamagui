f6500b16f3bf0f7f8a2228b9a6c9c7783b6f5d76
Alireza Zamani <alirezazamani2922@gmail.com>
2023-04-06 21:12:14 +0330

    feat(toast)!: remove `ToastImperativeProvider`, demos & docs improvements (#908)
    BREAKING CHANGE: Users will need to remove usage of `ToastImperativeProvider` and pass the options to `ToastProvider`.
    
    ---------
    
    Co-authored-by: natew <natewienert@gmail.com>

--- packages/toast/types/Toast.d.ts
+++ packages/toast/types/Toast.d.ts
@@ -1,5 +1,6 @@
 import { GetProps, TamaguiElement } from '@tamagui/core';
 import * as React from 'react';
+import { ToastNativePlatform, ToastNativeValue, useToast } from './ToastImperative.js';
 import { ToastProps } from './ToastImpl';
 import { ToastProvider, ToastProviderProps, createToastScope } from './ToastProvider';
 import { ToastViewport, ToastViewportProps } from './ToastViewport';
@@ -432,6 +433,6 @@ declare const Toast: ((props: Omit<ToastProps & React.RefAttributes<TamaguiEleme
         [x: string]: undefined;
     }>>, "ref">) & React.RefAttributes<TamaguiElement>>;
 };
-export { Toast, ToastProvider, ToastViewport, createToastScope, };
-export type { ToastActionProps, ToastCloseProps, ToastDescriptionProps, ToastProps, ToastProviderProps, ToastTitleProps, ToastViewportProps, };
+export { Toast, ToastProvider, ToastViewport, createToastScope, useToast, };
+export type { ToastActionProps, ToastCloseProps, ToastDescriptionProps, ToastProps, ToastProviderProps, ToastTitleProps, ToastViewportProps, ToastNativeValue, ToastNativePlatform, };
 //# sourceMappingURL=Toast.d.ts.map
