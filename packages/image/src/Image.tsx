fd53a176432c7874a11a4b7ab9c6006490c63e33
natew <natewienert@gmail.com>
2023-04-08 06:39:51 -1000

    fix(image): move to source attribute and deprecate src + string shorthand to align with RN

--- packages/image/src/Image.tsx
+++ packages/image/src/Image.tsx
@@ -3,6 +3,7 @@ import {
   RadiusTokens,
   SizeTokens,
   StackProps,
+  ThemeValueFallback,
   isWeb,
   setupReactNative,
   styled,
@@ -26,12 +27,13 @@ type StyledImageProps = Omit<GetProps<typeof StyledImage>, 'borderRadius'> & {
   borderRadius?: RadiusTokens
 }
 
-type BaseProps = Omit<
-  StyledImageProps,
-  'source' | 'width' | 'height' | 'style' | 'onLayout'
-> & {
-  width: number | SizeTokens
-  height: number | SizeTokens
+type BaseProps = Omit<StyledImageProps, 'width' | 'height' | 'style' | 'onLayout'> & {
+  width: string | number | SizeTokens | ThemeValueFallback
+  height: string | number | SizeTokens | ThemeValueFallback
+
+  /**
+   * @deprecated use `source` instead
+   */
   src: string | StyledImageProps['source']
 }
 
@@ -48,17 +50,31 @@ type ImageType = React.FC<ImageProps> & {
   queryCache: RNImageType['queryCache']
 }
 
+let hasWarned = false
+
 export const Image = StyledImage.extractable(
   forwardRef((inProps: ImageProps, ref) => {
     const props = useMediaPropsActive(inProps)
-    const { src, ...rest } = props
-    const source =
+    const { src, source, ...rest } = props
+
+    if (process.env.NODE_ENV === 'development') {
+      if (typeof src === 'string') {
+        if (!hasWarned) {
+          hasWarned = true
+          console.warn(`Warning: The "src" prop on Image is deprecated, so doing <Image src="" /> is no longer valid.
+
+This is because you must specify width/height for images on React Native. Use the existing <Image source={{ uri: '', width: 100, height: 100 }} /> prop instead.`)
+        }
+      }
+    }
+
+    const finalSource =
       typeof src === 'string'
         ? { uri: src, ...(isWeb && { width: props.width, height: props.height }) }
-        : src
+        : source ?? src
 
     // must set defaultSource to allow SSR, default it to the same as src
-    return <StyledImage ref={ref} source={source} {...(rest as any)} />
+    return <StyledImage ref={ref} source={finalSource} {...(rest as any)} />
   })
 ) as any as ImageType
 
