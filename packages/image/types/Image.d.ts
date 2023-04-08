8635f493d24ec5dfc941e8caeba15d2127425c78
natew <natewienert@gmail.com>
2023-04-08 09:27:36 -1000

    fix toast type

--- packages/image/types/Image.d.ts
+++ packages/image/types/Image.d.ts
@@ -1,4 +1,4 @@
-import { GetProps, RadiusTokens, StackProps } from '@tamagui/core';
+import { GetProps, RadiusTokens, SizeTokens, StackProps, ThemeValueFallback } from '@tamagui/core';
 import React from 'react';
 import { Image as RNImage } from 'react-native';
 declare const StyledImage: import("@tamagui/core").TamaguiComponent<(import("react-native").ImageProps & Omit<StackProps, keyof import("react-native").ImageProps>) | (import("react-native").ImageProps & Omit<StackProps, keyof import("react-native").ImageProps> & Omit<{}, string | number> & {
@@ -13,7 +13,12 @@ declare const StyledImage: import("@tamagui/core").TamaguiComponent<(import("rea
 type StyledImageProps = Omit<GetProps<typeof StyledImage>, 'borderRadius'> & {
     borderRadius?: RadiusTokens;
 };
-type BaseProps = Omit<StyledImageProps, 'source' | 'style' | 'onLayout'> & {
+type BaseProps = Omit<StyledImageProps, 'width' | 'height' | 'style' | 'onLayout'> & {
+    width: string | number | SizeTokens | ThemeValueFallback;
+    height: string | number | SizeTokens | ThemeValueFallback;
+    /**
+     * @deprecated use `source` instead
+     */
     src: string | StyledImageProps['source'];
 };
 export type ImageProps = BaseProps & Omit<StackProps, keyof BaseProps>;
