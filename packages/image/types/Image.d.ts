66e8a22d4298988e19f02f9c23dee26d14300f19
natew <natewienert@gmail.com>
2023-04-08 22:00:04 -1000

    fix(image): make deprecated src optional

--- packages/image/types/Image.d.ts
+++ packages/image/types/Image.d.ts
@@ -19,7 +19,7 @@ type BaseProps = Omit<StyledImageProps, 'width' | 'height' | 'style' | 'onLayout
     /**
      * @deprecated use `source` instead to disambiguate width/height style from width/height of the actual image
      */
-    src: string | StyledImageProps['source'];
+    src?: string | StyledImageProps['source'];
 };
 export type ImageProps = BaseProps & Omit<StackProps, keyof BaseProps>;
 type RNImageType = typeof RNImage;
