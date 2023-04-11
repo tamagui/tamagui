b5e7e6203bcb30861c12cdff2625f73db383661c
natew <natewienert@gmail.com>
2023-04-10 19:46:18 -1000

    fix(card): fix unstyled false to default variant on card header and improve demo

--- packages/card/src/Card.tsx
+++ packages/card/src/Card.tsx
@@ -56,6 +56,10 @@ export const CardHeader = styled(ThemeableStack, {
       },
     },
   } as const,
+
+  defaultVariants: {
+    unstyled: false,
+  },
 })
 
 export const CardFooter = styled(CardHeader, {
