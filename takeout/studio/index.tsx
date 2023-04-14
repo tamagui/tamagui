a8b10f1f7b4759418685c9a426c20e7a858de21b
natew <natewienert@gmail.com>
2023-04-13 21:49:41 -1000

    studio
    Co-authored-by: Alireza Zamani <alirezazamani2922@gmail.com>

--- takeout/studio/index.tsx
+++ takeout/studio/index.tsx
@@ -1,12 +1,23 @@
 import '@tamagui/core/reset.css'
 import '@tamagui/polyfill-dev'
 
+import {
+  SessionContextProvider,
+  SupabaseClient,
+  useSession,
+  useSessionContext,
+  useSupabaseClient,
+  useUser,
+} from '@supabase/auth-helpers-react'
+import { createClient } from '@supabase/supabase-js'
 import { config as configBase } from '@tamagui/config'
-import React from 'react'
+import { useSharedAuth } from '@tamagui/site-shared'
+import React, { useEffect, useState } from 'react'
 import { createRoot } from 'react-dom/client'
-import { TamaguiProvider, createTamagui } from 'tamagui'
+import { H1, Paragraph, TamaguiProvider, YStack, createTamagui } from 'tamagui'
 
 import { Studio } from './src'
+import { checkForSponsorship } from './src/api/gql'
 
 globalThis['React'] = React
 
@@ -22,11 +33,20 @@ declare module 'tamagui' {
 }
 
 const App = () => {
+  const [supabaseClient] = useState(() =>
+    createClient(
+      (import.meta as any).env.VITE_PUBLIC_SUPABASE_URL!,
+      (import.meta as any).env.VITE_PUBLIC_SUPABASE_ANON_KEY!
+    )
+  )
+
   return (
-    <TamaguiProvider config={config} defaultTheme="light">
-      <link href="/fonts/inter.css" rel="stylesheet" />
-      <Studio />
-    </TamaguiProvider>
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      <TamaguiProvider config={config} defaultTheme="light">
+        <link href="/fonts/inter.css" rel="stylesheet" />
+        <Studio />
+      </TamaguiProvider>
+    </SessionContextProvider>
   )
 }
 
