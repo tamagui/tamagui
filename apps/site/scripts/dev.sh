7645ed9c015e82409ef052f70761f55c99446ada
Alireza Zamani <alirezazamani2922@gmail.com>
2023-04-18 16:14:59 +0330

    fix(site): enable studio

--- apps/site/scripts/dev.sh
+++ apps/site/scripts/dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-PORT=${PORT:-5005} NODE_OPTIONS='--no-deprecation' NODE_ENV=development yarn watch:site-data
+TAMAGUI_ENABLE_STUDIO=1 PORT=${PORT:-5005} NODE_OPTIONS='--no-deprecation' NODE_ENV=development yarn watch:site-data
