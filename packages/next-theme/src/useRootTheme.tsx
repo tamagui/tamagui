af5d1ed44ffd5e23f19103e7957439151e988aa0
natew <natewienert@gmail.com>
2023-04-18 20:35:30 -1000

    feat(next-theme): support fallback prop and allow dark theme detect for useRootTheme

--- packages/next-theme/src/useRootTheme.tsx
+++ packages/next-theme/src/useRootTheme.tsx
@@ -1,17 +1,22 @@
 import { useLayoutEffect, useState } from 'react'
 
-export const useRootTheme = ({ initial = 'light' }: { initial?: 'dark' | 'light' }) => {
-  const [val, setVal] = useState(initial)
+type ColorScheme = 'dark' | 'light'
+
+export const useRootTheme = ({ fallback = 'light' }: { fallback?: ColorScheme }) => {
+  const [val, setVal] = useState<ColorScheme>(fallback)
 
   if (typeof document !== 'undefined') {
     useLayoutEffect(() => {
       // @ts-ignore
       const classes = [...document.documentElement.classList]
-      const isInitial = classes.includes(`t_${initial}`)
-      if (!isInitial) {
-        const opposite = initial === 'light' ? 'dark' : 'light'
-        setVal(opposite)
-      }
+
+      const val: ColorScheme = classes.includes(`t_dark`)
+        ? 'dark'
+        : classes.includes(`t_light`)
+        ? 'light'
+        : fallback
+
+      setVal(val)
     }, [])
   }
 
