24af0f77db2d97e08ddb07b0b0055e6717ace976
natew <natewienert@gmail.com>
2023-04-19 21:39:00 -1000

    site: remove date-fns

--- apps/site/pages/blog/index.tsx
+++ apps/site/pages/blog/index.tsx
@@ -1,11 +1,10 @@
+import { getBlogLayout } from '@components/layouts/BlogLayout'
 import { TitleAndMetaTags } from '@components/TitleAndMetaTags'
 import { authors } from '@data/authors'
 import { getAllFrontmatter } from '@lib/mdx'
 import { NextLink } from 'components/NextLink'
-import { format, parseISO } from 'date-fns'
 import { H2, Paragraph, Spacer, XStack } from 'tamagui'
 
-import { getBlogLayout } from '@components/layouts/BlogLayout'
 import { ContainerLarge } from '../../components/Container'
 import { TamaguiCard } from '../../components/TamaguiCard'
 
@@ -25,7 +24,12 @@ export default function Blog({ frontmatters }) {
                 title={frontmatter.title}
                 subTitle={
                   <Paragraph o={0.5} cursor="inherit" theme="alt1" size="$3">
-                    {format(parseISO(frontmatter.publishedAt), 'MMMM yyyy')} by &nbsp;
+                    {Intl.DateTimeFormat('en-US', {
+                      weekday: 'short',
+                      year: 'numeric',
+                      day: 'numeric',
+                    }).format(new Date(frontmatter.publishedAt || ''))}{' '}
+                    by &nbsp;
                     {authors[frontmatter.by].name}
                   </Paragraph>
                 }
