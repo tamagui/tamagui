4b735ad188399c935dd7f5047c76f2a3c57b2591
natew <natewienert@gmail.com>
2023-04-20 14:24:59 -1000

    rename git-crypt binary to avoid overlapping system one

--- packages/git-crypt/create-binary.sh
+++ packages/git-crypt/create-binary.sh
@@ -3,4 +3,4 @@
 docker build . -t git-crypt-image
 docker run --name git-crypt-container git-crypt-image .
 sleep 5
-docker cp git-crypt-container:/usr/local/bin/git-crypt ./git-crypt
+docker cp git-crypt-container:/usr/local/bin/git-crypt ./git-crypt-bin
