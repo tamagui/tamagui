00d5e9e1e88745c9dd75b26513c8f01d73ef3b16
Fernando Rojo <frojo@sas.upenn.edu>
2023-04-21 16:15:03 -0400

    fix: RN 0.71.6

--- apps/kitchen-sink/metro.config.js
+++ apps/kitchen-sink/metro.config.js
@@ -19,6 +19,20 @@ config.resolver.extraNodeModules = {
   modules: workspaceRoot,
 }
 
+// https://github.com/facebook/react-native/issues/36794#issuecomment-1500880284
+// temp workaround for a ridiculous RN 0.71.6 bug
+config.server = {
+  ...config.server,
+  rewriteRequestUrl: (url) => {
+    if (!url.endsWith('.bundle')) {
+      return url
+    }
+    // https://github.com/facebook/react-native/issues/36794
+    // JavaScriptCore strips query strings, so try to re-add them with a best guess.
+    return url + '?platform=ios&dev=true&minify=false&modulesOnly=false&runModule=true'
+  }, // ...
+}
+
 console.log('config', config)
 
 module.exports = config
