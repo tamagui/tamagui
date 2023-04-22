785fb27255a3f7299d98bf0f2a3d658a2318b20f
natew <natewienert@gmail.com>
2023-04-21 22:26:17 -1000

    fix git-crypt to avoid confusing yarn bin

--- scripts/git-crypt-unlock.sh
+++ scripts/git-crypt-unlock.sh
@@ -5,6 +5,6 @@ if [[ $(file -b --mime-type apps/site/app/layout.tsx) == "text/html" ]]; then
 else
   echo "Unlocking..."
   echo "$GIT_CRYPT_KEY" | base64  -d > ./git-crypt-key
-  ./node_modules/.bin/git-crypt unlock ./git-crypt-key
+  ./node_modules/.bin/git-crypt-bin unlock ./git-crypt-key
   rm ./git-crypt-key
 fi
