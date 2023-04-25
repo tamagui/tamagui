5434a73276d52dcfe84ad72f907a9bca40b2b821
natew <natewienert@gmail.com>
2023-04-25 13:08:33 -1000

    v1.16.1

---
+++ packages/web/types/views/ThemeDebug.native.d.ts
@@ -0,0 +1,8 @@
+import { ChangedThemeResponse } from '../hooks/useTheme';
+import { ThemeProps } from '../types';
+export declare function ThemeDebug({ themeState, themeProps, children, }: {
+    themeState: ChangedThemeResponse;
+    themeProps: ThemeProps;
+    children: any;
+}): any;
+//# sourceMappingURL=ThemeDebug.native.d.ts.map
