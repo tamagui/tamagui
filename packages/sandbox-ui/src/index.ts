b75bb5747de683b9e06cfde5061d0d2be7637c59
ethan <egreenfeld@azswa.org>
2023-04-25 10:13:22 -0700

    error reproduction

--- packages/sandbox-ui/src/index.ts
+++ packages/sandbox-ui/src/index.ts
@@ -1,6 +1,8 @@
 // @ts-ignore
-export * from '@tamagui/lucide-icons'
+export * as lucideIcons from '@tamagui/lucide-icons'
 // @ts-ignore
+export { config } from './tamagui.config'
+
 export * from 'tamagui'
 // @ts-ignore
 export * from '@tamagui/toast'
