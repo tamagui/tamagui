cca78e08f83ead97f8836601247c779ac9381d8b
natew <natewienert@gmail.com>
2023-04-26 21:20:21 -1000

    remove encrypted

--- apps/site/pages/studio-app/load.tsx
+++
@@ -1,8 +0,0 @@
-import { getStudioLayout } from '@components/layouts/StudioLayout'
-import LoadPage from '@protected/studio/load/page'
-
-export default function Page() {
-  return <LoadPage />
-}
-
-Page.getLayout = getStudioLayout
