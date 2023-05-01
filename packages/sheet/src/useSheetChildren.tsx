57b1d2fd9ba4806fe716785235eeb47196d8c555
Alireza Zamani <alirezazamani2922@gmail.com>
2023-05-01 16:32:37 +0330

    fix(sheet): behind being visible when dragged up

--- packages/sheet/src/useSheetChildren.tsx
+++ packages/sheet/src/useSheetChildren.tsx
@@ -29,5 +29,15 @@ export function useSheetChildren(childrenProp: any) {
     handleComponent,
     overlayComponent,
     frameComponent,
+    bottomCoverComponent: frameComponent
+      ? React.cloneElement(frameComponent, {
+          children: null,
+          position: 'absolute',
+          bottom: -100,
+          height: 110,
+          left: 0,
+          right: 0,
+        })
+      : null,
   }
 }
--- packages/sheet/types/useSheetChildren.d.ts
+++ packages/sheet/types/useSheetChildren.d.ts
@@ -1,6 +1,15 @@
+import React from 'react';
 export declare function useSheetChildren(childrenProp: any): {
     handleComponent: null;
     overlayComponent: null;
     frameComponent: null;
+    bottomCoverComponent: React.DetailedReactHTMLElement<{
+        children: null;
+        position: string;
+        bottom: number;
+        height: number;
+        left: number;
+        right: number;
+    }, HTMLElement> | null;
 };
 //# sourceMappingURL=useSheetChildren.d.ts.map
