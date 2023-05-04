7c716629a46690dd928d8f3f93b841401b93e8cb
Ehsan Sarshar <ehsan.herai@gmail.com>
2023-05-04 11:56:54 +0430

    fix(cubic-bezier-animator): fix x,y being undefined bug

--- packages/cubic-bezier-animator/src/index.tsx
+++ packages/cubic-bezier-animator/src/index.tsx
@@ -25,10 +25,12 @@ export function animate(param: AnimateProps) {
   function frame(currentTime) {
     const elapsed = currentTime - start
     const progress = elapsed / param.duration
-    const x = toX ? easing(progress) * fromX! + toX : undefined // apply the easing function to the progress
-    const y = toY ? easing(progress) * fromY! + toY : undefined
-    const scaleX = toScaleX ? easing(progress) * fromScaleX! + toScaleX : undefined
-    const scaleY = toScaleY ? easing(progress) * fromScaleY! + toScaleY : undefined
+    const x = toX !== undefined ? easing(progress) * fromX! + toX : undefined // apply the easing function to the progress
+    const y = toY !== undefined ? easing(progress) * fromY! + toY : undefined
+    const scaleX =
+      toScaleX !== undefined ? easing(progress) * fromScaleX! + toScaleX : undefined
+    const scaleY =
+      toScaleY !== undefined ? easing(progress) * fromScaleY! + toScaleY : undefined
     param.onUpdate({ x, y, scaleX, scaleY })
 
     if (progress < 1) {
