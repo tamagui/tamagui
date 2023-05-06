f972c989331982c3c376e64ebfa510a3e172dbd8
Alireza Zamani <alirezazamani2922@gmail.com>
2023-05-06 18:16:23 +0330

    site: fix getURL returning localhost on prod

--- apps/site/lib/helpers.ts
+++ apps/site/lib/helpers.ts
@@ -4,8 +4,8 @@ export const getURL = () => {
   const url =
     process?.env?.URL && process.env.URL !== ''
       ? process.env.URL
-      : process?.env?.VERCEL_URL && process.env.VERCEL_URL !== ''
-      ? process.env.VERCEL_URL
+      : process?.env?.NEXT_PUBLIC_VERCEL_URL && process.env.NEXT_PUBLIC_VERCEL_URL !== ''
+      ? process.env.NEXT_PUBLIC_VERCEL_URL
       : 'http://localhost:5005'
   return url.includes('http') ? url : `https://${url}`
 }
