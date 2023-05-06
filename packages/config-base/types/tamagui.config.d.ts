0e0426beaf049498a32593537c04e7fd3f4e529a
natew <natewienert@gmail.com>
2023-05-06 07:25:37 -1000

    v1.23.1

--- packages/config-base/types/tamagui.config.d.ts
+++ packages/config-base/types/tamagui.config.d.ts
@@ -456,7 +456,29 @@ export declare const config: {
     fonts: {
         heading: import("@tamagui/web").GenericFont<9 | 15 | 10 | 11 | 12 | 14 | 16 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 13 | "true">;
         body: import("@tamagui/web").GenericFont<9 | 15 | 10 | 11 | 12 | 14 | 16 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 13 | "true">;
-        mono: import("@tamagui/web").GenericFont<9 | 15 | 10 | 11 | 12 | 14 | 16 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 13>;
+        mono: {
+            weight: {
+                1: string;
+            };
+            size: {
+                1: number;
+                2: number;
+                3: number;
+                4: number;
+                5: number;
+                6: number;
+                7: number;
+                8: number;
+                9: number;
+                10: number;
+                11: number;
+                12: number;
+                13: number;
+                14: number;
+                15: number;
+                16: number;
+            };
+        };
         silkscreen: import("@tamagui/web").GenericFont<9 | 15 | 10 | 11 | 12 | 14 | 16 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 13>;
     };
 };
