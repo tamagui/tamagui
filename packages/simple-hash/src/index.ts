d4a38d6c1a9832a6388c7a02258f2f6764ad2cec
natew <natewienert@gmail.com>
2023-05-07 13:32:13 -1000

    demo only adding outlined theme

--- packages/simple-hash/src/index.ts
+++ packages/simple-hash/src/index.ts
@@ -10,18 +10,15 @@ export const simpleHash = (str: string, hashMin = 10) => {
   for (let i = 0; i < len; i++) {
     const char = str.charCodeAt(i)
     // . => d0t
-    if (char === 46) {
-      valids += 'd0t'
-    }
+    if (char === 46) valids += 'd0t'
     // dont do more than 10 non-hashed to avoid getting too girthy
     if (isValidCSSCharCode(char) && len <= hashMin) {
       valids += str[i]
     } else {
-      hash = (hash << 5) - hash + char
-      hash &= hash // Convert to 32bit integer
+      hash = hashChar(hash, str[i])
     }
   }
-  const res = valids + (hash ? new Uint32Array([hash])[0].toString(36) : '')
+  const res = valids + (hash ? Math.abs(hash) : '')
   if (cache.size > 10_000) {
     cache.clear()
   }
@@ -29,7 +26,9 @@ export const simpleHash = (str: string, hashMin = 10) => {
   return res
 }
 
-export function isValidCSSCharCode(code: number) {
+const hashChar = (hash: number, c: string) => (Math.imul(31, hash) + c.charCodeAt(0)) | 0
+
+function isValidCSSCharCode(code: number) {
   return (
     // A-Z
     (code >= 65 && code <= 90) ||
--- packages/simple-hash/types/index.d.ts
+++ packages/simple-hash/types/index.d.ts
@@ -1,3 +1,2 @@
 export declare const simpleHash: (str: string, hashMin?: number) => any;
-export declare function isValidCSSCharCode(code: number): boolean;
 //# sourceMappingURL=index.d.ts.map
