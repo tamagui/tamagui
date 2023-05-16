16abacee54da4db533f7acaf0db4c411e02df99b
natew <natewienert@gmail.com>
2023-05-16 07:57:32 -1000

    fix tests for new wrapChildrenInText

--- packages/text-test/wrapChildrenInText.test.tsx
+++ packages/text-test/wrapChildrenInText.test.tsx
@@ -7,7 +7,7 @@ const Comp = () => {}
 const re = <div data-id={Math.random()} />
 const reID = re.props['data-id']
 
-test('should wrap all string children, not wrap others', () => {
+test('should wrap all string children', () => {
   const out = wrapChildrenInText(Comp, {
     children: [re, 'a', 'b', re, 'c', re, 'd'],
   }) as any
@@ -15,17 +15,12 @@ test('should wrap all string children, not wrap others', () => {
   expect(out[0].props['data-id']).toBe(reID)
 
   expect(out[1].type).toBe(Comp)
-  expect(out[1].props.children).toStrictEqual(['a', 'b'])
-
-  expect(out[2].props['data-id']).toBe(reID)
-
-  expect(out[3].type).toBe(Comp)
-  expect(out[3].props.children).toStrictEqual(['c'])
-
-  expect(out[4].props['data-id']).toBe(reID)
-
-  expect(out[5].type).toBe(Comp)
-  expect(out[5].props.children).toStrictEqual(['d'])
+  expect(out[1].props.children).toStrictEqual('a')
+  expect(out[2].props.children).toStrictEqual('b')
+  expect(out[3].props['data-id']).toBe(reID)
+  expect(out[4].props.children).toStrictEqual('c')
+  expect(out[5].props['data-id']).toBe(reID)
+  expect(out[6].props.children).toStrictEqual('d')
 })
 
 test('should first child string', () => {
@@ -34,7 +29,7 @@ test('should first child string', () => {
   }) as any
 
   expect(out[0].type).toBe(Comp)
-  expect(out[0].props.children).toStrictEqual(['a'])
+  expect(out[0].props.children).toStrictEqual('a')
 
   expect(out[1].props['data-id']).toBe(reID)
 })
