484b08a749e900a4a491be00c3c6d07821e654d3
Alireza Zamani <alirezazamani2922@gmail.com>
2023-05-22 20:31:13 +0330

    fix(web): default to `body` if no `defaultFont` is present

--- packages/web/src/createTamagui.ts
+++ packages/web/src/createTamagui.ts
@@ -241,8 +241,14 @@ ${runtimeStyles}`
 
   const getNewCSS: GetCSS = (opts) => getCSS({ ...opts, sinceLastCall: true })
 
-  // defaults to the first font to make life easier
-  const defaultFont = configIn.defaultFont || Object.keys(configIn.fonts)[0]
+  const fontKeys = Object.keys(configIn.fonts)
+
+  const defaultFont =
+    configIn.defaultFont ||
+    // uses font named "body" if present for compat
+    ('body' in configIn.fonts ? 'body' : false) ||
+    // defaults to the first font to make life easier
+    fontKeys[0]
 
   const config: TamaguiInternalConfig = {
     onlyAllowShorthands: false,
