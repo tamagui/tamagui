20901b3dd79d2f6a8d2b1b291f89e97a0c1d2e9f
Alireza Zamani <alirezazamani2922@gmail.com>
2023-05-24 16:26:40 +0330

    Revert "perf(popover): improve performance on Safari by avoiding rendering portal children unless visible"
    This reverts commit d987034668f21f71bf450f31afdc34cd55e109e8.

--- packages/popover/src/Popover.tsx
+++ packages/popover/src/Popover.tsx
@@ -260,15 +260,15 @@ function PopoverContentPortal(props: ScopedProps<PopoverContentTypeProps>) {
   // Portal the contents and add a transparent bg overlay to handle dismiss on native
   return (
     <Portal zIndex={zIndex}>
-      {!!context.open && !context.breakpointActive && (
-        <Theme forceClassName name={themeName}>
+      <Theme forceClassName name={themeName}>
+        {!!context.open && !context.breakpointActive && (
           <YStack
             fullscreen
             onPress={composeEventHandlers(props.onPress as any, context.onOpenToggle)}
           />
-          <Stack zIndex={(zIndex as number) + 1}>{contents}</Stack>
-        </Theme>
-      )}
+        )}
+        <Stack zIndex={(zIndex as number) + 1}>{contents}</Stack>
+      </Theme>
     </Portal>
   )
 }
