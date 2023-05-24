7a8c1ee710ea5e21a99b8012ec27372f43f684a5
natew <natewienert@gmail.com>
2023-05-23 21:04:25 -1000

    fix site remove contain breaking sheet

--- apps/site/components/Header.tsx
+++ apps/site/components/Header.tsx
@@ -332,7 +332,6 @@ const SmallMenu = React.memo(() => {
             miw={230}
             p="$3"
             ai="flex-end"
-            contain="paint layout"
             // display={open ? 'flex' : 'none'}
           >
             <HeaderLinks forceShowAllLinks />
