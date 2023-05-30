21f98097b7b5da1911f67c0d727d0039dccfb032
natew <natewienert@gmail.com>
2023-05-29 15:45:36 -1000

    internal: add useFocusable hook for easier usage of focusables

--- packages/focusable/src/focusableInputHOC.tsx
+++ packages/focusable/src/focusableInputHOC.tsx
@@ -1,70 +1,84 @@
 import { composeRefs } from '@tamagui/compose-refs'
-import { TamaguiComponent, isTamaguiComponent, useEvent } from '@tamagui/web'
-import { useCallback, useEffect, useRef } from 'react'
+import { TamaguiComponent, useEvent } from '@tamagui/web'
+import React, { useCallback, useEffect, useRef } from 'react'
 
 import { registerFocusable } from './registerFocusable'
 
-export function focusableInputHOC<A extends TamaguiComponent>(Component: A): A {
-  const component = Component.styleable(
-    (
-      props: {
-        id?: string
-        onChangeText?: (val: string) => void
-        value?: string
-        defaultValue?: string
-      },
-      ref
-    ) => {
-      const isInput = isTamaguiComponent(Component) && Component.staticConfig.isInput
-      const inputValue = useRef(props.value || props.defaultValue || '')
-      const unregisterFocusable = useRef<() => void | undefined>()
+type FocusableProps = {
+  id?: string
+  onChangeText?: (val: string) => void
+  value?: string
+  defaultValue?: string
+}
 
-      const inputRef = useCallback(
-        (input) => {
-          if (!props.id) return
-          if (!input) return
-          unregisterFocusable.current?.()
-          unregisterFocusable.current = registerFocusable(props.id, {
-            focus: input.focus,
+export function useFocusable({
+  isInput,
+  props,
+  ref,
+}: {
+  isInput?: boolean
+  props: FocusableProps
+  ref?: React.MutableRefObject<any>
+}) {
+  const { id, onChangeText, value, defaultValue } = props
+  const inputValue = useRef(value || defaultValue || '')
+  const unregisterFocusable = useRef<() => void | undefined>()
 
-            ...(isInput && {
-              // react-native doesn't support programmatic .select()
-              focusAndSelect() {
-                input.focus()
-                if (input.setSelection && typeof inputValue.current === 'string') {
-                  input.setSelection(0, inputValue.current.length)
-                }
-              },
-            }),
-          })
-        },
-        [isInput, props.id]
-      )
+  const inputRef = useCallback(
+    (input) => {
+      if (!id) return
+      if (!input) return
+      unregisterFocusable.current?.()
+      unregisterFocusable.current = registerFocusable(id, {
+        focus: input.focus,
 
-      const combinedRefs = composeRefs(ref, inputRef)
-
-      useEffect(() => {
-        return () => {
-          unregisterFocusable.current?.()
-        }
-      }, [])
-
-      const onChangeText = useEvent((value) => {
-        inputValue.current = value
-        props.onChangeText?.(value)
+        ...(isInput && {
+          // react-native doesn't support programmatic .select()
+          focusAndSelect() {
+            input.focus()
+            if (input.setSelection && typeof inputValue.current === 'string') {
+              input.setSelection(0, inputValue.current.length)
+            }
+          },
+        }),
       })
+    },
+    [isInput, id]
+  )
 
-      const finalProps = isInput
-        ? {
-            ...props,
-            onChangeText,
-          }
-        : props
+  const combinedRefs = composeRefs(ref, inputRef)
 
-      // @ts-expect-error
-      return <Component ref={combinedRefs} {...finalProps} />
+  useEffect(() => {
+    return () => {
+      unregisterFocusable.current?.()
     }
-  ) as any
+  }, [])
 
-  return component
+  return {
+    ref: combinedRefs,
+    onChangeText: useEvent((value) => {
+      inputValue.current = value
+      onChangeText?.(value)
+    }),
+  }
+}
+
+export function focusableInputHOC<A extends TamaguiComponent>(Component: A): A {
+  return Component.styleable((props: FocusableProps, ref) => {
+    const isInput = Component.staticConfig?.isInput
+    const { ref: combinedRef, onChangeText } = useFocusable({
+      ref,
+      props,
+      isInput,
+    })
+    const finalProps = isInput
+      ? {
+          ...props,
+          onChangeText,
+        }
+      : props
+
+    // @ts-expect-error
+    return <Component ref={combinedRef} {...finalProps} />
+  }) as any
 }
--- packages/focusable/types/focusableInputHOC.d.ts
+++ packages/focusable/types/focusableInputHOC.d.ts
@@ -1,3 +1,19 @@
 import { TamaguiComponent } from '@tamagui/web';
+import React from 'react';
+type FocusableProps = {
+    id?: string;
+    onChangeText?: (val: string) => void;
+    value?: string;
+    defaultValue?: string;
+};
+export declare function useFocusable({ isInput, props, ref, }: {
+    isInput?: boolean;
+    props: FocusableProps;
+    ref?: React.MutableRefObject<any>;
+}): {
+    ref: (node: any) => void;
+    onChangeText: (value: any) => void;
+};
 export declare function focusableInputHOC<A extends TamaguiComponent>(Component: A): A;
+export {};
 //# sourceMappingURL=focusableInputHOC.d.ts.map
