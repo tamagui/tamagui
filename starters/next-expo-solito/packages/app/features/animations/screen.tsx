04832b02eff07fd4fd3362bf4421941874310314
Benjamin <benschac@gmail.com>
2023-06-13 09:54:29 -0400

    remove animations pages, move examples to index

--- starters/next-expo-solito/packages/app/features/animations/screen.tsx
+++ starters/next-expo-solito/packages/app/features/animations/screen.tsx
@@ -1,11 +1,5 @@
 import { Paragraph, Sheet, YStack, useToastController } from '@my/ui'
 import { Button as RNButton, View } from 'react-native'
-import Animated, {
-  Easing,
-  useAnimatedStyle,
-  useSharedValue,
-  withTiming,
-} from 'react-native-reanimated'
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
 import { useLink } from 'solito/link'
@@ -103,79 +97,3 @@ export const positions = [
     },
   },
 ]
-
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10)
-
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  }
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    }
-  })
-
-  return (
-    <View
-      style={{
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <Animated.View
-        style={[{ width: 100, height: 80, backgroundColor: 'black', margin: 30 }, style]}
-      />
-      <RNButton
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350
-        }}
-      />
-    </View>
-  )
-}
-function SheetDemo() {
-  const [open, setOpen] = useState(false)
-  const [position, setPosition] = useState(0)
-  const toast = useToastController()
-
-  return (
-    <>
-      <Button
-        size="$6"
-        icon={open ? ChevronDown : ChevronUp}
-        circular
-        onPress={() => setOpen((x) => !x)}
-      />
-      <Sheet
-        modal
-        open={open}
-        onOpenChange={setOpen}
-        snapPoints={[80]}
-        position={position}
-        onPositionChange={setPosition}
-        dismissOnSnapToBottom
-      >
-        <Sheet.Overlay />
-        <Sheet.Frame ai="center" jc="center">
-          <Sheet.Handle />
-          <Button
-            size="$6"
-            circular
-            icon={ChevronDown}
-            onPress={() => {
-              setOpen(false)
-              toast.show('Sheet closed!', {
-                message: 'Just showing how toast works...',
-              })
-            }}
-          />
-        </Sheet.Frame>
-      </Sheet>
-    </>
-  )
-}
--- starters/next-expo-solito/packages/app/features/home/screen.tsx
+++ starters/next-expo-solito/packages/app/features/home/screen.tsx
@@ -8,18 +8,26 @@ import {
   useToastController,
   XStack,
   YStack,
+  H3,
 } from '@my/ui'
+
+import { Square, useControllableState, useEvent } from 'tamagui'
+import { Button as RNButton, View } from 'react-native'
 import { ChevronDown, ChevronUp } from '@tamagui/lucide-icons'
 import React, { useState } from 'react'
 import { useLink } from 'solito/link'
 
+import { Play } from '@tamagui/lucide-icons'
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated'
+
 export function HomeScreen() {
   const linkProps = useLink({
     href: '/user/nate',
-  })
-
-  const animationsLink = useLink({
-    href: '/animations',
   })
 
   return (
@@ -48,14 +56,15 @@ export function HomeScreen() {
           </Anchor>
         </Paragraph>
       </YStack>
+      <H3>Reanimated 2 Animation</H3>
+      <ReanimatedAnimationDemo />
+      <H3>Tamagui Animation</H3>
+      <TamaguiAnimationDemo />
 
       <XStack>
         <Button {...linkProps}>Link to user</Button>
       </XStack>
 
-      <XStack>
-        <Button {...animationsLink}>Animations</Button>
-      </XStack>
       <SheetDemo />
     </YStack>
   )
@@ -102,3 +111,116 @@ function SheetDemo() {
     </>
   )
 }
+
+function ReanimatedAnimationDemo() {
+  const randomWidth = useSharedValue(10)
+
+  const config = {
+    duration: 500,
+    easing: Easing.bezier(0.5, 0.01, 0, 1),
+  }
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: withTiming(randomWidth.value, config),
+    }
+  })
+
+  return (
+    <View
+      style={{
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <Animated.View
+        style={[{ width: 100, height: 80, backgroundColor: 'black', margin: 30 }, style]}
+      />
+      <RNButton
+        title="toggle"
+        onPress={() => {
+          randomWidth.value = Math.random() * 350
+        }}
+      />
+    </View>
+  )
+}
+
+export function TamaguiAnimationDemo(props) {
+  const [positionI, setPositionI] = useControllableState({
+    strategy: 'most-recent-wins',
+    prop: props.position,
+    defaultProp: 0,
+  })
+  const position = positions[positionI]
+  const onPress = useEvent(() => {
+    setPositionI((x) => {
+      return (x + 1) % positions.length
+    })
+  })
+
+  return (
+    <>
+      <Square
+        animation={props.animation || 'bouncy'}
+        onPress={onPress}
+        size={104}
+        borderColor="$borderColor"
+        borderWidth={1}
+        borderRadius="$9"
+        backgroundColor="$color9"
+        hoverStyle={{
+          scale: 1.1,
+        }}
+        pressStyle={{
+          scale: 0.9,
+        }}
+        {...position}
+      ></Square>
+      <Button
+        position="absolute"
+        bottom={20}
+        left={20}
+        icon={Play}
+        theme={props.tint}
+        size="$5"
+        circular
+        onPress={onPress}
+      />
+    </>
+  )
+}
+
+const positions = [
+  {
+    x: 0,
+    y: 0,
+    scale: 1,
+    rotate: '0deg',
+  },
+  {
+    x: -50,
+    y: -50,
+    scale: 0.5,
+    rotate: '-45deg',
+    hoverStyle: {
+      scale: 0.6,
+    },
+    pressStyle: {
+      scale: 0.4,
+    },
+  },
+  {
+    x: 50,
+    y: 50,
+    scale: 1,
+    rotate: '180deg',
+    hoverStyle: {
+      scale: 1.1,
+    },
+    pressStyle: {
+      scale: 0.9,
+    },
+  },
+]
