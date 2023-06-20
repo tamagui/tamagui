7a26e4cb6b0c6f1371c5689b95d2b9a41204fd1f
natew <natewienert@gmail.com>
2023-06-20 12:59:45 -0400

    v1.35.2

--- packages/helpers-icon/types/themed.d.ts
+++ packages/helpers-icon/types/themed.d.ts
@@ -1,4 +1,8 @@
 import React from 'react';
 import { IconProps } from './IconProps';
-export declare function themed(Component: React.FC<IconProps>): (propsIn: IconProps) => JSX.Element;
+export declare function themed(Component: React.FC<IconProps>, opts?: {
+    defaultThemeColor: string;
+    defaultStrokeWidth: number;
+    fallbackColor: string;
+}): (propsIn: IconProps) => JSX.Element;
 //# sourceMappingURL=themed.d.ts.map
