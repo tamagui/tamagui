e92b0202243dd86a1bbeccc125d7b5c355ac1b3c
Alireza Zamani <alirezazamani2922@gmail.com>
2023-06-26 11:43:33 +0330

    feat(create-tamagui): `--skip-cloning` option

--- packages/create-tamagui/src/steps/simple-web.ts
+++ packages/create-tamagui/src/steps/simple-web.ts
@@ -8,15 +8,17 @@ const useYarn = packageManager === 'yarn'
 const runCommand = (scriptName: string) =>
   `${packageManager} ${useYarn ? '' : 'run '}${scriptName}`
 
-const main: ExtraSteps = async ({ projectName }) => {
+const main: ExtraSteps = async ({ isFullClone, projectName }) => {
   // rome-ignore lint/nursery/noConsoleLog: <explanation>
-  console.log(`
-${chalk.green.bold('Done!')} created a new project under ./${projectName}
+  if (isFullClone) {
+    console.log(`
+  ${chalk.green.bold('Done!')} created a new project under ./${projectName}
 
 cd into the project using:
   ${chalk.green('cd')} ${projectName}
-
-Inside that directory:
+`)
+  }
+  console.log(`
   To start the Next.js development server, run:
     ${chalk.green(runCommand('web'))}
 `)
