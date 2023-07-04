bd0233ce45b61c244a2bd0c2738df11de40608a9
natew <natewienert@gmail.com>
2023-07-03 21:45:14 -1000

    revert next update due to useSyncExternalStore bug

--- apps/site/lib/docsRoutes.ts
+++ apps/site/lib/docsRoutes.ts
@@ -159,7 +159,7 @@ export const docsRoutes = [
   {
     pages: [
       { title: 'Design Systems', route: '/docs/guides/design-systems' },
-      { title: 'Creating Themes', route: '/docs/guides/theme-builder' },
+      { title: 'Creating Custom Themes', route: '/docs/guides/theme-builder' },
       { title: 'How to Build a Button', route: '/docs/guides/how-to-build-a-button' },
       { title: 'Developing', route: '/docs/guides/developing' },
       { title: 'Next.js', route: '/docs/guides/next-js' },
