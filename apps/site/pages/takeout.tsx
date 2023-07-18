54eafd3d447db757c185846a429aa5560cdb4361
Alireza Zamani <alirezazamani2922@gmail.com>
2023-07-18 18:44:14 +0330

    site: add youtube embed to takeout

--- apps/site/pages/takeout.tsx
+++ apps/site/pages/takeout.tsx
@@ -738,7 +738,8 @@ export default function TakeoutPage({
                   </MunroP>
                 </ThemeTint>
 
-                <HeartsRow />
+                {/* <HeartsRow /> */}
+                <PromoVideo />
 
                 <Paragraph size="$10" $sm={{ size: '$9' }} $xs={{ size: '$8' }} fow="400">
                   Takeout 🥡 is a bootstrap that delivers on years of effort putting
@@ -2992,5 +2993,19 @@ const PromotionInput = () => {
         </Paragraph>
       )}
     </AnimatePresence>
+  )
+}
+
+const PromoVideo = () => {
+  return (
+    <iframe
+      width="100%"
+      height="400"
+      src="https://www.youtube-nocookie.com/embed/fg4zJncNMr0"
+      title="YouTube video player"
+      frameBorder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowFullScreen
+    ></iframe>
   )
 }
