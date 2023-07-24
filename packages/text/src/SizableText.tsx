17cf127ea1495c46c0a0c4a1bb2ee31c31a448d8
natew <natewienert@gmail.com>
2023-07-24 09:53:12 -1000

    fix(web): improve correctness of variants + fontFamily + tokens + parents changing fontFamily but still passing to children

--- packages/text/src/SizableText.tsx
+++ packages/text/src/SizableText.tsx
@@ -13,6 +13,15 @@ export const SizableText = styled(Text, {
     },
 
     size: getFontSized,
+
+    // this is a bit odd but we need it because otherwise if a parent sets
+    // a new fontFamily it wont go through size variant unless size also set
+    fontFamily: {
+      '...': (_, extras) => {
+        const size = extras.props['size'] || '$true'
+        return getFontSized(size, extras)
+      },
+    },
   } as const,
 
   defaultVariants: {
