66d4c305d2359604ffe7f94da0bacd7b4cfe8003
natew <natewienert@gmail.com>
2023-07-28 20:49:03 -1000

    add starter note on renaming

--- packages/create-tamagui/src/steps/next-expo-solito.ts
+++ packages/create-tamagui/src/steps/next-expo-solito.ts
@@ -25,11 +25,14 @@ cd into the project using:
     ${chalk.green(runCommand('native'))}
 
   You can also create Expo development builds by doing:
+  
     ${chalk.green(`cd apps/expo`)} 
     then:
     ${chalk.green(runCommand('ios'))} 
     or...
     ${chalk.green(runCommand('android'))}
+
+  Be sure to replace [yourprojectsname] with the uid you'd like for your app.
 `)
 }
 
