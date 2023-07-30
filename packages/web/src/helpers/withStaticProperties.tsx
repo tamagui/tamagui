3abc3b45dc1cb3efe1ea8b9aa2d279eded4a80d3
natew <natewienert@gmail.com>
2023-07-29 14:39:18 -1000

    fix(web): simplify withStaticProperties logic to avoid cloning when it doesnt need to

--- packages/web/src/helpers/withStaticProperties.tsx
+++ packages/web/src/helpers/withStaticProperties.tsx
@@ -9,19 +9,18 @@ export const withStaticProperties = function <A extends Function, B>(
   // clone component if already wrapped once
   const next = (() => {
     if (component[Decorated]) {
-      return forwardRef((props, ref) =>
+      const _ = forwardRef((props, ref) =>
         createElement(component as any, { ...props, ref })
       )
+      // attach existing things again
+      for (const key in component) {
+        const v = component[key]
+        // @ts-ignore
+        _[key] = v && typeof v === 'object' ? { ...v } : v
+      }
     }
     return component
   })()
-
-  // attach existing things again
-  for (const key in component) {
-    const v = component[key]
-    // @ts-ignore
-    next[key] = v && typeof v === 'object' ? { ...v } : v
-  }
 
   // add new things
   Object.assign(next, staticProps)
