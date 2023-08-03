f1411d1f831130c23dd0b68f603f44f47586e785
natew <natewienert@gmail.com>
2023-08-02 19:48:37 -1000

    v1.49.0

--- apps/kitchen-sink/tests/Shadows.test.tsx
+++ apps/kitchen-sink/tests/Shadows.test.tsx
@@ -10,5 +10,5 @@ test.beforeEach(async ({ page }) => {
 test(`shadows work`, async ({ page }) => {
   const button = page.locator('#shadowed')
   const styles = await getStyles(button)
-  expect(styles.boxShadow).toBe(`rgba(0, 0, 0, 0.085) 0px 0px 60px 0px`)
+  expect(styles.boxShadow).toBe(`rgba(0, 0, 0, 0.086) 0px 0px 60px 0px`)
 })
