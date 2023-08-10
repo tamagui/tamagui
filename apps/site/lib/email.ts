f8dcb80021bd60e4e31a147d1d56b65927fa1dd0
Alireza Zamani <alirezazamani2922@gmail.com>
2023-08-10 12:08:33 +0330

    site: takeout welcome email

--- apps/site/lib/email.ts
+++ apps/site/lib/email.ts
@@ -8,20 +8,12 @@ if (!serverToken) {
 
 const client = new postmark.ServerClient(serverToken)
 
-export function sendTakeoutWelcomeEmail(email: string, { subId }: { subId: string }) {
-  return client.sendEmail({
+export function sendTakeoutWelcomeEmail(email: string, { name }: { name: string }) {
+  return client.sendEmailWithTemplate({
     From: 'support@tamagui.dev',
     To: email,
-    Subject: 'Takeout Purchase 🥡',
-    TextBody: `
-Thank you for purchasing from Takeout!
-
-You can get access to Takeout and private Discord channels from here: https://tamagui.dev/account/subscriptions
-Your subscription ID: ${subId}
-
-We hope you enjoy.
-
-The Tamagui Team
-`,
+    TemplateId: 32624033,
+    // TemplateAlias: 'takeout-welcome',
+    TemplateModel: { name, product_name: 'Takeout' },
   })
 }
