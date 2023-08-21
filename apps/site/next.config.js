f73829ce076f82aab0e554bebc4f3f7d22e64a88
Alireza Zamani <alirezazamani2922@gmail.com>
2023-08-21 18:40:02 +0330

    site: update cors rules

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -106,10 +106,10 @@ module.exports = function (name, { defaultConfig }) {
         {
           source: "/api/:path*",
           headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "tamagui.dev,www.tamagui.dev,studio.tamagui.dev" },
-            { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            // { key: "Access-Control-Allow-Credentials", value: "true" },
+            // { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+            // { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
         }
       ]
