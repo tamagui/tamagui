d1c5a5381fa65b93a7061cfbd60060374cfc4537
natew <natewienert@gmail.com>
2023-08-23 10:29:21 -1000

    v1.55.1

--- scripts/release.ts
+++ scripts/release.ts
@@ -373,14 +373,15 @@ async function run() {
     const gitTag = `${tagPrefix}${version}`
 
     if (!rePublish || reRun || finish) {
+      await spawnify(`git add -A`)
+      await spawnify(`git commit -m ${gitTag}`)
+      await spawnify(`git tag ${gitTag}`)
+
       if (!dirty) {
         // pull once more before pushing so if there was a push in interim we get it
         await spawnify(`git pull --rebase origin master`)
       }
 
-      await spawnify(`git add -A`)
-      await spawnify(`git commit -m ${gitTag}`)
-      await spawnify(`git tag ${gitTag}`)
       await spawnify(`git push origin head`)
       await spawnify(`git push origin ${gitTag}`)
       // rome-ignore lint/nursery/noConsoleLog: <explanation>
