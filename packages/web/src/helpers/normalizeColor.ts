76cc5bda01ab469965a098776e80ac51751a210b
natew <natewienert@gmail.com>
2023-08-23 16:03:50 -1000

    perf(web): use @react-native/normalize-color to align with rnw and avoid duplicate dep

--- packages/web/src/helpers/normalizeColor.ts
+++ packages/web/src/helpers/normalizeColor.ts
@@ -1,26 +1,26 @@
 import { isWeb } from '@tamagui/constants'
-import * as NCC from '@tamagui/normalize-css-color'
+import { normalizeCSSColor, rgba } from '@tamagui/normalize-css-color'
 
-export const rgba = NCC.rgba
-export const names = NCC.names
+export { rgba } from '@tamagui/normalize-css-color'
 
 export const normalizeColor = (color?: string | null, opacity?: number) => {
   if (!color) return
   if (color[0] === '$') return color
   if (isWeb && opacity === 1) return color
-  const colorProcessed = NCC.normalizeCSSColor(color)
-  if (typeof colorProcessed === 'number') {
+  const colorProcessed = normalizeCSSColor(color)
+  if (colorProcessed != null) {
     const { r, g, b, a } = rgba(colorProcessed)
-    const o = +(opacity ?? a ?? 1)
+    const o = opacity ?? a ?? 1
     const alpha = o.toFixed(2)
     return `rgba(${r},${g},${b},${alpha})`
   }
+  if (process.env.NODE_ENV === 'development') {
+    console.warn(`Unknown color value: ${color}`)
+  }
   if (process.env.TAMAGUI_TARGET === 'native') {
-    if (process.env.NODE_ENV === 'development') {
-      console.warn(`Unknown color value: ${color}`)
-    }
     // avoids errors
     return
   }
+  // on web pass it through could be a var or something
   return color
 }
