9e4d669b0733d6aedf0ee487f399baf9379c3447
natew <natewienert@gmail.com>
2023-08-26 11:27:19 -1000

    fix(normalize-color): make import RN compat

--- packages/normalize-css-color/src/index.ts
+++ packages/normalize-css-color/src/index.ts
@@ -1,6 +1,8 @@
-import normalizeColor from '@react-native/normalize-color'
+import * as normalizeColor from '@react-native/normalize-color'
 
-export const normalizeCSSColor = normalizeColor as (color: string) => number | null
+export const normalizeCSSColor = normalizeColor.default as (
+  color: string
+) => number | null
 
 export function rgba(colorInt: number) {
   var r = Math.round((colorInt & 0xff000000) >>> 24)
