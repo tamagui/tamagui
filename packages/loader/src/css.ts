78e177f89bb5890ac5b21a2eb08b7278adc1e4c3
natew <natewienert@gmail.com>
2023-08-27 13:15:44 -1000

    fix(loader): avoid using in-memory cache for style info and simplify code by simply always passing cssData as a buffer, fixing issues with running multiple webpack configurations in the same process

--- packages/loader/src/css.ts
+++ packages/loader/src/css.ts
@@ -1,32 +1,13 @@
-import { ExtractedResponse, TamaguiOptions } from '@tamagui/static'
+import { TamaguiOptions } from '@tamagui/static'
 import type { LoaderContext } from 'webpack'
-
-export const extractedInfoByFile = new Map<string, ExtractedResponse>()
-export const stylePathToFilePath = new Map<string, string>()
 
 export default function loader(this: LoaderContext<TamaguiOptions>) {
   this.async()
   const options = { ...this.getOptions() }
-  const sourcePath = `${this.resourcePath}`
-  let out = ''
-  if (options.cssPath) {
-    // get in memory info
-    const pathKey = stylePathToFilePath.get(sourcePath) ?? sourcePath
-    const info = extractedInfoByFile.get(pathKey)
-    // clear memory
-    stylePathToFilePath.delete(sourcePath)
-    extractedInfoByFile.delete(pathKey)
-    const out = info?.styles
-    return this.callback(null, out || '')
-  } else if (options.cssData) {
-    // get output CSS
-    out = Buffer.from(options.cssData, 'base64').toString('utf-8')
-  }
-
+  let out = Buffer.from(options.cssData, 'base64').toString('utf-8')
   if (out) {
     // use original JS sourcemap
     return this.callback(null, out || '')
   }
-
   this.callback({ message: 'No CSS found', name: 'missing_css' })
 }
