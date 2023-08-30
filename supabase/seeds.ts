0a0c0478740a7b53b1ff1b55744a616297b9cdde
benjamin <benschac@gmail.com>
2023-08-30 10:18:58 -0400

    add: profile climbs seed, climb read api, me router

--- supabase/seeds.ts
+++ supabase/seeds.ts
@@ -1,5 +1,4 @@
 import { supabase } from 'app/utils/supabase/client.native';
-// require('ts-node').register()
 
 import { faker } from '@faker-js/faker'
 import { add } from 'date-fns'
@@ -8,8 +7,11 @@ import { Database, } from './types'
 import { Enums, Tables } from './helpers';
 
 type Climbs = Tables<'climbs'>
+type ProfileClimbs = Tables<'profile_climbs'>
+type Profiles = Tables<'profiles'>
+
 const supabaseInstance = createClient<Database>("http://localhost:54321", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0")
-async function createClimbs(supabase: SupabaseClient<Database>, user: AuthResponse['data']['user']) {
+async function createClimbs(supabase: SupabaseClient<Database>, users: AuthResponse[], admin: AuthResponse['data']['user']) {
   // Create 10 climbs from faker data with the Shape of Tables['climbs'] and Enums['climb_type']
   // created at should be now to 1 week ago
   // created by should be the user id from the auth response
@@ -20,44 +22,62 @@ async function createClimbs(supabase: SupabaseClient<Database>, user: AuthRespon
   // name should be a random string of 10-25 characters with the user's first name in it and fake verb
   // the start time should be between 7am and 10pm, and the duration should be between 20 minutes and 5 hours, but the climb cannot end after 10pm
 
-  const climbs = Array.from({ length: 10 }).map((_, i) => {
+  if (!admin) {
+    throw new Error('no admin')
+  }
 
-    // past date between now and 1 week ago
+  const usersClimbs = users.map((user) => {
+    const climbs = Array.from({ length: 4 }).map((_, i) => {
+      const start = add(faker.date.between(
+        {
+          from: new Date(),
+          to: add(new Date(), { weeks: 3 }),
+        }), {
+        hours: faker.number.int({ min: 0, max: 4 }),
+        minutes: faker.number.int({ min: 0, max: 59 }),
+      })
 
 
-    const start = add(faker.date.between(
-      {
-        from: new Date(),
-        to: add(new Date(), { weeks: 3 }),
-      }), {
-      hours: faker.number.int({ min: 0, max: 4 }),
-      minutes: faker.number.int({ min: 0, max: 59 }),
+      return {
+        created_at: faker.date.between({
+          from: add(new Date(), { weeks: -1 }),
+          to: new Date(),
+        }).toISOString(),
+        created_by: user?.data.user?.id as string,
+        start: start.toISOString(),
+        duration: add(start, {
+          hours: faker.number.int({ min: 0, max: 4 }),
+          minutes: faker.number.int({ min: 0, max: 59 }),
+        }).toISOString(),
+        type: faker.helpers.arrayElement(['boulder', 'lead_rope', 'top_rope']) as Enums<'climb_type'>,
+        name: `${user?.data?.user?.user_metadata.first_name} ${faker.helpers.arrayElement(['climbs', 'sends', 'projects', 'attempts'])} ${faker.helpers.arrayElement(['a', 'the', 'my'])} ${faker.helpers.arrayElement(['red', 'blue', 'green', 'yellow', 'purple', 'black'])} ${faker.helpers.arrayElement(['route', 'problem', 'boulder'])}`,
+      }
     })
-    return {
+
+
+    // make sure admin has a climb from every user
+    climbs.push({
       created_at: faker.date.between({
         from: add(new Date(), { weeks: -1 }),
         to: new Date(),
       }).toISOString(),
-      created_by: user?.id as string,
-      start: start.toISOString(),
-      duration: add(start, {
-        hours: faker.number.int({ min: 0, max: 4 }),
-        minutes: faker.number.int({ min: 0, max: 59 }),
-      }).toISOString(),
+      created_by: admin?.id,
+      start: add(new Date(), { days: -1 }).toISOString(),
+      duration: add(new Date(), { days: -1, hours: 1 }).toISOString(),
       type: faker.helpers.arrayElement(['boulder', 'lead_rope', 'top_rope']) as Enums<'climb_type'>,
-      id: i + 1,
-      name: `${user?.user_metadata.first_name} ${faker.helpers.arrayElement(['climbs', 'sends', 'projects', 'attempts'])} ${faker.helpers.arrayElement(['a', 'the', 'my'])} ${faker.helpers.arrayElement(['red', 'blue', 'green', 'yellow', 'purple', 'black'])} ${faker.helpers.arrayElement(['route', 'problem', 'boulder'])}`,
-    }
-  })
+      name: `${user?.data.user?.user_metadata.first_name} ${faker.helpers.arrayElement(['climbs', 'sends', 'projects', 'attempts'])} ${faker.helpers.arrayElement(['a', 'the', 'my'])} ${faker.helpers.arrayElement(['red', 'blue', 'green', 'yellow', 'purple', 'black'])} ${faker.helpers.arrayElement(['route', 'problem', 'boulder'])}`,
+    })
 
-  const { data, error } = await supabase.from('climbs').insert(climbs)
+    return climbs
+  }).flatMap((climbs) => climbs)
+
+  const { error } = await supabase.from('climbs').insert(usersClimbs)
 
   if (error) {
     console.log(error)
     return
   }
 
-  return data
 
 }
 
@@ -106,18 +126,81 @@ async function createUsers(supabase: SupabaseClient<Database>, count: number) {
   return results
 }
 
+
+
+
+async function createProfileClimbs(supabase: SupabaseClient<Database>, climbs: Climbs[], profiles: Profiles[], admin: AuthResponse['data']['user']) {
+  // Create a single profile climb for every climb for every profile
+  // Every other profile should have 2 profile climbs for every climb
+  // The profile climbs should be created in the last 2 weeks and 2 weeks in the future
+  // The first profile climb should be created by the user who created the climb
+  // The second profile climb should be created by a random user
+  if (!admin) {
+    throw new Error('no admin')
+  }
+
+  const profileClimbs = climbs.map((climb) => {
+    const result: Omit<ProfileClimbs, 'id'>[] = []
+    const created_at = faker.date.between({
+      from: add(new Date(), { weeks: -2 }),
+      to: add(new Date(), { weeks: 2 }),
+    }).toISOString()
+
+    // first profile climb, the user who created the climb
+    result.push({
+      created_at,
+      profile_id: climb.created_by,
+      climb_id: climb.id,
+    })
+
+    // second profile climb, a random user
+    // every other climb should have 2 profile climbs
+
+    if (climb.id % 3 === 0) {
+      result.push({
+        created_at,
+        profile_id: admin.id,
+        climb_id: climb.id,
+      })
+    } else if (climb.id % 2 === 0) {
+      result.push({
+        created_at,
+        profile_id: faker.helpers.arrayElement(profiles).id,
+        climb_id: climb.id,
+      })
+    }
+
+    return result
+  }).flatMap((climbs) => climbs)
+
+
+  const { error } = await supabase.from('profile_climbs').insert(profileClimbs)
+
+  if (error) {
+    console.log(error)
+  }
+
+
+}
+
 async function main() {
   console.log('Running seeds with faker data')
   const benjamin = await createBenjamin(supabaseInstance)
   const users = await createUsers(supabaseInstance, 10)
 
 
+
   if (!benjamin) {
     console.log('no benjamin')
     return
   }
-  const benjaminsClimbs = await createClimbs(supabaseInstance, benjamin.user)
+  await createClimbs(supabaseInstance, users, benjamin.user)
+  const climbs = await supabaseInstance.from('climbs').select('*');
+  const profiles = await supabaseInstance.from('profiles').select('*');
+  await createProfileClimbs(supabaseInstance, climbs?.data ?? [], profiles.data ?? [], benjamin.user)
 }
+
+
 
 
 
