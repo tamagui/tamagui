d2bdef75e13be75441f63cda6329fe27eefbdc2e
natew <natewienert@gmail.com>
2023-08-30 11:38:32 -1000

    fix(web): createTheme type should just pass through what it receives, deprecate createTheme since its not needed

--- packages/web/src/createTheme.ts
+++ packages/web/src/createTheme.ts
@@ -4,10 +4,6 @@ type GenericTheme = { [key: string]: string | Variable }
 
 // export const themeToVariableToValueMap = new WeakMap<any, Record<string, string>>()
 
-export const createTheme = <Theme extends GenericTheme>(
-  theme: Theme
-): {
-  [K in keyof Theme]: Theme[K] | string | Variable
-} => {
+export const createTheme = <Theme extends GenericTheme>(theme: Theme): Theme => {
   return theme
 }
