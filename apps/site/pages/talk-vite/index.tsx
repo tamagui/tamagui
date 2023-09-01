03edfa4d5b0aa7209a2f8468398be264bc73753d
natew <natewienert@gmail.com>
2023-08-31 19:04:31 -1000

    vite talk slides

--- apps/site/pages/talk-vite/index.tsx
+++ apps/site/pages/talk-vite/index.tsx
@@ -3,10 +3,13 @@ import { NextSeo } from 'next-seo'
 import { useEffect, useState } from 'react'
 import { Spacer, XStack, YStack } from 'tamagui'
 
+import { Slide, SlideStepItem } from '../../components/Slide'
 import { Slides, slideDimensions } from '../../components/Slides'
 import { ThemeToggle } from '../../components/ThemeToggle'
+import slideWhy from '../talk/slides/slide-why'
+import TitleSlide from '../talk/slides/slide1'
 
-export default function TamaguiTalk() {
+export default function TamaguiViteTalk() {
   const [isMounted, setIsMounted] = useState(false)
 
   useEffect(() => {
@@ -42,12 +45,203 @@ export default function TamaguiTalk() {
       </YStack>
 
       <Slides
-        slides={
-          [
-            //
-          ]
-        }
+        slides={[
+          SlideGuy,
+          Slide1,
+          () => (
+            <Slide
+              theme="orange"
+              steps={[
+                [
+                  {
+                    type: 'callout',
+                    size: '$15',
+                    content: `What if we could...`,
+                  },
+                ],
+              ]}
+            />
+          ),
+          () => (
+            <Slide
+              theme="yellow"
+              steps={[
+                [
+                  {
+                    type: 'callout',
+                    size: '$15',
+                    content: `Write native and web apps with...`,
+                  },
+                ],
+              ]}
+            />
+          ),
+          () => (
+            <Slide
+              theme="green"
+              steps={[
+                [
+                  {
+                    type: 'callout',
+                    size: '$15',
+                    content: `...a single codebase...`,
+                  },
+                ],
+              ]}
+            />
+          ),
+          () => (
+            <Slide
+              theme="blue"
+              steps={[
+                [
+                  {
+                    type: 'callout',
+                    size: '$15',
+                    content: `...a single bundler...`,
+                  },
+                ],
+              ]}
+            />
+          ),
+          () => (
+            <Slide
+              theme="purple"
+              steps={[
+                [
+                  {
+                    type: 'callout',
+                    size: '$15',
+                    content: `...a single set of routes...`,
+                  },
+                ],
+              ]}
+            />
+          ),
+          () => (
+            <Slide
+              theme="pink"
+              steps={[
+                [
+                  {
+                    type: 'callout',
+                    size: '$14',
+                    content: `...and a single data abstraction...`,
+                  },
+                ],
+              ]}
+            />
+          ),
+          () => (
+            <Slide
+              theme="red"
+              steps={[
+                [
+                  {
+                    type: 'callout',
+                    size: '$15',
+                    content: `...without sacrifice?`,
+                  },
+                ],
+              ]}
+            />
+          ),
+          () => <Slide title="How?" theme="purple" steps={[bp4, bp3, bp1, bp2]} />,
+        ]}
       />
     </YStack>
   )
+}
+
+const bp1 = [
+  {
+    type: 'bullet-point',
+    content: [
+      {
+        type: 'code-inline',
+        props: {
+          size: '$12',
+        },
+        content: 'React Native',
+      },
+      {
+        type: 'text',
+        content: 'Single platform for native and web.',
+      },
+    ],
+  } satisfies SlideStepItem,
+]
+
+const bp2 = [
+  {
+    type: 'bullet-point',
+    content: [
+      {
+        type: 'code-inline',
+        props: {
+          size: '$12',
+        },
+        content: 'Tamagui',
+      },
+      {
+        type: 'text',
+        content: 'Single performant UI kit for native and web.',
+      },
+    ],
+  } satisfies SlideStepItem,
+]
+
+const bp3 = [
+  {
+    type: 'bullet-point',
+    content: [
+      {
+        type: 'code-inline',
+        props: {
+          size: '$12',
+        },
+        content: 'Expo Router',
+      },
+      {
+        type: 'text',
+        content: 'Single file system routes for native and web.',
+      },
+    ],
+  } satisfies SlideStepItem,
+]
+
+const bp4 = [
+  {
+    type: 'bullet-point',
+    content: [
+      {
+        type: 'code-inline',
+        props: {
+          size: '$12',
+        },
+        content: 'Vite',
+      },
+      {
+        type: 'text',
+        content: 'Single bundler for native and web.',
+      },
+    ],
+  } satisfies SlideStepItem,
+]
+
+const SlideGuy = () => (
+  <Slide
+    steps={[
+      [
+        {
+          type: 'centered',
+          content: [<TamaguiLogo scale={3} />],
+        },
+      ],
+    ]}
+  />
+)
+
+const Slide1 = () => {
+  return <TitleSlide subTitle=" " />
 }
