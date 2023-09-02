06bdc163b97d40f8ae0b5e77a3062d7881c70d21
Alireza Zamani <alirezazamani2922@gmail.com>
2023-09-02 23:04:52 +0330

    save

--- apps/kitchen-sink/metro.config.js
+++ apps/kitchen-sink/metro.config.js
@@ -1,52 +1,5 @@
-const { makeMetroConfig } = require('@rnx-kit/metro-config')
-const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks')
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = makeMetroConfig({
-  projectRoot: __dirname,
-  resolver: {
-    resolveRequest: MetroSymlinksResolver(),
-  },
-})
+const config = getDefaultConfig(__dirname);
 
-// // Learn more https://docs.expo.io/guides/customizing-metro
-// /**
-//  * @type {import('expo/metro-config')}
-//  */
-//
-// const path = require('path')
-
-// const projectRoot = __dirname
-// const workspaceRoot = path.resolve(__dirname, '..')
-
-// const config = getDefaultConfig(projectRoot)
-
-// config.watchFolders = [workspaceRoot]
-// config.resolver.nodeModulesPaths = [
-//   path.resolve(projectRoot, 'node_modules'),
-//   path.resolve(workspaceRoot, 'node_modules'),
-// ]
-
-// config.resolver.extraNodeModules = {
-//   modules: workspaceRoot,
-// }
-
-// console.log('config', config)
-
-// module.exports = config
-
-// // const defaultSourceExts =
-// //   require('metro-config/src/defaults/defaults').sourceExts
-
-// // module.exports = {
-// //   transformer: {
-// //     getTransformOptions: () => ({
-// //       transform: {
-// //         experimentalImportSupport: true,
-// //         inlineRequires: true,
-// //       },
-// //     }),
-// //   },
-// //   resolver: {
-// //     sourceExts: [...defaultSourceExts, 'cjs'],
-// //   },
-// // }
+module.exports = config;
