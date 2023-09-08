fb5baa47382d9b29fab46b0f58f4b60e47fdb0da
Alireza Zamani <alirezazamani2922@gmail.com>
2023-09-08 12:09:05 +0330

    site: update cors headers to test studio on prod

--- apps/site/lib/cors.ts
+++ apps/site/lib/cors.ts
@@ -2,10 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next'
 
 export function setupCors(req: NextApiRequest, res: NextApiResponse) {
   const origin = req.headers.origin
+  console.log({ origin })
   if (
     typeof origin === 'string' &&
     (origin.endsWith('tamagui.dev') || origin.endsWith('localhost:1421'))
   ) {
+    console.warn('hit')
     res.setHeader('Access-Control-Allow-Origin', origin)
     res.setHeader('Access-Control-Allow-Credentials', 'true')
   }
