1b55063122fceda9bb354fcced0acdd5513a2a75
Alireza Zamani <alirezazamani2922@gmail.com>
2023-09-08 12:16:37 +0330

    site: remove next config headers altogether

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -101,19 +101,19 @@ module.exports = function (name, { defaultConfig }) {
         skipDefaultConversion: true,
       },
     },
-    async headers() {
-      return [
-        {
-          source: "/api/:path*",
-          headers: [
-            // { key: "Access-Control-Allow-Origin", value: "*" },
-            // { key: "Access-Control-Allow-Credentials", value: "true" },
-            // { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-            // { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ]
-    },
+    // async headers() {
+    //   return [
+    //     {
+    //       source: "/api/:path*",
+    //       headers: [
+    // { key: "Access-Control-Allow-Origin", value: "*" },
+    // { key: "Access-Control-Allow-Credentials", value: "true" },
+    // { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+    // { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+    //       ]
+    //     }
+    //   ]
+    // },
     images: {
       remotePatterns: [
         {
