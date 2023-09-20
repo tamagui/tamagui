0268bc09ef22e430a9fdef319deeb11bdbe8ea6f
Ehsan Sarshar <ehsan.herai@gmail.com>
2023-09-20 23:42:44 +0430

    feat(createStyleContext): add scope to the context

--- packages/web/src/helpers/createStyledContext.tsx
+++ packages/web/src/helpers/createStyledContext.tsx
@@ -1,44 +1,70 @@
-import React, { createContext, useContext, useMemo } from 'react'
+import React, { createContext, useContext, useMemo } from "react";
 
-import { objectIdentityKey } from './objectIdentityKey'
+import { objectIdentityKey } from "./objectIdentityKey";
 
-export type StyledContext<Props extends Object = any> = Omit<
-  React.Context<Props>,
-  'Provider'
-> & {
-  context: React.Context<Props>
-  props: Object
+export type StyledContext<Props extends Object = any> = Omit<React.Context<Props>, "Provider"> & {
+  context: React.Context<Props>;
+  props: Object | undefined;
   Provider: React.ProviderExoticComponent<
-    Partial<Props> & {
-      children?: React.ReactNode
+    Partial<Props | undefined> & {
+      children?: React.ReactNode;
+      scope?: string;
     }
-  >
-}
+  >;
+  useStyledContext: (scope?: string) => Props;
+};
 
 export function createStyledContext<VariantProps extends Record<string, any>>(
-  props: VariantProps
+  defaultValues?: VariantProps,
 ): StyledContext<VariantProps> {
-  const OGContext = createContext<VariantProps>(props)
-  const OGProvider = OGContext.Provider
-  const Context = OGContext as any as StyledContext<VariantProps>
+  const OGContext = createContext<VariantProps | undefined>(defaultValues);
+  const OGProvider = OGContext.Provider;
+  const Context = OGContext as any as StyledContext<VariantProps>;
+  const scopedContext = new Map<string, React.Context<VariantProps | undefined>>();
 
   const Provider = ({
     children,
+    scope,
     ...values
-  }: VariantProps & { children?: React.ReactNode }) => {
+  }: VariantProps & { children?: React.ReactNode; scope: string }) => {
     const value = useMemo(() => {
       return {
-        ...props,
+        // this ! is a workaround for ts error
+        ...defaultValues!,
         ...values,
+      };
+    }, [objectIdentityKey(values)]);
+    if (scope) {
+      const ScopedContext = scopedContext.get(scope);
+      if (ScopedContext) {
+        return <ScopedContext.Provider value={value}>{children}</ScopedContext.Provider>;
+      } else {
+        const NewlyCreatedScopedContext = createContext<VariantProps | undefined>(defaultValues);
+        scopedContext.set(scope, NewlyCreatedScopedContext);
+        return (
+          <NewlyCreatedScopedContext.Provider value={value}>
+            {children}
+          </NewlyCreatedScopedContext.Provider>
+        );
       }
-    }, [objectIdentityKey(values)])
-    return <OGProvider value={value}>{children}</OGProvider>
-  }
+    } else {
+      return <OGProvider value={value}>{children}</OGProvider>;
+    }
+  };
+
+  // use consumerComponent just to give a better error message
+  const useStyledContext = (scope?: string) => {
+    const context = scope ? scopedContext.get(scope) : OGContext;
+    return useContext(context!) as VariantProps;
+  };
 
   // @ts-ignore
-  Context.Provider = Provider
-  Context.props = props
-  Context.context = OGContext
+  Context.Provider = Provider;
+  Context.props = defaultValues;
+  Context.context = OGContext as React.Context<VariantProps>;
+  Context.useStyledContext = useStyledContext;
 
-  return Context
+  return Context;
 }
+
+export type Scoped<T> = T & { scope?: string };
--- packages/web/types/helpers/createStyledContext.d.ts
+++ packages/web/types/helpers/createStyledContext.d.ts
@@ -1,10 +1,15 @@
-import React from 'react';
-export type StyledContext<Props extends Object = any> = Omit<React.Context<Props>, 'Provider'> & {
+import React from "react";
+export type StyledContext<Props extends Object = any> = Omit<React.Context<Props>, "Provider"> & {
     context: React.Context<Props>;
-    props: Object;
-    Provider: React.ProviderExoticComponent<Partial<Props> & {
+    props: Object | undefined;
+    Provider: React.ProviderExoticComponent<Partial<Props | undefined> & {
         children?: React.ReactNode;
+        scope?: string;
     }>;
+    useStyledContext: (scope?: string) => Props;
 };
-export declare function createStyledContext<VariantProps extends Record<string, any>>(props: VariantProps): StyledContext<VariantProps>;
+export declare function createStyledContext<VariantProps extends Record<string, any>>(defaultValues?: VariantProps): StyledContext<VariantProps>;
+export type Scoped<T> = T & {
+    scope?: string;
+};
 //# sourceMappingURL=createStyledContext.d.ts.map
