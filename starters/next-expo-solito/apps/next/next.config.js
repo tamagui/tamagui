802fcd1dba55145c2dd7e04b836aa3ed6144fd1d
natew <natewienert@gmail.com>
2023-09-20 18:09:32 -1000

    fix(starter): update next config for new version

--- starters/next-expo-solito/apps/next/next.config.js
+++ starters/next-expo-solito/apps/next/next.config.js
@@ -74,18 +74,7 @@ module.exports = function () {
       'expo-modules-core',
     ],
     experimental: {
-      /*
-       A few notes before enabling app directory:
-
-       - App dir is not yet stable - Usage of this for production apps is discouraged.
-       - Tamagui doesn't support usage in React Server Components yet (usage with 'use client' is supported).
-       - Solito doesn't support app dir at the moment - You'll have to remove Solito.
-       - The `/app` in this starter has the same routes as the `/pages` directory. You should probably remove `/pages` after enabling this.
-      */
-      appDir: false,
-      // optimizeCss: true,
       scrollRestoration: true,
-      legacyBrowsers: false,
     },
   }
 
