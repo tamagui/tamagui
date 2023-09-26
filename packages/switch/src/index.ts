7f06e0f05095fb6b2ca5e9da3aae0caba4a15128
_nderscore <_@nderscore.com>
2023-09-26 14:44:32 -0400

    fix(switch): export SwitchContext for headless consumers

--- packages/switch/src/index.ts
+++ packages/switch/src/index.ts
@@ -2,6 +2,7 @@ import { createSwitch } from './createSwitch'
 import { SwitchFrame, SwitchThumb } from './Switch'
 
 export * from './Switch'
+export * from './SwitchContext'
 export * from './createSwitch'
 
 export const Switch = createSwitch({
