15621bee8c23ca9a8ee85c21836b29a1b30c05b9
benjamin <benschac@gmail.com>
2023-09-27 13:31:31 -0400

    update eslint config next

--- apps/next/.eslintrc.js
+++ apps/next/.eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['next', 'custom'],
+  overrides: [
+    {
+      files: ['next.config.js'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 'off',
+      },
+    },
+  ],
 }
