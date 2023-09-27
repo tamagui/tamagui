08de35a5cb74f800a642a47e6b42069f71614597
benjamin <benschac@gmail.com>
2023-09-27 18:31:44 -0400

    update repo script

--- update_repo.mjs
+++ update_repo.mjs
@@ -2,34 +2,33 @@
 
 import { $ } from 'zx'
 
-
-
 // Ensure that the KNOWN_COMMIT argument is passed
 if (!process.argv[2]) {
-  console.error("Please provide the known commit as an argument.");
-  process.exit(1);
+  console.error('Please provide the known commit as an argument.')
+  process.exit(1)
 }
 
-const KNOWN_COMMIT = process.argv[2];
+const KNOWN_COMMIT = process.argv[2]
 
 // Ensure that we're on the desired branch
-await $`git checkout dev`;
+await $`git checkout dev`
 
 // Fetch the latest commits from the 'dev' branch of the 'upstream' remote
-await $`git fetch upstream dev`;
+await $`git fetch unistack dev`
 
 // Get all the commit hashes after the known commit from upstream/dev branch, excluding merge commits
-const { stdout: COMMIT_HASHES } = await $`git log ${KNOWN_COMMIT}..unistack/dev --pretty=format:"%H" --no-merges`;
+const { stdout: COMMIT_HASHES } =
+  await $`git log ${KNOWN_COMMIT}..unistack/dev --pretty=format:"%H" --no-merges`
 
 // Split hashes by newline and filter out empty strings
-const commits = COMMIT_HASHES.split('\n').filter(Boolean);
+const commits = COMMIT_HASHES.split('\n').filter(Boolean)
 
 // Cherry-pick each commit one by one
 for (const commit of commits) {
-  await $`git cherry-pick ${commit}`;
+  await $`git cherry-pick ${commit}`
 }
 
 // Run 'yarn install' from the root directory
-await $`yarn install`;
+await $`yarn install`
 
-export { };
+export {}
