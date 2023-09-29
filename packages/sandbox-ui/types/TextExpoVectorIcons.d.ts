cc020d75fa822bc4d47afee045c4161a4a1f9105
Alireza Zamani <alirezazamani2922@gmail.com>
2023-09-29 10:06:54 +0330

    chore: remove unused .d.ts files

--- packages/sandbox-ui/types/TextExpoVectorIcons.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-export declare const TestExpoVectorIcons: () => JSX.Element;
-//# sourceMappingURL=TextExpoVectorIcons.d.ts.map
--- packages/sandbox-ui/types/animatinos.web.d.ts
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=animatinos.web.d.ts.map
