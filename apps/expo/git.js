3dddf5d92f191bbfbfe31cfcd2046ad76391db30
benjamin <benschac@gmail.com>
2023-10-07 11:52:55 -0400

    add git command

--- apps/expo/git.js
+++ apps/expo/git.js
@@ -2,5 +2,6 @@ const fs = require('fs')
 const execSync = require('child_process').execSync
 
 const commitHash = execSync('git rev-parse HEAD').toString().trim()
+const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 
-fs.writeFileSync('git.json', JSON.stringify({ commitHash }))
+fs.writeFileSync('git.json', JSON.stringify({ commitHash, branchName }))
--- apps/next/git.js
+++ apps/next/git.js
@@ -1,6 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const fs = require('fs')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const execSync = require('child_process').execSync
 
 const commitHash = execSync('git rev-parse HEAD').toString().trim()
+const branchName = execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
 
-fs.writeFileSync('git.json', JSON.stringify({ commitHash }))
+fs.writeFileSync('git.json', JSON.stringify({ commitHash, branchName }))
