7e9efae3359f6f75d70513c7ea8a574bdea3d88a
natew <natewienert@gmail.com>
2023-10-19 10:11:18 -0400

    fix(core): make staticConfig.memo enable memo on a per-component basis, so styled({}, { memo: true }) works

--- packages/web/src/createComponent.tsx
+++ packages/web/src/createComponent.tsx
@@ -1329,7 +1329,7 @@ export function createComponent<
 
   let res: ComponentType = component as any
 
-  if (process.env.TAMAGUI_FORCE_MEMO) {
+  if (process.env.TAMAGUI_FORCE_MEMO || staticConfig.memo) {
     res = memo(res) as any
   }
 
