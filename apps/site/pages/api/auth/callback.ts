a23d05d9ae60ee2408239c2fb8069b4e9a3151be
natew <natewienert@gmail.com>
2023-10-22 19:22:21 -0400

    site: add supabase code exchange api route

---
+++ apps/site/pages/api/auth/callback.ts
@@ -0,0 +1,21 @@
+import { createPagesServerClient } from '@supabase/auth-helpers-nextjs'
+import { NextApiHandler } from 'next'
+
+import { supabaseCookieOptions } from '../../../lib/supabase-utils'
+
+const handler: NextApiHandler = async (req, res) => {
+  const { code } = req.query
+
+  if (code) {
+    const supabase = createPagesServerClient(
+      { req, res },
+      { cookieOptions: supabaseCookieOptions }
+    )
+
+    await supabase.auth.exchangeCodeForSession(String(code))
+  }
+
+  res.redirect('/')
+}
+
+export default handler
