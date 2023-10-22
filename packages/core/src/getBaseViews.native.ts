a974c8274ec1df9925f6d76bf490f491d7513165
natew <natewienert@gmail.com>
2023-10-22 12:55:55 -0400

    perf(core): use RCTView for views to improve runtime performance on native

--- packages/core/src/getBaseViews.native.ts
+++ packages/core/src/getBaseViews.native.ts
@@ -1,9 +1,12 @@
 export function getBaseViews() {
   const native = require('react-native')
+  const View = require('react-native/Libraries/Components/View/ViewNativeComponent')
+  const TextAncestor = require('react-native/Libraries/Text/TextAncestor')
 
   return {
-    View: native.View || native.default.View,
+    View,
     Text: native.Text || native.default.Text,
+    TextAncestor,
     Pressable: native.Pressable || native.default.Pressable,
   }
 }
