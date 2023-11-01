fa89bb06db98c930e81ad11eb86368ea10ca01d0
natew <natewienert@gmail.com>
2023-10-31 16:09:51 -1000

    fix regression on style merge

--- packages/web/src/helpers/getSplitStyles.tsx
+++ packages/web/src/helpers/getSplitStyles.tsx
@@ -1427,7 +1427,7 @@ function mergeStylePropIntoStyle(styleState: GetStyleState, cur: Object[] | Obje
   if (!cur) return
   const styles = Array.isArray(cur) ? cur : [cur]
   for (const style of styles) {
-    const isRNW = cur['$$css']
+    const isRNW = style['$$css']
     if (isRNW) {
       Object.assign(styleState.classNames, style)
     } else {
