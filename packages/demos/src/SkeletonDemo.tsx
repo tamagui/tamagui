df5cc3743f99d98cabf0125045d0eac40f96e729
benjamin <benschac@gmail.com>
2023-11-17 11:23:37 -0500

    add skeleton to demo

--- packages/demos/src/SkeletonDemo.tsx
+++ packages/demos/src/SkeletonDemo.tsx
@@ -1,13 +1,27 @@
-import { Paragraph, XStack } from 'tamagui'
+import { MotiView } from '@tamagui/animations-moti'
+import { Skeleton } from '@tamagui/skeleton'
+import { Paragraph, Spacer, XStack } from 'tamagui'
+
 // TODO: do packages/tamagui/src pattern for Skeleton
 // import { Skeleton } from 'tamagui/skeleton'
 
 export function SkeletonDemo() {
+  const colorMode = 'light'
   return (
     <XStack space>
-      {/* tokens */}
-
-      <Paragraph>Hello</Paragraph>
+      <MotiView
+        transition={{
+          type: 'timing',
+        }}
+      >
+        <Skeleton colorMode={colorMode} radius="round" height={75} width={75} />
+        <Spacer />
+        <Skeleton colorMode={colorMode} width={250} />
+        <Spacer height={8} />
+        <Skeleton colorMode={colorMode} width={'100%'} />
+        <Spacer height={8} />
+        <Skeleton colorMode={colorMode} width={'100%'} />
+      </MotiView>
     </XStack>
   )
 }
--- packages/skeleton/src/expo.tsx
+++ packages/skeleton/src/expo.tsx
@@ -2,7 +2,7 @@
 import { LinearGradient } from 'expo-linear-gradient'
 import React from 'react'
 
-import SkeletonNative from './skeleton-new'
+import SkeletonNative from './skeleton'
 import { MotiSkeletonProps } from './types'
 
 export default function SkeletonExpo(props: Omit<MotiSkeletonProps, 'Gradient'>) {
