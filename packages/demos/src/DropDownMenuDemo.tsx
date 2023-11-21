21a245da45526c4ae2dd322d7a84a7070c75c55f
TheEhsanSarshar <ehsan.herai@gmail.com>
2023-11-21 16:43:20 +0430

    feat(DropDownMenu): add demo

---
+++ packages/demos/src/DropDownMenuDemo.tsx
@@ -0,0 +1,82 @@
+import {
+  DropdownMenu,
+  DropdownMenuArrow,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from '@tamagui/dropdown-menu'
+import { ChevronRight } from '@tamagui/lucide-icons'
+import { Button, Text } from 'tamagui'
+
+export function DropdoownMenuDemo() {
+  return (
+    <DropdownMenu placement="bottom">
+      <DropdownMenuTrigger width={200} asChild>
+        <Button>
+          <Button.Text>yes trigger</Button.Text>
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuPortal>
+        <DropdownMenuContent
+          borderWidth={1}
+          width={200}
+          ai="flex-start"
+          borderColor="$borderColor"
+          enterStyle={{ y: -10, opacity: 0 }}
+          exitStyle={{ y: -10, opacity: 0 }}
+          elevate
+          animation={[
+            'quick',
+            {
+              opacity: {
+                overshootClamping: true,
+              },
+            },
+          ]}
+        >
+          <DropdownMenuItem>
+            <Text>New Tab ⌘+T</Text>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Text>New Window ⌘+N</Text>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Text>New Private Window ⇧+⌘+N</Text>
+          </DropdownMenuItem>
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <Text>
+                More Tools
+                <ChevronRight />
+              </Text>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <DropdownMenuItem className="DropdownMenuItem">
+                  <Text>Save Page As… ⌘+S</Text>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="DropdownMenuItem">
+                  <Text>Create Shortcut…</Text>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="DropdownMenuItem">
+                  <Text>Name Window…</Text>
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem className="DropdownMenuItem">
+                  <Text>Developer Tools</Text>
+                </DropdownMenuItem>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          <DropdownMenuArrow size={'$3'} borderColor={'$borderColor'} borderWidth={1} />
+        </DropdownMenuContent>
+      </DropdownMenuPortal>
+    </DropdownMenu>
+  )
+}
---
+++ packages/demos/types/DropDownMenuDemo.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="react" />
+export declare function DropdoownMenuDemo(): JSX.Element;
+//# sourceMappingURL=DropDownMenuDemo.d.ts.map
