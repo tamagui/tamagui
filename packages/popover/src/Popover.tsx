e4215cab010b657816f6a625b397f40c5ff61d88
Ehsan sarshar <46188784+TheEhsanSarshar@users.noreply.github.com>
2023-11-23 00:26:34 +0430

    fix(Popover): fix scope missing for Android context (#1903)
    * fix(Popover:android): add context scope
    
    * fix(Popover): remove scope from popover
    
    * fix(PopoverDemo): bring back removed sheet

--- packages/popover/src/Popover.tsx
+++ packages/popover/src/Popover.tsx
@@ -244,9 +244,10 @@ function PopoverRepropagateContext(props: {
   children: any
   context: any
   popperContext: any
+  scope: string
 }) {
   return (
-    <PopperContext.Provider {...props.popperContext}>
+    <PopperContext.Provider scope={props.scope} {...props.popperContext}>
       <PopoverContext.Provider {...props.context}>
         {props.children}
       </PopoverContext.Provider>
@@ -266,7 +267,11 @@ function PopoverContentPortal(props: ScopedPopoverProps<PopoverContentTypeProps>
   // native doesnt support portals
   if (Platform.OS === 'android' || Platform.OS === 'ios') {
     contents = (
-      <PopoverRepropagateContext popperContext={popperContext} context={context}>
+      <PopoverRepropagateContext
+        scope={__scopePopover || POPOVER_SCOPE}
+        popperContext={popperContext}
+        context={context}
+      >
         {props.children}
       </PopoverRepropagateContext>
     )
