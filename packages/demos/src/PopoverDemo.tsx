5a31e9e70c0aa5ac1d3e1e39ce6c70cc91997da0
TheEhsanSarshar <ehsan.herai@gmail.com>
2023-11-22 19:02:16 +0430

    fix(Popover:android): add context scope

--- packages/demos/src/PopoverDemo.tsx
+++ packages/demos/src/PopoverDemo.tsx
@@ -32,19 +32,6 @@ export function Demo({
         <Button icon={Icon} />
       </Popover.Trigger>
 
-      <Adapt when="sm" platform="touch">
-        <Popover.Sheet modal dismissOnSnapToBottom>
-          <Popover.Sheet.Frame padding="$4">
-            <Adapt.Contents />
-          </Popover.Sheet.Frame>
-          <Popover.Sheet.Overlay
-            animation="lazy"
-            enterStyle={{ opacity: 0 }}
-            exitStyle={{ opacity: 0 }}
-          />
-        </Popover.Sheet>
-      </Adapt>
-
       <Popover.Content
         borderWidth={1}
         borderColor="$borderColor"
