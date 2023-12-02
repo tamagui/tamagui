9fd97eb518ca913b3a4dc231a97ef37403b6418e
natew <natewienert@gmail.com>
2023-12-02 09:57:39 -1000

    fix(web): fix Spacer producing too complex types

--- packages/web/src/createComponent.tsx
+++ packages/web/src/createComponent.tsx
@@ -49,6 +49,7 @@ import {
   SpaceValue,
   SpacerProps,
   StackProps,
+  StackPropsBase,
   StaticConfig,
   StyleableOptions,
   TamaguiComponent,
@@ -1394,7 +1395,7 @@ const getSpacerSize = (size: SizeTokens | number | boolean, { tokens }) => {
 // dont used styled() here to avoid circular deps
 // keep inline to avoid circular deps
 // @ts-expect-error we override
-export const Spacer = createComponent<SpacerProps>({
+export const Spacer = createComponent<SpacerProps, TamaguiElement, StackPropsBase>({
   acceptsClassName: true,
   memo: true,
   componentName: 'Spacer',
