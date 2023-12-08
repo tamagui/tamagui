26cd69ef9b1d4ab1727dbecdbd4e0b298f6f7daf
Ehsan Sarshar <ehsan.herai@gmail.com>
2023-12-08 15:10:03 +0430

    feat(bento): initial boilerplate

---
+++ apps/site/pages/bento/[section]/[part].tsx
@@ -0,0 +1,37 @@
+import * as sections from '@tamagui/bento'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { useRouter } from 'next/router'
+
+export default function page() {
+  const router = useRouter()
+  const params = router.query
+  const Comp = sections[params.section][params.part]
+
+  return <Comp />
+}
+
+export const getStaticPaths = (async () => {
+  return {
+    paths: [
+      {
+        params: {
+          section: 'forms',
+          part: 'auth',
+        },
+      },
+      {
+        params: {
+          section: 'forms',
+          part: 'inputs',
+        },
+      },
+    ],
+    fallback: false,
+  }
+}) satisfies GetStaticPaths
+
+export const getStaticProps = () => {
+  return {
+    props: { repo: '' },
+  }
+}
---
+++ apps/site/pages/bento/index.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'tamagui'
+
+export default function page() {
+  return <Text>I'm a page</Text>
+}
