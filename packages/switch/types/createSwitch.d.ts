fe03161c5df23fd3892ffed357c6b3a89e84d72a
Ehsan Sarshar <ehsan.herai@gmail.com>
2023-12-11 19:32:19 +0430

    fix(menu): package versions

--- packages/switch/types/createSwitch.d.ts
+++ packages/switch/types/createSwitch.d.ts
@@ -26,13 +26,6 @@ export declare function createSwitch<F extends SwitchComponent, T extends Switch
     disableActiveTheme?: boolean;
     Frame?: F;
     Thumb?: T;
-
-}): React.ForwardRefExoticComponent<Omit<Omit<import("react-native").ViewProps, "display" | "children" | "style" | ("onLayout" | keyof import("react-native").GestureResponderHandlers)> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").WebOnlyPressEvents & import("@tamagui/core").TamaguiComponentPropsBaseBase & {
-    style?: import("@tamagui/core").StyleProp<import("react-native").ViewStyle | React.CSSProperties | (React.CSSProperties & import("react-native").ViewStyle)>;
-} & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core").PseudoProps<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>>> & import("@tamagui/core").MediaProps<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>> & import("@tamagui/core").PseudoProps<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase> & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStylePropsBase>>>> & SwitchSharedProps & SwitchExtraProps, "children"> & {
-    children?: React.ReactNode | ((checked: boolean) => React.ReactNode);
-} & React.RefAttributes<TamaguiElement>> & {
-
 }): import("@tamagui/core").ReactComponentWithRef<Omit<SwitchProps, keyof SwitchExtraProps> & SwitchExtraProps, any> & {
     staticConfig: import("@tamagui/core").StaticConfig;
     extractable: <X>(a: X, staticConfig?: Partial<import("@tamagui/core").StaticConfig> | undefined) => X;
@@ -41,7 +34,6 @@ export declare function createSwitch<F extends SwitchComponent, T extends Switch
     __baseProps: any;
     __variantProps: SwitchSharedProps & SwitchExtraProps;
 } & {
-
     Thumb: import("@tamagui/core").TamaguiComponent<SwitchBaseProps, any, any, SwitchSharedProps, {}>;
 };
 export {};
