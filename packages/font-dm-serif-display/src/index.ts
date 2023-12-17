f308fe3232c249cf71f708aa13b58810ca7c6418
natew <natewienert@gmail.com>
2023-12-16 18:33:44 -1000

    studio: cleanup and bugfix

--- packages/font-dm-serif-display/src/index.ts
+++ packages/font-dm-serif-display/src/index.ts
@@ -1,7 +1,7 @@
 import type { FillInFont, GenericFont } from '@tamagui/core'
 import { createFont, getVariableValue, isWeb } from '@tamagui/core'
 
-const LINE_HEIGHT = 1.6
+const LINE_HEIGHT = 1.2
 
 export const createDmSerifDisplayFont = <A extends GenericFont>(
   font: Partial<A> = {},
