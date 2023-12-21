ea72fd0ed0609745e96715d8bffe692cab464a38
Ehsan Sarshar <ehsan.herai@gmail.com>
2023-12-21 15:45:59 +0430

    feat(DropdownMenu:headless): initial Draft

--- packages/dropdown-menu/types/createDropdownMenu.d.ts
+++ packages/dropdown-menu/types/createDropdownMenu.d.ts
@@ -2,6 +2,7 @@ import { Menu, MenuProps, MenuSubProps } from '@tamagui/menu';
 import * as React from 'react';
 import { Button, TamaguiElement } from 'tamagui';
 type Direction = 'ltr' | 'rtl';
+export declare const DROPDOWN_MENU_CONTEXT = "DropdownMenuContext";
 type ScopedProps<P> = P & {
     __scopeDropdownMenu?: string;
 };
@@ -13,7 +14,6 @@ interface DropdownMenuProps extends MenuProps {
     onOpenChange?(open: boolean): void;
     modal?: boolean;
 }
-export declare const DROPDOWN_MENU_CONTEXT = "DropdownMenuContext";
 type PrimitiveButtonProps = React.ComponentPropsWithoutRef<typeof Button>;
 interface DropdownMenuTriggerProps extends PrimitiveButtonProps {
     onKeydown?(event: React.KeyboardEvent): void;
@@ -57,7 +57,7 @@ interface DropdownMenuSubTriggerProps extends MenuSubTriggerProps {
 type MenuSubContentProps = React.ComponentPropsWithoutRef<typeof Menu.SubContent>;
 interface DropdownMenuSubContentProps extends MenuSubContentProps {
 }
-export declare const DropdownMenu: {
+export declare function createDropdownMenu(): {
     (props: ScopedProps<DropdownMenuProps>): JSX.Element;
     displayName: string;
 } & {
@@ -164,7 +164,7 @@ export declare const DropdownMenu: {
     } & React.RefAttributes<TamaguiElement> & Omit<import("tamagui").StackProps, "display" | "pointerEvents" | "children" | "onLayout" | "onStartShouldSetResponder" | "onMoveShouldSetResponder" | "onResponderEnd" | "onResponderGrant" | "onResponderReject" | "onResponderMove" | "onResponderRelease" | "onResponderStart" | "onResponderTerminationRequest" | "onResponderTerminate" | "onStartShouldSetResponderCapture" | "onMoveShouldSetResponderCapture" | "style" | "hitSlop" | "id" | "removeClippedSubviews" | "testID" | "nativeID" | "collapsable" | "needsOffscreenAlphaCompositing" | "renderToHardwareTextureAndroid" | "focusable" | "shouldRasterizeIOS" | "isTVSelectable" | "hasTVPreferredFocus" | "tvParallaxProperties" | "tvParallaxShiftDistanceX" | "tvParallaxShiftDistanceY" | "tvParallaxTiltAngle" | "tvParallaxMagnification" | "onTouchStart" | "onTouchMove" | "onTouchEnd" | "onTouchCancel" | "onTouchEndCapture" | "onPointerEnter" | "onPointerEnterCapture" | "onPointerLeave" | "onPointerLeaveCapture" | "onPointerMove" | "onPointerMoveCapture" | "onPointerCancel" | "onPointerCancelCapture" | "onPointerDown" | "onPointerDownCapture" | "onPointerUp" | "onPointerUpCapture" | "accessible" | "accessibilityActions" | "accessibilityLabel" | "aria-label" | "accessibilityRole" | "accessibilityState" | "aria-busy" | "aria-checked" | "aria-disabled" | "aria-expanded" | "aria-selected" | "aria-labelledby" | "accessibilityHint" | "accessibilityValue" | "aria-valuemax" | "aria-valuemin" | "aria-valuenow" | "aria-valuetext" | "onAccessibilityAction" | "importantForAccessibility" | "aria-hidden" | "aria-live" | "aria-modal" | "role" | "accessibilityLiveRegion" | "accessibilityLabelledBy" | "accessibilityElementsHidden" | "accessibilityViewIsModal" | "onAccessibilityEscape" | "onAccessibilityTap" | "onMagicTap" | "accessibilityIgnoresInvertColors" | "accessibilityLanguage" | "gap" | "columnGap" | "rowGap" | "contain" | "touchAction" | "cursor" | "outlineColor" | "outlineOffset" | "outlineStyle" | "outlineWidth" | "space" | "spaceDirection" | "separator" | "animation" | "animateOnly" | "userSelect" | "backgroundColor" | "borderBlockColor" | "borderBlockEndColor" | "borderBlockStartColor" | "borderBottomColor" | "borderBottomEndRadius" | "borderBottomLeftRadius" | "borderBottomRightRadius" | "borderBottomStartRadius" | "borderColor" | "borderCurve" | "borderEndColor" | "borderEndEndRadius" | "borderEndStartRadius" | "borderLeftColor" | "borderRadius" | "borderRightColor" | "borderStartColor" | "borderStartEndRadius" | "borderStartStartRadius" | "borderStyle" | "borderTopColor" | "borderTopEndRadius" | "borderTopLeftRadius" | "borderTopRightRadius" | "borderTopStartRadius" | "opacity" | "alignContent" | "alignItems" | "alignSelf" | "aspectRatio" | "borderBottomWidth" | "borderEndWidth" | "borderLeftWidth" | "borderRightWidth" | "borderStartWidth" | "borderTopWidth" | "borderWidth" | "bottom" | "end" | "flex" | "flexBasis" | "flexDirection" | "flexGrow" | "flexShrink" | "flexWrap" | "height" | "justifyContent" | "left" | "margin" | "marginBottom" | "marginEnd" | "marginHorizontal" | "marginLeft" | "marginRight" | "marginStart" | "marginTop" | "marginVertical" | "maxHeight" | "maxWidth" | "minHeight" | "minWidth" | "overflow" | "padding" | "paddingBottom" | "paddingEnd" | "paddingHorizontal" | "paddingLeft" | "paddingRight" | "paddingStart" | "paddingTop" | "paddingVertical" | "position" | "right" | "start" | "top" | "width" | "zIndex" | "direction" | "shadowColor" | "shadowOffset" | "shadowOpacity" | "shadowRadius" | "transform" | "transformMatrix" | "rotation" | "scaleX" | "scaleY" | "translateX" | "translateY" | `$${string}` | `$${number}` | `$theme-${string}` | `$theme-${number}` | "x" | "y" | "perspective" | "scale" | "skewX" | "skewY" | "matrix" | "rotate" | "rotateY" | "rotateX" | "rotateZ" | "key" | "onPress" | "onPressIn" | "onPressOut" | "onLongPress" | "disabled" | "ref" | "theme" | "group" | "onFocus" | "onHoverIn" | "onHoverOut" | "onMouseEnter" | "onMouseLeave" | "onMouseDown" | "onMouseUp" | "onBlur" | "target" | "asChild" | "dangerouslySetInnerHTML" | "debug" | "className" | "themeShallow" | "themeInverse" | "tag" | "untilMeasured" | "componentName" | "tabIndex" | "disableOptimization" | "forceStyle" | "disableClassName" | "onScroll" | "hoverStyle" | "pressStyle" | "focusStyle" | "exitStyle" | "enterStyle" | "elevationAndroid" | "rel" | "download" | "dataSet" | "onScrollShouldSetResponder" | "onScrollShouldSetResponderCapture" | "onSelectionChangeShouldSetResponder" | "onSelectionChangeShouldSetResponderCapture" | "href" | "hrefAttrs"> & {
         readonly fullscreen?: boolean | undefined;
         readonly elevation?: number | import("tamagui").SizeTokens | undefined;
-    }>>, keyof DropdownMenuTriggerProps | "__scopeDropdownMenu"> & DropdownMenuTriggerProps & {
+    }>>, "__scopeDropdownMenu" | keyof DropdownMenuTriggerProps> & DropdownMenuTriggerProps & {
         __scopeDropdownMenu?: string | undefined;
     }, TamaguiElement, Omit<import("react-native").ViewProps, "display" | "pointerEvents" | "children" | "onLayout" | keyof import("react-native").GestureResponderHandlers | "style"> & import("@tamagui/core").ExtendBaseStackProps & import("@tamagui/core").WebOnlyPressEvents & import("@tamagui/core").TamaguiComponentPropsBaseBase & {
         style?: import("@tamagui/core").StyleProp<import("react-native").ViewStyle | React.CSSProperties | (React.CSSProperties & import("react-native").ViewStyle)>;
@@ -1117,4 +1117,4 @@ export declare const DropdownMenu: {
     ItemIcon: import("tamagui").TamaguiComponent<any, any, {} & void, {}, {}>;
 };
 export type { DropdownMenuProps, DropdownMenuTriggerProps, DropdownMenuPortalProps, DropdownMenuContentProps, DropdownMenuGroupProps, DropdownMenuLabelProps, DropdownMenuItemProps, DropdownMenuCheckboxItemProps, DropdownMenuRadioGroupProps, DropdownMenuRadioItemProps, DropdownMenuItemIndicatorProps, DropdownMenuArrowProps, DropdownMenuSubProps, DropdownMenuSubTriggerProps, DropdownMenuSubContentProps, };
-//# sourceMappingURL=NonNativeDropdownMenu.d.ts.map
+//# sourceMappingURL=createDropdownMenu.d.ts.map
