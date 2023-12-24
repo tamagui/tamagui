ef83904129bd3ec92e29d6445d0d9d519ae837a3
natew <natewienert@gmail.com>
2023-12-23 20:11:40 -1000

    remove(theme-builder): remove all buildThemeSuite functionality from theme-builder as weve improved the theme-builder export in studio to output the more powerful and better documented createThemeBuilder

--- packages/theme-builder/src/index.ts
+++ packages/theme-builder/src/index.ts
@@ -1,7 +1,3 @@
 export * from './ThemeBuilder'
 export * from '@tamagui/create-theme'
 export * from './buildMask'
-export * from './types'
-
-// theme suite builders
-export * from './buildThemeSuite'
