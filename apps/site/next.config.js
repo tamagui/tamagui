423a5f2d1261df717a2558564a369cf1b4ca7f74
natew <natewienert@gmail.com>
2023-12-27 09:54:50 -1000

    site: test single file output on site

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -22,6 +22,7 @@ const plugins = [
     openAnalyzer: process.env.ANALYZE === 'true',
   }),
   withTamagui({
+    cssFileName: `tamagui.[contenthash].css`,
     useReactNativeWebLite: true,
     config: './tamagui.config.ts',
     themeBuilder: {
