14976e4689c9eb0e473bddaac5a51a192a3f53e0
natew <natewienert@gmail.com>
2023-12-27 14:29:56 -1000

    fix(web): avoid passing className when asChild="except-style", clean up and remove old React Native data- compat

--- apps/site/next.config.js
+++ apps/site/next.config.js
@@ -22,7 +22,7 @@ const plugins = [
     openAnalyzer: process.env.ANALYZE === 'true',
   }),
   withTamagui({
-    emitSingleCSSFile: true,
+    emitSingleCSSFile: process.env.NODE_ENV === 'production',
     useReactNativeWebLite: true,
     config: './tamagui.config.ts',
     themeBuilder: {
