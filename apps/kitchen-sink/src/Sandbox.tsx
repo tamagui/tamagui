f2376d43f40bdfca52ba12870edf0e00ca728f55
natew <natewienert@gmail.com>
2023-12-29 09:08:41 -1000

    fix(core): fix merging media queries - fix them not being deeply merged when set in styled() but changed in component props

--- apps/kitchen-sink/src/Sandbox.tsx
+++ apps/kitchen-sink/src/Sandbox.tsx
@@ -2,11 +2,27 @@
 // import './wdyr'
 
 import { View } from 'react-native'
+import { Square, styled } from 'tamagui'
+
+const Test = styled(Square, {
+  $gtSm: {
+    backgroundColor: 'red',
+  },
+})
 
 export const Sandbox = () => {
   return (
     <View style={{ width: '100%', height: '100%', padding: 50 }}>
-      <></>
+      <>
+        <Test
+          debug="verbose"
+          size={100}
+          $gtSm={{
+            borderWidth: 2,
+            borderColor: 'blue',
+          }}
+        />
+      </>
     </View>
   )
 }
