5846f99250942cf78bd2ab3a5bdaae97454d2107
natew <natewienert@gmail.com>
2023-12-30 10:30:59 -1000

    fix(static): fix emitSingleCSSFile to use browserlist >=0.5% targets when combining and minifying css

--- packages/static/src/minifyCSS.ts
+++ packages/static/src/minifyCSS.ts
@@ -1,10 +1,12 @@
-import { transform } from 'lightningcss'
+import browserslist from 'browserslist'
+import { browserslistToTargets, transform } from 'lightningcss'
 
 export function minifyCSS(input: string) {
   return transform({
     filename: 'style.css',
     code: Buffer.from(input),
     minify: true,
+    targets: browserslistToTargets(browserslist('>= 0.5%')),
     sourceMap: true,
   })
 }
