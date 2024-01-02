3c0bba1c9b4b46ac81f0023a5abd8b1579cdf710
natew <natewienert@gmail.com>
2024-01-02 10:48:27 -1000

    update types for transformOrigin

--- packages/web/types/views/Configuration.d.ts
+++ packages/web/types/views/Configuration.d.ts
@@ -4,6 +4,9 @@ interface ConfigurationProps {
     animationDriver?: AnimationDriver | null;
     children: React.ReactNode;
 }
-export declare const Configuration: (props: ConfigurationProps) => JSX.Element;
+export declare const Configuration: {
+    (props: ConfigurationProps): JSX.Element;
+    displayName: string;
+};
 export {};
 //# sourceMappingURL=Configuration.d.ts.map
