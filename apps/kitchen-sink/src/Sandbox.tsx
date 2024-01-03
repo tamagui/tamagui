ee8a4e33a04f28dca55a63c1835f8d7eb36c2c20
natew <natewienert@gmail.com>
2024-01-03 13:30:38 -1000

    switch sandbox

--- apps/kitchen-sink/src/Sandbox.tsx
+++ apps/kitchen-sink/src/Sandbox.tsx
@@ -3,7 +3,7 @@
 
 import { useState } from 'react'
 import { View } from 'react-native'
-import { Button, H2, Square, styled } from 'tamagui'
+import { Button, H2, Label, Separator, Square, Switch, XStack, styled } from 'tamagui'
 
 const Test = styled(Square, {
   $gtSm: {
@@ -16,9 +16,28 @@ export const Sandbox = () => {
   return (
     <View style={{ width: '100%', height: '100%', padding: 50 }}>
       <>
-        <Button disabled={disabled}>Go to user page</Button>
-        <Button onPress={() => setDisabled(!disabled)}>Go to tabs page</Button>
+        <SwitchWithLabel size="$4" />
       </>
     </View>
+  )
+}
+export function SwitchWithLabel(props: { size: SizeTokens; defaultChecked?: boolean }) {
+  const id = `switch-${props.size.toString().slice(1)}-${props.defaultChecked ?? ''}}`
+  return (
+    <XStack width={200} alignItems="center" space="$4">
+      <Label
+        paddingRight="$0"
+        minWidth={90}
+        justifyContent="flex-end"
+        size={props.size}
+        htmlFor={id}
+      >
+        Accept
+      </Label>
+      <Separator minHeight={20} vertical />
+      <Switch id={id} size={props.size} defaultChecked={props.defaultChecked}>
+        <Switch.Thumb animation="quick" />
+      </Switch>
+    </XStack>
   )
 }
