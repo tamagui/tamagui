35f79e42c6ea8963aa3d73c6f1a1d520773bbca4
Alireza Zamani <alirezazamani2922@gmail.com>
2024-01-05 12:26:22 +0330

    fix merge issues

--- packages/switch/src/index.ts
+++ packages/switch/src/index.ts
@@ -1,12 +1,9 @@
-import { SwitchContext } from '@tamagui/switch-headless'
-
 import { createSwitch } from './createSwitch'
 import { SwitchFrame, SwitchThumb } from './Switch'
 
 export * from './createSwitch'
 export * from './StyledContext'
 export * from './Switch'
-export { SwitchContext }
 
 export const Switch = createSwitch({
   Frame: SwitchFrame,
