bca502a3d15dc40febca01cfcc60f08515c1c889
natew <natewienert@gmail.com>
2024-01-06 03:17:50 -1000

    fix(web): export ViewProps that are the same as StackProps

--- packages/web/src/views/View.tsx
+++ packages/web/src/views/View.tsx
@@ -5,6 +5,7 @@ import { createComponent } from '../createComponent'
 import type { StackProps, StackPropsBase, TamaguiElement } from '../types'
 
 export type View = TamaguiElement
+export type ViewProps = StackProps
 
 export const View = createComponent<StackProps, View, StackPropsBase>({
   acceptsClassName: true,
