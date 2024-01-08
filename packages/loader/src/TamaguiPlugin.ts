cd8a44392a8eed5f4c0b3c57eedb199ed063a08a
natew <natewienert@gmail.com>
2024-01-07 14:51:43 -1000

    cleanup

--- packages/loader/src/TamaguiPlugin.ts
+++ packages/loader/src/TamaguiPlugin.ts
@@ -103,27 +103,29 @@ export class TamaguiPlugin {
     )
   }
 
-  defaultAliases = Object.fromEntries(
-    this.safeResolves([
-      ['@tamagui/core/reset.css', '@tamagui/core/reset.css'],
-      ['@tamagui/core', '@tamagui/core'],
-      ['@tamagui/web', '@tamagui/web'],
-      // web specific light react-native-svg, optional, can use svgs but had issues with compat
-      ['react-native-svg', '@tamagui/react-native-svg'],
-      // fixes https://github.com/kentcdodds/mdx-bundler/issues/143
-      ['react/jsx-runtime.js', 'react/jsx-runtime'],
-      ['react/jsx-runtime', 'react/jsx-runtime'],
-      ['react/jsx-dev-runtime.js', 'react/jsx-dev-runtime'],
-      ['react/jsx-dev-runtime', 'react/jsx-dev-runtime'],
-      ['react-native-reanimated', 'react-native-reanimated'],
-      ['react-native$', 'react-native-web'],
-      ['react-native-web$', 'react-native-web'],
-      ['@testing-library/react-native', '@tamagui/proxy-worm'],
-      ['@gorhom/bottom-sheet$', '@gorhom/bottom-sheet'],
-      // fix reanimated 3
-      ['react-native/Libraries/Renderer/shims/ReactFabric', '@tamagui/proxy-worm'],
-    ])
-  )
+  get defaultAliases() {
+    return Object.fromEntries(
+      this.safeResolves([
+        ['@tamagui/core/reset.css', '@tamagui/core/reset.css'],
+        ['@tamagui/core', '@tamagui/core'],
+        ['@tamagui/web', '@tamagui/web'],
+        // web specific light react-native-svg, optional, can use svgs but had issues with compat
+        ['react-native-svg', '@tamagui/react-native-svg'],
+        // fixes https://github.com/kentcdodds/mdx-bundler/issues/143
+        ['react/jsx-runtime.js', 'react/jsx-runtime'],
+        ['react/jsx-runtime', 'react/jsx-runtime'],
+        ['react/jsx-dev-runtime.js', 'react/jsx-dev-runtime'],
+        ['react/jsx-dev-runtime', 'react/jsx-dev-runtime'],
+        ['react-native-reanimated', 'react-native-reanimated'],
+        ['react-native$', 'react-native-web'],
+        ['react-native-web$', 'react-native-web'],
+        ['@testing-library/react-native', '@tamagui/proxy-worm'],
+        ['@gorhom/bottom-sheet$', '@gorhom/bottom-sheet'],
+        // fix reanimated 3
+        ['react-native/Libraries/Renderer/shims/ReactFabric', '@tamagui/proxy-worm'],
+      ])
+    )
+  }
 
   apply(compiler: Compiler) {
     if (compiler.watchMode && !this.options.disableWatchConfig) {
