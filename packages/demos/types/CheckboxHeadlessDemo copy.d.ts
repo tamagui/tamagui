d0bfdf85f8414f9fa3a41d9be9031d1556e33f18
Alireza Zamani <alirezazamani2922@gmail.com>
2024-01-10 11:29:10 +0330

    fix: remove irrelevant d.ts files

--- packages/demos/types/CheckboxHeadlessDemo copy.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-export declare function CheckboxHeadlessDemo(): JSX.Element;
-//# sourceMappingURL=CheckboxHeadlessDemo%20copy.d.ts.map
--- packages/demos/types/SwitchHeadlessDemo copy.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference types="react" />
-export declare function SwitchHeadlessDemo(): JSX.Element;
-//# sourceMappingURL=SwitchHeadlessDemo%20copy.d.ts.map
