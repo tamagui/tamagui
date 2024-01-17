9b78ccb887ff30da09064b09d0600de037c3c2b0
natew <natewienert@gmail.com>
2024-01-16 23:02:38 -1000

    expo router cleanup config

--- starters/expo-router/metro.config.js
+++ starters/expo-router/metro.config.js
@@ -9,19 +9,37 @@ const config = getDefaultConfig(__dirname, {
   isCSSEnabled: true,
 })
 
-if (process.env.IS_TAMAGUI_DEV) {
-  /**
-   * This is just for the tamagui monorepo, can delete:
-   */
+// for some reason rnxkit is much more reliable at resolving
+const { makeMetroConfig } = require('@rnx-kit/metro-config')
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks')
+const config2 = makeMetroConfig({
+  projectRoot: __dirname,
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+  },
+})
+config.resolver.resolveRequest = config2.resolver.resolveRequest
+config.resolver.extraNodeModules = config2.resolver.extraNodeModules
+config.resolver.resolverMainFields = config2.resolver.resolverMainFields
 
-  // generic monorepo config:
+// 2. Enable Tamagui
+const { withTamagui } = require('@tamagui/metro-plugin')
+module.exports = withTamagui(config, {
+  components: ['tamagui'],
+  config: './tamagui.config.ts',
+  outputCSS: './tamagui.css',
+})
+
+/**
+ * You can delete this, its just for internal use in tamagui monorepo:
+ */
+if (process.env.IS_TAMAGUI_DEV) {
   const projectRoot = __dirname
   const projectNM = path.resolve(projectRoot, 'node_modules')
   const workspaceRoot = path.join(__dirname, '../..')
   const workspaceNM = path.resolve(workspaceRoot, 'node_modules')
   config.watchFolders = [workspaceRoot, projectRoot]
   config.resolver.nodeModulesPaths = [projectNM, workspaceNM]
-
   // delete some duplicates that metro gets tripped up by
   const dirs = ['react', 'react-dom']
   for (const name of dirs) {
@@ -38,17 +56,4 @@ if (process.env.IS_TAMAGUI_DEV) {
       } catch {}
     }
   }
-
-  /**
-   * End monorepo / tamagui specific config
-   */
 }
-
-// 2. Enable Tamagui
-const { withTamagui } = require('@tamagui/metro-plugin')
-module.exports = withTamagui(config, {
-  components: ['tamagui'],
-  config: './tamagui.config.ts',
-  outputCSS: './tamagui.css',
-  disableMinifyCSS: true,
-})
