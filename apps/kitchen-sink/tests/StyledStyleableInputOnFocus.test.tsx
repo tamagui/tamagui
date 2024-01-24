63255fdbf60d62a14e4fcf6694c7cf1e40ad7d5f
Ehsan Sarshar <ehsan.herai@gmail.com>
2024-01-24 23:21:35 +0430

    feat(test): add more test cases for styled

---
+++ apps/kitchen-sink/tests/StyledStyleableInputOnFocus.test.tsx
@@ -0,0 +1,27 @@
+import { expect, test } from '@playwright/test'
+import { setupPage } from './test-utils'
+
+test.beforeEach(async ({ page }) => {
+  await setupPage(page, { name: 'StyledStyleableInputOnFocus', type: 'useCase' })
+})
+
+test(`styled styleable input + onFocus`, async ({ page }) => {
+  const input = page.locator('#onFocus')
+
+  await input.focus()
+
+  await page.waitForFunction(
+    async () => {
+      const inputElement = document.querySelector('#onFocus')
+      return inputElement?.getAttribute('data-onfocus') === 'true'
+    },
+    { timeout: 2000 }
+  )
+
+  const styles = await input.evaluate((el) => {
+    return window.getComputedStyle(el)
+  })
+
+  expect(styles.borderColor).toBe(`rgb(0, 0, 255)`)
+  expect(styles.borderWidth).toBe(`10px`)
+})
