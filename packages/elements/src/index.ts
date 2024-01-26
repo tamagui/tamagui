a71e893f62772d498166b80ebe6322a12359b7a2
natew <natewienert@gmail.com>
2024-01-25 21:05:22 -1000

    site(docs): more Stack => View

--- packages/elements/src/index.ts
+++ packages/elements/src/index.ts
@@ -1,46 +1,46 @@
-import { Stack, styled } from '@tamagui/web'
+import { View, styled } from '@tamagui/web'
 
-export const Section = styled(Stack, {
+export const Section = styled(View, {
   name: 'Section',
   tag: 'section',
   flexDirection: 'column',
   accessibilityRole: 'summary',
 })
 
-export const Article = styled(Stack, {
+export const Article = styled(View, {
   name: 'Article',
   tag: 'article',
   flexDirection: 'column',
 })
 
-export const Main = styled(Stack, {
+export const Main = styled(View, {
   name: 'Main',
   tag: 'main',
   flexDirection: 'column',
 })
 
-export const Header = styled(Stack, {
+export const Header = styled(View, {
   name: 'Header',
   tag: 'header',
   accessibilityRole: 'header',
   flexDirection: 'column',
 })
 
-export const Aside = styled(Stack, {
+export const Aside = styled(View, {
   name: 'Aside',
   tag: 'aside',
   flexDirection: 'column',
   // accessibilityRole: 'complementary',
 })
 
-export const Footer = styled(Stack, {
+export const Footer = styled(View, {
   name: 'Footer',
   tag: 'footer',
   flexDirection: 'column',
   // accessibilityRole: 'contentinfo',
 })
 
-export const Nav = styled(Stack, {
+export const Nav = styled(View, {
   name: 'Nav',
   tag: 'nav',
   flexDirection: 'column',
