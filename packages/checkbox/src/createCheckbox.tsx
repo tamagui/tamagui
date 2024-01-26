35392b663bfc8d5e96c3a1225b833fc21a6d0848
Alireza Zamani <alirezazamani2922@gmail.com>
2024-01-26 22:09:01 +0330

    chore: lint

--- packages/checkbox/src/createCheckbox.tsx
+++ packages/checkbox/src/createCheckbox.tsx
@@ -91,138 +91,142 @@ export function createCheckbox<
   ensureContext(Frame)
   ensureContext(Indicator)
 
-  const FrameComponent = Frame.styleable<CheckboxProps>(function Checkbox(_props, forwardedRef) {
-    const {
-      scaleSize = 0.45,
-      sizeAdjust = 0,
-      scaleIcon,
-      checked: checkedProp,
-      defaultChecked,
-      onCheckedChange,
-      native,
-      unstyled = false,
-      ...props
-    } = _props
-    const propsActive = useProps(props)
+  const FrameComponent = Frame.styleable<CheckboxProps>(
+    function Checkbox(_props, forwardedRef) {
+      const {
+        scaleSize = 0.45,
+        sizeAdjust = 0,
+        scaleIcon,
+        checked: checkedProp,
+        defaultChecked,
+        onCheckedChange,
+        native,
+        unstyled = false,
+        ...props
+      } = _props
+      const propsActive = useProps(props)
 
-    // TODO: this could be null - fix the type
-    const styledContext = React.useContext(CheckboxStyledContext)
-    let adjustedSize = 0
-    let size = 0
-    if (!unstyled) {
-      adjustedSize = getVariableValue(
-        getSize(propsActive.size ?? styledContext?.size ?? '$true', {
-          shift: sizeAdjust,
-        })
-      ) as number
-      size = scaleSize ? Math.round(adjustedSize * scaleSize) : adjustedSize
-    }
+      // TODO: this could be null - fix the type
+      const styledContext = React.useContext(CheckboxStyledContext)
+      let adjustedSize = 0
+      let size = 0
+      if (!unstyled) {
+        adjustedSize = getVariableValue(
+          getSize(propsActive.size ?? styledContext?.size ?? '$true', {
+            shift: sizeAdjust,
+          })
+        ) as number
+        size = scaleSize ? Math.round(adjustedSize * scaleSize) : adjustedSize
+      }
 
-    const [checked = false, setChecked] = useControllableState({
-      prop: checkedProp,
-      defaultProp: defaultChecked!,
-      onChange: onCheckedChange,
-    })
-
-    const { checkboxProps, checkboxRef, bubbleInput } = useCheckbox(
-      // @ts-ignore
-      propsActive,
-      [checked, setChecked],
-      forwardedRef
-    )
-
-    const renderNative = shouldRenderNativePlatform(native)
-    if (renderNative === 'web') {
-      return (
-        <input
-          type="checkbox"
-          defaultChecked={isIndeterminate(checked) ? false : checked}
-          tabIndex={-1}
-          ref={checkboxRef as any}
-          disabled={checkboxProps.disabled}
-          style={{
-            appearance: 'auto',
-            accentColor: 'var(--color6)',
-            ...(checkboxProps.style as any), // TODO: any
-          }}
-        />
-      )
-    }
-
-    return (
-      <CheckboxContext.Provider
-        value={{
-          checked,
-          disabled: checkboxProps.disabled,
-        }}
-      >
-        <CheckboxStyledContext.Provider
-          size={propsActive.size ?? styledContext?.size ?? '$true'}
-          scaleIcon={scaleIcon ?? styledContext?.scaleIcon ?? 1}
-        >
-          <Frame
-            {...(!unstyled && {
-              width: size,
-              height: size,
-            })}
-            tag="button"
-            ref={checkboxRef}
-            {...(unstyled === false && {
-              size,
-              theme: checked ? 'active' : null,
-            })}
-            // expected variants
-            checked={checked}
-            disabled={checkboxProps.disabled}
-            {...checkboxProps}
-          >
-            {propsActive.children}
-          </Frame>
-          {bubbleInput}
-        </CheckboxStyledContext.Provider>
-      </CheckboxContext.Provider>
-    )
-  })
-
-  const IndicatorComponent = Indicator.styleable<CheckboxIndicatorProps>((props, forwardedRef) => {
-    const {
-      // __scopeCheckbox,
-      children: childrenProp,
-      forceMount,
-      disablePassStyles,
-      unstyled = false,
-      ...indicatorProps
-    } = props
-    const styledContext = React.useContext(CheckboxStyledContext)
-    let children = childrenProp
-
-    if (!unstyled) {
-      const iconSize =
-        (typeof styledContext.size === 'number'
-          ? styledContext.size * 0.65
-          : getFontSize(styledContext.size as any)) * styledContext.scaleIcon
-      const theme = useTheme()
-      const getThemedIcon = useGetThemedIcon({ size: iconSize, color: theme.color })
-
-      const childrens = React.Children.toArray(childrenProp)
-      children = childrens.map((child) => {
-        if (disablePassStyles || !React.isValidElement(child)) {
-          return child
-        }
-        return getThemedIcon(child)
+      const [checked = false, setChecked] = useControllableState({
+        prop: checkedProp,
+        defaultProp: defaultChecked!,
+        onChange: onCheckedChange,
       })
-    }
 
-    const context = useContext(CheckboxContext)
-    if (forceMount || isIndeterminate(context.checked) || context.checked === true)
-      return (
-        <Indicator pointerEvents="none" {...indicatorProps} ref={forwardedRef}>
-          {children}
-        </Indicator>
+      const { checkboxProps, checkboxRef, bubbleInput } = useCheckbox(
+        // @ts-ignore
+        propsActive,
+        [checked, setChecked],
+        forwardedRef
       )
 
-    return null
-  })
+      const renderNative = shouldRenderNativePlatform(native)
+      if (renderNative === 'web') {
+        return (
+          <input
+            type="checkbox"
+            defaultChecked={isIndeterminate(checked) ? false : checked}
+            tabIndex={-1}
+            ref={checkboxRef as any}
+            disabled={checkboxProps.disabled}
+            style={{
+              appearance: 'auto',
+              accentColor: 'var(--color6)',
+              ...(checkboxProps.style as any), // TODO: any
+            }}
+          />
+        )
+      }
+
+      return (
+        <CheckboxContext.Provider
+          value={{
+            checked,
+            disabled: checkboxProps.disabled,
+          }}
+        >
+          <CheckboxStyledContext.Provider
+            size={propsActive.size ?? styledContext?.size ?? '$true'}
+            scaleIcon={scaleIcon ?? styledContext?.scaleIcon ?? 1}
+          >
+            <Frame
+              {...(!unstyled && {
+                width: size,
+                height: size,
+              })}
+              tag="button"
+              ref={checkboxRef}
+              {...(unstyled === false && {
+                size,
+                theme: checked ? 'active' : null,
+              })}
+              // expected variants
+              checked={checked}
+              disabled={checkboxProps.disabled}
+              {...checkboxProps}
+            >
+              {propsActive.children}
+            </Frame>
+            {bubbleInput}
+          </CheckboxStyledContext.Provider>
+        </CheckboxContext.Provider>
+      )
+    }
+  )
+
+  const IndicatorComponent = Indicator.styleable<CheckboxIndicatorProps>(
+    (props, forwardedRef) => {
+      const {
+        // __scopeCheckbox,
+        children: childrenProp,
+        forceMount,
+        disablePassStyles,
+        unstyled = false,
+        ...indicatorProps
+      } = props
+      const styledContext = React.useContext(CheckboxStyledContext)
+      let children = childrenProp
+
+      if (!unstyled) {
+        const iconSize =
+          (typeof styledContext.size === 'number'
+            ? styledContext.size * 0.65
+            : getFontSize(styledContext.size as any)) * styledContext.scaleIcon
+        const theme = useTheme()
+        const getThemedIcon = useGetThemedIcon({ size: iconSize, color: theme.color })
+
+        const childrens = React.Children.toArray(childrenProp)
+        children = childrens.map((child) => {
+          if (disablePassStyles || !React.isValidElement(child)) {
+            return child
+          }
+          return getThemedIcon(child)
+        })
+      }
+
+      const context = useContext(CheckboxContext)
+      if (forceMount || isIndeterminate(context.checked) || context.checked === true)
+        return (
+          <Indicator pointerEvents="none" {...indicatorProps} ref={forwardedRef}>
+            {children}
+          </Indicator>
+        )
+
+      return null
+    }
+  )
 
   return withStaticProperties(FrameComponent, {
     Indicator: IndicatorComponent,
