b6899a3ed53b183c5ec24d06d45edb8e155e2ad5
natew <natewienert@gmail.com>
2024-01-26 23:24:14 -1000

    chore: fix some package.json source and avoid git-crypt errors for contributors

---
+++ scripts/watch-ts.ts
@@ -0,0 +1,13 @@
+import { spawn, ChildProcess } from 'child_process'
+
+let child: ChildProcess
+
+if (process.env.IS_TAMAGUI_DEV) {
+  child = spawn(`yarn`, `typecheck -w`.split(' '), { stdio: 'inherit' })
+} else {
+  child = spawn(`yarn`, `typecheck:locked -w`.split(' '), { stdio: 'inherit' })
+}
+
+child.on('close', (code) => {
+  console.info(`Exited with code ${code}`)
+})
