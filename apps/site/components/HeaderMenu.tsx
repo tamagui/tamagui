8676b3044636284d3b3eee49702ffb4640257971
natew <natewienert@gmail.com>
2024-01-28 21:05:01 -1000

    site: tweak

--- apps/site/components/HeaderMenu.tsx
+++ apps/site/components/HeaderMenu.tsx
@@ -166,8 +166,8 @@ const HeaderMenuContent = React.memo(function HeaderMenuContent() {
       style={{ backdropFilter: 'blur(18px)' }}
       br="$6"
     >
-      <Popover.Arrow bg="$color5" size="$4" borderWidth={0} o={0.9} />
-      <YStack fullscreen bg="$color5" zi={0} br="$6" o={0.9} />
+      <Popover.Arrow bg="$color5" size="$4" borderWidth={0} o={0.75} />
+      <YStack fullscreen bg="$color5" zi={0} br="$6" o={0.75} />
 
       <Popover.ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
         <YStack aria-label="Home menu contents" miw={230} p="$3" ai="flex-end">
