467883a8070bef8ea3ca5664697a67f5987fc905
natew <natewienert@gmail.com>
2024-01-29 20:28:47 -1000

    site: fix safari blurs

--- apps/site/components/HeaderMenu.tsx
+++ apps/site/components/HeaderMenu.tsx
@@ -163,14 +163,16 @@ const HeaderMenuContent = React.memo(function HeaderMenuContent() {
       shadowOpacity={0.2}
       zIndex={100000000}
       trapFocus
-      className="blur-8"
       br="$6"
       {...{
-        style: { backdropFilter: 'blur(18px)' },
+        style: {
+          WebkitBackdropFilter: 'blur(12px)',
+          backdropFilter: 'blur(12px)',
+        },
       }}
     >
-      <Popover.Arrow bg="$color5" size="$4" borderWidth={0} o={0.95} />
-      <YStack fullscreen bg="$color5" zi={0} br="$6" o={0.95} />
+      <Popover.Arrow bg="$color5" size="$4" borderWidth={0} o={0.85} />
+      <YStack fullscreen bg="$color5" zi={0} br="$6" o={0.85} />
 
       <Popover.ScrollView showsVerticalScrollIndicator={false} style={{ flex: 1 }}>
         <YStack aria-label="Home menu contents" miw={230} p="$3" ai="flex-end">
