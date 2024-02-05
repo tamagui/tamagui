b5ec24a01a5739920a5c01b4c36f92070cd89c70
Ehsan Sarshar <ehsan.herai@gmail.com>
2024-02-05 10:44:13 +0430

    bento - make code fetch to work properly

--- apps/site/pages/api/bento/code/[...slug].tsx
+++ apps/site/pages/api/bento/code/[...slug].tsx
@@ -1,20 +1,24 @@
-import { protectApiRoute } from "@lib/protectApiRoute";
-import fs from "fs";
-import path from "path";
+import { protectApiRoute } from '@lib/protectApiRoute'
+import fs from 'fs'
+import path from 'path'
 
-const CODE_ASSETS_DIR = "./bento";
+const CODE_ASSETS_DIR =
+  process.env.NODE_ENV === 'development' ? './.next/bento' : './bento'
+
 const handler = async (req, res) => {
-	const { user } = await protectApiRoute({ req, res });
-	// TODO: check if correct user is authorized to access this file
-	if (user.role !== "bento") res.status(401).json({ error: "Unauthorized" });
-	const codePath = req.query.slug?.join("/");
-	const filePath = path.resolve(`${CODE_ASSETS_DIR}/${codePath}`);
-	if (!filePath.startsWith(path.resolve(CODE_ASSETS_DIR))) {
-		res.status(404).json({ error: "Not found" });
-	}
-	const fileBuffer = fs.readFileSync(filePath + ".txt");
-	res.setHeader("Content-Type", "text/plain");
-	return res.send(fileBuffer);
-};
+  if (process.env.NODE_ENV === 'production') {
+    const { user } = await protectApiRoute({ req, res })
+    // TODO: check if correct user is authorized to access this file
+    if (user.role !== 'bento') res.status(401).json({ error: 'Unauthorized' })
+  }
+  const codePath = req.query.slug?.join('/')
+  const filePath = path.resolve(`${CODE_ASSETS_DIR}/${codePath}`)
+  if (!filePath.startsWith(path.resolve(CODE_ASSETS_DIR))) {
+    res.status(404).json({ error: 'Not found' })
+  }
+  const fileBuffer = fs.readFileSync(filePath + '.txt')
+  res.setHeader('Content-Type', 'text/plain')
+  return res.send(fileBuffer)
+}
 
-export default handler;
+export default handler
