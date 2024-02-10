30a72083aa636b91b3475b7310cf2fa975fa5f68
natew <natewienert@gmail.com>
2024-02-09 22:02:16 -1000

    chore: make easier to work on expo router in repo

--- starters/expo-router/metro.config.js
+++ starters/expo-router/metro.config.js
@@ -2,15 +2,45 @@
 const { getDefaultConfig } = require('expo/metro-config')
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname, {
+let config = getDefaultConfig(__dirname, {
   // [Web-only]: Enables CSS support in Metro.
   isCSSEnabled: true,
 })
 
 // 2. Enable Tamagui
 const { withTamagui } = require('@tamagui/metro-plugin')
-module.exports = withTamagui(config, {
+config = withTamagui(config, {
   components: ['tamagui'],
   config: './tamagui.config.ts',
   outputCSS: './tamagui-web.css',
 })
+
+// REMOVE THIS (just for tamagui internal devs):
+if (process.env.IS_TAMAGUI_DEV) {
+  // just have to run first:
+  // yarn link ~/tamagui --all
+  const fs = require('fs')
+  const path = require('path')
+  const projectRoot = __dirname
+  const monorepoRoot = path.resolve(projectRoot, '../..')
+  config.watchFolders = [monorepoRoot]
+  config.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(monorepoRoot, 'node_modules'),
+  ]
+  // have to manually de-deupe
+  try {
+    fs.rmSync(path.join(projectRoot, 'node_modules', 'react'), {
+      recursive: true,
+      force: true,
+    })
+  } catch {}
+  try {
+    fs.rmSync(path.join(projectRoot, 'node_modules', 'react-dom'), {
+      recursive: true,
+      force: true,
+    })
+  } catch {}
+}
+
+module.exports = config
