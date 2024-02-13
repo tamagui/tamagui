9594e748504f46b4fe2b6ff30d4b2d8d757e63a0
Alireza Zamani <alirezazamani2922@gmail.com>
2024-02-13 13:16:11 +0330

    site: add `components/` to the storage dir

--- apps/site/pages/api/bento/code/[...slug].tsx
+++ apps/site/pages/api/bento/code/[...slug].tsx
@@ -25,7 +25,9 @@ const handler = apiRoute(async (req, res) => {
 
   const codePath = slugsArray.join('/')
 
-  const fileResult = await supabaseAdmin.storage.from('bento').download(`${codePath}.tsx`)
+  const fileResult = await supabaseAdmin.storage
+    .from('bento')
+    .download(`components/${codePath}.tsx`)
   if (fileResult.error) {
     console.error(fileResult.error)
     res.status(404).json({ message: 'Not found' })
