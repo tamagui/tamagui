8718d50206dfd1b88950575f28df8c93fa8da455
natew <natewienert@gmail.com>
2024-02-16 11:48:51 -1000

    fix expo router babel config

--- starters/expo-router/babel.config.js
+++ starters/expo-router/babel.config.js
@@ -2,18 +2,5 @@ module.exports = (api) => {
   api.cache(true)
   return {
     presets: ['babel-preset-expo'],
-    plugins:
-      api.platform === 'web'
-        ? []
-        : [
-            [
-              '@tamagui/babel-plugin',
-              {
-                platform: 'native',
-                components: ['tamagui'],
-                config: 'tamagui.config.ts',
-              },
-            ],
-          ],
   }
 }
