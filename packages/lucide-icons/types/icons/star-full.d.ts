17fd44de041e8286136a706a5bc8afcf613314b4
Banks Nussman <banks@nussman.us>
2024-02-16 18:31:00 -0500

    add full star icon

---
+++ packages/lucide-icons/types/icons/star-full.d.ts
@@ -0,0 +1,4 @@
+import React from 'react';
+import type { IconProps } from '@tamagui/helpers-icon';
+export declare const StarFull: React.NamedExoticComponent<IconProps>;
+//# sourceMappingURL=star-full.d.ts.map
