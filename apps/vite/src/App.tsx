cf8a8a7ce56db400c45230966aeff1b72acb3ffb
Banks Nussman <banks@nussman.us>
2024-02-17 02:13:30 -0500

    idk

--- apps/vite/src/App.tsx
+++ apps/vite/src/App.tsx
@@ -1,38 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { TamaguiProvider, Button } from 'tamagui';
 import config from './tamagui.config';
-import './App.css'
 
-function App() {
+export function App() {
   const [count, setCount] = useState(0)
 
   return (
     <TamaguiProvider config={config}>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Button theme="red" opacity={0} enterStyle={{ opacity: 1 }} animation="slow">Test</Button>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button
+        theme="red"
+        opacity={1}
+        enterStyle={{ opacity: 0 }}
+        pressStyle={{ scale: 1.5 }}
+        onPress={() => setCount(c => c + 1)}
+        animation="slow"
+      >
+        Count {count}
+      </Button>
     </TamaguiProvider>
   )
 }
-
-export default App
--- apps/vite/src/main.tsx
+++ apps/vite/src/main.tsx
@@ -1,10 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { App } from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />
 )
