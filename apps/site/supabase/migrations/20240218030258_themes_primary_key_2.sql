326f8855de1c1c0e7b96eb064cefdad5fe7e16d8
natew <natewienert@gmail.com>
2024-02-18 09:06:37 -1000

    fix ci migrations

--- apps/site/supabase/migrations/20240218030258_themes_primary_key_2.sql
+++ apps/site/supabase/migrations/20240218030258_themes_primary_key_2.sql
@@ -1,3 +1,14 @@
+IF NOT exists (
+  select
+    constraint_name
+  from
+    information_schema.table_constraints
+  where
+    table_name = 'studio_themes'
+    and constraint_type = 'PRIMARY KEY'
+) then
 
 alter table
-  studio_themes add primary key (id, user_id, uid);
+  studio_themes
+add
+  primary key (id, user_id, uid);
