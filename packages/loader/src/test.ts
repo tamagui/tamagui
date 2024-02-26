0ba5390301f2126f8222efb61ef7e97467014493
Ehsan sarshar <46188784+TheEhsanSarshar@users.noreply.github.com>
2024-02-26 19:07:00 +0430

    bento: final checkbox and radio improvement feb (#2339)

--- packages/loader/src/test.ts
+++ packages/loader/src/test.ts
@@ -2,6 +2,5 @@ export default async function loader(this: any, sourceIn: Buffer | string) {
   this.cacheable(true)
   const callback = this.async()
   const sourcePath = `${this.resourcePath}`
-  // console.log('ye', sourcePath, this.getOptions().isServer)
   return callback(null, sourceIn)
 }
