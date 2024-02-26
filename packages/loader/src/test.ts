bc7ac0409283bf0978b401faa9cd8cf2caabe615
Ehsan Sarshar <ehsan.herai@gmail.com>
2024-02-26 13:44:58 +0430

    fix: ts errors

--- packages/loader/src/test.ts
+++ packages/loader/src/test.ts
@@ -3,7 +3,5 @@ export default async function loader(this: any, sourceIn: Buffer | string) {
   const callback = this.async()
   const sourcePath = `${this.resourcePath}`
 
-  console.log('ye', sourcePath, this.getOptions().isServer)
-
   return callback(null, sourceIn)
 }
