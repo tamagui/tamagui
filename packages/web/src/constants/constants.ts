676669095ec8fb89c138545551c5bbdafcbf8fea
Mimameid <ibrahimkeser@outlook.de>
2024-03-12 20:38:39 +0100

    Revert wrong formatting

--- packages/web/src/constants/constants.ts
+++ packages/web/src/constants/constants.ts
@@ -1,22 +1,22 @@
-export const THEME_NAME_SEPARATOR = "_";
-export const THEME_CLASSNAME_PREFIX = "t_";
-export const CSS_VARIABLE_PREFIX = "";
-export const FONT_DATA_ATTRIBUTE_NAME = "data-tamagui-font";
+export const THEME_NAME_SEPARATOR = '_'
+export const THEME_CLASSNAME_PREFIX = 't_'
+export const CSS_VARIABLE_PREFIX = ''
+export const FONT_DATA_ATTRIBUTE_NAME = 'data-tamagui-font'
 
-export const stackDefaultStyles = {};
+export const stackDefaultStyles = {}
 
 export const webViewFlexCompatStyles = {
-  display: "flex",
-  alignItems: "stretch",
-  flexDirection: "column",
-  flexBasis: "auto",
-  boxSizing: "border-box",
-  position: "relative",
+  display: 'flex',
+  alignItems: 'stretch',
+  flexDirection: 'column',
+  flexBasis: 'auto',
+  boxSizing: 'border-box',
+  position: 'relative',
   minHeight: 0,
   minWidth: 0,
   flexShrink: 0,
-};
+}
 
-if (process.env.TAMAGUI_TARGET === "web") {
-  Object.assign(stackDefaultStyles, webViewFlexCompatStyles);
+if (process.env.TAMAGUI_TARGET === 'web') {
+  Object.assign(stackDefaultStyles, webViewFlexCompatStyles)
 }
