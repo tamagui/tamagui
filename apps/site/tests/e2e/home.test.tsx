9ae3014e7c323d8177755945f9bab8e55d483182
Ehsan Sarshar <ehsan.herai@gmail.com>
2024-03-22 15:17:26 +0430

    uncomment home logs

--- apps/site/tests/e2e/home.test.tsx
+++ apps/site/tests/e2e/home.test.tsx
@@ -21,8 +21,8 @@ test(`Loads home screen with no errors or logs`, async ({ page }) => {
   })
 
   await page.goto('/')
-  // expect(logs.error.length).toBe(0)
-  // expect(logs.warn.length).toBe(0)
+  expect(logs.error.length).toBe(0)
+  expect(logs.warn.length).toBe(0)
 })
 
 test(`Loads home screen content properly`, async ({ page }) => {
