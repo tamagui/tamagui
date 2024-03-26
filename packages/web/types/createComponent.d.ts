bae6a1fd4148c128397cc683884736094f2e7999
Ehsan sarshar <46188784+TheEhsanSarshar@users.noreply.github.com>
2024-03-26 23:15:16 +0430

    feat(core): add group support to usePropsAndStyle (#2472)
    * feat(core): add group support to usePropsAndStyle
    
    * feat(core): extract group subscribe to a custom hook
    
    * fix(core): useProps better types
    
    * fix(core): useSubscribeToGroup change disabled to isDisabled
    
    * fix(core): better useSubscribeToGroup
    
    * fix(core): covert useSubscribeToGroup to a non hook version
    
    * lint new remix app
    
    * chore(adapt): better error messages
    
    * site: remove log
    
    * fix lint
    
    * fix tests
    
    * fix(vite-plugin): fix compiler not running tamagui initial build and load immediately
    
    * various chores, remix working build time
    
    * feat(adapt): ios android disctinction (#2464)
    
    * feat(web): Support css variable prefixing via environment variable using TAMAGUI_CSS_VARIABLE_PREFIX (#2397)
    
    * Support css variable prefix via TAMAGUI_CSS_VARIABLE_PREFIX environment variable
    
    * Fix reverse creating themes from stylesheets
    
    * Revert wrong formatting
    
    * Move prefix resolution to constants
    
    * Add minimal helper function
    
    * chore(web): fix TAMAGUI_CSS_VARIABLE_PREFIX test
    
    * 192 release image
    
    * fix(core): fix reset.css to reset body margin to 0
    
    * chore: upgrade node, ugrade esbuild/esbuild-register, fix some tests, upgrade deps
    
    * fix use-store import on site
    
    * vite plugin compat with both esm and non esm
    
    ---------
    
    Co-authored-by: natew <natewienert@gmail.com>
    Co-authored-by: arekkubaczkowski <arek.kubaczkowski@gmail.com>
    Co-authored-by: Ibrahim Keser <ibrahimkeser@outlook.de>

--- packages/web/types/createComponent.d.ts
+++ packages/web/types/createComponent.d.ts
@@ -1,6 +1,27 @@
 import React from 'react';
-import type { DebugProp, SpaceDirection, SpaceValue, SpacerProps, SpacerStyleProps, StackNonStyleProps, StaticConfig, TamaguiComponent, TamaguiElement } from './types';
+import type { ComponentContextI, DebugProp, DisposeFn, SpaceDirection, SpaceValue, SpacerProps, SpacerStyleProps, StackNonStyleProps, StackProps, StaticConfig, TamaguiComponent, TamaguiComponentStateRef, TamaguiElement, TamaguiInternalConfig, TextProps } from './types';
+import type { TamaguiComponentState } from './interfaces/TamaguiComponentState';
 export declare const mouseUps: Set<Function>;
+export declare const useComponentState: (props: StackProps | TextProps | Record<string, any>, { animationDriver, groups }: ComponentContextI, staticConfig: StaticConfig, config: TamaguiInternalConfig) => {
+    curStateRef: TamaguiComponentStateRef;
+    disabled: any;
+    groupName: string;
+    hasAnimationProp: boolean;
+    hasEnterStyle: boolean;
+    isAnimated: boolean;
+    isExiting: boolean;
+    isHydrated: boolean;
+    presence: import("./types").UsePresenceResult | null;
+    presenceState: import("./types").PresenceContextProps | null | undefined;
+    setState: React.Dispatch<React.SetStateAction<TamaguiComponentState>>;
+    setStateShallow: (next?: Partial<TamaguiComponentState> | undefined) => void;
+    shouldAvoidClasses: boolean;
+    state: TamaguiComponentState;
+    stateRef: React.MutableRefObject<TamaguiComponentStateRef>;
+    supportsCSSVars: boolean | undefined;
+    willBeAnimated: boolean;
+    willBeAnimatedClient: boolean;
+};
 export declare function createComponent<ComponentPropTypes extends Record<string, any> = {}, Ref extends TamaguiElement = TamaguiElement, BaseProps = never, BaseStyles extends Object = never>(staticConfig: StaticConfig): TamaguiComponent<ComponentPropTypes, Ref, BaseProps, BaseStyles, {}>;
 export declare function Unspaced(props: {
     children?: any;
@@ -19,4 +40,13 @@ export type SpacedChildrenProps = {
     debug?: DebugProp;
 };
 export declare function spacedChildren(props: SpacedChildrenProps): React.ReactNode;
+export declare const isDisabled: (props: any) => any;
+export declare const subscribeToContextGroup: ({ disabled, setStateShallow, pseudoGroups, mediaGroups, componentContext, state, }: {
+    disabled?: boolean | undefined;
+    setStateShallow: (next?: Partial<TamaguiComponentState> | undefined) => void;
+    pseudoGroups?: Set<string> | undefined;
+    mediaGroups?: Set<string> | undefined;
+    componentContext: ComponentContextI;
+    state: TamaguiComponentState;
+}) => DisposeFn | undefined;
 //# sourceMappingURL=createComponent.d.ts.map
