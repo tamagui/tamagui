e3242561fe4c0d7d4b192ddfca30754455ab9393
natew <natewienert@gmail.com>
2024-03-25 22:58:06 -1000

    chore(web): fix TAMAGUI_CSS_VARIABLE_PREFIX test

--- packages/web/src/helpers/registerCSSVariable.ts
+++ packages/web/src/helpers/registerCSSVariable.ts
@@ -1,4 +1,3 @@
-import { CSS_VARIABLE_PREFIX } from '../constants/constants'
 import type { Variable } from '../createVariable'
 import { createCSSVariable, getVariableValue } from '../createVariable'
 import type { VariableVal } from '../types'
@@ -8,9 +7,10 @@ export const registerCSSVariable = (v: Variable | VariableVal) => {
 }
 
 export const variableToCSS = (v: Variable, unitless = false) => {
-  return `--${CSS_VARIABLE_PREFIX}${createCSSVariable(v.name, false)}:${
-    !unitless && typeof v.val === 'number' ? `${v.val}px` : v.val
-  }`
+  return `--${process.env.TAMAGUI_CSS_VARIABLE_PREFIX || ''}${createCSSVariable(
+    v.name,
+    false
+  )}:${!unitless && typeof v.val === 'number' ? `${v.val}px` : v.val}`
 }
 
 export const tokensValueToVariable = new Map<any, any>()
