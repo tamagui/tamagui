cc95550aa68dce2b63a3abc1500b12be5663a37b
Alireza Zamani <alirezazamani2922@gmail.com>
2024-03-28 10:19:17 +0330

    site: rework purchase modal on takeout & bento (#2474)
    * site: rework purchase modal on takeout & bento
    
    * site: fix issues with purchase modal imports
    
    * site: responsive fixes for the new purchase modal
    
    * site: rework coupon handling on checkout
    
    * site: update the "takeout + bento" coupon
    
    * site: update wording on price tiers
    
    * site: animations for purchase modal

--- apps/site/hooks/useTakeoutStore.tsx
+++ apps/site/hooks/useTakeoutStore.tsx
@@ -1,15 +1,14 @@
 import { createUseStore } from '@tamagui/use-store'
-import type Stripe from 'stripe'
 
 class TakeoutStore {
   showPurchase = false
-  showFaq = false
-  showAgreement = false
-  showPolicies = false
-  promoInputIsOpen = false
-  appliedCoupon: Stripe.Coupon | null = null
-  appliedPromoCode: string | null = null
-  selectedProductsIds: string[] = []
+  showTakeoutFaq = false
+  showTakeoutAgreement = false
+  showBentoFaq = false
+  showBentoAgreement = false
+  showTakeoutPolicies = false
+  showBentoPolicies = false
+  disableAutomaticDiscount = false
 }
 
 export const useTakeoutStore = createUseStore(TakeoutStore)
