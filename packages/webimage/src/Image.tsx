47fbf9da1f956adf105e9a10a9304d3192cc89d6
Ehsan Sarshar <ehsan.herai@gmail.com>
2024-04-02 01:36:40 +0430

    fix(webimage): better implementation

--- packages/webimage/src/Image.tsx
+++ packages/webimage/src/Image.tsx
@@ -1,99 +1,83 @@
 import { isWeb } from '@tamagui/constants'
 import type { StackProps } from '@tamagui/core'
-import { View, styled } from '@tamagui/core'
-import { forwardRef } from 'react'
+import { Stack, View, getTokenForKey, getTokenValue, styled } from '@tamagui/core'
+import { HTMLProps, forwardRef } from 'react'
 import { Image as RNImage } from 'react-native'
 
 import type { ImageSourcePropType, ImageProps as RNImageProps } from 'react-native'
 
-export type ImageProps = Partial<Omit<RNImageProps, keyof StackProps | 'source'>> &
-  StackProps & {
-    /**
-     * @deprecated
-     */
-    source?: ImageSourcePropType
-  }
+// export type ImageProps = Partial<Omit<RNImageProps, keyof StackProps | 'source'>> &
+//   StackProps & {
+//     /**
+//      * @deprecated
+//      */
+//     source?: ImageSourcePropType
+//   }
 
-type RNImageType = typeof RNImage
+// type RNImageType = typeof RNImage
 
-type ImageType = React.FC<ImageProps> & {
-  getSize: RNImageType['getSize']
-  getSizeWithHeaders: RNImageType['getSizeWithHeaders']
-  prefetch: RNImageType['prefetch']
-  prefetchWithMetadata: RNImageType['prefetchWithMetadata']
-  abortPrefetch: RNImageType['abortPrefetch']
-  queryCache: RNImageType['queryCache']
-}
+// type ImageType = React.FC<ImageProps> & {
+//   getSize: RNImageType['getSize']
+//   getSizeWithHeaders: RNImageType['getSizeWithHeaders']
+//   prefetch: RNImageType['prefetch']
+//   prefetchWithMetadata: RNImageType['prefetchWithMetadata']
+//   abortPrefetch: RNImageType['abortPrefetch']
+//   queryCache: RNImageType['queryCache']
+// }
 
 const StyledImage = styled(isWeb ? View : RNImage, {
   name: 'Image',
+  ...(isWeb && {
+    tag: 'img',
+  }),
 })
+
+type ImageProps = StackProps & Omit<HTMLImageElement, 'width' | 'height'>
 
 let hasWarned = false
 
-export const Image = StyledImage.styleable<{ cat: boolean }>(
-  forwardRef((inProps, ref) => {
-    // if (process.env.NODE_ENV === 'development') {
-    //   if (typeof src === 'string') {
-    //     if (
-    //       (typeof props.width === 'string' && props.width[0] !== '$') ||
-    //       (typeof props.height === 'string' && props.height[0] !== '$')
-    //     ) {
-    //       if (!hasWarned) {
-    //         hasWarned = true
-    //         console.warn(
-    //           `React Native expects a numerical width/height. If you want to use a percent you must define the "source" prop with width, height, and uri.`
-    //         )
-    //       }
-    //     }
-    //   }
-    // }
+export const Image = StyledImage.styleable<Partial<ImageProps>>((inProps, ref) => {
+  const { src, alt, srcset, referrerPolicy, crossOrigin, width, height, ...webProps } =
+    inProps
+  const bothPlatfromProps = {
+    src,
+    alt,
+    srcset,
+    referrerPolicy,
+    crossOrigin,
+    width,
+    height,
+  }
+  let nativeOnlyProps: RNImageProps | undefined
+  if (process.env.TAMAGUI_TARGET === 'native') {
+    nativeOnlyProps = {
+      source: {
+        uri: src,
+        width:
+          typeof width === 'string' && width[0] === '$'
+            ? getTokenValue(width as any)
+            : width,
+        height:
+          typeof height === 'string' && height[0] === '$'
+            ? getTokenValue(height as any)
+            : height,
+      },
+    }
+  }
 
-    // let finalSource =
-    //   typeof src === 'string'
-    //     ? { uri: src, ...(isWeb && { width: props.width, height: props.height }) }
-    //     : source ?? src
+  return (
+    <StyledImage
+      {...(process.env.TAMAGUI_TARGET === 'web' && webProps)}
+      {...(nativeOnlyProps as any)}
+      ref={ref}
+      {...bothPlatfromProps}
+    />
+  )
+})
 
-    // if (finalSource && typeof finalSource === 'object') {
-    //   if (process.env.TAMAGUI_TARGET === 'native') {
-    //     // fix: normalize import style images
-    //     if (!Array.isArray(finalSource)) {
-    //       if (typeof finalSource.uri === 'number') {
-    //         finalSource = finalSource.uri
-    //         if (source && typeof source === 'object' && !Array.isArray(source)) {
-    //           // @ts-ignore
-    //           style.width ??= source.width
-    //           // @ts-ignore
-    //           style.height ??= source.height
-    //         }
-    //       }
-    //     }
-    //   }
-
-    //   // require compat across native/web
-    //   if (finalSource['default']) {
-    //     finalSource = finalSource['default']
-    //   }
-    // }
-
-    // must set defaultSource to allow SSR, default it to the same as src
-
-    return (
-      <StyledImage
-        // ref={ref}
-        {...inProps}
-
-        // source={finalSource} style={style}
-
-        // {...(rest as any)}
-      />
-    )
-  })
-) as any as ImageType
-
-Image.getSize = RNImage.getSize
-Image.getSizeWithHeaders = RNImage.getSizeWithHeaders
-Image.prefetch = RNImage.prefetch
-Image.prefetchWithMetadata = RNImage.prefetchWithMetadata
-Image.abortPrefetch = RNImage.abortPrefetch
-Image.queryCache = RNImage.queryCache
+// Image.getSize = RNImage.getSize
+// Image.getSizeWithHeaders = RNImage.getSizeWithHeaders
+// Image.prefetch = RNImage.prefetch
+// Image.prefetchWithMetadata = RNImage.prefetchWithMetadata
+// Image.abortPrefetch = RNImage.abortPrefetch
+// Image.queryCache = RNImage.queryCache
