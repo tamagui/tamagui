7e17d42444c3cf9c7934827c84a5098ba9dab3cd
Ehsan Sarshar <ehsan.herai@gmail.com>
2024-04-04 22:23:56 +0430

    fix(Image): remove unnecessary forwardRef

--- packages/image/src/Image.tsx
+++ packages/image/src/Image.tsx
@@ -49,58 +49,56 @@ type ImageType = React.FC<ImageProps> & {
 
 let hasWarned = false
 
-export const Image = StyledImage.styleable<ImageProps>(
-  forwardRef((inProps, ref) => {
-    const [props, style] = usePropsAndStyle(inProps)
-    const { src, source, ...rest } = props
+export const Image = StyledImage.styleable<ImageProps>((inProps, ref) => {
+  const [props, style] = usePropsAndStyle(inProps)
+  const { src, source, ...rest } = props
 
-    if (process.env.NODE_ENV === 'development') {
-      if (typeof src === 'string') {
-        if (
-          (typeof props.width === 'string' && props.width[0] !== '$') ||
-          (typeof props.height === 'string' && props.height[0] !== '$')
-        ) {
-          if (!hasWarned) {
-            hasWarned = true
-            console.warn(
-              `React Native expects a numerical width/height. If you want to use a percent you must define the "source" prop with width, height, and uri.`
-            )
+  if (process.env.NODE_ENV === 'development') {
+    if (typeof src === 'string') {
+      if (
+        (typeof props.width === 'string' && props.width[0] !== '$') ||
+        (typeof props.height === 'string' && props.height[0] !== '$')
+      ) {
+        if (!hasWarned) {
+          hasWarned = true
+          console.warn(
+            `React Native expects a numerical width/height. If you want to use a percent you must define the "source" prop with width, height, and uri.`
+          )
+        }
+      }
+    }
+  }
+
+  let finalSource =
+    typeof src === 'string'
+      ? { uri: src, ...(isWeb && { width: props.width, height: props.height }) }
+      : source ?? src
+
+  if (finalSource && typeof finalSource === 'object') {
+    if (process.env.TAMAGUI_TARGET === 'native') {
+      // fix: normalize import style images
+      if (!Array.isArray(finalSource)) {
+        if (typeof finalSource.uri === 'number') {
+          finalSource = finalSource.uri
+          if (source && typeof source === 'object' && !Array.isArray(source)) {
+            // @ts-ignore
+            style.width ??= source.width
+            // @ts-ignore
+            style.height ??= source.height
           }
         }
       }
     }
 
-    let finalSource =
-      typeof src === 'string'
-        ? { uri: src, ...(isWeb && { width: props.width, height: props.height }) }
-        : source ?? src
-
-    if (finalSource && typeof finalSource === 'object') {
-      if (process.env.TAMAGUI_TARGET === 'native') {
-        // fix: normalize import style images
-        if (!Array.isArray(finalSource)) {
-          if (typeof finalSource.uri === 'number') {
-            finalSource = finalSource.uri
-            if (source && typeof source === 'object' && !Array.isArray(source)) {
-              // @ts-ignore
-              style.width ??= source.width
-              // @ts-ignore
-              style.height ??= source.height
-            }
-          }
-        }
-      }
-
-      // require compat across native/web
-      if (finalSource['default']) {
-        finalSource = finalSource['default']
-      }
+    // require compat across native/web
+    if (finalSource['default']) {
+      finalSource = finalSource['default']
     }
+  }
 
-    // must set defaultSource to allow SSR, default it to the same as src
-    return <StyledImage ref={ref} source={finalSource} style={style} {...(rest as any)} />
-  })
-) as any as ImageType
+  // must set defaultSource to allow SSR, default it to the same as src
+  return <StyledImage ref={ref} source={finalSource} style={style} {...(rest as any)} />
+}) as any as ImageType
 
 Image.getSize = RNImage.getSize
 Image.getSizeWithHeaders = RNImage.getSizeWithHeaders
