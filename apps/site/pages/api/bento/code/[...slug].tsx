23e55a0a21d046e3f177ee0a0b6adf134e8245bb
ROFI <rofi@Rodrigos-MacBook-Pro.local>
2024-04-11 18:17:01 -0400

    bento: add apiOssBentoRoute to [...slug] endpoint handler

--- apps/site/pages/api/bento/code/[...slug].tsx
+++ apps/site/pages/api/bento/code/[...slug].tsx
@@ -1,41 +1,44 @@
+import { apiOssBentoRoute } from '@lib/apiOssBentoRoute'
 import { HandledResponseTermination, apiRoute } from '@lib/apiRoute'
 import { authorizeUserAccess } from '@lib/authorizeUserAccess'
 import { protectApiRoute } from '@lib/protectApiRoute'
 import { supabaseAdmin } from '@lib/supabaseAdmin'
 
-const handler = apiRoute(async (req, res) => {
-  if (process.env.NODE_ENV === 'production') {
-    const { supabase } = await protectApiRoute({ req, res })
-    await authorizeUserAccess(
-      {
-        req,
-        res,
-        supabase,
-      },
-      {
-        checkForBentoAccess: true,
-      }
-    )
-  }
-  const slugsArray = Array.isArray(req.query.slug)
-    ? req.query.slug
-    : typeof req.query.slug === 'string'
-      ? [req.query.slug]
-      : []
+const handler = apiOssBentoRoute(
+  apiRoute(async (req, res) => {
+    if (process.env.NODE_ENV === 'production') {
+      const { supabase } = await protectApiRoute({ req, res })
+      await authorizeUserAccess(
+        {
+          req,
+          res,
+          supabase,
+        },
+        {
+          checkForBentoAccess: true,
+        }
+      )
+    }
+    const slugsArray = Array.isArray(req.query.slug)
+      ? req.query.slug
+      : typeof req.query.slug === 'string'
+        ? [req.query.slug]
+        : []
 
-  const codePath = slugsArray.join('/')
+    const codePath = slugsArray.join('/')
 
-  const fileResult = await supabaseAdmin.storage
-    .from('bento')
-    .download(`merged/${codePath}.tsx`)
-  if (fileResult.error) {
-    console.error(fileResult.error)
-    res.status(404).json({ message: 'Not found' })
-    throw new HandledResponseTermination(`File ${codePath} not found`)
-  }
+    const fileResult = await supabaseAdmin.storage
+      .from('bento')
+      .download(`merged/${codePath}.tsx`)
+    if (fileResult.error) {
+      console.error(fileResult.error)
+      res.status(404).json({ message: 'Not found' })
+      throw new HandledResponseTermination(`File ${codePath} not found`)
+    }
 
-  res.setHeader('Content-Type', 'text/plain')
-  return res.send(await fileResult.data.text())
-})
+    res.setHeader('Content-Type', 'text/plain')
+    return res.send(await fileResult.data.text())
+  })
+)
 
 export default handler
