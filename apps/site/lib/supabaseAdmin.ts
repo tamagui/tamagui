10ddfb9dd3b2f993e8d2fe87e63b01e8a9cf1b9c
ROFI <rofi@Rodrigos-MacBook-Pro.local>
2024-04-11 18:12:27 -0400

    bento: add getBentoCode fn to supabaseAdmin module

--- apps/site/lib/supabaseAdmin.ts
+++ apps/site/lib/supabaseAdmin.ts
@@ -1,21 +1,32 @@
-import type { SupabaseClient } from '@supabase/supabase-js'
-import { createClient } from '@supabase/supabase-js'
-import type { Price, Product } from 'site-types'
-import type Stripe from 'stripe'
+import type { SupabaseClient } from "@supabase/supabase-js";
+import { createClient } from "@supabase/supabase-js";
+import type { Price, Product } from "site-types";
+import type Stripe from "stripe";
 
-import { sendProductPurchaseEmail } from './email'
-import { toDateTime } from './helpers'
-import { stripe } from './stripe'
-import type { Database } from './supabase-types'
+import { sendProductPurchaseEmail } from "./email";
+import { toDateTime } from "./helpers";
+import { stripe } from "./stripe";
+import type { Database } from "./supabase-types";
 
 // Note: supabaseAdmin uses the SERVICE_ROLE_KEY which you must only use in a secure server-side context
 // as it has admin priviliges and overwrites RLS policies!
 export const supabaseAdmin = process.env.NEXT_PUBLIC_SUPABASE_URL
   ? createClient<Database>(
-      process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-      process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+      process.env.NEXT_PUBLIC_SUPABASE_URL || "",
+      process.env.SUPABASE_SERVICE_ROLE_KEY || ""
     )
-  : ((() => {}) as any as SupabaseClient<Database>)
+  : ((() => {}) as any as SupabaseClient<Database>);
+
+export const getBentoCode = async (codePath: string) => {
+  const { data, error } = await supabaseAdmin.storage
+    .from("bento")
+    .download(`merged/${codePath}.tsx`);
+  if (error) {
+    console.error(error);
+    throw new Error(`Error getting bento code for ${codePath}`);
+  }
+  return data.text();
+};
 
 export const upsertProductRecord = async (product: Stripe.Product) => {
   const productData: Product = {
@@ -25,23 +36,24 @@ export const upsertProductRecord = async (product: Stripe.Product) => {
     description: product.description ?? undefined,
     image: product.images?.[0] ?? null,
     metadata: product.metadata,
-  }
+  };
 
-  const { error } = await supabaseAdmin.from('products').upsert([productData])
-  if (error) throw error
-  console.info(`Product inserted/updated: ${product.id}`)
-}
+  const { error } = await supabaseAdmin.from("products").upsert([productData]);
+  if (error) throw error;
+  console.info(`Product inserted/updated: ${product.id}`);
+};
 
-export const deleteProductRecord = async (id: Stripe.Product['id']) => {
-  const { error } = await supabaseAdmin.from('products').delete().eq('id', id)
-  if (error) throw error
-  console.info(`Product deleted: ${id}`)
-}
+export const deleteProductRecord = async (id: Stripe.Product["id"]) => {
+  const { error } = await supabaseAdmin.from("products").delete().eq("id", id);
+  if (error) throw error;
+  console.info(`Product deleted: ${id}`);
+};
 
 export const upsertPriceRecord = async (price: Stripe.Price) => {
   const priceData: Price = {
     id: price.id,
-    product_id: typeof price.product === 'string' ? price.product : price.product.id,
+    product_id:
+      typeof price.product === "string" ? price.product : price.product.id,
     active: price.active,
     currency: price.currency,
     description: price.nickname ?? undefined,
@@ -51,54 +63,55 @@ export const upsertPriceRecord = async (price: Stripe.Price) => {
     interval_count: price.recurring?.interval_count,
     trial_period_days: price.recurring?.trial_period_days,
     metadata: price.metadata,
-  }
+  };
 
   // @ts-expect-error
-  const { error } = await supabaseAdmin.from('prices').upsert([priceData])
-  if (error) throw error
-  console.info(`Price inserted/updated: ${price.id}`)
+  const { error } = await supabaseAdmin.from("prices").upsert([priceData]);
+  if (error) throw error;
+  console.info(`Price inserted/updated: ${price.id}`);
 
   // we call this to create associated subscription prices upfront - instead of creating it on the fly when someone purchases a one-time price.
-  await getOrCreateRenewalPriceId(price)
-}
+  await getOrCreateRenewalPriceId(price);
+};
 
-export const deletePriceRecord = async (id: Stripe.Price['id']) => {
-  const { error } = await supabaseAdmin.from('prices').delete().eq('id', id)
-  if (error) throw error
-  console.info(`Price deleted: ${id}`)
-}
+export const deletePriceRecord = async (id: Stripe.Price["id"]) => {
+  const { error } = await supabaseAdmin.from("prices").delete().eq("id", id);
+  if (error) throw error;
+  console.info(`Price deleted: ${id}`);
+};
 
 export const createOrRetrieveCustomer = async ({
   email,
   uuid,
 }: {
-  email: string
-  uuid: string
+  email: string;
+  uuid: string;
 }) => {
   const { data, error } = await supabaseAdmin
-    .from('customers')
-    .select('stripe_customer_id')
-    .eq('id', uuid)
-    .single()
+    .from("customers")
+    .select("stripe_customer_id")
+    .eq("id", uuid)
+    .single();
   if (error) {
     // No customer record found, let's create one.
-    const customerData: { metadata: { supabaseUUID: string }; email?: string } = {
-      metadata: {
-        supabaseUUID: uuid,
-      },
-    }
-    if (email) customerData.email = email
-    const customer = await stripe.customers.create(customerData)
+    const customerData: { metadata: { supabaseUUID: string }; email?: string } =
+      {
+        metadata: {
+          supabaseUUID: uuid,
+        },
+      };
+    if (email) customerData.email = email;
+    const customer = await stripe.customers.create(customerData);
     // Now insert the customer ID into our Supabase mapping table.
     const { error: supabaseError } = await supabaseAdmin
-      .from('customers')
-      .insert([{ id: uuid, stripe_customer_id: customer.id }])
-    if (supabaseError) throw supabaseError
-    console.info(`New customer created and inserted for ${uuid}.`)
-    return customer.id
+      .from("customers")
+      .insert([{ id: uuid, stripe_customer_id: customer.id }]);
+    if (supabaseError) throw supabaseError;
+    console.info(`New customer created and inserted for ${uuid}.`);
+    return customer.id;
   }
-  if (data) return data.stripe_customer_id
-}
+  if (data) return data.stripe_customer_id;
+};
 
 /**
  * Copies the billing details from the payment method to the customer object.
@@ -108,20 +121,20 @@ const copyBillingDetailsToCustomer = async (
   payment_method: Stripe.PaymentMethod
 ) => {
   //Todo: check this assertion
-  const customer = payment_method.customer as string
-  const { name, phone, address } = payment_method.billing_details
-  if (!name || !phone || !address) return
+  const customer = payment_method.customer as string;
+  const { name, phone, address } = payment_method.billing_details;
+  if (!name || !phone || !address) return;
   //@ts-ignore
-  await stripe.customers.update(customer, { name, phone, address })
+  await stripe.customers.update(customer, { name, phone, address });
   const { error } = await supabaseAdmin
-    .from('users')
+    .from("users")
     .update({
       // billing_address: address,
       // payment_method: payment_method[payment_method.type],
     })
-    .eq('id', uuid)
-  if (error) throw error
-}
+    .eq("id", uuid);
+  if (error) throw error;
+};
 
 export const manageSubscriptionStatusChange = async (
   subscriptionId: string,
@@ -130,19 +143,19 @@ export const manageSubscriptionStatusChange = async (
 ) => {
   // Get customer's UUID from mapping table.
   const { data: customerData, error: noCustomerError } = await supabaseAdmin
-    .from('customers')
-    .select('id')
-    .eq('stripe_customer_id', customerId)
-    .single()
+    .from("customers")
+    .select("id")
+    .eq("stripe_customer_id", customerId)
+    .single();
 
-  if (noCustomerError) throw noCustomerError
+  if (noCustomerError) throw noCustomerError;
 
-  const { id: uuid } = customerData || {}
+  const { id: uuid } = customerData || {};
   const subscription = await stripe.subscriptions.retrieve(subscriptionId, {
-    expand: ['default_payment_method'],
-  })
+    expand: ["default_payment_method"],
+  });
 
-  const { error } = await supabaseAdmin.from('subscriptions').upsert([
+  const { error } = await supabaseAdmin.from("subscriptions").upsert([
     {
       id: subscription.id,
       user_id: uuid,
@@ -175,23 +188,27 @@ export const manageSubscriptionStatusChange = async (
         ? (toDateTime(subscription.trial_end) as unknown as string)
         : null,
     },
-  ])
-  if (error) throw error
+  ]);
+  if (error) throw error;
   const { error: deletionError } = await supabaseAdmin
-    .from('subscription_items')
+    .from("subscription_items")
     .delete()
-    .eq('subscription_id', subscription.id)
-  if (deletionError) throw deletionError
-  const { error: insertionError } = await supabaseAdmin.from('subscription_items').insert(
-    subscription.items.data.map((item) => ({
-      id: item.id,
-      subscription_id: subscription.id,
-      price_id: typeof item.price === 'string' ? item.price : item.price.id,
-    }))
-  )
-  if (insertionError) throw insertionError
+    .eq("subscription_id", subscription.id);
+  if (deletionError) throw deletionError;
+  const { error: insertionError } = await supabaseAdmin
+    .from("subscription_items")
+    .insert(
+      subscription.items.data.map((item) => ({
+        id: item.id,
+        subscription_id: subscription.id,
+        price_id: typeof item.price === "string" ? item.price : item.price.id,
+      }))
+    );
+  if (insertionError) throw insertionError;
 
-  console.info(`Inserted/updated subscription [${subscription.id}] for user [${uuid}]`)
+  console.info(
+    `Inserted/updated subscription [${subscription.id}] for user [${uuid}]`
+  );
   // For a new subscription copy the billing details to the customer object.
   // NOTE: This is a costly operation and should happen at the very end.
   if (createAction && subscription.default_payment_method && uuid) {
@@ -199,7 +216,7 @@ export const manageSubscriptionStatusChange = async (
     await copyBillingDetailsToCustomer(
       uuid,
       subscription.default_payment_method as Stripe.PaymentMethod
-    )
+    );
   }
 
   // legacy way of handling 50% renewals:
@@ -211,59 +228,66 @@ export const manageSubscriptionStatusChange = async (
   // }
 
   if (createAction) {
-    const user = await supabaseAdmin.auth.admin.getUserById(customerData.id)
+    const user = await supabaseAdmin.auth.admin.getUserById(customerData.id);
 
     if (user.error) {
-      throw user.error
+      throw user.error;
     }
-    const email = user.data.user.email
+    const email = user.data.user.email;
     if (!email) {
-      throw new Error(`No email found for user ${user.data.user.id}`)
+      throw new Error(`No email found for user ${user.data.user.id}`);
     }
 
     const userModel = await supabaseAdmin
-      .from('users')
-      .select('id, full_name')
-      .eq('id', user.data.user.id)
-      .single()
+      .from("users")
+      .select("id, full_name")
+      .eq("id", user.data.user.id)
+      .single();
 
     if (userModel.error) {
-      throw userModel.error
+      throw userModel.error;
     }
 
     const subscribedProducts = await Promise.all(
       subscription.items.data.map((item) =>
         stripe.products.retrieve(item.price.product as string)
       )
-    )
-    const userName = userModel.data.full_name ?? email.split('@').shift()!
+    );
+    const userName = userModel.data.full_name ?? email.split("@").shift()!;
     const includesTakeoutStarter = subscribedProducts.some(
-      (product) => product.metadata.slug === 'universal-starter'
-    )
+      (product) => product.metadata.slug === "universal-starter"
+    );
     if (includesTakeoutStarter) {
       await sendProductPurchaseEmail(email, {
         name: userName,
-        product_name: 'Takeout',
-      })
-      console.info(`Takeout purchase email request sent to Postmark for ${email}`)
+        product_name: "Takeout",
+      });
+      console.info(
+        `Takeout purchase email request sent to Postmark for ${email}`
+      );
     }
     const includesBento = subscribedProducts.some(
-      (product) => product.metadata.slug === 'bento'
-    )
+      (product) => product.metadata.slug === "bento"
+    );
     if (includesBento) {
       await sendProductPurchaseEmail(email, {
         name: userName,
-        product_name: 'Bento',
-      })
-      console.info(`Bento purchase email request sent to Postmark for ${email}`)
+        product_name: "Bento",
+      });
+      console.info(
+        `Bento purchase email request sent to Postmark for ${email}`
+      );
     }
   }
-}
+};
 
 export async function deleteSubscriptionRecord(sub: Stripe.Subscription) {
-  const { error } = await supabaseAdmin.from('subscriptions').delete().eq('id', sub.id)
-  if (error) throw error
-  console.error(`Deleted subscription: ${sub.id}`)
+  const { error } = await supabaseAdmin
+    .from("subscriptions")
+    .delete()
+    .eq("id", sub.id);
+  if (error) throw error;
+  console.error(`Deleted subscription: ${sub.id}`);
 }
 
 export async function addRenewalSubscription(
@@ -271,65 +295,69 @@ export async function addRenewalSubscription(
   options?: { toltReferral?: string }
 ) {
   const session = await stripe.checkout.sessions.retrieve(sessionFromEvent.id, {
-    expand: ['line_items'],
-  })
+    expand: ["line_items"],
+  });
 
   const prices = await Promise.all(
     session.line_items!.data.map((lineItem) =>
       stripe.prices.retrieve(lineItem.price!.id, {
-        expand: ['product'],
+        expand: ["product"],
       })
     )
-  )
+  );
 
   const customerId =
-    typeof session.customer === 'string' ? session.customer : session.customer!.id
+    typeof session.customer === "string"
+      ? session.customer
+      : session.customer!.id;
 
   const { data: userData, error: userError } = await supabaseAdmin
-    .from('customers')
-    .select('*')
-    .eq('stripe_customer_id', customerId)
-    .single()
+    .from("customers")
+    .select("*")
+    .eq("stripe_customer_id", customerId)
+    .single();
   if (userError) {
-    throw userError
+    throw userError;
   }
-  const userId = userData.id
+  const userId = userData.id;
 
-  const renewalPriceIds: string[] = []
+  const renewalPriceIds: string[] = [];
   for (const price of prices) {
     if (price.metadata.is_lifetime) {
       // create ownership record for the user if it's for lifetime
-      await supabaseAdmin.from('product_ownership').insert({
+      await supabaseAdmin.from("product_ownership").insert({
         price_id: price.id,
         user_id: userId,
-      })
-      continue
+      });
+      continue;
     }
     // else, we need to:
     // 1. get or create a recurring price
     // 2. set up a subscription to the price with 1 year of trial so that it starts charging after 1y
     // we add the 1y trial because the user just paid for the first year
-    if (typeof price.product === 'string' || price.product.deleted) {
-      console.warn('no product object - returning')
-      continue
+    if (typeof price.product === "string" || price.product.deleted) {
+      console.warn("no product object - returning");
+      continue;
     }
-    const renewalPriceId = await getOrCreateRenewalPriceId(price)
+    const renewalPriceId = await getOrCreateRenewalPriceId(price);
     if (!renewalPriceId) {
-      console.warn('no has_renewals metadata found - returning')
-      continue
+      console.warn("no has_renewals metadata found - returning");
+      continue;
     }
-    renewalPriceIds.push(renewalPriceId)
+    renewalPriceIds.push(renewalPriceId);
   }
-  console.info('creating the sub...', renewalPriceIds)
+  console.info("creating the sub...", renewalPriceIds);
   if (renewalPriceIds.length > 0) {
-    const toltReferral = options?.toltReferral ?? null
+    const toltReferral = options?.toltReferral ?? null;
 
     const cardPaymentMethods = await stripe.paymentMethods.list({
       customer: customerId,
-      type: 'card',
-    })
-    const paymentMethod = cardPaymentMethods.data[0]
-    const collectionMethod = paymentMethod ? 'charge_automatically' : 'send_invoice'
+      type: "card",
+    });
+    const paymentMethod = cardPaymentMethods.data[0];
+    const collectionMethod = paymentMethod
+      ? "charge_automatically"
+      : "send_invoice";
     const renewalSub = await stripe.subscriptions.create({
       ...(toltReferral && {
         metadata: {
@@ -338,7 +366,7 @@ export async function addRenewalSubscription(
       }),
       customer: customerId,
       collection_method: collectionMethod,
-      ...(collectionMethod === 'charge_automatically'
+      ...(collectionMethod === "charge_automatically"
         ? {
             default_payment_method: paymentMethod.id,
           }
@@ -354,8 +382,8 @@ export async function addRenewalSubscription(
       items: renewalPriceIds.map((id) => ({
         price: id,
       })),
-    })
-    console.info(renewalSub)
+    });
+    console.info(renewalSub);
   }
 }
 
@@ -369,52 +397,53 @@ export async function addRenewalSubscription(
 
 export async function getOrCreateRenewalPriceId(price: Stripe.Price) {
   let product =
-    price.product && typeof price.product === 'object'
+    price.product && typeof price.product === "object"
       ? (price.product as Stripe.Product)
-      : await stripe.products.retrieve(price.product)
+      : await stripe.products.retrieve(price.product);
   if (
     !product.metadata.has_renewals || // this product doesn't need renewal prices
-    price.type === 'recurring' || // this price is already a subscription price - not having this check might cause an infinite loop of creating prices
+    price.type === "recurring" || // this price is already a subscription price - not having this check might cause an infinite loop of creating prices
     price.metadata.is_lifetime // there is no need for creating a subscription as this is a lifetime purchase
   ) {
-    return null
+    return null;
   }
 
-  let renewalPriceId = price.metadata.renewal_price_id
+  let renewalPriceId = price.metadata.renewal_price_id;
   if (!price.metadata.renewal_price_id) {
     const subscriptionPrice = await stripe.prices.create({
-      product: typeof price.product === 'string' ? price.product : price.product.id,
-      currency: 'USD',
+      product:
+        typeof price.product === "string" ? price.product : price.product.id,
+      currency: "USD",
       nickname: `${price.nickname} | Subscription for ${price.id} (Auto-generated)`,
-      recurring: { interval: 'year', interval_count: 1 },
+      recurring: { interval: "year", interval_count: 1 },
       unit_amount: price.unit_amount!, // change this to `unit_amount: price.unit_amount! / 2` for 50% prices (make sure to remove old prices to trigger this)
       metadata: {
         hide_from_lists: 1,
       },
-    })
-    renewalPriceId = subscriptionPrice.id
+    });
+    renewalPriceId = subscriptionPrice.id;
     await stripe.prices.update(price.id, {
       metadata: {
         ...price.metadata,
         renewal_price_id: subscriptionPrice.id,
       },
-    })
+    });
   }
-  return renewalPriceId
+  return renewalPriceId;
 }
 
 export async function populateStripeData() {
   // products
-  const products = await stripe.products.list({ limit: 100 })
+  const products = await stripe.products.list({ limit: 100 });
   for (const product of products.data) {
-    await upsertProductRecord(product)
-    console.info('populated product', product.name)
+    await upsertProductRecord(product);
+    console.info("populated product", product.name);
   }
 
   // prices
-  const prices = await stripe.prices.list({ limit: 100 })
+  const prices = await stripe.prices.list({ limit: 100 });
   for (const price of prices.data) {
-    await upsertPriceRecord(price)
-    console.info('populated price ', price.nickname)
+    await upsertPriceRecord(price);
+    console.info("populated price ", price.nickname);
   }
 }
