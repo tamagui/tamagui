0b3fe423dfc177aeaeff768e0738b7457d1d49f3
natew <natewienert@gmail.com>
2024-04-12 08:00:47 -1000

    fix(sheet): set pointer-events none on inner Frame element not the parent animated frame, so the Sheet.Overlay can accept touch events more consistently

--- packages/sheet/src/SheetImplementationCustom.tsx
+++ packages/sheet/src/SheetImplementationCustom.tsx
@@ -439,7 +439,9 @@ export const SheetImplementationCustom = themeable(
             ref={ref}
             {...panResponder?.panHandlers}
             onLayout={handleAnimationViewLayout}
-            pointerEvents={open && !shouldHideParentSheet ? 'auto' : 'none'}
+            {...((shouldHideParentSheet || !open) && {
+              pointerEvents: 'none',
+            })}
             {...(!isDragging && {
               // @ts-ignore for CSS driver this is necessary to attach the transition
               animation,
