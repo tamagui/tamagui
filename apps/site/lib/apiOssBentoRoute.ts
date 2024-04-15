c43cf14c0837b6aa1e8dbcde7049f7753b385ed8
ROFI <rofi@Rodrigos-MacBook-Pro.local>
2024-04-15 13:44:24 -0400

    bento: remove else clause

--- apps/site/lib/apiOssBentoRoute.ts
+++ apps/site/lib/apiOssBentoRoute.ts
@@ -5,25 +5,19 @@ import { getBentoCode } from './supabaseAdmin'
 
 export function apiOssBentoRoute(handler: NextApiHandler) {
   return async (req, res) => {
-    const slugsArray = Array.isArray(req.query.slug)
-      ? req.query.slug
-      : typeof req.query.slug === 'string'
-        ? [req.query.slug]
-        : []
-
-    const codePath = slugsArray.join('/')
-
     try {
+      const slugsArray = Array.isArray(req.query.slug)
+        ? req.query.slug
+        : typeof req.query.slug === 'string'
+          ? [req.query.slug]
+          : []
+      const codePath = slugsArray.join('/')
       if (!OSS_COMPONENTS.includes(slugsArray[slugsArray.length - 1])) {
-        console.log('not a OSS component')
         return handler(req, res)
-      } else {
-        console.log('it IS a OSS component')
-        const fileResult = await getBentoCode(codePath)
-        res.setHeader('Content-Type', 'text/plain')
-        res.send(fileResult)
-        return
       }
+      const fileResult = await getBentoCode(codePath)
+      res.setHeader('Content-Type', 'text/plain')
+      res.send(fileResult)
     } catch (err) {
       const message = err instanceof Error ? err.message : `${err}`
       console.error(`Error serving API Route: ${message}`, err.stack)
