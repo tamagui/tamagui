ce4f5f954da038aa0948a0d3efa3a51cfdeba7b6
ROFI <rofi@Rodrigos-MacBook-Pro.local>
2024-04-15 12:46:35 -0400

    bento: fix: protectApiRoute was throwing without any catch on the calling code block

--- apps/site/pages/api/bento/code/[...slug].tsx
+++ apps/site/pages/api/bento/code/[...slug].tsx
@@ -5,7 +5,9 @@ import { protectApiRoute } from '@lib/protectApiRoute'
 import { supabaseAdmin } from '@lib/supabaseAdmin'
 
 const handler = apiOssBentoRoute(async (req, res) => {
-  if (process.env.NODE_ENV === 'production') {
+  try {
+    console.log('before authorize access pass', req.query.slug)
+
     const { supabase } = await protectApiRoute({ req, res })
     await authorizeUserAccess(
       {
@@ -17,26 +19,31 @@ const handler = apiOssBentoRoute(async (req, res) => {
         checkForBentoAccess: true,
       }
     )
+    console.log('after authorize access pass')
+
+    const slugsArray = Array.isArray(req.query.slug)
+      ? req.query.slug
+      : typeof req.query.slug === 'string'
+        ? [req.query.slug]
+        : []
+
+    const codePath = slugsArray.join('/')
+
+    const fileResult = await supabaseAdmin.storage
+      .from('bento')
+      .download(`merged/${codePath}.tsx`)
+    if (fileResult.error) {
+      console.error(fileResult.error)
+      res.status(404).json({ message: 'Not found' })
+      throw new HandledResponseTermination(`File ${codePath} not found`)
+    }
+
+    res.setHeader('Content-Type', 'text/plain')
+    return res.send(await fileResult.data.text())
+  } catch (err) {
+    console.error(err)
+    res.status(401).json({ error: err.message })
   }
-  const slugsArray = Array.isArray(req.query.slug)
-    ? req.query.slug
-    : typeof req.query.slug === 'string'
-      ? [req.query.slug]
-      : []
-
-  const codePath = slugsArray.join('/')
-
-  const fileResult = await supabaseAdmin.storage
-    .from('bento')
-    .download(`merged/${codePath}.tsx`)
-  if (fileResult.error) {
-    console.error(fileResult.error)
-    res.status(404).json({ message: 'Not found' })
-    throw new HandledResponseTermination(`File ${codePath} not found`)
-  }
-
-  res.setHeader('Content-Type', 'text/plain')
-  return res.send(await fileResult.data.text())
 })
 
 export default handler
