ad4792aade394130586a9aa1fa8097337e560a27
natew <natewienert@gmail.com>
2024-04-24 22:17:21 -1000

    remove website

--- apps/website/app/components/FrontmatterContext.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-import type { Frontmatter } from '../frontmatter'
-
-export const FrontmatterContext = React.createContext<Frontmatter>({} as any)
--- apps/website/app/lib/octokit.ts
+++
@@ -1,9 +0,0 @@
-import { Octokit } from 'octokit'
-import fetch from 'node-fetch'
-
-export const octokit = new Octokit({
-  auth: process.env.GITHUB_ADMIN_TOKEN,
-  request: {
-    fetch,
-  },
-})
