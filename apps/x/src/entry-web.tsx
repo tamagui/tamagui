154efda576050a630ebaa12496c0ed83c1cbf5b4
natew <natewienert@gmail.com>
2024-05-08 20:47:37 -1000

    fix css

--- apps/x/src/entry-web.tsx
+++ apps/x/src/entry-web.tsx
@@ -8,27 +8,25 @@ const routes = import.meta.glob('../app/**/*.tsx')
 
 export function App({ path }: { path: string }) {
   return (
-    <>
-      <Root
-        routes={routes}
-        path={path}
-        navigationContainerProps={
-          {
-            theme: {
-              dark: true,
-              colors: {
-                primary: 'rgb(0, 122, 255)',
-                background: 'transparent',
-                card: 'rgb(255, 255, 255)',
-                text: 'rgb(28, 28, 30)',
-                border: 'rgb(216, 216, 216)',
-                notification: 'rgb(255, 59, 48)',
-              },
+    <Root
+      routes={routes}
+      path={path}
+      navigationContainerProps={
+        {
+          theme: {
+            dark: true,
+            colors: {
+              primary: 'rgb(0, 122, 255)',
+              background: 'transparent',
+              card: 'rgb(255, 255, 255)',
+              text: 'rgb(28, 28, 30)',
+              border: 'rgb(216, 216, 216)',
+              notification: 'rgb(255, 59, 48)',
             },
-          } as any
-        }
-      />
-    </>
+          },
+        } as any
+      }
+    />
   )
 }
 
