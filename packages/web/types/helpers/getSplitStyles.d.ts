4f3abfa46302a59fdee4c0bc87537554aa9dac82
natew <natewienert@gmail.com>
2024-05-11 08:13:37 -1000

    fix types

--- packages/web/types/helpers/getSplitStyles.d.ts
+++ packages/web/types/helpers/getSplitStyles.d.ts
@@ -1,5 +1,5 @@
 import type { TamaguiComponentState } from '../interfaces/TamaguiComponentState';
-import type { ComponentContextI, DebugProp, GetStyleResult, GetStyleState, SplitStyleProps, StaticConfig, TextStyleProps, ThemeParsed } from '../types';
+import type { ComponentContextI, DebugProp, GetStyleResult, GetStyleState, SplitStyleProps, StaticConfig, TextStyle, ThemeParsed } from '../types';
 export type SplitStyles = ReturnType<typeof getSplitStyles>;
 export type SplitStyleResult = ReturnType<typeof getSplitStyles>;
 type StyleSplitter = (props: {
@@ -7,7 +7,7 @@ type StyleSplitter = (props: {
 }, staticConfig: StaticConfig, theme: ThemeParsed, themeName: string, componentState: TamaguiComponentState, styleProps: SplitStyleProps, parentSplitStyles?: GetStyleResult | null, context?: ComponentContextI, elementType?: string, debug?: DebugProp, skipThemeTokenResolution?: boolean) => GetStyleResult;
 export declare const PROP_SPLIT = "-";
 export declare const getSplitStyles: StyleSplitter;
-export declare const getSubStyle: (styleState: GetStyleState, subKey: string, styleIn: Object, avoidMergeTransform?: boolean) => TextStyleProps;
+export declare const getSubStyle: (styleState: GetStyleState, subKey: string, styleIn: Object, avoidMergeTransform?: boolean) => TextStyle;
 export declare const useSplitStyles: StyleSplitter;
 export {};
 //# sourceMappingURL=getSplitStyles.d.ts.map
