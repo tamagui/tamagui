dca0575830c8612182dfdc082c1933fac80a294d
natew <natewienert@gmail.com>
2024-05-12 07:45:57 -1000

    temp fix for babel-plugin-fully-specified

--- apps/bento/types/components/elements/buttons/DisabledButtons.d.ts
+++
@@ -1,6 +0,0 @@
-/** ------ EXAMPLE ------ */
-export declare function DisabledButtons(): import("react/jsx-runtime").JSX.Element;
-export declare namespace DisabledButtons {
-    var fileName: string;
-}
-//# sourceMappingURL=DisabledButtons.d.ts.map
--- apps/bento/types/components/forms/checkboxes/VerticalCheckboxes.d.ts
+++
@@ -1,6 +0,0 @@
-/** ------ EXAMPLE ------ */
-export declare function VerticalCheckboxes(): import("react/jsx-runtime").JSX.Element;
-export declare namespace VerticalCheckboxes {
-    var fileName: string;
-}
-//# sourceMappingURL=VerticalCheckboxes.d.ts.map
