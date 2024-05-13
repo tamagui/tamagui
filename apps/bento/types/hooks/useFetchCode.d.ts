2024abe0c7cb062414fce8e95ae3c71986bc8f8c
ROFI <rofi@rodrigos-mbp.lan>
2024-05-13 09:08:00 -0400

    bento: type changes missing on merge

--- apps/bento/types/hooks/useFetchCode.d.ts
+++ apps/bento/types/hooks/useFetchCode.d.ts
@@ -3,7 +3,7 @@ type Params = {
 };
 export declare function useFetchCode(params?: Params): {
     data: string | undefined;
-    status: "error" | "loading" | "success" | "idle";
+    status: "error" | "success" | "loading" | "idle";
     fetchData: (url: string) => Promise<void>;
 };
 export {};
