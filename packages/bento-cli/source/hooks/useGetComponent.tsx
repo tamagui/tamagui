0d10f7e7e217b314866ca91573403ccffbd937fd
ROFI <rofi@rodrigos-mbp.lan>
2024-05-15 11:01:23 -0400

    bento: biome fixes

--- packages/bento-cli/source/hooks/useGetComponent.tsx
+++ packages/bento-cli/source/hooks/useGetComponent.tsx
@@ -1,57 +1,57 @@
 // @ts-nocheck
-import { useContext, useEffect, useMemo, useState } from "react";
-import fetch from "node-fetch";
-import useSWR from "swr";
-import { AppContext } from "../commands/index.js";
-import { Octokit } from "octokit";
+import { useContext, useEffect, useMemo, useState } from 'react'
+import fetch from 'node-fetch'
+import useSWR from 'swr'
+import { AppContext } from '../commands/index.js'
+import { Octokit } from 'octokit'
 
 export const useGetComponent = async () => {
-	const { install, tokenStore } = useContext(AppContext);
-	const { access_token } = tokenStore.get("token") ?? {};
-	const [githubData, setGithubData] = useState(null);
+  const { install, tokenStore } = useContext(AppContext)
+  const { access_token } = tokenStore.get?.('token') ?? {}
+  const [githubData, setGithubData] = useState(null)
 
-	useEffect(() => {
-		if (!access_token) return;
-		const octokit = new Octokit({
-			auth: access_token,
-		});
+  useEffect(() => {
+    if (!access_token) return
+    const octokit = new Octokit({
+      auth: access_token,
+    })
 
-		const fetchGithubData = async () => {
-			const { data } = await octokit.rest.users.getAuthenticated();
-			setGithubData(data);
-		};
-		fetchGithubData();
-	}, [access_token]);
+    const fetchGithubData = async () => {
+      const { data } = await octokit.rest.users.getAuthenticated()
+      setGithubData(data)
+    }
+    fetchGithubData()
+  }, [access_token])
 
-	const fetcher = async (url: string) => {
-		const res = await fetch(url, {
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${access_token}`,
-			},
-		});
-		if (!res.ok) {
-			const error = new Error("An error occurred while fetching the data.");
-			error.info = await res.json();
-			error.status = res.status;
-			throw error;
-		}
-		const result = await res.text();
-		return result;
-	};
+  const fetcher = async (url: string) => {
+    const res = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${access_token}`,
+      },
+    })
+    if (!res.ok) {
+      const error = new Error('An error occurred while fetching the data.')
+      error.info = await res.json()
+      error.status = res.status
+      throw error
+    }
+    const result = await res.text()
+    return result
+  }
 
-	const codePath = useMemo(() => {
-		// if (!installComponent) return "";
-		// return `https://tamagui.dev/api/bento/code/${installComponent.category}/${installComponent.categorySection}/${installComponent.fileName}`;
-		// return "https://tamagui.dev/api/bento/code/elements/tables/UsersTable"; //free
-		// return "https://tamagui.dev/api/bento/code/elements/tables/Basic"; //paid
-		return `http://localhost:5005/api/bento/code/${install.installingComponent?.category}/${install.installingComponent?.categorySection}/${install.installingComponent?.fileName}?userGithubId=${githubData?.node_id}`; //paid
-	}, [install,githubData]);
+  const codePath = useMemo(() => {
+    // if (!installComponent) return "";
+    // return `https://tamagui.dev/api/bento/code/${installComponent.category}/${installComponent.categorySection}/${installComponent.fileName}`;
+    // return "https://tamagui.dev/api/bento/code/elements/tables/UsersTable"; //free
+    // return "https://tamagui.dev/api/bento/code/elements/tables/Basic"; //paid
+    return `http://localhost:5005/api/bento/code/${install.installingComponent?.category}/${install.installingComponent?.categorySection}/${install.installingComponent?.fileName}?userGithubId=${githubData?.node_id}` //paid
+  }, [install, githubData])
 
-	const { data, error, isLoading } = useSWR<string>(
-		githubData?.id ? codePath : null,
-		fetcher,
-	);
+  const { data, error, isLoading } = useSWR<string>(
+    githubData?.id ? codePath : null,
+    fetcher
+  )
 
-	return { data, error, isLoading };
-};
+  return { data, error, isLoading }
+}
--- packages/bento-cli/source/hooks/useInstallComponent.tsx
+++ packages/bento-cli/source/hooks/useInstallComponent.tsx
@@ -1,102 +1,98 @@
 // @ts-nocheck
-import { useContext, useEffect } from "react";
-import { useGetComponent } from "./useGetComponent.js";
-import { AppContext } from "../commands/index.js";
-import { mkdirSync, existsSync, promises as fs } from "node:fs";
-import path from "node:path";
-import { set } from "zod";
+import { useContext, useEffect } from 'react'
+import { useGetComponent } from './useGetComponent.js'
+import { AppContext } from '../commands/index.js'
+import { mkdirSync, existsSync, promises as fs } from 'node:fs'
+import path from 'node:path'
+import { set } from 'zod'
 
-const hasAppDir = () => existsSync(path.join(process.cwd(), "app"));
+const hasAppDir = () => existsSync(path.join(process.cwd(), 'app'))
 
-const hasSrcDir = () => existsSync(path.join(process.cwd(), "src"));
+const hasSrcDir = () => existsSync(path.join(process.cwd(), 'src'))
 
 const createUIDir = () =>
-    mkdirSync(path.join(process.cwd(), "packages", "ui"), {
-        recursive: true,
-    });
+  mkdirSync(path.join(process.cwd(), 'packages', 'ui'), {
+    recursive: true,
+  })
 
 const hasPackagesAndUIDir = () => {
-    const packagesDir = path.join(process.cwd(), "packages");
-    const uiDir = path.join(packagesDir, "ui");
-    const srcDir = path.join(uiDir, "src");
-    return existsSync(packagesDir) && existsSync(uiDir) && existsSync(srcDir);
-};
+  const packagesDir = path.join(process.cwd(), 'packages')
+  const uiDir = path.join(packagesDir, 'ui')
+  const srcDir = path.join(uiDir, 'src')
+  return existsSync(packagesDir) && existsSync(uiDir) && existsSync(srcDir)
+}
 
 const getComponentsFromTextFile = (components) => {
-    const startOfTheFileRegex = /\/\*\* START of the file (.+\.tsx) \*\//;
-    const lines = components.split("\n");
-    let accContent = "";
-    let componentName = "";
-    const allComponents: { name: string; content: string }[] = [];
-    lines.forEach((line, index) => {
-        const matchedLine = line.match(startOfTheFileRegex);
-        if (matchedLine) {
-            const fileName = matchedLine[1];
-            if (componentName) {
-                allComponents.push({ name: componentName, content: accContent });
-            }
-            componentName = fileName;
-            accContent = "";
-        } else {
-            accContent += `${line}\n`;
-        }
-    });
-    if (componentName) {
-        allComponents.push({ name: componentName, content: accContent });
-    }
-    return allComponents;
-};
-
-const installComponent = async ({component, setInstall}) => {
-    const components = getComponentsFromTextFile();
-    if (hasPackagesAndUIDir()) {
-        // we need more checks but for now this is enough to test install of components.
-        // check if the components/ui folder exists else create it
-        // missing is to check if the component is present
-        // think of adding later on the --overwrite flag in this piece of the process
-        // install component here
-        await Promise.all(components.map(component =>
-            fs.writeFile(
-                path.join(process.cwd(), "packages", "ui", "src", component.name),
-                component.content
-            )
-        ));
-
-        setInstall((prev) => ({
-            installingComponent: null,
-            installedComponents: [
-                ...prev.installedComponents,
-                install.installingComponent,
-            ],
-        }));
+  const startOfTheFileRegex = /\/\*\* START of the file (.+\.tsx) \*\//
+  const lines = components.split('\n')
+  let accContent = ''
+  let componentName = ''
+  const allComponents: { name: string; content: string }[] = []
+  lines.forEach((line, index) => {
+    const matchedLine = line.match(startOfTheFileRegex)
+    if (matchedLine) {
+      const fileName = matchedLine[1]
+      if (componentName) {
+        allComponents.push({ name: componentName, content: accContent })
+      }
+      componentName = fileName
+      accContent = ''
     } else {
-        //TODO: add support for non monorepo projects
-        console.log("no monorepo no install");
-
-        setInstall((prev) => ({
-            installingComponent: null,
-            installedComponents: [
-                ...prev.installedComponents,
-                install.installingComponent,
-            ],
-        }));
+      accContent += `${line}\n`
     }
-};
+  })
+  if (componentName) {
+    allComponents.push({ name: componentName, content: accContent })
+  }
+  return allComponents
+}
+
+const installComponent = async ({ component, setInstall }) => {
+  console.log('on installComponent fn')
+
+  const components = getComponentsFromTextFile()
+  if (hasPackagesAndUIDir()) {
+    // we need more checks but for now this is enough to test install of components.
+    // check if the components/ui folder exists else create it
+    // missing is to check if the component is present
+    // think of adding later on the --overwrite flag in this piece of the process
+    // install component here
+    await Promise.all(
+      components.map((component) =>
+        fs.writeFile(
+          path.join(process.cwd(), 'packages', 'ui', 'src', component.name),
+          component.content
+        )
+      )
+    )
+
+    setInstall((prev) => ({
+      installingComponent: null,
+      installedComponents: [...prev.installedComponents, install.installingComponent],
+    }))
+  } else {
+    //TODO: add support for non monorepo projects
+    console.log('no monorepo no install')
+
+    setInstall((prev) => ({
+      installingComponent: null,
+      installedComponents: [...prev.installedComponents, install.installingComponent],
+    }))
+  }
+}
 
 export const useInstallComponent = () => {
-    const { install, setInstall } = useContext(AppContext);
-	const {
-		data: component,
-		isLoading: isLoadingComponent,
-		error: errorComponent,
-	} = useGetComponent();
+  const { install, setInstall } = useContext(AppContext)
+  const {
+    data: component,
+    isLoading: isLoadingComponent,
+    error: errorComponent,
+  } = useGetComponent()
 
-    useEffect(() => {
-
-		// if (install.installingComponent && component) {
-		if (component) {
-			installComponent({component, setInstall});
-		}
-	}, [install?.installingComponent]);
-
+  useEffect(() => {
+    // if (install.installingComponent && component) {
+    if (component) {
+      installComponent({ component, setInstall })
+    }
+  }, [install?.installingComponent])
 }
