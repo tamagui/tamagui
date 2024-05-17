58c5b8eeb94fe66b5f95a2fdfafde8b9ef6fc2bb
ROFI <rofi@rodrigos-mbp.lan>
2024-05-16 21:47:00 -0400

    bento: biome format

--- packages/bento-cli/source/commands/add.tsx
+++ packages/bento-cli/source/commands/add.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import { Text } from "ink";
-import zod from "zod";
+import React from 'react'
+import { Text } from 'ink'
+import zod from 'zod'
 
-export const args = zod.tuple([zod.string(), zod.string()]);
+export const args = zod.tuple([zod.string(), zod.string()])
 
 type Props = {
-	args: zod.infer<typeof args>;
-};
+  args: zod.infer<typeof args>
+}
 
 // const categories = ['forms', 'elements', 'shells', 'animations', 'ecommerce', 'user']
 export default function Add({ args }: Props) {
-	// if `bento add` show Select with categories.
-	// then show Select with category sections.
-	// if `bento add <category>` show Select with category sections.
-	return (
-		<Text>
-			Adding now {args[0]} to {args[1]}
-		</Text>
-	);
+  // if `bento add` show Select with categories.
+  // then show Select with category sections.
+  // if `bento add <category>` show Select with category sections.
+  return (
+    <Text>
+      Adding now {args[0]} to {args[1]}
+    </Text>
+  )
 }
