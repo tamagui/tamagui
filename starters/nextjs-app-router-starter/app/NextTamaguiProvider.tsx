bca9664df70926a713f7554adaf24f0cb47fdb67
Simun Kordis <kordis.simun@gmail.com>
2024-05-21 14:19:26 +0200

    updated next tamagui provider

--- starters/nextjs-app-router-starter/app/NextTamaguiProvider.tsx
+++ starters/nextjs-app-router-starter/app/NextTamaguiProvider.tsx
@@ -11,6 +11,7 @@ import { TamaguiProvider, createTamagui } from 'tamagui'
 import { config } from '@tamagui/config/v3'
 
 // you usually export this from a tamagui.config.ts file
+// import tamaguiConfig from './tamagui.config.ts';
 const tamaguiConfig = createTamagui(config)
 
 // make TypeScript type everything based on your config
