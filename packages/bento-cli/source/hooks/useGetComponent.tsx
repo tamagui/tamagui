27b0a0787ae1c4fbe09187c8c25fbd698da30025
Rodrigo Figueroa <rodrigofigueroa.name@gmail.com>
2024-05-21 21:52:57 -0400

    bento: refactor: /code endpoint  (#2663)
    * bento: force return after response to make middleware to stop propagating
    
    * bento: refactor of bento code api handler
    
    * bento: fix: add missing env to .env.local that was breaking /code request
    
    * bento: add try catch to jwt sign step
    
    * bento: biome format and add await to installComponent call
    
    ---------
    
    Co-authored-by: ROFI <rofi@rodrigos-mbp.lan>
    Co-authored-by: ROFI <rofi@Rodrigos-MacBook-Pro.local>

--- packages/bento-cli/source/hooks/useGetComponent.tsx
+++ packages/bento-cli/source/hooks/useGetComponent.tsx
@@ -1,69 +1,57 @@
 // @ts-nocheck
-import { useContext, useEffect, useMemo, useState } from 'react'
-import fetch from 'node-fetch'
-import useSWR from 'swr'
-import { AppContext } from '../commands/index.js'
-import { Octokit } from 'octokit'
-import { installComponent } from './useInstallComponent.js'
-
+import { useContext, useEffect, useMemo, useState } from 'react';
+import fetch from 'node-fetch';
+import useSWR from 'swr';
+import { AppContext } from '../commands/index.js';
+import { Octokit } from 'octokit';
+import { installComponent } from './useInstallComponent.js';
 export const useGetComponent = async () => {
-  const { install, tokenStore, setInstall } = useContext(AppContext)
-  const { access_token } = tokenStore.get?.('token') ?? {}
-  const [githubData, setGithubData] = useState(null)
-
+  const { install, tokenStore, setInstall } = useContext(AppContext);
+  const { access_token } = tokenStore.get?.('token') ?? {};
+  const [githubData, setGithubData] = useState(null);
   useEffect(() => {
     if (!access_token) {
-      return
+      return;
     }
     const octokit = new Octokit({
       auth: access_token,
-    })
-
+    });
     const fetchGithubData = async () => {
-      const { data } = await octokit.rest.users.getAuthenticated()
-      setGithubData(data)
-    }
+      const { data } = await octokit.rest.users.getAuthenticated();
+      setGithubData(data);
+    };
+    fetchGithubData();
+  }, [access_token, install.installingComponent]);
 
-    fetchGithubData()
-  }, [access_token, install.installingComponent])
-
-  const fetcher = async (url: string) => {
+  const fetcher = async (url) => {
     const res = await fetch(url, {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${access_token}`,
       },
-    })
+    });
     if (!res.ok) {
-      const error = new Error('An error occurred while fetching the data.')
-      error.info = await res.json()
-      error.status = res.status
-      throw error
+      const error = new Error('An error occurred while fetching the data.');
+      error.info = await res.json();
+      error.status = res.status;
+      throw error;
     }
-    const result = await res.text()
-    return result
-  }
-
+    const result = await res.text();
+    return result;
+  };
   const codePath = useMemo(() => {
     // const BASE_URL =
     //   process.env.NODE_ENV === 'production'
     //     ? 'http://tamagui.dev'
     //     : 'http://localhost:5005'
-
-
-    const BASE_URL = 'https://tamagui.dev'
-    return `${BASE_URL}/api/bento/code/${install.installingComponent?.category}/${install.installingComponent?.categorySection}/${install.installingComponent?.fileName}?userGithubId=${githubData?.node_id}` //paid
-  }, [install, githubData])
-
-  const { data, error, isLoading } = useSWR<string>(
-    githubData?.id && install.installingComponent ? codePath : null,
-    fetcher
-  )
-
-  if (error) console.log('error:', error)
-
-  if (data) {
-    installComponent({ component: data, setInstall, install })
+    const BASE_URL = 'http://tamagui.dev';
+    return `${BASE_URL}/api/bento/code/${install.installingComponent?.category}/${install.installingComponent?.categorySection}/${install.installingComponent?.fileName}?userGithubId=${githubData?.node_id}`; //paid
+  }, [install, githubData]);
+  const { data, error, isLoading } = useSWR(githubData?.id && install.installingComponent ? codePath : null, fetcher);
+  if (error && error.info?.error?.includes('not authenticated')) {
   }
-  return { data, error, isLoading }
-}
+  if (data) {
+    await installComponent({ component: data, setInstall, install });
+  }
+  return { data, error, isLoading };
+};
