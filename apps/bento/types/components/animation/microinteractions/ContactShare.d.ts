5db6ba13cb31978cf0d394d42387959fc550a967
Šimun Kordiš <kordis.simun@gmail.com>
2024-06-05 00:16:09 +0200

    native fixes

---
+++ apps/bento/types/components/animation/microinteractions/ContactShare.d.ts
@@ -0,0 +1,6 @@
+/** ------ EXAMPLE ------ */
+export declare const ContactShare: {
+    (): import("react/jsx-runtime").JSX.Element;
+    fileName: string;
+};
+//# sourceMappingURL=ContactShare.d.ts.map
