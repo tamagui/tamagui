c29a22812ec7b17131748f07fe1b72f529c2c532
natew <natewienert@gmail.com>
2024-06-07 07:48:01 -1000

    chore: upgrade floating-ui versions

--- apps/x/app/test.tsx
+++ apps/x/app/test.tsx
@@ -1,5 +1,39 @@
 import { TooltipDemo } from '@tamagui/demos'
+import { useEffect } from 'react'
+import { Button, Paragraph, Tooltip } from 'tamagui'
 
 export default function TestPage() {
-  return <TooltipDemo />
+  useEffect(() => {
+    console.warn('hi mom')
+  }, [])
+
+  return (
+    <Tooltip>
+      <Tooltip.Trigger>
+        <Button size={100} als="center" circular />
+      </Tooltip.Trigger>
+      <Tooltip.Content
+        debug="verbose"
+        enterStyle={{ x: 0, y: -5, opacity: 0, scale: 0.9 }}
+        exitStyle={{ x: 0, y: -5, opacity: 0, scale: 0.9 }}
+        scale={1}
+        x={0}
+        y={0}
+        opacity={1}
+        animation={[
+          'quick',
+          {
+            opacity: {
+              overshootClamping: true,
+            },
+          },
+        ]}
+      >
+        <Tooltip.Arrow />
+        <Paragraph size="$2" lineHeight="$1">
+          Hello world
+        </Paragraph>
+      </Tooltip.Content>
+    </Tooltip>
+  )
 }
