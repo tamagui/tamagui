796c2652f09a6fc1ada9aebb22ad1f0b24b0c5c9
natew <natewienert@gmail.com>
2024-06-07 07:47:51 -1000

    perf(web): avoid double renders after initial hydration for enter animations

---
+++ packages/web/src/hooks/useDidHydrateOnce.tsx
@@ -0,0 +1,28 @@
+import { useEffect } from 'react'
+
+let didHydrateOnce = false
+
+// if set on the root tamagui provider, we now can know when things are fully hydrated
+// we cheat a bit why just resetting as we render components below
+
+export function useDidHydrateOnceRoot() {
+  useEffect(() => {
+    const tm = setInterval(() => {
+      if (Date.now() - last > 32) {
+        didHydrateOnce = true
+      }
+    }, 16)
+    return () => {
+      clearTimeout(tm)
+    }
+  }, [])
+}
+
+let last = Date.now()
+
+export function useDidHydrateOnce() {
+  if (!didHydrateOnce) {
+    last = Date.now()
+  }
+  return didHydrateOnce
+}
