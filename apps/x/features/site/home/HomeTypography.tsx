82bb3a5f34c420c2c6d2864308c4621300ea2f78
ROFI <rofi@rodrigos-mbp.lan>
2024-06-10 11:26:18 -0400

    port: HomeTypography diff
    diff:
    - site removed use of passThrough prop from Delay

--- apps/x/features/site/home/HomeTypography.tsx
+++ apps/x/features/site/home/HomeTypography.tsx
@@ -235,10 +235,9 @@ const Delay = ({ children, by, passThrough }) => {
   const [done, setDone] = useState(false)
 
   useEffect(() => {
-    if (passThrough) return
     const showTimer = setTimeout(() => setDone(true), by)
     return () => clearTimeout(showTimer)
-  }, [passThrough])
+  })
 
   if (passThrough) {
     return children
