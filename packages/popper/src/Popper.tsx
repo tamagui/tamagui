22efb556b3ddc281c325a1f830176522fd082b2d
natew <natewienert@gmail.com>
2024-06-15 07:29:18 -1000

    fix(popper): avoid flicker on open

--- packages/popper/src/Popper.tsx
+++ packages/popper/src/Popper.tsx
@@ -150,6 +150,9 @@ export function Popper(props: ScopedPopperProps<PopperProps>) {
       if (!(refs.reference.current && refs.floating.current)) {
         return
       }
+
+      floating.update()
+
       // Only call this when the floating element is rendered
       return autoUpdate(refs.reference.current, refs.floating.current, floating.update)
     }, [open, floating.update, refs.floating, refs.reference])
