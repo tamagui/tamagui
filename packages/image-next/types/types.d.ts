fa537aa8d9d65b348e567b3457540b8c185a7d82
natew <natewienert@gmail.com>
2024-06-23 17:45:31 -1000

    fix(image-next): fix types to depend only on web

--- packages/image-next/types/types.d.ts
+++ packages/image-next/types/types.d.ts
@@ -1,9 +1,9 @@
-import type { StackProps } from '@tamagui/core';
+import type { ViewProps } from '@tamagui/web';
 import type { Image as RNImage } from 'react-native';
 import type { ImageResizeMode, ImageSourcePropType, ImageProps as RNImageProps } from 'react-native';
 type RNImageType = typeof RNImage;
-type KeyofStackProps = keyof StackProps;
-export type ImageProps = StackProps & Omit<RNImageProps, KeyofStackProps | 'source' | 'resizeMode' | 'style'> & {
+type KeyofViewProps = keyof ViewProps;
+export type ImageProps = ViewProps & Omit<RNImageProps, KeyofViewProps | 'source' | 'resizeMode' | 'style'> & {
     /**
      * @deprecated
      * use src instead
@@ -16,7 +16,7 @@ export type ImageProps = StackProps & Omit<RNImageProps, KeyofStackProps | 'sour
     resizeMode?: ImageResizeMode;
     objectFit?: React.CSSProperties['objectFit'];
     objectPosition?: React.CSSProperties['objectPosition'];
-} & Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'width' | 'height' | KeyofStackProps> & Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'width' | 'height' | 'style'>;
+} & Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'width' | 'height' | KeyofViewProps> & Omit<React.ImgHTMLAttributes<HTMLImageElement>, 'width' | 'height' | 'style'>;
 export type ImageType = React.FC<Partial<ImageProps>> & {
     getSize: RNImageType['getSize'];
     getSizeWithHeaders: RNImageType['getSizeWithHeaders'];
