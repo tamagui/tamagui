0e06b8e790345bac165c2ad053919f717dcc4ac1
natew <natewienert@gmail.com>
2024-06-24 17:07:17 -1000

    fix(static): set TAMAGUI_KEEP_THEMES and TAMAGUI_IS_SERVER when loading tamagui config to avoid tree shaking themes

--- packages/static/src/helpers/requireTamaguiCore.ts
+++ packages/static/src/helpers/requireTamaguiCore.ts
@@ -9,5 +9,20 @@ export function requireTamaguiCore(
   if (!platform) {
     throw new Error(`No platform given to requireTamaguiCore`)
   }
-  return ogRequire(platform === 'native' ? '@tamagui/core/native' : '@tamagui/core')
+
+  // avoid tree shaking out themes
+  const og1 = process.env.TAMAGUI_IS_SERVER
+  const og2 = process.env.TAMAGUI_KEEP_THEMES
+  process.env.TAMAGUI_IS_SERVER ||= '1'
+  process.env.TAMAGUI_KEEP_THEMES ||= '1'
+
+  const exported = ogRequire(
+    platform === 'native' ? '@tamagui/core/native' : '@tamagui/core'
+  )
+
+  // restore back
+  process.env.TAMAGUI_IS_SERVER = og1
+  process.env.TAMAGUI_KEEP_THEMES = og2
+
+  return exported
 }
