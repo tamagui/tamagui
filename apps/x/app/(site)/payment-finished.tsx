80cb870bcc28d16f060d6decc2280409e972d5ad
natew <natewienert@gmail.com>
2024-06-24 14:16:54 -1000

    payment finished page

---
+++ apps/x/app/(site)/payment-finished.tsx
@@ -0,0 +1,31 @@
+import { NextSeo } from 'next-seo'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import { Spinner, YStack } from 'tamagui'
+import { UserGuard } from '~/features/user/useUser'
+
+export default function PaymentFinishedPage() {
+  const router = useRouter()
+
+  useEffect(() => {
+    // take some time for stripe to hit our webhook
+    const id = setTimeout(() => {
+      router.replace('/account/items')
+    }, 5_000)
+    return () => {
+      clearTimeout(id)
+    }
+  }, [router.replace])
+
+  return (
+    <>
+      <NextSeo title="Account" />
+
+      <UserGuard>
+        <YStack ai="center" flex={1} jc="center">
+          <Spinner size="large" />
+        </YStack>
+      </UserGuard>
+    </>
+  )
+}
