ba6d76540dc67a6f9ac1ba7f06a88631ada947b4
Pokai Chang <mail@zeta.vg>
2024-07-01 19:44:42 +0800

    fix(babel-plugin-fully-specified): incorrect extension

--- code/packages/babel-plugin-fully-specified/src/index.ts
+++ code/packages/babel-plugin-fully-specified/src/index.ts
@@ -290,7 +290,7 @@ function evaluateTargetModule({
     // 2. then try with all others
     for (const extension of tryExtensions) {
       if (existsSync(targetFile + extension)) {
-        return { module: module + '.mjs', extension }
+        return { module: module + extension, extension }
       }
     }
   } else if (esExtensions.includes(filenameExtension)) {
