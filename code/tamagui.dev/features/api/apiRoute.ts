5ed7fdef49bf5b646244c4598e4e4246cbf77b25
natew <natewienert@gmail.com>
2024-07-05 07:42:07 -1000

    ensure stream releases

--- code/tamagui.dev/features/api/apiRoute.ts
+++ code/tamagui.dev/features/api/apiRoute.ts
@@ -67,10 +67,18 @@ async function streamToString(stream: ReadableStream) {
   const reader = stream.getReader()
   const decoder = new TextDecoder()
   let result = ''
-  while (true) {
-    const { done, value } = await reader.read()
-    if (done) break
-    result += decoder.decode(value, { stream: true })
+
+  try {
+    while (true) {
+      const { done, value } = await reader.read()
+      if (done) break
+      result += decoder.decode(value, { stream: true })
+    }
+  } catch (error) {
+    console.error('Error reading the stream:', error)
+  } finally {
+    reader.releaseLock()
   }
+
   return result
 }
