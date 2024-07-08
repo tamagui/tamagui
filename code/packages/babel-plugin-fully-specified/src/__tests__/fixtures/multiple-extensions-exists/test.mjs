b400c1bb858fb9d96760ab9fd2eeff8b8ac7abb3
Pokai Chang <mail@zeta.vg>
2024-07-09 02:41:54 +0800

    feat(babel-plugin-fully-specified): implement and use forceExtension (#2769)
    * Revert "Revert "fix(babel-plugin-fully-specified): support transforming dynamic `import()`s (#2761)""
    
    * test(babel-plugin-fully-specified): add more tests
    
    * feat(babel-plugin-fully-specified): implement and use forceExtension
    
    * chore(babel-plugin-fully-specified): exclude tests from build
    
    * chore(babel-plugin-fully-specified): exclude tests from build
    
    The correct way.

---
+++ code/packages/babel-plugin-fully-specified/src/__tests__/fixtures/multiple-extensions-exists/test.mjs
@@ -0,0 +1,2 @@
+// Expected to be transformed to use the `.mjs` extension, which is same as this file.
+import { foo, bar } from './modules/module'
--- code/packages/babel-plugin-fully-specified/types/index.d.ts
+++ code/packages/babel-plugin-fully-specified/types/index.d.ts
@@ -1,27 +1,20 @@
-import type { ConfigAPI, NodePath, PluginPass } from '@babel/core';
-import type { ImportSpecifier, ImportDeclaration, ExportAllDeclaration, StringLiteral, ExportSpecifier, ExportDeclaration, ExportNamedDeclaration } from '@babel/types';
-type ImportDeclarationFunc = (specifiers: Array<ImportSpecifier>, source: StringLiteral) => ImportDeclaration;
-type ExportNamedDeclarationFunc = (declaration?: ExportDeclaration, specifiers?: Array<ExportSpecifier>, source?: StringLiteral) => ExportNamedDeclaration;
-type ExportAllDeclarationFunc = (source: StringLiteral) => ExportAllDeclaration;
-type PathDeclaration = NodePath & {
-    node: ImportDeclaration & ExportNamedDeclaration & ExportAllDeclaration;
-};
-interface FullySpecifiedOptions {
-    declaration: ImportDeclarationFunc | ExportNamedDeclarationFunc | ExportAllDeclarationFunc;
-    makeNodes: (path: PathDeclaration) => Array<PathDeclaration>;
-    ensureFileExists: boolean;
+import type { ConfigAPI, PluginObj } from '@babel/core';
+export interface FullySpecifiedOptions {
+    ensureFileExists: boolean | {
+        /**
+         * If you're doing a non-in-place transformation (for example, outputting `.mjs` from `.js`) with `ensureFileExists` enabled, it's possible that the transform will be incorrect due to the imported file is not transformed and written into place yet (for example, we have `foo.js` and `bar.js` and we're transforming them into `foo.mjs` and `bar.mjs` respectively, in `bar.js` we have `import { ... } from './foo.js'` which we expect to be transformed into `import { ... } from './foo.mjs'`, but if `foo.mjs` is not transformed and written yet, it will be transformed into `import { ... } from './foo.js'` because `foo.mjs` can't be found at that time).
+         *
+         * To solve this, you can set this option to `'.mjs'` to force the extension to be transformed into that specified extension.
+         */
+        forceExtension?: string;
+    };
     esExtensionDefault: string;
+    /** List of all extensions which we try to find. */
     tryExtensions: Array<string>;
+    /** List of extensions that can run in Node.js or in the Browser. */
     esExtensions: Array<string>;
+    /** List of packages that also should be transformed with this plugin. */
     includePackages: Array<string>;
 }
-export default function FullySpecified(api: ConfigAPI, options: FullySpecifiedOptions): {
-    name: string;
-    visitor: {
-        ImportDeclaration: (path: PathDeclaration, { file: { opts: { filename }, }, }: PluginPass) => void;
-        ExportNamedDeclaration: (path: PathDeclaration, { file: { opts: { filename }, }, }: PluginPass) => void;
-        ExportAllDeclaration: (path: PathDeclaration, { file: { opts: { filename }, }, }: PluginPass) => void;
-    };
-};
-export {};
+export default function FullySpecified(api: ConfigAPI, rawOptions: FullySpecifiedOptions): PluginObj;
 //# sourceMappingURL=index.d.ts.map
