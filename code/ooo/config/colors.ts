9f4a6207b2df7b5145990d6537a40d53fc96ade3
natew <natewienert@gmail.com>
2024-08-18 09:20:02 -1000

    docs layout

--- code/ooo/config/colors.ts
+++ code/ooo/config/colors.ts
@@ -1,5 +1,35 @@
-import { yellow, yellowDark, gray, grayDark } from '@tamagui/colors'
+import { gray, grayDark } from '@tamagui/colors'
 import { objectFromEntries, objectKeys, postfixObjKeys } from '~/helpers/typeHelpers'
+
+const yellow = {
+  yellow1: 'hsl(60, 54.0%, 98.5%)',
+  yellow2: 'hsl(52, 100%, 95.5%)',
+  yellow3: 'hsl(55, 100%, 90.9%)',
+  yellow4: 'hsl(54, 100%, 86.6%)',
+  yellow5: 'hsl(52, 97.9%, 82.0%)',
+  yellow6: 'hsl(50, 89.4%, 76.1%)',
+  yellow7: 'hsl(47, 80.4%, 68.0%)',
+  yellow8: 'hsl(48, 100%, 46.1%)',
+  yellow9: 'hsl(53, 92.0%, 50.0%)',
+  yellow10: 'hsl(50, 100%, 48.5%)',
+  yellow11: 'hsl(42, 90%, 24.0%)',
+  yellow12: 'hsl(40, 55.0%, 13.5%)',
+}
+
+const yellowDark = {
+  yellow1: 'hsl(45, 100%, 5.5%)',
+  yellow2: 'hsl(46, 100%, 6.7%)',
+  yellow3: 'hsl(45, 100%, 8.7%)',
+  yellow4: 'hsl(45, 100%, 10.4%)',
+  yellow5: 'hsl(47, 100%, 12.1%)',
+  yellow6: 'hsl(49, 100%, 14.3%)',
+  yellow7: 'hsl(49, 90.3%, 18.4%)',
+  yellow8: 'hsl(50, 100%, 22.0%)',
+  yellow9: 'hsl(53, 92.0%, 50.0%)',
+  yellow10: 'hsl(54, 100%, 68.0%)',
+  yellow11: 'hsl(48, 100%, 47.0%)',
+  yellow12: 'hsl(53, 100%, 91.0%)',
+}
 
 const colorTokens = {
   light: {
