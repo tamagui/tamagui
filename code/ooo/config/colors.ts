64985d15187739d1a878564a06e6efe09e389b94
natew <natewienert@gmail.com>
2024-08-28 21:33:25 -1000

    ooo design and words

--- code/ooo/config/colors.ts
+++ code/ooo/config/colors.ts
@@ -12,22 +12,22 @@ const yellow = {
   yellow8: 'hsl(48, 100%, 46.1%)',
   yellow9: 'hsl(53, 92.0%, 50.0%)',
   yellow10: 'hsl(50, 100%, 48.5%)',
-  yellow11: 'hsl(42, 90%, 24.0%)',
+  yellow11: 'hsl(42, 90%, 34.0%)',
   yellow12: 'hsl(40, 55.0%, 13.5%)',
 }
 
 const yellowDark = {
-  yellow1: 'hsl(45, 100%, 5.5%)',
-  yellow2: 'hsl(46, 100%, 6.7%)',
-  yellow3: 'hsl(45, 100%, 8.7%)',
-  yellow4: 'hsl(45, 100%, 10.4%)',
-  yellow5: 'hsl(47, 100%, 12.1%)',
-  yellow6: 'hsl(49, 100%, 14.3%)',
+  yellow1: 'hsl(45, 70%, 5.5%)',
+  yellow2: 'hsl(46, 70%, 6.7%)',
+  yellow3: 'hsl(45, 70%, 8.7%)',
+  yellow4: 'hsl(45, 70%, 10.4%)',
+  yellow5: 'hsl(47, 70%, 12.1%)',
+  yellow6: 'hsl(49, 70%, 14.3%)',
   yellow7: 'hsl(49, 90.3%, 18.4%)',
-  yellow8: 'hsl(50, 100%, 22.0%)',
-  yellow9: 'hsl(53, 92.0%, 50.0%)',
-  yellow10: 'hsl(54, 100%, 68.0%)',
-  yellow11: 'hsl(48, 100%, 47.0%)',
+  yellow8: 'hsl(53, 100%, 25.0%)',
+  yellow9: 'hsl(53, 92.0%, 40.0%)',
+  yellow10: 'hsl(54, 100%, 60.0%)',
+  yellow11: 'hsl(48, 100%, 80.0%)',
   yellow12: 'hsl(53, 100%, 91.0%)',
 }
 
