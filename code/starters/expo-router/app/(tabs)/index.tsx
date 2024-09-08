26242ddca836132b81ee5ecf375eeb5ed91cbf33
jonsherrard <jon@devular.com>
2024-09-09 00:33:23 +0100

    feature(starters): Move expo-router to new architecture and latest packages

--- code/starters/expo-router/app/(tabs)/index.tsx
+++ code/starters/expo-router/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { ToastControl } from 'app/CurrentToast'
 
 export default function TabOneScreen() {
   return (
-    <YStack f={1} ai="center" gap="$8" px="$10" pt="$5">
+    <YStack f={1} ai="center" gap="$8" px="$10" pt="$5" bg="$background">
       <H2>Tamagui + Expo</H2>
 
       <ToastControl />
