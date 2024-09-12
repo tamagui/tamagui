c9fff947b64c471b6db8595462422a6e9fbd1068
Jon Sherrard <jon@devular.com>
2024-09-12 02:18:39 +0100

    `starters/expo-router` enable React Native "New Architecture" (#2910)
    * feature(starters): expo-router with new architecture enabled
    
    * feature(starters): New Arch System Theme powered by Tamagui tokens
    
    * chore(yarn): update lockfile
    
    * refactor: Update RootLayout providers for useTheme hook compatibility
    
    - Refactored RootLayout to include Providers component.
    - Moved Provider wrapping to a separate Providers component.
    - Adjusted ThemeProvider and StatusBar placement within RootLayoutNav.
    
    Implementation reason: This change ensures that the useTheme hook runs properly by restructuring the RootLayout to include a dedicated Providers component, thereby improving theme management and consistency across the application.
    
    Files changed:
    - code/starters/expo-router-new-arch/app/_layout.tsx
    
    * feature(starters): Move expo-router to new architecture and latest packages
    
    * chore(starters): amend app.json config
    
    * chore(starters): amend expo-router package.json config
    
    * chore(yarn): upgrade yarn

--- code/starters/expo-router/app/(tabs)/two.tsx
+++ code/starters/expo-router/app/(tabs)/two.tsx
@@ -2,7 +2,7 @@ import { Text, View } from 'tamagui'
 
 export default function TabTwoScreen() {
   return (
-    <View flex={1} alignItems="center" justifyContent="center">
+    <View flex={1} alignItems="center" justifyContent="center" bg="$background">
       <Text fontSize={20} color="$blue10">
         Tab Two
       </Text>
