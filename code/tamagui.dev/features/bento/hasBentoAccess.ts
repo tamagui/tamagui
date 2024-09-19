ab3e2e6b0097e2ac561e0c0f0fe4996b6b74e357
natew <natewienert@gmail.com>
2024-09-19 07:24:21 -1000

    fix duplicate export

--- code/tamagui.dev/features/bento/hasBentoAccess.ts
+++ code/tamagui.dev/features/bento/hasBentoAccess.ts
@@ -35,21 +35,3 @@ export const hasBentoAccessV2 = async (userId: string) => {
 
   return Boolean(result?.data?.length)
 }
-
-export const hasBentoAccessV2 = async (userId: string) => {
-  const result = await supabaseAdmin
-    .from('product_ownership')
-    .select(`
-      *,
-      prices (
-        *,
-        products (
-          *
-        )
-      )
-    `)
-    .eq('user_id', userId)
-    .eq('prices.products.name', 'Bento')
-
-  return Boolean(result?.data?.length)
-}
