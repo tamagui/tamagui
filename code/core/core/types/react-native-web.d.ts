8c0462c9106e3d65058d3706f431aeb28ef96def
natew <natewienert@gmail.com>
2024-09-21 09:11:19 -1000

    optimize site

---
+++ code/core/core/types/react-native-web.d.ts
@@ -0,0 +1,12 @@
+export * from '@tamagui/react-native-use-responder-events';
+export * from '@tamagui/react-native-use-pressable';
+export * from './index';
+export declare const Platform: {
+    OS: string;
+    select(obj: any): any;
+};
+export declare const StyleSheet: {
+    create(obj: any): any;
+};
+export declare const Pressable: () => null;
+//# sourceMappingURL=react-native-web.d.ts.map
--- code/ooo/config/fonts.ts
+++ code/ooo/config/fonts.ts
@@ -1,7 +1,61 @@
-import { createInterFont } from '@tamagui/font-inter'
+import type { FillInFont, GenericFont } from '@tamagui/core'
 import { createFont } from 'tamagui'
 
-const heading = createInterFont(
+const defaultSizes = {
+  1: 11,
+  2: 12,
+  3: 13,
+  4: 14,
+  true: 14,
+  5: 16,
+  6: 18,
+  7: 20,
+  8: 23,
+  9: 30,
+  10: 46,
+  11: 55,
+  12: 62,
+  13: 72,
+  14: 92,
+  15: 114,
+  16: 134,
+} as const
+
+function createMainFont<A extends GenericFont>(
+  font: Partial<A> = {},
+  {
+    sizeLineHeight = (size) => size + 10,
+    sizeSize = (size) => size * 1,
+  }: {
+    sizeLineHeight?: (fontSize: number) => number
+    sizeSize?: (size: number) => number
+  } = {}
+): FillInFont<A, keyof typeof defaultSizes> {
+  // merge to allow individual overrides
+  const size = Object.fromEntries(
+    Object.entries({
+      ...defaultSizes,
+      ...font.size,
+    }).map(([k, v]) => [k, sizeSize(+v)])
+  )
+  return createFont({
+    family:
+      '-apple-system, Inter, system-ui, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif',
+    lineHeight: Object.fromEntries(
+      Object.entries(size).map(([k, v]) => [k, sizeLineHeight(v)])
+    ),
+    weight: {
+      4: '300',
+    },
+    letterSpacing: {
+      4: 0,
+    },
+    ...(font as any),
+    size,
+  })
+}
+
+const heading = createMainFont(
   {
     size: {
       5: 13,
@@ -42,7 +96,7 @@ const heading = createInterFont(
   { sizeLineHeight: (size) => Math.round(size * 1.1 + (size < 30 ? 10 : 5)) }
 )
 
-const body = createInterFont(
+const body = createMainFont(
   {
     weight: {
       1: '400',
