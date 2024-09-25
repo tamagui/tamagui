1ff67dabdf4bae88a56377a68605bf85e8e521b2
natew <natewienert@gmail.com>
2024-09-25 09:59:44 -1000

    various design

--- code/ooo/config/colors.ts
+++ code/ooo/config/colors.ts
@@ -3,12 +3,12 @@ import { objectFromEntries, objectKeys, postfixObjKeys } from '../helpers/typeHe
 
 const yellow = {
   yellow1: 'hsl(60, 54.0%, 98.5%)',
-  yellow2: 'hsl(52, 100%, 95.5%)',
-  yellow3: 'hsl(55, 100%, 90.9%)',
-  yellow4: 'hsl(54, 100%, 86.6%)',
-  yellow5: 'hsl(52, 97.9%, 82.0%)',
-  yellow6: 'hsl(50, 89.4%, 76.1%)',
-  yellow7: 'hsl(47, 80.4%, 68.0%)',
+  yellow2: 'hsl(52, 100%, 93.5%)',
+  yellow3: 'hsl(55, 100%, 88.9%)',
+  yellow4: 'hsl(54, 100%, 83.6%)',
+  yellow5: 'hsl(52, 97.9%, 78.0%)',
+  yellow6: 'hsl(50, 89.4%, 72.1%)',
+  yellow7: 'hsl(47, 80.4%, 60.0%)',
   yellow8: 'hsl(48, 100%, 46.1%)',
   yellow9: 'hsl(53, 92.0%, 50.0%)',
   yellow10: 'hsl(50, 100%, 48.5%)',
