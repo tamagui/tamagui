ba7bd278a7b2ecd0ea15b18e54e3fe428e223024
natew <natewienert@gmail.com>
2024-09-26 07:11:27 -1000

    various wording

--- code/ooo/routes.d.ts
+++ code/ooo/routes.d.ts
@@ -3,7 +3,7 @@ import type { VXSRouter } from 'vxs'
 declare module 'vxs' {
   export namespace VXSRouter {
     export interface __routes<T extends string = string> extends Record<string, unknown> {
-      StaticRoutes: `/` | `/_sitemap` | `/docs` | `/duck`
+      StaticRoutes: `/` | `/Footer` | `/_sitemap` | `/docs` | `/duck`
       DynamicRoutes: `/docs/${VXSRouter.SingleRoutePart<T>}`
       DynamicRouteTemplate: `/docs/[slug]`
       IsTyped: true
