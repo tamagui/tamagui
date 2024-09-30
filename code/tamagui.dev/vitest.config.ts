7146c694f75b8d2df48f62a960b7a921ac32be4d
jonsherrard <jon@devular.com>
2024-09-30 14:37:47 +0100

    try happy-dom for tests in ci

--- code/tamagui.dev/vitest.config.ts
+++ code/tamagui.dev/vitest.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['**/node_modules/**', '**/dist/**'],
     root: __dirname,
