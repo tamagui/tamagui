b2a1021095aaa66d8c00bc4591fc75b20a992cf3
jonsherrard <jon@devular.com>
2024-09-30 16:36:09 +0100

    testing vitest configs in ci

--- code/tamagui.dev/vitest.config.ts
+++ code/tamagui.dev/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    environment: 'node',
+    environment: 'jsdom',
     include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     exclude: ['**/node_modules/**', '**/dist/**'],
     root: __dirname,
