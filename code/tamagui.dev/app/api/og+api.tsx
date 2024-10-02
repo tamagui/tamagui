617ccd09d59f390ddda09bd3738de4b3b7c5a68f
natew <natewienert@gmail.com>
2024-10-02 08:38:25 -1000

    chore: upgrade vxrn

--- code/tamagui.dev/app/api/og+api.tsx
+++ code/tamagui.dev/app/api/og+api.tsx
@@ -3,6 +3,8 @@ import type { ReactElement } from 'react'
 import { getURL } from 'one'
 import { apiRoute } from '~/features/api/apiRoute'
 
+console.log('wtf', getURL())
+
 export default apiRoute(async (req) => {
   const { ImageResponse } = await import('@vercel/og')
 
