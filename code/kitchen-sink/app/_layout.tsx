942fed84bb789002b7305bec5aa02449aa761be4
natew <natewienert@gmail.com>
2024-10-08 13:11:22 -1000

    cleanup kitchen sink for one

---
+++ code/kitchen-sink/app/_layout.tsx
@@ -0,0 +1,3 @@
+import { Slot } from 'one'
+
+export default () => <Slot />
--- code/kitchen-sink/vite.config.ts
+++ code/kitchen-sink/vite.config.ts
@@ -1,19 +1,13 @@
 import { tamaguiPlugin } from '@tamagui/vite-plugin'
-import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
-
-const tamaguiConfig = {
-  components: ['@tamagui/sandbox-ui', 'tamagui'],
-  config: 'tamagui.config.ts',
-}
 
 export default defineConfig({
   clearScreen: true,
   plugins: [
-    react(),
-    tamaguiPlugin({
-      ...tamaguiConfig,
-      optimize: process.env.EXTRACT === '1',
-    }),
+    // tamaguiPlugin({
+    //   components: ['@tamagui/sandbox-ui', 'tamagui'],
+    //   config: 'src/tamagui.config.ts',
+    //   optimize: process.env.EXTRACT === '1',
+    // }),
   ].filter(Boolean),
 })
