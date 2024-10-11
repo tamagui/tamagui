45893a79670650635418b7b7ee72edc4cbf85dc8
jonsherrard <jon@devular.com>
2024-10-11 16:11:52 +0100

    add a guide to using tamagui with one

--- code/tamagui.dev/features/docs/docsRoutes.tsx
+++ code/tamagui.dev/features/docs/docsRoutes.tsx
@@ -61,6 +61,7 @@ export const docsRoutes = [
       { title: 'Next.js', route: '/docs/guides/next-js' },
       { title: 'Expo', route: '/docs/guides/expo' },
       { title: 'Vite', route: '/docs/guides/vite' },
+      { title: 'One', route: '/docs/guides/one' },
       { title: 'Webpack', route: '/docs/guides/webpack' },
       { title: 'Metro', route: '/docs/guides/metro' },
       { title: 'create-tamagui', route: '/docs/guides/create-tamagui-app' },
