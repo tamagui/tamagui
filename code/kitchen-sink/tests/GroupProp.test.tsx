b3a710065e073fb7bfdbdcf90c10bf3dc86973a8
jonsherrard <jon@devular.com>
2024-10-12 17:39:08 +0100

    Revert "test different test structure for group test"
    This reverts commit 4acc07964556fc500ef74f2c5c601adecfa59ae6.

--- code/kitchen-sink/tests/GroupProp.test.tsx
+++ code/kitchen-sink/tests/GroupProp.test.tsx
@@ -1,36 +1,28 @@
 import { expect, test } from '@playwright/test'
-import type { Page } from '@playwright/test'
 
 import { setupPage } from './test-utils'
 import { getStyles } from './utils'
 
-let page: Page
-
-test.beforeAll(async ({ browser }) => {
-  page = await browser.newPage()
+test.beforeEach(async ({ page }) => {
   await setupPage(page, { name: 'GroupProp', type: 'useCase' })
 })
 
-test.afterAll(async () => {
-  await page.close()
-})
-
-test(`group prop - styled()`, async () => {
+test(`group prop - styled()`, async ({ page }) => {
   const styles = await getStyles(page.locator('#styled').first())
   expect(styles.backgroundColor).toBe(`rgb(255, 0, 0)`)
 })
 
-test(`group prop - styled() + media unmatched`, async () => {
+test(`group prop - styled() + media unmatched`, async ({ page }) => {
   const styles = await getStyles(page.locator('#styled-media-unmatched').first())
   expect(styles.backgroundColor).toBe(`rgb(255, 0, 0)`)
 })
 
-test(`group prop - styled() + media matched`, async () => {
+test(`group prop - styled() + media matched`, async ({ page }) => {
   const styles = await getStyles(page.locator('#styled-media-matched').first())
   expect(styles.backgroundColor).toBe(`rgb(0, 255, 0)`)
 })
 
-test(`group prop - inline`, async () => {
+test(`group prop - inline`, async ({ page }) => {
   const styles = await getStyles(page.locator('#inline').first())
   expect(styles.backgroundColor).toBe(`rgb(0, 0, 255)`)
 })
