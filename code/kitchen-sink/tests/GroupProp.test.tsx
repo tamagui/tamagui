4acc07964556fc500ef74f2c5c601adecfa59ae6
jonsherrard <jon@devular.com>
2024-10-12 16:11:46 +0100

    test different test structure for group test

--- code/kitchen-sink/tests/GroupProp.test.tsx
+++ code/kitchen-sink/tests/GroupProp.test.tsx
@@ -1,28 +1,36 @@
 import { expect, test } from '@playwright/test'
+import type { Page } from '@playwright/test'
 
 import { setupPage } from './test-utils'
 import { getStyles } from './utils'
 
-test.beforeEach(async ({ page }) => {
+let page: Page
+
+test.beforeAll(async ({ browser }) => {
+  page = await browser.newPage()
   await setupPage(page, { name: 'GroupProp', type: 'useCase' })
 })
 
-test(`group prop - styled()`, async ({ page }) => {
+test.afterAll(async () => {
+  await page.close()
+})
+
+test(`group prop - styled()`, async () => {
   const styles = await getStyles(page.locator('#styled').first())
   expect(styles.backgroundColor).toBe(`rgb(255, 0, 0)`)
 })
 
-test(`group prop - styled() + media unmatched`, async ({ page }) => {
+test(`group prop - styled() + media unmatched`, async () => {
   const styles = await getStyles(page.locator('#styled-media-unmatched').first())
   expect(styles.backgroundColor).toBe(`rgb(255, 0, 0)`)
 })
 
-test(`group prop - styled() + media matched`, async ({ page }) => {
+test(`group prop - styled() + media matched`, async () => {
   const styles = await getStyles(page.locator('#styled-media-matched').first())
   expect(styles.backgroundColor).toBe(`rgb(0, 255, 0)`)
 })
 
-test(`group prop - inline`, async ({ page }) => {
+test(`group prop - inline`, async () => {
   const styles = await getStyles(page.locator('#inline').first())
   expect(styles.backgroundColor).toBe(`rgb(0, 0, 255)`)
 })
