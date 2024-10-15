aec7ad64c5495222706643c73b524e5f729b9271
natew <natewienert@gmail.com>
2024-10-15 17:09:49 +0900

    force clean build in release script

--- scripts/release.ts
+++ scripts/release.ts
@@ -196,7 +196,7 @@ async function run() {
 
     if (!skipBuild && !finish) {
       // lets do a full clean and build:force, to ensure we dont have weird cached or leftover files
-      await spawnify(`yarn clean:build`)
+      await spawnify(`yarn build:force`)
       await checkDistDirs()
     }
 
