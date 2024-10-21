48b12cfba75387ba4816bff239607b32840a3d65
natew <natewienert@gmail.com>
2024-10-22 08:35:29 +0900

    fix serve script

--- scripts/ci-serve.sh
+++ scripts/ci-serve.sh
@@ -7,6 +7,8 @@ if [ "$app" = "one" ]; then
   yarn workspace @tamagui/ooo serve:railway
 elif [ "$app" = "takeout" ]; then
   yarn workspace @tamagui/takeout serve:railway
+elif [ "$app" = "docs" ]; then
+  yarn workspace @tamagui/one-docs serve:railway
 else
   yarn workspace @tamagui/dot-dev serve:railway
 fi
