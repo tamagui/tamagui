5468cd1edfa2ed5ece19ed526397a2e551f93d5f
natew <natewienert@gmail.com>
2024-12-03 10:00:07 -1000

    turn off black friday

---
+++ code/bento/types/components/provider/Provider.d.ts
@@ -0,0 +1,3 @@
+import type { TamaguiProviderProps } from 'tamagui';
+export declare function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>): import("react/jsx-runtime").JSX.Element;
+//# sourceMappingURL=Provider.d.ts.map
