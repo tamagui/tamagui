9986edc6ca23ca89be4417d6309e16e74c88f9cb
natew <natewienert@gmail.com>
2024-12-06 10:41:18 -1000

    fix(core): in test mode, ignore jestAnimatedStyle props

--- code/core/web/src/helpers/getSplitStyles.tsx
+++ code/core/web/src/helpers/getSplitStyles.tsx
@@ -234,6 +234,10 @@ export const getSplitStyles: StyleSplitter = (
     let keyInit = keyOg
     let valInit = props[keyInit]
 
+    if (process.env.NODE_ENV === 'test' && keyInit === 'jestAnimatedStyle') {
+      continue
+    }
+
     if (process.env.NODE_ENV === 'development' && debug === 'profile') {
       // @ts-expect-error
       time`prop-${keyInit}`
