6f9fdacab66d89b1968d5a3669d2196f0ec670bb
natew <natewienert@gmail.com>
2025-01-16 08:09:18 -1000

    perf: compile v4 tamagui themes for type performance

--- code/core/themes/src/v4.tsx
+++ code/core/themes/src/v4.tsx
@@ -1,3 +1,5 @@
 export * from './v4-createTheme'
-export { tamaguiThemes } from './v4-tamagui'
 export { tokens } from './v3-themes'
+
+import * as tt from './v4-tamagui-out'
+export const tamaguiThemes = tt
