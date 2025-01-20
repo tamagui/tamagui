33376ecb50b9f23eab175925dd8b79983a667ba9
natew <natewienert@gmail.com>
2025-01-20 08:53:28 -1000

    feat(build): when isolatedDeclarations detected, use oxc-transform

--- code/core/use-window-dimensions/types/index.d.ts
+++ code/core/use-window-dimensions/types/index.d.ts
@@ -1,13 +1,12 @@
 /**
- * SSR safe useWindowDimensions
- */
+* SSR safe useWindowDimensions
+*/
 type Size = {
-    width: number;
-    height: number;
+	width: number;
+	height: number;
 };
-export declare function configureInitialWindowDimensions(next: Size): void;
-export declare function useWindowDimensions({ serverValue, }?: {
-    serverValue?: Size;
-}): Size;
+export declare function configureInitialWindowDimensions(next: Size);
+export declare function useWindowDimensions({ serverValue }?: { serverValue?: Size });
 export {};
+
 //# sourceMappingURL=index.d.ts.map
