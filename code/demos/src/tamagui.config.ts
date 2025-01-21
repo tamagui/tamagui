fb9a138737c8706b6668114d6f563f4ce295bf3c
natew <natewienert@gmail.com>
2025-01-21 12:25:02 -1000

    fix demos types

--- code/demos/src/tamagui.config.ts
+++ code/demos/src/tamagui.config.ts
@@ -1,9 +1,13 @@
 import { config } from '@tamagui/config/v3'
+import { createTamagui } from 'tamagui'
 
 export { config } from '@tamagui/config/v3'
-export default config
 
-export type Conf = typeof config
+const conf = createTamagui(config)
+
+export default conf
+
+export type Conf = typeof conf
 
 declare module 'tamagui' {
   interface TamaguiCustomConfig extends Conf {}
