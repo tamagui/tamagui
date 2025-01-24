876bdf7ea9cc1aef69fec555b2a3aae08dae056b
natew <natewienert@gmail.com>
2025-01-24 13:38:50 -1000

    fix(build): support .web extensions for platform specific builds

--- code/core/constants/src/constants.native.ts
+++ code/core/constants/src/constants.native.ts
@@ -1,4 +1,5 @@
 import { useLayoutEffect, type useEffect } from 'react'
+import { Platform } from 'react-native'
 
 export const isWeb: boolean = false
 export const isWindowDefined: boolean = false
@@ -8,6 +9,11 @@ export const useIsomorphicLayoutEffect: typeof useEffect = useLayoutEffect
 export const isChrome: boolean = false
 export const isWebTouchable: boolean = false
 export const isTouchable: boolean = true
-export const isAndroid: boolean = false
-export const isIos: boolean = process.env.TEST_NATIVE_PLATFORM === 'ios'
-export const currentPlatform: 'web' | 'ios' | 'native' | 'android' = 'native'
+export const isAndroid: boolean =
+  Platform.OS === 'android' || process.env.TEST_NATIVE_PLATFORM === 'android'
+export const isIos: boolean =
+  Platform.OS === 'ios' || process.env.TEST_NATIVE_PLATFORM === 'ios'
+
+const platforms = { ios: 'ios', android: 'android' } as const
+export const currentPlatform: 'web' | 'ios' | 'native' | 'android' =
+  platforms[Platform.OS] || 'native'
