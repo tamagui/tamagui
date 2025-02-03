1052fd4bb7388de7f2ead6295472561b95810189
natew <natewienert@gmail.com>
2025-02-03 04:43:54 -1000

    chore: move v4 tokens out for safety and llm parsing

--- code/core/themes/types/v4.d.ts
+++ code/core/themes/types/v4.d.ts
@@ -1,4 +1,4 @@
-export { tokens } from './v3-tokens';
+export { tokens } from './v4-tokens';
 export { createThemes } from '@tamagui/theme-builder';
 export { tamaguiThemes } from './v4-tamagui';
 //# sourceMappingURL=v4.d.ts.map
