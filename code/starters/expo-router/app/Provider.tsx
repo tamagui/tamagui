4f4b0b7f67d2eb54638a19b45462f67ba7be3ce3
Zarin <thesharifi.maruf@gmail.com>
2025-02-03 19:33:51 +0430

    remove toast from expo router default starter so Go works

--- code/starters/expo-router/app/Provider.tsx
+++ code/starters/expo-router/app/Provider.tsx
@@ -1,7 +1,7 @@
 import { useColorScheme } from 'react-native'
 import { TamaguiProvider, type TamaguiProviderProps } from 'tamagui'
-import { ToastProvider, ToastViewport } from '@tamagui/toast'
-import { CurrentToast } from './CurrentToast'
+// import { ToastProvider, ToastViewport } from '@tamagui/toast'
+// import { CurrentToast } from './CurrentToast'
 import { config } from '../tamagui.config'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
@@ -13,12 +13,12 @@ export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'conf
       defaultTheme={colorScheme === 'dark' ? 'dark' : 'light'}
       {...rest}
     >
-      <ToastProvider
+      {/* <ToastProvider
         swipeDirection="horizontal"
         duration={6000}
         native={
           [
-            /* uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go */
+            // uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go
             // 'mobile'
           ]
         }
@@ -26,7 +26,7 @@ export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'conf
         {children}
         <CurrentToast />
         <ToastViewport top="$8" left={0} right={0} />
-      </ToastProvider>
+      </ToastProvider> */}
     </TamaguiProvider>
   )
 }
