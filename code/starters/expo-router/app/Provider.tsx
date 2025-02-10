8f18e26e3287b06d52430b367be880e415993ad5
Bảo Hà <baronn.ha@gmail.com>
2025-02-10 13:45:54 +0700

    revert(expo-router): Enable ToastProvider in Expo Router starter

--- code/starters/expo-router/app/Provider.tsx
+++ code/starters/expo-router/app/Provider.tsx
@@ -1,7 +1,7 @@
 import { useColorScheme } from 'react-native'
 import { TamaguiProvider, type TamaguiProviderProps } from 'tamagui'
-// import { ToastProvider, ToastViewport } from '@tamagui/toast'
-// import { CurrentToast } from './CurrentToast'
+import { ToastProvider, ToastViewport } from '@tamagui/toast'
+import { CurrentToast } from './CurrentToast'
 import { config } from '../tamagui.config'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
@@ -13,7 +13,7 @@ export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'conf
       defaultTheme={colorScheme === 'dark' ? 'dark' : 'light'}
       {...rest}
     >
-      {/* <ToastProvider
+      <ToastProvider
         swipeDirection="horizontal"
         duration={6000}
         native={
@@ -26,7 +26,7 @@ export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'conf
         {children}
         <CurrentToast />
         <ToastViewport top="$8" left={0} right={0} />
-      </ToastProvider> */}
+      </ToastProvider>
     </TamaguiProvider>
   )
 }
