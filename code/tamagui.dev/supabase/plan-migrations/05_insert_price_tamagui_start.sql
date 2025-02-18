8aeefd1e70cf48552acf99fadef856c68b63f793
poteboy <poteboy.dev@gmail.com>
2025-02-18 12:42:37 +0900

    modify price sql

--- code/tamagui.dev/supabase/plan-migrations/05_insert_price_tamagui_start.sql
+++ code/tamagui.dev/supabase/plan-migrations/05_insert_price_tamagui_start.sql
@@ -3,13 +3,13 @@
 -- The metadata may include information about auto-renewal options.
 INSERT INTO prices (id, product_id, active, unit_amount, currency, type, interval, interval_count, trial_period_days, metadata)
 VALUES (
-  'price_1QrujmFQGtHoG6xc4UIilvAy',
+  'price_1QthHSFQGtHoG6xcDOEuFsrW',
   'prod_RlRd2DVrG0frHe', -- Tamagui Pro
   true,
-  2000,  -- equivalent to $20
+  24000,  -- equivalent to $20/month
   'usd',
   'recurring',
-  'month',
+  'year',
   1,
   NULL,
   '{"auto_renew": true}'
