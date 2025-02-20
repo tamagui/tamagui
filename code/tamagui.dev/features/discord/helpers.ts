0020b087317e2961dcf0dbcd695c243dd0270bb8
natew <natewienert@gmail.com>
2025-02-19 17:07:02 -1000

    fix discord again

--- code/tamagui.dev/features/discord/helpers.ts
+++ code/tamagui.dev/features/discord/helpers.ts
@@ -1,13 +1,13 @@
+import { Client, GatewayIntentBits } from '@discordjs/core'
+import { REST } from '@discordjs/rest'
+import { WebSocketManager } from '@discordjs/ws'
+
 export const TAKEOUT_ROLE_ID = '1131082605052301403'
 export const TAMAGUI_DISCORD_GUILD_ID = '909986013848412191'
 export const TAKEOUT_GROUP_ID = '1131249991256657950' // group id
 export const DEFAULT_ROLE_ID = '909986013848412191' // @everyone
 
 export const getDiscordClient = async () => {
-  const { Client, GatewayIntentBits } = await import('@discordjs/core')
-  const { REST } = await import('@discordjs/rest')
-  const { WebSocketManager } = await import('@discordjs/ws')
-
   const token = process.env.DISCORD_BOT_TOKEN!
 
   const rest = new REST({ version: '10' }).setToken(token)
