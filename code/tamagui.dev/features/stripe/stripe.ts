bcc311f91e5c5202d5ea8441fe57644310d3a683
poteboy <poteboy.dev@gmail.com>
2025-02-24 15:56:21 +0900

    fix to correctyly load env variable

--- code/tamagui.dev/features/stripe/stripe.ts
+++ code/tamagui.dev/features/stripe/stripe.ts
@@ -1,16 +1,12 @@
 import Stripe from 'stripe'
 
-export const stripe = new Stripe(
-  process.env.STRIPE_SECRET_KEY ??
-    'sk_test_51MlzkhAbFJBp9fF3V3iJ2iUAijWn103UD7fdPa8V7gnbuVwwpIldBopGDqetvXDKrx3J3BrLnvRTDDLoMmildqOp00daeJS35c',
-  {
-    // https://github.com/stripe/stripe-node#configuration
-    apiVersion: '2020-08-27',
-    // Register this as an official Stripe plugin.
-    // https://stripe.com/docs/building-plugins#setappinfo
-    appInfo: {
-      name: 'Next.js Subscription Starter',
-      version: '0.1.0',
-    },
-  }
-)
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  // https://github.com/stripe/stripe-node#configuration
+  apiVersion: '2020-08-27',
+  // Register this as an official Stripe plugin.
+  // https://stripe.com/docs/building-plugins#setappinfo
+  appInfo: {
+    name: 'Next.js Subscription Starter',
+    version: '0.1.0',
+  },
+})
