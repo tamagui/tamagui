af03d38cb4f3956688d82fc8d9d8c991870f8c7f
poteboy <poteboy.dev@gmail.com>
2025-03-13 22:45:46 +0900

    cleanup claim api code

--- code/tamagui.dev/features/user/claim-product.ts
+++ code/tamagui.dev/features/user/claim-product.ts
@@ -48,51 +48,6 @@ export const claimTakeoutForProPlan = async (args: ClaimProductArgs) => {
   return claimData
 }
 
-/**
- * @deprecated
- * Since we're migrating to the new Pro plan, we no longer need to switch on the claim_type
- */
-export const claimProductAccess = async (args: ClaimProductArgs) => {
-  const { product } = args
-
-  const metadata = product.metadata
-  if (typeof metadata !== 'object' || !metadata || Array.isArray(metadata)) {
-    throw new Error('bad metadata')
-  }
-
-  let claimData: Awaited<ReturnType<ClaimFunction>>
-
-  // check the product claim type and call the related claim function
-  if (!metadata.claim_type) {
-    throw new Error(`no claim_type present on the product metadata`)
-  }
-  switch (metadata.claim_type) {
-    case 'repo_access':
-      claimData = await claimRepositoryAccess({ ...args, metadata })
-      break
-    case 'supabase_storage_download':
-      claimData = await getDownloadFromSupabaeStorage({ ...args, metadata })
-      break
-    case 'send_to_link':
-      claimData = await sendUserToLink({ ...args, metadata })
-      break
-    default:
-      throw new Error(`unsupported claim_type ${metadata.claim_type} on product metadata`)
-  }
-
-  if (claimData.data) {
-    await supabaseAdmin.from('claims').insert({
-      product_id: product.id,
-      subscription_id: args.type === 'subscription' ? args.subscription.id : undefined,
-      product_ownership_id:
-        args.type === 'product_ownership' ? args.productOwnership.id : undefined,
-      data: { claim_type: metadata.claim_type, ...claimData.data },
-    })
-  }
-
-  return claimData
-}
-
 type ClaimFunction = (
   args: ClaimProductArgs & {
     metadata: {
@@ -162,50 +117,5 @@ const claimRepositoryAccess: ClaimFunction = async ({ user, metadata, request })
     throw new ClaimError(
       'Invitation failed. It could be that you are already invited. Check your email or GitHub notifications for the invite. Otherwise, contact support@tamagui.dev or get help on Discord.'
     )
-  }
-}
-
-const getDownloadFromSupabaeStorage: ClaimFunction = async ({ user, metadata }) => {
-  if (typeof metadata.supabase_storage_bucket !== 'string') {
-    throw new Error('No supabase_storage_bucket is associated to the product metadata.')
-  }
-  if (typeof metadata.supabase_storage_path !== 'string') {
-    throw new Error('No supabase_storage_path is associated to the product metadata.')
-  }
-  try {
-    const { data, error } = await supabaseAdmin.storage
-      .from(metadata.supabase_storage_bucket)
-      .createSignedUrl(metadata.supabase_storage_path, 60 * 60, {})
-    if (error) {
-      throw new Error(error.message)
-    }
-    return {
-      data: {
-        signed_url: data.signedUrl,
-        download_requested_at: new Date().toISOString(),
-      },
-      url: data.signedUrl,
-    }
-  } catch (error) {
-    console.error(
-      `Failed to get a signed url for bucket: "${metadata.supabase_storage_bucket}" and path: "${metadata.supabase_storage_path}", error: ${error}`,
-      error
-    )
-    throw new ClaimError(
-      'Failed to download. Please contact support@tamagui.dev or get help on Discord.'
-    )
-  }
-}
-
-const sendUserToLink: ClaimFunction = async ({ user, metadata }) => {
-  if (typeof metadata.usage_link !== 'string') {
-    console.error('No metadata.usage_link present')
-    throw new ClaimError(
-      'Failed to redirect you. Please contact support@tamagui.dev or get help on Discord.'
-    )
-  }
-  return {
-    data: null,
-    url: metadata.usage_link,
   }
 }
