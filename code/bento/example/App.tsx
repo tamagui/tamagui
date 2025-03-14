2a65366cbf0a5efff02268df824a1aa54afffb62
Bảo Hà <baronn.ha@gmail.com>
2025-03-14 15:28:33 +0700

    bento: Refactor App component to use Accordions and enhance AccordionItem animations

--- code/bento/example/App.tsx
+++ code/bento/example/App.tsx
@@ -5,39 +5,18 @@ import { Separator, Theme, View, XStack, YStack } from 'tamagui'
 import { useSchemeSetting } from '@vxrn/color-scheme'
 
 import { Provider } from '../src/components/provider/Provider'
-import { accordions } from '../src/sections/elements'
+import { accordions as Accordions } from '../src/sections/elements'
 
 if (typeof require !== 'undefined') {
   globalThis['React'] = require('react') // webpack
 }
 
-export default function App() {
-  return <Frame>{accordions({ isProUser: false, showAppropriateModal: () => {} })}</Frame>
-}
-
-const Frame = (props: { children: any }) => {
-  const params = new URLSearchParams(window.location.search)
+const App = (props: { children: any }) => {
   const [{ scheme: theme }] = useSchemeSetting()
-
-  const [screenshot] = useState(params.has('screenshot'))
-  const showThemeSwitch = !screenshot
-  const splitView = params.has('splitView')
-  const centered = params.has('centered')
 
   return (
     <Provider defaultTheme={theme as any}>
       <link href="/fonts/inter.css" rel="stylesheet" />
-
-      {screenshot && (
-        <style
-          type="text/css"
-          dangerouslySetInnerHTML={{
-            __html: `
-            html, body, #root { background-color: transparent !important; }
-          `,
-          }}
-        />
-      )}
 
       <style
         type="text/css"
@@ -49,40 +28,14 @@ const Frame = (props: { children: any }) => {
       />
 
       <YStack fullscreen w="100%" ai="center">
-        <Theme name={screenshot ? 'whale' : undefined}>
-          <View py="$12" w="100%" h="full" maxWidth={1280}>
-            <YStack
-              {...(centered && {
-                ai: 'center',
-                jc: 'center',
-              })}
-              f={1}
-              h="100%"
-            >
-              {props.children}
-            </YStack>
-
-            {splitView ? (
-              <>
-                <Separator vertical />
-                <Theme name="dark">
-                  <YStack
-                    f={1}
-                    {...(centered && {
-                      ai: 'center',
-                      jc: 'center',
-                      h: '100%',
-                    })}
-                    bg={screenshot ? 'transparent' : '$background'}
-                  >
-                    {props.children}
-                  </YStack>
-                </Theme>
-              </>
-            ) : null}
-          </View>
-        </Theme>
+        <View py="$12" w="100%" h="full" maxWidth={1280}>
+          <YStack w="100%">
+            <Accordions isProUser={false} showAppropriateModal={() => {}} />
+          </YStack>
+        </View>
       </YStack>
     </Provider>
   )
 }
+
+export default App
