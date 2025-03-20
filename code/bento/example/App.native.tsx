7ce50de835b5d0490600c1b2def3db4d0b5b46e6
Bảo Hà <baronn.ha@gmail.com>
2025-03-20 18:04:04 +0700

    bento: add RatingButton component and integrate into App and buttons showcase

--- code/bento/example/App.native.tsx
+++ code/bento/example/App.native.tsx
@@ -2,12 +2,12 @@ import 'react-native-gesture-handler'
 
 import { useFonts } from 'expo-font'
 import { useState } from 'react'
-import { LayoutAnimation, LogBox, useColorScheme } from 'react-native'
+import { LogBox, useColorScheme } from 'react-native'
 import { Provider, BottomView } from './components'
 import { type ThemeName, View, YStack } from 'tamagui'
 import { ThemePicker } from './components/ThemePicker'
 import { Background } from './components/Background'
-import { List } from '../src/components'
+import { Buttons } from '../src/components'
 
 LogBox.ignoreAllLogs()
 
@@ -28,25 +28,13 @@ export default function App() {
   return (
     <Provider defaultTheme={colorScheme}>
       <YStack theme={themeName} flex={1}>
-        {/* <Background themeName={themeName} /> */}
-        {/* <View flex={1} justifyContent="center" ai="center" p="$4">
-          <Datepickers.Calendar />
+        <Background themeName={themeName} />
+        <View flex={1} justifyContent="center" ai="center" p="$4">
+          <Buttons.RatingButton />
           <ThemePicker themeColor={colorScheme} setThemeColor={setThemeName} />
-        </View> */}
-        <List.WheelList />
+        </View>
         <BottomView title="WheelList" />
       </YStack>
     </Provider>
   )
 }
-
-const colors: ThemeName[] = [
-  'neonBlue',
-  'cyan',
-  'forest',
-  'teal',
-  'orangeRed',
-  'burgundy',
-  'royalBlue',
-  'supreme',
-]
