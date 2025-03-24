a635c027d5fec2d2d88ca250aac6b677886c63d0
natew <natewienert@gmail.com>
2025-03-24 08:06:08 -1000

    fix(portal): use a root div to fix an esoteric react freeze bug when rendering into document.documentElement

--- code/ui/portal/src/Portal.tsx
+++ code/ui/portal/src/Portal.tsx
@@ -1,22 +1,44 @@
 import '@tamagui/polyfill-dev'
 
-import { isServer } from '@tamagui/constants'
 import { YStack } from '@tamagui/stacks'
+import { useDidFinishSSR } from '@tamagui/use-did-finish-ssr'
+import { useStackedZIndex } from '@tamagui/z-index-stack'
 import * as React from 'react'
 import { createPortal } from 'react-dom'
-
-import { useStackedZIndex } from '@tamagui/z-index-stack'
 import { getStackedZIndexProps } from './helpers'
 import type { PortalProps } from './PortalProps'
+
+// root div for inserting portals into
+// were creating this because otherwise we get bad freezes if you hydrate into document.documentElement
+let rootDiv: HTMLDivElement | null = null
+
+function getRootDiv() {
+  if (rootDiv) {
+    return rootDiv
+  }
+
+  const DEFAULT_PORTAL_ROOT = process.env.TAMAGUI_DEFAULT_PORTAL_ROOT
+    ? document.querySelector(process.env.TAMAGUI_DEFAULT_PORTAL_ROOT)!
+    : globalThis.document?.body
+
+  rootDiv = document.createElement('div')
+  rootDiv.style.display = 'contents'
+  rootDiv.id = `tamagui-root-portal`
+  DEFAULT_PORTAL_ROOT.appendChild(rootDiv)
+
+  rootDiv
+}
 
 // web only version
 
 export const Portal = React.memo((propsIn: PortalProps) => {
-  if (isServer) {
+  const isHydated = useDidFinishSSR()
+
+  if (!isHydated) {
     return null
   }
 
-  const { host = globalThis.document?.body, stackZIndex, children, ...props } = propsIn
+  const { host = getRootDiv(), stackZIndex, children, ...props } = propsIn
   const zIndex = useStackedZIndex(getStackedZIndexProps(propsIn))
 
   return createPortal(
