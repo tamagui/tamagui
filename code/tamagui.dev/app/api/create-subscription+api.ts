08f87759997241af403c3d386b8077da0e6a4af5
poteboy <poteboy.dev@gmail.com>
2025-03-28 14:45:03 +0900

    checkout from pro-team-plan branch

--- code/tamagui.dev/app/api/create-subscription+api.ts
+++ code/tamagui.dev/app/api/create-subscription+api.ts
@@ -9,12 +9,17 @@ import { STRIPE_PRODUCTS } from '~/features/stripe/products'
 const PRO_SUBSCRIPTION_PRICE_ID = STRIPE_PRODUCTS.PRO_SUBSCRIPTION.priceId
 const PRO_ONE_TIME_PRICE_ID = STRIPE_PRODUCTS.PRO_ONE_TIME.priceId
 
+// New Team Seats Price IDs
+const TEAM_SEATS_SUBSCRIPTION_PRICE_ID = STRIPE_PRODUCTS.PRO_TEAM_SEATS.priceId
+const TEAM_SEATS_ONE_TIME_PRICE_ID = STRIPE_PRODUCTS.PRO_TEAM_SEATS_ONE_TIME.priceId
 export default apiRoute(async (req) => {
   if (req.method !== 'POST') {
     return Response.json({ error: 'Method not allowed' }, { status: 405 })
   }
 
-  const { paymentMethodId, disableAutoRenew, couponId } = await req.json()
+  const { paymentMethodId, disableAutoRenew, couponId, teamSeats } = await req.json()
+
+  const teamSeatCount = Number(teamSeats)
 
   if (!paymentMethodId) {
     return Response.json({ error: 'Payment method ID is required' }, { status: 400 })
@@ -50,6 +55,14 @@ export default apiRoute(async (req) => {
         price: PRO_ONE_TIME_PRICE_ID,
       })
 
+      if (teamSeatCount > 0) {
+        await stripe.invoiceItems.create({
+          customer: stripeCustomerId,
+          price: TEAM_SEATS_ONE_TIME_PRICE_ID,
+          quantity: teamSeatCount,
+        })
+      }
+
       // Create and pay invoice
       const invoice = await stripe.invoices.create({
         customer: stripeCustomerId,
@@ -69,10 +82,18 @@ export default apiRoute(async (req) => {
         // clientSecret: await getClientSecret(paidInvoice),
       })
     } else {
+      let items: Stripe.SubscriptionCreateParams.Item[] = [
+        { price: PRO_SUBSCRIPTION_PRICE_ID },
+      ]
+
+      if (teamSeatCount > 0) {
+        items.push({ price: TEAM_SEATS_SUBSCRIPTION_PRICE_ID, quantity: teamSeatCount })
+      }
+
       // Create subscription
       const subscription = await stripe.subscriptions.create({
         customer: stripeCustomerId,
-        items: [{ price: PRO_SUBSCRIPTION_PRICE_ID }],
+        items,
         payment_behavior: 'default_incomplete',
         payment_settings: { save_default_payment_method: 'on_subscription' },
         expand: ['latest_invoice.payment_intent'],
