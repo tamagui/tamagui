d7b164157460ae76d9fdb62d0d0ce112979f78de
Hoa Nguyen <nguyengochoa2011@gmail.com>
2025-04-25 20:45:32 +0700

    Revert "fix(themes): fix to properly de-opt from theme optimization until next bug with css hydration is fixed"
    This reverts commit 97ad105a46e832938f8d0e8060fbed9609c4a2cb.

--- code/core/themes/src/v4.tsx
+++ code/core/themes/src/v4.tsx
@@ -1,4 +1,3 @@
 export { tokens } from './v4-tokens'
 export { createThemes } from '@tamagui/theme-builder'
 export { themes as tamaguiThemes } from './generated-v4-tamagui'
-export { themes as defaultThemes } from './generated-v4'
