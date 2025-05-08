09881888c0ca3daee0faa32acd8928221799e0ad
Pokai Chang <mail@zeta.vg>
2025-05-09 00:56:56 +0800

    test: setup ios native tests for kitchen-sink using appium

---
+++ code/kitchen-sink/tests/sample.test.ios.tsx
@@ -0,0 +1,14 @@
+import { describe, it, expect, test } from 'vitest'
+import { remote } from 'webdriverio'
+import { getWebDriverConfig } from '../testing-utils/getWebDriverConfig'
+const sharedTestOptions = { timeout: 10 * 60 * 1000, retry: 3 }
+
+test('basic iOS test', sharedTestOptions, async () => {
+  const driver = await remote(await getWebDriverConfig())
+
+  const titleElement = await driver.$(
+    '//*[contains(@label, "Kitchen Sink") or contains(@value, "Kitchen Sink") or contains(@name, "Kitchen Sink") or contains(text(), "Kitchen Sink")]'
+  )
+  await titleElement.waitForDisplayed({ timeout: 20_000 })
+  expect((await titleElement.getText()).includes('Kitchen Sink')).toBe(true)
+})
