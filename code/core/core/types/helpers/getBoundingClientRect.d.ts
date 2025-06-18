690cea8c82f83af33f4fc0bbc70bb223a2f3c5b4
natew <natewienert@gmail.com>
2025-06-17 20:31:47 -1000

    feat: bring back best of async and sync strategies from before, default to new balanced method and document better

--- code/core/core/types/helpers/getBoundingClientRect.d.ts
+++ code/core/core/types/helpers/getBoundingClientRect.d.ts
@@ -1,2 +1,3 @@
 export declare const getBoundingClientRect: (node: HTMLElement | null) => undefined | DOMRect;
+export declare const getBoundingClientRectAsync: (element: HTMLElement) => Promise<DOMRectReadOnly | undefined>;
 //# sourceMappingURL=getBoundingClientRect.d.ts.map
--- code/core/core/types/hooks/useElementLayout.d.ts
+++ code/core/core/types/hooks/useElementLayout.d.ts
@@ -1,7 +1,7 @@
 import { type TamaguiComponentStateRef } from '@tamagui/web';
 import type { RefObject } from 'react';
-type LayoutMeasurementStatus = 'inactive' | 'active';
-export declare function setOnLayoutStrategy(state: LayoutMeasurementStatus): void;
+type LayoutMeasurementStrategy = 'off' | 'sync' | 'async';
+export declare function setOnLayoutStrategy(state: LayoutMeasurementStrategy): void;
 export type LayoutValue = {
     x: number;
     y: number;
@@ -19,6 +19,8 @@ export type LayoutEvent = {
 };
 export declare const getElementLayoutEvent: (target: HTMLElement) => LayoutEvent;
 export declare const measureLayout: (node: HTMLElement, relativeTo: HTMLElement | null, callback: (x: number, y: number, width: number, height: number, left: number, top: number) => void) => void;
+export declare const getElementLayoutEventAsync: (target: HTMLElement) => Promise<LayoutEvent>;
+export declare const measureLayoutAsync: (node: HTMLElement, relativeTo: HTMLElement | null, callback: (x: number, y: number, width: number, height: number, left: number, top: number) => void) => Promise<void>;
 export declare function useElementLayout(ref: RefObject<TamaguiComponentStateRef>, onLayout?: ((e: LayoutEvent) => void) | null): void;
 export {};
 //# sourceMappingURL=useElementLayout.d.ts.map
