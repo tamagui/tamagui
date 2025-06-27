0c3f4da81a1f9fe4bc29c02c84bb1443b543149a
natew <natewienert@gmail.com>
2025-06-26 17:41:36 -1000

    perf(core): avoid more re-renders on initial mount when disableSSR is enabled

--- code/core/web/src/views/Configuration.tsx
+++ code/core/web/src/views/Configuration.tsx
@@ -1,16 +1,25 @@
-import React from 'react'
-
+import { ClientOnly, ClientOnlyContext } from '@tamagui/use-did-finish-ssr'
+import React, { useContext } from 'react'
 import { ComponentContext } from '../contexts/ComponentContext'
 import type { AnimationDriver } from '../types'
 
 interface ConfigurationProps {
   animationDriver?: AnimationDriver | null
+  disableSSR?: boolean
   children: React.ReactNode
 }
 
 export const Configuration = (props: ConfigurationProps) => {
   const current = React.useContext(ComponentContext)
-  return <ComponentContext.Provider {...current} {...props} />
+  const clientOnly = useContext(ClientOnlyContext)
+
+  const children = <ComponentContext.Provider {...current} {...props} />
+
+  if (clientOnly) {
+    return <ClientOnly>{children}</ClientOnly>
+  }
+
+  return children
 }
 
 Configuration['displayName'] = 'Configuration'
