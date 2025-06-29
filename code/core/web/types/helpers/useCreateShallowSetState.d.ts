0ef862d61b25f8732b37689a8e47d868c64fe5d4
natew <natewienert@gmail.com>
2025-06-29 10:28:50 -1000

    cleanups on site

--- code/core/web/types/helpers/useCreateShallowSetState.d.ts
+++
@@ -1,5 +0,0 @@
-import { type DebugProp } from '@tamagui/is-equal-shallow';
-export type CallbackSetState<State> = (next: (cb: State) => State) => void;
-export declare function useCreateShallowSetState<State extends Record<string, unknown>>(setter: CallbackSetState<State>, debugIn?: DebugProp): (next: Partial<State>) => void;
-export { isEqualShallow, mergeIfNotShallowEqual } from '@tamagui/is-equal-shallow';
-//# sourceMappingURL=useCreateShallowSetState.d.ts.map
--- code/packages/is-equal-shallow/types/index.d.ts
+++ code/packages/is-equal-shallow/types/index.d.ts
@@ -1,7 +1,8 @@
-type DebugProp = null | undefined | boolean | 'profile' | 'verbose' | 'break';
+type DebugProp = null | undefined | boolean | "profile" | "verbose" | "break";
 export type CallbackSetState<State> = (next: (cb: State) => State) => void;
 export declare function useCreateShallowSetState<State extends Record<string, unknown>>(setter: CallbackSetState<State>, debugIn?: DebugProp): (next: Partial<State>) => void;
 export declare function mergeIfNotShallowEqual<State extends Record<string, unknown>>(prev: State, next: Partial<State>, debug?: DebugProp): State;
-export declare function isEqualShallow(prev: any, next: any): boolean;
+export declare function isEqualShallow(prev: Object, next: Object): boolean;
 export {};
+
 //# sourceMappingURL=index.d.ts.map
