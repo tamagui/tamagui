8cdfa201d91290798d6759f82d98c816229ecc75
natew <natewienert@gmail.com>
2025-07-04 08:50:28 -1000

    fix(core): avoid re-parenting when presence context needs change

--- code/core/use-presence/src/PresenceContext.tsx
+++ code/core/use-presence/src/PresenceContext.tsx
@@ -4,6 +4,14 @@ import * as React from 'react'
 export const PresenceContext: React.Context<PresenceContextProps | null> =
   React.createContext<PresenceContextProps | null>(null)
 
-export const ResetPresence = (props: { children?: any }): React.JSX.Element => (
-  <PresenceContext.Provider value={null}>{props.children}</PresenceContext.Provider>
-)
+export const ResetPresence = (props: {
+  children?: React.ReactNode
+  disable?: boolean
+}): React.ReactNode => {
+  const parent = React.useContext(PresenceContext)
+  return (
+    <PresenceContext.Provider value={props.disable ? parent : null}>
+      {props.children}
+    </PresenceContext.Provider>
+  )
+}
