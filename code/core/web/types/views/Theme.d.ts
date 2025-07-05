9de007a1a39a13fad217d6cf87cfc73fc06f0b9f
natew <natewienert@gmail.com>
2025-07-05 13:02:00 -1000

    fix imports

--- code/core/web/types/views/Theme.d.ts
+++ code/core/web/types/views/Theme.d.ts
@@ -2,10 +2,16 @@ import type { MutableRefObject } from 'react';
 import React from 'react';
 import { type ThemeState } from '../hooks/useThemeState';
 import type { ThemeProps } from '../types';
+type ThemeComponentPropsOnly = ThemeProps & {
+    passThrough?: boolean;
+    contain?: boolean;
+};
 export declare const Theme: React.ForwardRefExoticComponent<ThemeProps & {
     passThrough?: boolean;
+    contain?: boolean;
 } & React.RefAttributes<unknown>>;
-export declare function getThemedChildren(themeState: ThemeState, children: any, props: ThemeProps, isRoot: boolean | undefined, stateRef: MutableRefObject<{
+export declare function getThemedChildren(themeState: ThemeState, children: any, props: ThemeComponentPropsOnly, isRoot: boolean | undefined, stateRef: MutableRefObject<{
     hasEverThemed?: boolean | 'wrapped';
 }>, passThrough?: boolean): any;
+export {};
 //# sourceMappingURL=Theme.d.ts.map
