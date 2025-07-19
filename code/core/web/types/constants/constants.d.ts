b4a2187aa02de0670922c88ea49608c5d0526d88
Quan Nguyen <anhquan291@gmail.com>
2025-07-19 20:40:40 +0700

    v2: update stackDefaultStyles types

--- code/core/web/types/constants/constants.d.ts
+++ code/core/web/types/constants/constants.d.ts
@@ -2,9 +2,7 @@ import type { StackStyle } from '../types';
 export declare const THEME_NAME_SEPARATOR = "_";
 export declare const THEME_CLASSNAME_PREFIX = "t_";
 export declare const FONT_DATA_ATTRIBUTE_NAME = "data-tamagui-font";
-export declare const stackDefaultStyles: {
-    position: string;
-};
+export declare const stackDefaultStyles: {};
 export declare const webViewFlexCompatStyles: StackStyle;
 export declare const MISSING_THEME_MESSAGE: string;
 //# sourceMappingURL=constants.d.ts.map
