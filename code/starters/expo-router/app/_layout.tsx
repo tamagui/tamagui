f060705bb912b13b96d3eae6bceecb5616e55f0a
David Leuliette <dleuliette@gmail.com>
2025-07-29 09:12:43 +0200

    fix headerRight button sizing on expo-router template

--- code/starters/expo-router/app/_layout.tsx
+++ code/starters/expo-router/app/_layout.tsx
@@ -6,7 +6,7 @@ import { StatusBar } from 'expo-status-bar'
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
 import { SplashScreen, Stack } from 'expo-router'
-import { Provider } from '../components/Provider'
+import { Provider } from 'components/Provider'
 import { useTheme } from 'tamagui'
 
 export {
