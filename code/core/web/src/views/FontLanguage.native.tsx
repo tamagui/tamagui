1fa8a47e32d0bc483da15b79e6ec9d7486a3dfb5
natew <natewienert@gmail.com>
2025-08-04 15:59:54 -1000

    lint fix

--- code/core/web/src/views/FontLanguage.native.tsx
+++ code/core/web/src/views/FontLanguage.native.tsx
@@ -4,10 +4,12 @@ import { ComponentContext } from '../contexts/ComponentContext'
 import type { FontLanguageProps } from './FontLanguage.types'
 
 export const FontLanguage = ({ children, ...props }: FontLanguageProps) => {
-  const parentProps = React.useContext(ComponentContext);
+  const parentProps = React.useContext(ComponentContext)
   const language = React.useMemo(() => props, [JSON.stringify(props)])
   return (
-    <ComponentContext.Provider {...parentProps} language={language}>{children}</ComponentContext.Provider>
+    <ComponentContext.Provider {...parentProps} language={language}>
+      {children}
+    </ComponentContext.Provider>
   )
 }
 
