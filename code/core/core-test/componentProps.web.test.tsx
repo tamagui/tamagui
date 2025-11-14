55da58b96ee3a41a6b7180e80a2b6d5fc230f127
natew <natewienert@gmail.com>
2025-11-13 16:39:22 -1000

    fix tests

--- code/core/core-test/componentProps.web.test.tsx
+++ code/core/core-test/componentProps.web.test.tsx
@@ -27,7 +27,7 @@ describe('animation props', () => {
           style="display: contents;"
         >
           <div
-            class="_dsp-flex _ai-stretch _fd-column _fb-auto _bxs-border-box _pos-relative _mih-0px _miw-0px _fs-0 _bg-red _mt-200px _mr-200px _mb-200px _ml-200px _transform-translateX01303033"
+            class="_dsp-flex _ai-stretch _fd-column _fb-auto _bxs-border-box _pos-relative _mih-0px _miw-0px _fs-0 _bg-red _mt-200px _mr-200px _mb-200px _ml-200px _tr-translateX01303033"
             id="test-native-id"
           />
         </span>
