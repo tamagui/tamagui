8292f09965100543e53dc49c1608986c6e01a292
Quan Nguyen <anhquan291@gmail.com>
2025-11-25 00:25:02 -0500

    update kitchen sink

--- code/kitchen-sink/src/Navigation.tsx
+++ code/kitchen-sink/src/Navigation.tsx
@@ -1,10 +1,11 @@
+import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Sandbox } from './Sandbox'
 import { DemoScreen } from './features/demos/demo-screen'
+import { ColorSchemeToggle } from './features/home/ColorSchemeListItem'
 import { HomeScreen } from './features/home/screen'
 import { TestCasesScreen } from './features/testcases/screen'
 import { TestScreen } from './features/testcases/test-screen'
-import { ColorSchemeToggle } from './features/home/ColorSchemeListItem'
 
 const Stack = createNativeStackNavigator<{
   home: undefined
@@ -18,50 +19,74 @@ const Stack = createNativeStackNavigator<{
   sandbox: undefined
 }>()
 
+const linking = {
+  prefixes: [],
+  config: {
+    screens: {
+      home: '',
+      demo: 'demo/:id',
+      tests: 'tests',
+      test: 'test/:id',
+      sandbox: 'sandbox',
+    },
+  },
+}
+
 export function Navigation() {
   return (
-    <Stack.Navigator initialRouteName="home">
-      <Stack.Screen name="home" component={HomeScreen} />
-      <Stack.Screen
-        name="sandbox"
-        component={Sandbox}
-        options={{
-          title: 'Sandbox',
-          headerRight() {
-            return <ColorSchemeToggle />
-          },
-        }}
-      />
-      <Stack.Screen
-        name="demo"
-        component={DemoScreen}
-        options={{
-          title: 'Demo',
-          headerRight() {
-            return <ColorSchemeToggle />
-          },
-        }}
-      />
-      <Stack.Screen
-        name="tests"
-        component={TestCasesScreen}
-        options={{
-          title: 'Test Cases',
-          headerRight() {
-            return <ColorSchemeToggle />
-          },
-        }}
-      />
-      <Stack.Screen
-        name="test"
-        component={TestScreen}
-        options={{
-          title: 'Test Case',
-          headerRight() {
-            return <ColorSchemeToggle />
-          },
-        }}
-      />
-    </Stack.Navigator>
+    <NavigationContainer linking={linking}>
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen
+          name="home"
+          component={HomeScreen}
+          options={{
+            title: 'Home',
+            headerRight() {
+              return <ColorSchemeToggle />
+            },
+          }}
+        />
+        <Stack.Screen
+          name="sandbox"
+          component={Sandbox}
+          options={{
+            title: 'Sandbox',
+            headerRight() {
+              return <ColorSchemeToggle />
+            },
+          }}
+        />
+        <Stack.Screen
+          name="demo"
+          component={DemoScreen}
+          options={{
+            title: 'Demo',
+            headerRight() {
+              return <ColorSchemeToggle />
+            },
+          }}
+        />
+        <Stack.Screen
+          name="tests"
+          component={TestCasesScreen}
+          options={{
+            title: 'Test Cases',
+            headerRight() {
+              return <ColorSchemeToggle />
+            },
+          }}
+        />
+        <Stack.Screen
+          name="test"
+          component={TestScreen}
+          options={{
+            title: 'Test Case',
+            headerRight() {
+              return <ColorSchemeToggle />
+            },
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
