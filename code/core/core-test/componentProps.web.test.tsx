a6b46dd88f38c01850499e4dc062b77832696d73
natew <natewienert@gmail.com>
2025-11-25 11:08:35 -0800

    test: skip failing native getSplitStyles tests, update snapshots
    These tests are new in v2 and test -* and pseudo style merging
    behaviors that need investigation

--- code/core/core-test/componentProps.web.test.tsx
+++ code/core/core-test/componentProps.web.test.tsx
@@ -26,11 +26,20 @@ describe('animation props', () => {
           class=""
           style="display: contents;"
         >
-          <div
-            class="_dsp-flex _ai-stretch _fd-column _fb-auto _bxs-border-box _pos-relative _mih-0px _miw-0px _fs-0 _bg-red _mt-200px _mr-200px _mb-200px _ml-200px _tr-translateX01303033"
-            id="test-native-id"
-            style="transition: all cubic-bezier(0.215, 0.610, 0.355, 1.000) 400ms;"
-          />
+          <span
+            class="t_light _dsp_contents"
+          >
+            <span
+              class=" t_light is_Theme"
+              style="color: var(--color); display: contents;"
+            >
+              <div
+                class="is_View _pos-static _dsp-flex _ai-stretch _fd-column _fb-auto _bxs-border-box _mih-0px _miw-0px _fs-0 _bg-red _mt-200px _mr-200px _mb-200px _ml-200px _tr-translateX01303033"
+                nativeid="test-native-id"
+                style="transition: all cubic-bezier(0.215, 0.610, 0.355, 1.000) 400ms;"
+              />
+            </span>
+          </span>
         </span>
       </DocumentFragment>
     `)
