351d40f8e4b050a9439f40429db2e067cd73dc4c
natew <natewienert@gmail.com>
2025-11-28 18:05:10 -0800

    Revert "feat(portal): remove deep imports, move to to /legacy-portal export"
    Reason - need to go further with this, we should redo how we recommend
    setting up these components, instead use scope + mount at root and avoid
    portals altogether and re-propagation.
    
    This reverts commit 057e219b7174d4c518746e175e71c60c59963140.

--- code/ui/portal/src/Portal.native.tsx
+++ code/ui/portal/src/Portal.native.tsx
@@ -2,15 +2,41 @@ import { View } from '@tamagui/core'
 import { useStackedZIndex } from '@tamagui/z-index-stack'
 import * as React from 'react'
 import { RootTagContext } from 'react-native'
+import { IS_FABRIC, USE_NATIVE_PORTAL } from './constants'
 import { GorhomPortalItem } from './GorhomPortalItem'
 import { getStackedZIndexProps } from './helpers'
 import type { PortalProps } from './PortalProps'
-import { getCreatePortal } from './setupPortal'
+
+const createPortal = (() => {
+  if (IS_FABRIC) {
+    try {
+      const ReactFabricShimModule = require('react-native/Libraries/Renderer/shims/ReactFabric')
+
+      return (
+        ReactFabricShimModule?.default?.createPortal ?? ReactFabricShimModule.createPortal
+      )
+    } catch (err) {
+      console.info(`Note: error importing portal, defaulting to non-native portals`, err)
+      return null
+    }
+  }
+  try {
+    const ReactNativeShimModule =
+      require('react-native/Libraries/Renderer/shims/ReactNative')
+
+    return (
+      ReactNativeShimModule?.default?.createPortal ?? ReactNativeShimModule.createPortal
+    )
+  } catch (err) {
+    console.info(`Note: error importing portal, defaulting to non-native portals`, err)
+    return null
+  }
+})()
 
 export const Portal = (propsIn: PortalProps) => {
   const rootTag = React.useContext(RootTagContext)
   const zIndex = useStackedZIndex(getStackedZIndexProps(propsIn))
-  const createPortal = getCreatePortal()
+
   const { children, passThrough } = propsIn
 
   const contents = (
@@ -26,7 +52,7 @@ export const Portal = (propsIn: PortalProps) => {
     </View>
   )
 
-  if (!createPortal || !rootTag) {
+  if (!createPortal || !USE_NATIVE_PORTAL || !rootTag) {
     return (
       <GorhomPortalItem passThrough={passThrough} hostName="root">
         {contents}
