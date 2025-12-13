53fca2ebdf2a4addd50a1f7cf593c57c34770df2
natew <natewienert@gmail.com>
2025-12-13 09:19:53 -0600

    add skip checks option

--- scripts/release.ts
+++ scripts/release.ts
@@ -37,6 +37,7 @@ const skipTest =
 const skipNativeTests =
   process.argv.includes('--skip-native-test') ||
   process.argv.includes('--skip-native-tests')
+const skipChecks = process.argv.includes('--skip-checks')
 const skipBuild = shouldFinish || rePublish || process.argv.includes('--skip-build')
 const buildFast = process.argv.includes('--build-fast')
 const dryRun = process.argv.includes('--dry-run')
@@ -215,15 +216,17 @@ async function run() {
 
     // run checks
     if (!shouldFinish) {
-      console.info('run checks')
-      if (!skipTest) {
+      if (!skipChecks) {
+        console.info('run checks')
         await Promise.all([
           spawnify(`chmod ug+x ./node_modules/.bin/tamagui`),
-          // spawnify(`yarn playwright install`),
           spawnify(`yarn check`),
           spawnify(`yarn lint`),
         ])
         await spawnify(`yarn typecheck`)
+      }
+      if (!skipTest) {
+        console.info('run tests')
         await spawnify(`yarn test`, {
           env: {
             ...process.env,
