93a0df408a7e84e79a0bba76f00a11ac2a154aac
DaveyEke <daveydenco@gmail.com>
2025-12-17 06:57:55 +0100

    fix lint

--- code/demos/src/ProgressDemo.tsx
+++ code/demos/src/ProgressDemo.tsx
@@ -3,7 +3,6 @@ import type { SizeTokens } from 'tamagui'
 import { Button, Paragraph, Progress, Slider, XStack, YStack } from 'tamagui'
 import { Play, RotateCcw } from '@tamagui/lucide-icons'
 
-
 /**
  * Note: If you're going to use the vertical slider behavior on iOS, make sure to pass insets to TamaguiProvider:
  * <TamaguiProvider insets={useSafeAreaInsets()}>
@@ -29,7 +28,7 @@ export function ProgressDemo() {
           Size: {size}
         </Paragraph>
         <Progress key={key} size={sizeProp} value={progress} bg="$color5">
-          <Progress.Indicator  bg="$background" />
+          <Progress.Indicator bg="$background" />
         </Progress>
       </YStack>
 
