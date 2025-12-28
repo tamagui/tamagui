808b502c89214ad1ecb139fac8a12579aa1b1add
natew <natewienert@gmail.com>
2025-12-28 08:03:10 -1000

    refactor(ui): update Stacks, Text, and core components

--- code/ui/shapes/types/Circle.d.ts
+++ code/ui/shapes/types/Circle.d.ts
@@ -1,24 +1,18 @@
 import type { GetProps } from '@tamagui/web';
 export declare const Circle: import("@tamagui/web").TamaguiComponent<import("@tamagui/web").TamaDefer, import("@tamagui/web").TamaguiElement, import("@tamagui/core").RNTamaguiViewNonStyleProps, import("@tamagui/web").StackStyleBase, {
     elevation?: number | import("@tamagui/web").SizeTokens | undefined;
-    inset?: number | import("@tamagui/web").SizeTokens | {
-        top?: number;
-        bottom?: number;
-        left?: number;
-        right?: number;
-    } | null | undefined;
     size?: number | import("@tamagui/web").SizeTokens | undefined;
+    transparent?: boolean | undefined;
     fullscreen?: boolean | undefined;
     circular?: boolean | undefined;
+    backgrounded?: boolean | undefined;
+    radiused?: boolean | undefined;
     hoverTheme?: boolean | undefined;
     pressTheme?: boolean | undefined;
     focusTheme?: boolean | undefined;
+    padded?: boolean | undefined;
     elevate?: boolean | undefined;
     bordered?: number | boolean | undefined;
-    transparent?: boolean | undefined;
-    backgrounded?: boolean | undefined;
-    radiused?: boolean | undefined;
-    padded?: boolean | undefined;
     chromeless?: boolean | "all" | undefined;
 }, import("@tamagui/web").StaticConfigPublic & {
     memo: true;
