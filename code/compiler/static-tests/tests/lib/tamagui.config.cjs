af4bf509ef26fa859cfe66a84c714a0f5e1a2cec
natew <natewienert@gmail.com>
2025-12-28 01:17:58 -1000

    fix: handle invalid identifier in theme variables
    Use computed property access for theme variables with special characters
    (e.g., `$invalid-identifier` generates `theme["invalid-identifier"].get()`)
    
    Fixes #3737
    
    Co-Authored-By: vcup <me@vcup.moe>

--- code/compiler/static-tests/tests/lib/tamagui.config.cjs
+++ code/compiler/static-tests/tests/lib/tamagui.config.cjs
@@ -2,4 +2,7 @@ const { createTamagui } = require('@tamagui/core')
 
 const conf = require('@tamagui/config/v3').config
 
+// Add an invalid identifier color token (with hyphen) to test the fix for #3737
+conf.tokens.color['invaild-identifier'] = conf.tokens.color.white
+
 module.exports = createTamagui(conf)
