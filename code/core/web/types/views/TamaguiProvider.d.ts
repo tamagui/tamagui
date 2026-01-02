ec29bc6894243ce03a9c651fff4cd27c48b10f16
Quan Nguyen <anhquan291@gmail.com>
2026-01-02 12:27:11 -0300

    chore: types update

--- code/core/web/types/views/TamaguiProvider.d.ts
+++ code/core/web/types/views/TamaguiProvider.d.ts
@@ -1,5 +1,5 @@
 import type { TamaguiProviderProps } from '../types';
-export declare function TamaguiProvider({ children, disableInjectCSS, config, className, defaultTheme, reset, }: TamaguiProviderProps): import("react/jsx-runtime").JSX.Element;
+export declare function TamaguiProvider({ children, disableInjectCSS, config, className, defaultTheme, reset, insets, }: TamaguiProviderProps): import("react/jsx-runtime").JSX.Element;
 export declare namespace TamaguiProvider {
     var displayName: string;
 }
