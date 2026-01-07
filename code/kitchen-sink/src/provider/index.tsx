f8d296b9c05633989080a3d90bbdc2c8487e36ca
Dave Mkpa-Eke <157598952+DaveyEke@users.noreply.github.com>
2026-01-07 22:43:17 +0100

    fix: add insets to the tamaguu provider for kitchen-sink (#3782)

--- code/kitchen-sink/src/provider/index.tsx
+++ code/kitchen-sink/src/provider/index.tsx
@@ -1,6 +1,8 @@
 import { ToastProvider } from '@tamagui/toast'
+import { isWeb } from '@tamagui/constants'
 import type { TamaguiProviderProps } from 'tamagui'
 import { TamaguiProvider } from 'tamagui'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 import config from '../tamagui.config'
 
@@ -8,8 +10,9 @@ export function Provider({
   children,
   ...rest
 }: Omit<Partial<TamaguiProviderProps>, 'config'>) {
+  const insets = isWeb ? undefined : useSafeAreaInsets()
   return (
-    <TamaguiProvider config={config} defaultTheme="light" {...rest}>
+    <TamaguiProvider config={config} defaultTheme="light" insets={insets} {...rest}>
       <ToastProvider swipeDirection="horizontal">{children}</ToastProvider>
     </TamaguiProvider>
   )
