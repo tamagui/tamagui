67d7bc7bec5b5b214a8175953b387c66748f8750
natew <natewienert@gmail.com>
2026-01-11 11:45:18 -1000

    chore: regenerate types with contentContainerStyle fix

--- code/ui/popover/types/Popover.d.ts
+++ code/ui/popover/types/Popover.d.ts
@@ -204,19 +204,19 @@ export declare const Popover: React.ForwardRefExoticComponent<Omit<PopperProps, 
         };
     };
     ScrollView: React.ForwardRefExoticComponent<Omit<import("@tamagui/core").TamaguiComponentPropsBaseBase & import("react-native").ScrollViewProps, keyof import("@tamagui/core").StackStyleBase | "fullscreen" | "contentContainerStyle"> & import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStyleBase & {
-        readonly contentContainerStyle?: Partial<import("@tamagui/core").GetFinalProps<import("react-native").ScrollViewProps, import("@tamagui/core").StackStyleBase, {}>> | undefined;
+        readonly contentContainerStyle?: Partial<import("@tamagui/core").WithThemeShorthandsPseudosMedia<import("@tamagui/core").StackStyleBase, {}>> | undefined;
     }> & {
         fullscreen?: boolean | undefined;
     } & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStyleBase & {
-        readonly contentContainerStyle?: Partial<import("@tamagui/core").GetFinalProps<import("react-native").ScrollViewProps, import("@tamagui/core").StackStyleBase, {}>> | undefined;
+        readonly contentContainerStyle?: Partial<import("@tamagui/core").WithThemeShorthandsPseudosMedia<import("@tamagui/core").StackStyleBase, {}>> | undefined;
     }>> & import("@tamagui/core").WithPseudoProps<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStyleBase & {
-        readonly contentContainerStyle?: Partial<import("@tamagui/core").GetFinalProps<import("react-native").ScrollViewProps, import("@tamagui/core").StackStyleBase, {}>> | undefined;
+        readonly contentContainerStyle?: Partial<import("@tamagui/core").WithThemeShorthandsPseudosMedia<import("@tamagui/core").StackStyleBase, {}>> | undefined;
     }> & {
         fullscreen?: boolean | undefined;
     } & import("@tamagui/core").WithShorthands<import("@tamagui/core").WithThemeValues<import("@tamagui/core").StackStyleBase & {
-        readonly contentContainerStyle?: Partial<import("@tamagui/core").GetFinalProps<import("react-native").ScrollViewProps, import("@tamagui/core").StackStyleBase, {}>> | undefined;
+        readonly contentContainerStyle?: Partial<import("@tamagui/core").WithThemeShorthandsPseudosMedia<import("@tamagui/core").StackStyleBase, {}>> | undefined;
     }>>> & import("@tamagui/core").WithMediaProps<import("@tamagui/core").WithThemeShorthandsAndPseudos<import("@tamagui/core").StackStyleBase & {
-        readonly contentContainerStyle?: Partial<import("@tamagui/core").GetFinalProps<import("react-native").ScrollViewProps, import("@tamagui/core").StackStyleBase, {}>> | undefined;
+        readonly contentContainerStyle?: Partial<import("@tamagui/core").WithThemeShorthandsPseudosMedia<import("@tamagui/core").StackStyleBase, {}>> | undefined;
     }, {
         fullscreen?: boolean | undefined;
     }>> & {
