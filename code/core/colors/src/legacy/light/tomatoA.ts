633966b3ba5783d78fbc0918984a42e6ff1d34ea
natew <natewienert@gmail.com>
2026-01-11 12:21:22 -1000

    avoid breaking old config colors

---
+++ code/core/colors/src/legacy/light/tomatoA.ts
@@ -0,0 +1,14 @@
+export const tomatoA = {
+  tomatoA1: 'hsla(0, 100%, 51.0%, 0.012)',
+  tomatoA2: 'hsla(8, 100%, 51.0%, 0.032)',
+  tomatoA3: 'hsla(7, 100%, 50.2%, 0.067)',
+  tomatoA4: 'hsla(8, 100%, 50.1%, 0.114)',
+  tomatoA5: 'hsla(7, 99.5%, 47.9%, 0.173)',
+  tomatoA6: 'hsla(9, 99.9%, 46.2%, 0.255)',
+  tomatoA7: 'hsla(10, 99.8%, 43.6%, 0.365)',
+  tomatoA8: 'hsla(10, 99.5%, 41.8%, 0.499)',
+  tomatoA9: 'hsla(10, 99.9%, 43.8%, 0.820)',
+  tomatoA10: 'hsla(10, 100%, 41.8%, 0.859)',
+  tomatoA11: 'hsla(10, 99.9%, 38.8%, 0.922)',
+  tomatoA12: 'hsla(10, 99.0%, 7.4%, 0.934)',
+}
