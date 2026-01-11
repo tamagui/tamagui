ecf2cd5b8851ef68a56fda3a41e0762bd8d783b4
natew <natewienert@gmail.com>
2026-01-11 08:33:10 -1000

    site: update code example for is_View removal

--- code/tamagui.dev/features/mdx/codeExamples.tsx
+++ code/tamagui.dev/features/mdx/codeExamples.tsx
@@ -119,7 +119,7 @@ const _cn5 = " _fos-16px"
 const _cn4 = " _fos-22px"
 const _cn3 = " _bg-180kg62 _col-b5vn3b _mt-0px _mr-0px _mb-0px _ml-0px _ww-break-word _bxs-border-box _ff-System _dsp-inline  "
 const _cn2 = "  font_System"
-const _cn = "  is_View _fd-column _miw-0px _mih-0px _pos-relative _bxs-border-box _fb-auto _dsp-flex _fs-0 _ai-stretch  _w-550px _pr-1aj14ca _pl-1aj14ca  _pr-_gtSm_lrpixp _pl-_gtSm_lrpixp"`,
+const _cn = "  _fd-column _miw-0px _mih-0px _pos-relative _bxs-border-box _fb-auto _dsp-flex _fs-0 _ai-stretch  _w-550px _pr-1aj14ca _pl-1aj14ca  _pr-_gtSm_lrpixp _pl-_gtSm_lrpixp"`,
           language: 'tsx',
         },
         {
