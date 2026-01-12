4e61d96ab57c43609ae3f255063359560da09e62
natew <natewienert@gmail.com>
2026-01-11 20:38:23 -1000

    fix: update snapshot for nativeID to id change

--- code/core/core-test/componentProps.web.test.tsx
+++ code/core/core-test/componentProps.web.test.tsx
@@ -35,7 +35,7 @@ describe('animation props', () => {
             >
               <div
                 class="_dsp-flex _ai-stretch _fd-column _fb-auto _bxs-border-box _mih-0px _miw-0px _fs-0 _bg-red _mt-200px _mr-200px _mb-200px _ml-200px _tr-translateX01303033"
-                nativeid="test-native-id"
+                id="test-native-id"
                 style="transition: all cubic-bezier(0.215, 0.610, 0.355, 1.000) 400ms;"
               />
             </span>
