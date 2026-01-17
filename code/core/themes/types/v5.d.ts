93dd2d170b40b785716c3eac4895658f94fe5ca0
natew <natewienert@gmail.com>
2026-01-16 22:26:15 -1000

    fix(themes): export more v5 building blocks

--- code/core/themes/types/v5.d.ts
+++ code/core/themes/types/v5.d.ts
@@ -1,5 +1,5 @@
 export { tokens } from './v5-tokens';
-export { createThemes } from '@tamagui/theme-builder';
+export { createThemes, defaultComponentThemes } from '@tamagui/theme-builder';
 export { themes } from './generated-v5';
-export { createV5Theme } from './v5-themes';
+export { createV5Theme, defaultChildrenThemes, defaultGrandChildrenThemes, defaultDarkPalette, defaultLightPalette, } from './v5-themes';
 //# sourceMappingURL=v5.d.ts.map
