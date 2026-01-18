f1341395538e1e37594c8c680d27552e090f6c1a
natew <natewienert@gmail.com>
2026-01-18 00:13:11 -1000

    various conversions

--- code/core/colors/README.md
+++ code/core/colors/README.md
@@ -1,9 +1,5 @@
 Based on radix colors
 
-## Installation
-
-`yarn add @tamagui/colors`
-
 ## Authors
 
 - Colm Tuite ([@colmtuite](https://twitter.com/colmtuite))
--- scripts/run.sh
+++ scripts/run.sh
@@ -2,12 +2,4 @@
 
 args="$@"
 
-# yarn workspaces foreach -Api -j unlimited run $args
-
-# if command -v bun >/dev/null 2>&1; then
-#   bun run --filter '*' $args
-# else
-#   turbo run build --no-cache --force -- $args
-# fi
-
 ultra -r --no-pretty --concurrency 400 $args
