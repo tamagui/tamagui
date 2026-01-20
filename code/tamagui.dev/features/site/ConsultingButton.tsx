cfcff722f6f5cbe3c95e4d49b075d659f56aad4b
natew <natewienert@gmail.com>
2026-01-20 12:58:00 -1000

    fix lint

--- code/tamagui.dev/features/site/ConsultingButton.tsx
+++ code/tamagui.dev/features/site/ConsultingButton.tsx
@@ -5,22 +5,22 @@ export const ConsultingButton = (props: ButtonProps) => {
   return (
     <Theme name="black">
       <Button.Frame
-      borderWidth={0.5}
-      borderColor="$color6"
-      size="$3"
-      rounded="$10"
-      hoverStyle={{
-        borderColor: '$color7',
-        bg: '$color5',
-      }}
-      {...props}
-    >
-      <Button.Text fontFamily="$silkscreen" fontSize={12}>
-        Consulting
-      </Button.Text>
-      <YStack display="inline-flex" mx="$2">
-        <AddevenIcon scale={0.75} />
-      </YStack>
+        borderWidth={0.5}
+        borderColor="$color6"
+        size="$3"
+        rounded="$10"
+        hoverStyle={{
+          borderColor: '$color7',
+          bg: '$color5',
+        }}
+        {...props}
+      >
+        <Button.Text fontFamily="$silkscreen" fontSize={12}>
+          Consulting
+        </Button.Text>
+        <YStack display="inline-flex" mx="$2">
+          <AddevenIcon scale={0.75} />
+        </YStack>
       </Button.Frame>
     </Theme>
   )
