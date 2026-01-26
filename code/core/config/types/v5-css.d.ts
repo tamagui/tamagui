3f55c29768256154c014127862fb0d00a56a464c
natew <natewienert@gmail.com>
2026-01-26 04:53:32 -1000

    canary2.0.0-1769439083296

--- code/core/config/types/v5-css.d.ts
+++ code/core/config/types/v5-css.d.ts
@@ -22,4 +22,28 @@ export declare const animationsCSS: import("@tamagui/web").AnimationDriver<{
     quickest: string;
     quickestLessBouncy: string;
 }>;
+export declare const animations: import("@tamagui/web").AnimationDriver<{
+    '0ms': string;
+    '50ms': string;
+    '75ms': string;
+    '100ms': string;
+    '200ms': string;
+    '250ms': string;
+    '300ms': string;
+    '400ms': string;
+    '500ms': string;
+    superBouncy: string;
+    bouncy: string;
+    superLazy: string;
+    lazy: string;
+    medium: string;
+    slowest: string;
+    slow: string;
+    quick: string;
+    quickLessBouncy: string;
+    quicker: string;
+    quickerLessBouncy: string;
+    quickest: string;
+    quickestLessBouncy: string;
+}>;
 //# sourceMappingURL=v5-css.d.ts.map
--- code/core/config/types/v5-reanimated.d.ts
+++ code/core/config/types/v5-reanimated.d.ts
@@ -99,4 +99,105 @@ export declare const animationsReanimated: import("@tamagui/web").AnimationDrive
         stiffness: number;
     };
 }>;
+export declare const animations: import("@tamagui/web").AnimationDriver<{
+    '0ms': {
+        type: "timing";
+        duration: number;
+    };
+    '50ms': {
+        type: "timing";
+        duration: number;
+    };
+    '75ms': {
+        type: "timing";
+        duration: number;
+    };
+    '100ms': {
+        type: "timing";
+        duration: number;
+    };
+    '200ms': {
+        type: "timing";
+        duration: number;
+    };
+    '250ms': {
+        type: "timing";
+        duration: number;
+    };
+    '300ms': {
+        type: "timing";
+        duration: number;
+    };
+    '400ms': {
+        type: "timing";
+        duration: number;
+    };
+    '500ms': {
+        type: "timing";
+        duration: number;
+    };
+    superBouncy: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    bouncy: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    superLazy: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    medium: {
+        damping: number;
+        stiffness: number;
+        mass: number;
+    };
+    lazy: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    slowest: {
+        damping: number;
+        stiffness: number;
+    };
+    slow: {
+        damping: number;
+        stiffness: number;
+    };
+    quick: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    quickLessBouncy: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    quicker: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    quickerLessBouncy: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    quickest: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+    quickestLessBouncy: {
+        damping: number;
+        mass: number;
+        stiffness: number;
+    };
+}>;
 //# sourceMappingURL=v5-reanimated.d.ts.map
