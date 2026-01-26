f163f28803fc11da3b53533019199e554abdbe6d
natew <natewienert@gmail.com>
2026-01-26 10:03:06 -1000

    wip(toast): toast v2 updates and test cleanup

--- code/starters/expo-router/components/CurrentToast.tsx
+++ code/starters/expo-router/components/CurrentToast.tsx
@@ -1,44 +1,15 @@
-import { Toast, useToastController, useToastState } from '@tamagui/toast'
-import { Button, H4, XStack, YStack, isWeb } from 'tamagui'
-
-export function CurrentToast() {
-  const currentToast = useToastState()
-
-  if (!currentToast || currentToast.isHandledNatively) return null
-
-  return (
-    <Toast
-      key={currentToast.id}
-      duration={currentToast.duration}
-      viewportName={currentToast.viewportName}
-      enterStyle={{ opacity: 0, scale: 0.5, y: -25 }}
-      exitStyle={{ opacity: 0, scale: 1, y: -20 }}
-      y={isWeb ? '$12' : 0}
-      theme="accent"
-      rounded="$6"
-      transition="quick"
-    >
-      <YStack items="center" p="$2" gap="$2">
-        <Toast.Title fontWeight="bold">{currentToast.title}</Toast.Title>
-        {!!currentToast.message && (
-          <Toast.Description>{currentToast.message}</Toast.Description>
-        )}
-      </YStack>
-    </Toast>
-  )
-}
+import { toast } from '@tamagui/toast'
+import { Button, H4, XStack, YStack } from 'tamagui'
 
 export function ToastControl() {
-  const toast = useToastController()
-
   return (
     <YStack gap="$2" items="center">
       <H4>Toast demo</H4>
       <XStack gap="$2" justify="center">
         <Button
           onPress={() => {
-            toast.show('Successfully saved!', {
-              message: "Don't worry, we've got your data.",
+            toast.success('Successfully saved!', {
+              description: "Don't worry, we've got your data.",
             })
           }}
         >
@@ -46,7 +17,7 @@ export function ToastControl() {
         </Button>
         <Button
           onPress={() => {
-            toast.hide()
+            toast.dismiss()
           }}
         >
           Hide
--- code/starters/expo-router/components/Provider.tsx
+++ code/starters/expo-router/components/Provider.tsx
@@ -1,7 +1,6 @@
 import { useColorScheme } from 'react-native'
 import { TamaguiProvider, type TamaguiProviderProps } from 'tamagui'
-import { ToastProvider, ToastViewport } from '@tamagui/toast'
-import { CurrentToast } from './CurrentToast'
+import { Toaster } from '@tamagui/toast'
 import { config } from '../tamagui.config'
 
 export function Provider({
@@ -16,20 +15,8 @@ export function Provider({
       defaultTheme={colorScheme === 'dark' ? 'dark' : 'light'}
       {...rest}
     >
-      <ToastProvider
-        swipeDirection="horizontal"
-        duration={6000}
-        native={
-          [
-            // uncomment the next line to do native toasts on mobile. NOTE: it'll require you making a dev build and won't work with Expo Go
-            // 'mobile'
-          ]
-        }
-      >
-        {children}
-        <CurrentToast />
-        <ToastViewport top="$8" left={0} right={0} />
-      </ToastProvider>
+      {children}
+      <Toaster position="top-center" />
     </TamaguiProvider>
   )
 }
