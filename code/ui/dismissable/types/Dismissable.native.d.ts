fec6200b507f0ef5e4130b4648a8475de11d64da
natew <natewienert@gmail.com>
2026-01-31 15:34:04 -1000

    fix(dismissable): export dispatchDiscreteCustomEvent stub for native
    Required for create-menu to bundle correctly on native.

--- code/ui/dismissable/types/Dismissable.native.d.ts
+++ code/ui/dismissable/types/Dismissable.native.d.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 import type { DismissableBranchProps, DismissableProps } from './DismissableProps';
+export declare function dispatchDiscreteCustomEvent<E extends CustomEvent>(_target: E['target'], _event: E): void;
 export declare const Dismissable: React.ForwardRefExoticComponent<DismissableProps & React.RefAttributes<unknown>>;
 export declare const DismissableBranch: React.ForwardRefExoticComponent<DismissableBranchProps & React.RefAttributes<unknown>>;
 //# sourceMappingURL=Dismissable.native.d.ts.map
