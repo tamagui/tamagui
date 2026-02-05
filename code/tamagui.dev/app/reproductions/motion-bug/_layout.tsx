bb38d84b641c35a09346a0c1fe3816df6480edb7
natew <natewienert@gmail.com>
2026-02-05 04:22:25 -1000

    site: fix motion-bug reproduction import path

--- code/tamagui.dev/app/reproductions/motion-bug/_layout.tsx
+++ code/tamagui.dev/app/reproductions/motion-bug/_layout.tsx
@@ -1,5 +1,5 @@
 import { Configuration } from '@tamagui/web'
-import { animationsMotion } from '@tamagui/tamagui-dev-config/motion'
+import { animationsMotion } from '@tamagui/tamagui-dev-config/v5-motion'
 import { Slot } from 'one'
 import { Header } from '~/features/site/header/Header'
 
