8777d674065952d19baf70e6d21354ad8a79c6df
DaveyEke <daveydenco@gmail.com>
2026-02-09 18:48:44 +0100

    add kitchen-sink usecase

--- code/kitchen-sink/src/usecases/PlaceholderTextColor.tsx
+++ code/kitchen-sink/src/usecases/PlaceholderTextColor.tsx
@@ -1,5 +1,20 @@
-import { Button, Input } from 'tamagui'
+import { Input, Text, YStack } from 'tamagui'
 
 export const PlaceholderTextColor = () => (
-  <Input placeholder="hello world" placeholderTextColor="$color10" />
+  <YStack gap="$3" p="$4">
+    <Text>Raw color "red":</Text>
+    <Input placeholder="should be red" placeholderTextColor="red" />
+
+    <Text>Raw hex "#00ff00":</Text>
+    <Input placeholder="should be green" placeholderTextColor="#00ff00" />
+
+    <Text>Token "$color10":</Text>
+    <Input placeholder="should be $color10" placeholderTextColor="$color10" />
+
+    <Text>Token "$blue10":</Text>
+    <Input placeholder="should be blue" placeholderTextColor="$blue10" />
+
+    <Text>No placeholderTextColor (default):</Text>
+    <Input placeholder="default placeholder color" />
+  </YStack>
 )
