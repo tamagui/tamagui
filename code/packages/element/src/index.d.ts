9e8f501516122cbb9d96472ce06cde2fe4dedf1f
Quan Nguyen <anhquan291@gmail.com>
2026-02-10 16:31:47 +0700

    fix: remove deprecated getWebElement and related utility functions

--- code/packages/element/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './useWebRef'
-export * from './useNativeRef'
-export * from './getWebElement'
-export * from './types'
--- code/packages/element/src/index.js
+++
@@ -1,5 +0,0 @@
-export * from './useWebRef'
-export * from './useNativeRef'
-export * from './getWebElement'
-export * from './types'
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJpbmRleC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSxjQUFjLGFBQWEsQ0FBQTtBQUMzQixjQUFjLGdCQUFnQixDQUFBO0FBQzlCLGNBQWMsaUJBQWlCLENBQUE7QUFDL0IsY0FBYyxTQUFTLENBQUEifQ==
