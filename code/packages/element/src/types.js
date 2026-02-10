7879a4a1f35ece9ce438a89999b8425f89f34953
Quan Nguyen <anhquan291@gmail.com>
2026-02-10 16:31:47 +0700

    fix: remove deprecated getWebElement and related utility functions

--- code/packages/element/src/types.js
+++
@@ -1,2 +0,0 @@
-export {}
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidHlwZXMuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJ0eXBlcy50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
