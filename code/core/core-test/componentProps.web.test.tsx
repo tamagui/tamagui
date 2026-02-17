ba844815cdfdd7861c51a06a77e1fa81e8170736
natew <natewienert@gmail.com>
2026-02-17 11:35:56 -1000

    fix: update componentProps snapshot to match TamaguiRoot Theme wrapper refactor

--- code/core/core-test/componentProps.web.test.tsx
+++ code/core/core-test/componentProps.web.test.tsx
@@ -17,18 +17,17 @@ describe('animation props', () => {
     expect(tree.asFragment()).toMatchInlineSnapshot(`
       <DocumentFragment>
         <span
-          class="font_body"
-          style="display: contents; font-family: var(--f-family);"
+          class="t_light _dsp_contents"
         >
           <span
-            class="t_light _dsp_contents"
+            class=" t_light is_Theme"
+            style="color: var(--color); display: contents;"
           >
             <span
-              class=" t_light is_Theme"
-              style="color: var(--color); display: contents;"
+              class="_dsp_contents  font_body"
             >
               <div
-                class="_pos-static _dsp-flex _ai-stretch _fd-column _fb-auto _bxs-border-box _mih-0px _miw-0px _fs-0 _bg-red _mt-200px _mr-200px _mb-200px _ml-200px _tr-translateX01303033"
+                class="_t_d_font _pos-static _dsp-flex _ai-stretch _fd-column _fb-auto _bxs-border-box _mih-0px _miw-0px _fs-0 _bg-red _mt-200px _mr-200px _mb-200px _ml-200px _tr-translateX01303033"
                 id="test-id"
                 style="transition: all cubic-bezier(0.215, 0.610, 0.355, 1.000) 400ms;"
               />
