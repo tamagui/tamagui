299130bc7cd603c7135ee67d8832e6b881f029d2
natew <natewienert@gmail.com>
2026-03-17 19:31:58 -1000

    cleanups and export comapt for pressability

--- code/core/core/types/createOptimizedView.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function createOptimizedView(children: any, viewProps: Record<string, any>, baseViews: any): void;
-//# sourceMappingURL=createOptimizedView.d.ts.map
--- code/core/core/types/getBaseViews.native.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function getBaseViews(): {
-    View: any;
-    Text: any;
-    TextAncestor: any;
-    StyleSheet: any;
-    Pressable: any;
-};
-//# sourceMappingURL=getBaseViews.native.d.ts.map
