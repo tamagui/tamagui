654aa7c8ae3fc9b41c9d1991fdc3e5f4c45979bc
natew <natewienert@gmail.com>
2026-03-23 20:27:28 -1000

    chore(core): remove pressability hack! no more deep imports

--- code/core/core/src/vendor/Pressability.native.tsx
+++
@@ -1,4 +0,0 @@
-export const Pressability =
-  require('react-native/Libraries/Pressability/Pressability').default
-export const usePressability =
-  require('react-native/Libraries/Pressability/usePressability').default
