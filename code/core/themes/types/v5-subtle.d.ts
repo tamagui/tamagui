b4358e80d1fc6e6d3749f79df025cfd53877c8fd
natew <natewienert@gmail.com>
2026-04-05 16:40:29 -1000

    canary2.0.0-rc.36-1775443057426

--- code/core/themes/types/v5-subtle.d.ts
+++ code/core/themes/types/v5-subtle.d.ts
@@ -1,6 +1,7 @@
 export { createThemes } from '@tamagui/theme-builder';
 export { themes } from './generated-v5-subtle';
 export type { Theme as V5SubtleTheme, ThemeNames as V5SubtleThemeNames, Themes as V5SubtleThemes, } from './generated-v5-subtle';
+export { subtleChildrenThemes, v5SubtlePaletteAdjustments } from './subtleChildrenThemes';
 export { v5Templates } from './v5-templates';
 export * from './v5-themes-subtle';
 export { tokens } from './v5-tokens';
