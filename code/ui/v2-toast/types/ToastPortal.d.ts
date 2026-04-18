4fba3501bf600af2e1114a041cc3222caa387d72
natew <natewienert@gmail.com>
2026-04-17 14:08:15 -1000

    chore: remove old v2 toast experiment

--- code/ui/v2-toast/types/ToastPortal.d.ts
+++
@@ -1,8 +0,0 @@
-import type { ReactNode } from 'react';
-import { type ToastProviderContextValue } from './ToastProvider';
-export declare function ToastPortal(props: {
-    children: ReactNode;
-    zIndex?: number;
-    context: ToastProviderContextValue;
-}): import("react/jsx-runtime").JSX.Element;
-//# sourceMappingURL=ToastPortal.d.ts.map
--- code/ui/v2-toast/types/createNativeToast.native.d.ts
+++
@@ -1,4 +0,0 @@
-import type { CreateNativeToastsFn, HideNativeToastsFn } from './types';
-export declare const createNativeToast: CreateNativeToastsFn;
-export declare const hideNativeToast: HideNativeToastsFn;
-//# sourceMappingURL=createNativeToast.native.d.ts.map
