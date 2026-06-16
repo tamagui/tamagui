0a097c11ec4da6ebaa03630e424519e85b360884
natew <natewienert@gmail.com>
2026-06-16 10:30:02 -1000

    chore: migrate remix-starter from Remix v2 (EOL) to React Router 7 (clears EOL transitive advisories: tar/turbo-stream/estree-util-value-to-estree/vite/undici/js-yaml)

--- code/starters/remix/vite.config.ts
+++ code/starters/remix/vite.config.ts
@@ -1,4 +1,4 @@
-import { vitePlugin as remix } from '@remix-run/dev'
+import { reactRouter } from '@react-router/dev/vite'
 import { defineConfig } from 'vite'
 import { tamaguiPlugin } from '@tamagui/vite-plugin'
 
@@ -7,13 +7,7 @@ export default defineConfig({
     tsconfigPaths: true,
   },
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-      },
-    }),
+    reactRouter(),
     tamaguiPlugin(),
   ],
 })
