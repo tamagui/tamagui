4a8930502c265987ad0f13cbc90aaca331d642ee
natew <natewienert@gmail.com>
2026-07-08 19:49:41 -1000

    fix(dialog): remove dead code and gate remove-scroll

--- code/ui/dialog/src/Dialog.tsx
+++ code/ui/dialog/src/Dialog.tsx
@@ -71,8 +71,6 @@ type DialogProps = ScopedProps<{
   onAnimationComplete?: (info: { open: boolean }) => void
 }>
 
-type NonNull<A> = Exclude<A, void | null>
-
 type DialogContextValue = {
   forceMount?: boolean
   keepChildrenMounted?: boolean
@@ -83,9 +81,9 @@ type DialogContextValue = {
   titleId: string
   descriptionId: string
   onOpenToggle(): void
-  open: NonNull<DialogProps['open']>
-  onOpenChange: NonNull<DialogProps['onOpenChange']>
-  modal: NonNull<DialogProps['modal']>
+  open: Exclude<DialogProps['open'], void | null>
+  onOpenChange: Exclude<DialogProps['onOpenChange'], void | null>
+  modal: Exclude<DialogProps['modal'], void | null>
   dialogScope: DialogScopes
   adaptScope: string
   onAnimationComplete?: DialogProps['onAnimationComplete']
@@ -225,9 +223,7 @@ const DialogPortalItem = ({
   return isAdapted ? (
     <AdaptPortalContents scope={context.adaptScope}>{content}</AdaptPortalContents>
   ) : context.modal ? (
-    <PortalItem hostName={context.modal ? 'root' : context.adaptScope}>
-      {content}
-    </PortalItem>
+    <PortalItem hostName="root">{content}</PortalItem>
   ) : (
     content
   )
@@ -358,9 +354,6 @@ const PassthroughTheme = ({
 
 const OVERLAY_NAME = 'DialogOverlay'
 
-/**
- * exported for internal use with extractable()
- */
 export const DialogOverlayFrame = styled(YStack, {
   name: OVERLAY_NAME,
   zIndex: 1,
@@ -417,11 +410,6 @@ const DialogOverlay = createStyledHOC(DialogOverlayFrame)<DialogOverlayExtraProp
     return (
       <DialogOverlayFrame
         data-state={getState(context.open)}
-        // TODO: this will be apply for v2
-        // onPress={() => {
-        //   // if the overlay is pressed, close the dialog
-        //   context.onOpenChange(false)
-        // }}
         // We re-enable pointer-events prevented by `Dialog.Content` to allow scrolling the overlay.
         pointerEvents={context.open ? 'auto' : 'none'}
         {...overlayProps}
@@ -442,12 +430,6 @@ const DialogContentFrame = styled(ThemeableStack, {
   zIndex: 2,
 
   variants: {
-    size: {
-      '...size': (val, extras) => {
-        return {}
-      },
-    },
-
     unstyled: {
       false: {
         position: 'relative',
@@ -464,7 +446,6 @@ const DialogContentFrame = styled(ThemeableStack, {
   } as const,
 
   defaultVariants: {
-    size: true,
     unstyled: process.env.TAMAGUI_HEADLESS === '1',
   },
 })
@@ -496,7 +477,7 @@ const DialogContent = createStyledHOC(DialogContentFrame)<DialogContentExtraProp
     }
 
     return (
-      <RemoveScroll enabled={context.open}>
+      <RemoveScroll enabled={context.open && context.modal}>
         <div data-remove-scroll-container className="_dsp_contents">
           {contents}
         </div>
@@ -886,13 +867,9 @@ const DescriptionWarning: React.FC<DescriptionWarningProps> = ({
  * Dialog
  * -----------------------------------------------------------------------------------------------*/
 
-export type DialogHandle = {
-  open: (val: boolean) => void
-}
-
 const Dialog = withStaticProperties(
-  createRefComponent<{ open: (val: boolean) => void }, DialogProps>(
-    function Dialog(props, ref) {
+  createRefComponent<TamaguiElement, DialogProps>(
+    function Dialog(props) {
       const {
         scope = '',
         children,
@@ -943,14 +920,6 @@ const Dialog = withStaticProperties(
         onAnimationComplete,
       } satisfies DialogContextValue
 
-      React.useImperativeHandle(
-        ref,
-        () => ({
-          open: setOpen,
-        }),
-        [setOpen]
-      )
-
       return (
         <AdaptParent
           scope={adaptScope}
@@ -979,8 +948,6 @@ const Dialog = withStaticProperties(
     Adapt,
   }
 )
-
-const getAdaptScope = (dialogScope: string) => `DialogAdapt${dialogScope}`
 
 const DialogSheetController = (
   props: ScopedProps<{
