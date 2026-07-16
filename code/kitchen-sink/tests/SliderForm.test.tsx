2fbd17552e3785dd6b4ee1fbb5df211ba64e280d
natew <natewienert@gmail.com>
2026-07-16 09:51:46 -1000

    chore: format SliderForm test

--- code/kitchen-sink/tests/SliderForm.test.tsx
+++ code/kitchen-sink/tests/SliderForm.test.tsx
@@ -7,7 +7,9 @@ test('slider values participate in form submission', async ({ page }) => {
 
   const form = page.getByTestId('slider-form')
   const values = async () =>
-    form.evaluate((element) => Array.from(new FormData(element as HTMLFormElement).getAll('range')))
+    form.evaluate((element) =>
+      Array.from(new FormData(element as HTMLFormElement).getAll('range'))
+    )
 
   await expect(page.locator('input[type="hidden"][name="range"]')).toHaveCount(2)
   await expect.poll(values).toEqual(['10', '90'])
