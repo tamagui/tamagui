d1a833b4d5e31f19b9263c62ed8c43948644828d
natew <natewienert@gmail.com>
2026-07-30 19:41:46 -1000

    fix(helpers): withStaticProperties clones already-decorated components so extending a shared compound (bento AvatarsTooltip) no longer swaps Tooltip.Content globally and teleports open tooltips
    Agentbus-Session: m2405

--- code/kitchen-sink/src/usecases/index.web.ts
+++ code/kitchen-sink/src/usecases/index.web.ts
@@ -260,6 +260,8 @@ const loaders: Record<string, () => ComponentType<any>> = {
   TooltipGlobalPatternCase: () =>
     require('./TooltipGlobalPatternCase').TooltipGlobalPatternCase,
   TooltipGroupCase: () => require('./TooltipGroupCase').TooltipGroupCase,
+  TooltipStaticClobberCase: () =>
+    require('./TooltipStaticClobberCase').TooltipStaticClobberCase,
   TooltipToolbarRowCase: () => require('./TooltipToolbarRowCase').TooltipToolbarRowCase,
   TooltipPositionJumpCase: () =>
     require('./TooltipPositionJumpCase').TooltipPositionJumpCase,
