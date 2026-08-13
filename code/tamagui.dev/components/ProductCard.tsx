2d0c7f6a10f5ab1d5229fbad0e547f54a31f8e28
natew <natewienert@gmail.com>
2026-08-12 16:10:06 -1000

    site: use program-eligible rotate for conditional card rotation
    Agentbus-Session: m5375

--- code/tamagui.dev/components/ProductCard.tsx
+++ code/tamagui.dev/components/ProductCard.tsx
@@ -67,7 +67,7 @@ export function ProductCard({ product, children, ...props }) {
             transition="quicker"
             y="-10 lg:-6 group-hover/card:-4-5"
             x="-0-25 group-hover/card:-3-5"
-            rotateZ="-5deg group-hover/card:0deg"
+            rotate="-5deg group-hover/card:0deg"
             scale="1 group-hover/card:1.15"
           >
             {Icon}
