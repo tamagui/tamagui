3db11f49b670f6d9c7cafbf2c3fd129fe9efa8f7
natew <natewienert@gmail.com>
2026-08-12 16:10:05 -1000

    test(kitchen-sink): replace stale gtSm media key caught by tamagui check
    Agentbus-Session: m5375

--- code/kitchen-sink/src/usecases/CodeExamplesInput.tsx
+++ code/kitchen-sink/src/usecases/CodeExamplesInput.tsx
@@ -1,7 +1,7 @@
 import { SandboxHeading, View } from '@tamagui/sandbox-ui'
 
 export const CodeExamplesInput = (props) => (
-  <View px="2 gtSm:6" width={550}>
+  <View px="2 sm:6" width={550}>
     <SandboxHeading size={props.big ? 'large' : 'small'}>
       Lorem ipsum dolor.
     </SandboxHeading>
