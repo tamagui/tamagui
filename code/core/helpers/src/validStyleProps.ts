53cbbd3963181fbef2b7d4719d0ed24319185701
natew <natewienert@gmail.com>
2026-08-31 10:52:01 -1000

    fix(types): preserve exact style prop keys
    Team-Machine-Session: r13197

--- code/core/helpers/src/validStyleProps.ts
+++ code/core/helpers/src/validStyleProps.ts
@@ -9,18 +9,13 @@ import { toStylePropsObject as toObj } from './toStylePropsObject'
 export { tokenCategories } from './tokenCategories'
 
 export const cssShorthandLonghands = toObj(
-  'borderWidth borderStyle borderColor borderTopWidth borderTopStyle borderTopColor ' +
-    'borderRightWidth borderRightStyle borderRightColor borderBottomWidth borderBottomStyle ' +
-    'borderBottomColor borderLeftWidth borderLeftStyle borderLeftColor outlineWidth outlineStyle outlineColor outlineOffset'
+  'borderWidth borderStyle borderColor borderTopWidth borderTopStyle borderTopColor borderRightWidth borderRightStyle borderRightColor borderBottomWidth borderBottomStyle borderBottomColor borderLeftWidth borderLeftStyle borderLeftColor outlineWidth outlineStyle outlineColor outlineOffset'
 )
 
 const textColors = toObj('color textDecorationColor textShadowColor')
 
 const nonAnimatableViewProps = toObj(
-  'alignContent alignItems alignSelf backfaceVisibility borderCurve borderStyle ' +
-    'borderBlockStyle borderBlockEndStyle borderBlockStartStyle borderInlineStyle ' +
-    'borderInlineEndStyle borderInlineStartStyle boxSizing cursor direction display ' +
-    'flexDirection flexWrap isolation justifyContent mixBlendMode outlineStyle overflow pointerEvents position visibility'
+  'alignContent alignItems alignSelf backfaceVisibility borderCurve borderStyle borderBlockStyle borderBlockEndStyle borderBlockStartStyle borderInlineStyle borderInlineEndStyle borderInlineStartStyle boxSizing cursor direction display flexDirection flexWrap isolation justifyContent mixBlendMode outlineStyle overflow pointerEvents position visibility'
 )
 
 const nonAnimatableFontProps = toObj('fontFamily fontStyle fontVariant textTransform')
@@ -44,10 +39,7 @@ export const nonAnimatableStyleProps = toObj(
 
 export const stylePropsUnitless = toObj(
   nonAnimatableUnitlessProps,
-  'animationIterationCount aspectRatio borderImageOutset borderImageSlice borderImageWidth ' +
-    'columnCount flex flexGrow flexOrder flexPositive flexShrink flexNegative fontWeight ' +
-    'gridRow gridRowEnd gridRowGap gridRowStart gridColumn gridColumnEnd gridColumnGap gridColumnStart ' +
-    'opacity order orphans tabSize widows zIndex zoom scale scaleX scaleY scaleZ shadowOpacity'
+  'animationIterationCount aspectRatio borderImageOutset borderImageSlice borderImageWidth columnCount flex flexGrow flexOrder flexPositive flexShrink flexNegative fontWeight gridRow gridRowEnd gridRowGap gridRowStart gridColumn gridColumnEnd gridColumnGap gridColumnStart opacity order orphans tabSize widows zIndex zoom scale scaleX scaleY scaleZ shadowOpacity'
 )
 
 export const stylePropsTransform = toObj(
@@ -56,25 +48,9 @@ export const stylePropsTransform = toObj(
 
 export const stylePropsView = toObj(
   nonAnimatableViewProps,
-  'borderBottomWidth borderLeftWidth borderRightWidth borderBlockWidth borderBlockEndWidth ' +
-    'borderBlockStartWidth borderInlineWidth borderInlineEndWidth borderInlineStartWidth ' +
-    'borderTopWidth borderWidth transform transformOrigin borderEndWidth borderStartWidth ' +
-    'bottom end flexBasis gap columnGap rowGap left margin marginBlock marginBlockEnd ' +
-    'marginBlockStart marginInline marginInlineStart marginInlineEnd marginBottom marginEnd ' +
-    'marginHorizontal marginLeft marginRight marginStart marginTop marginVertical padding ' +
-    'paddingBottom paddingInline paddingBlock paddingBlockEnd paddingBlockStart paddingInlineEnd ' +
-    'paddingInlineStart paddingEnd paddingHorizontal paddingLeft paddingRight paddingStart ' +
-    'paddingTop paddingVertical right start top inset insetBlock insetBlockEnd insetBlockStart ' +
-    'insetInline insetInlineEnd insetInlineStart shadowOffset backgroundColor borderColor ' +
-    'borderBlockStartColor borderBlockEndColor borderBlockColor borderBottomColor borderInlineColor ' +
-    'borderInlineStartColor borderInlineEndColor borderTopColor borderLeftColor borderRightColor ' +
-    'borderEndColor borderStartColor shadowColor outlineColor ' +
-    (process.env.TAMAGUI_TARGET === 'web' ? 'caretColor ' : '') +
-    'borderRadius borderTopLeftRadius borderTopRightRadius borderBottomLeftRadius borderBottomRightRadius ' +
-    'borderTopStartRadius borderTopEndRadius borderBottomStartRadius borderBottomEndRadius ' +
-    'borderStartStartRadius borderStartEndRadius borderEndStartRadius borderEndEndRadius ' +
-    'width height minWidth minHeight maxWidth maxHeight blockSize minBlockSize maxBlockSize ' +
-    'inlineSize minInlineSize maxInlineSize shadowRadius',
+  'borderBottomWidth borderLeftWidth borderRightWidth borderBlockWidth borderBlockEndWidth borderBlockStartWidth borderInlineWidth borderInlineEndWidth borderInlineStartWidth borderTopWidth borderWidth transform transformOrigin borderEndWidth borderStartWidth bottom end flexBasis gap columnGap rowGap left margin marginBlock marginBlockEnd marginBlockStart marginInline marginInlineStart marginInlineEnd marginBottom marginEnd marginHorizontal marginLeft marginRight marginStart marginTop marginVertical padding paddingBottom paddingInline paddingBlock paddingBlockEnd paddingBlockStart paddingInlineEnd paddingInlineStart paddingEnd paddingHorizontal paddingLeft paddingRight paddingStart paddingTop paddingVertical right start top inset insetBlock insetBlockEnd insetBlockStart insetInline insetInlineEnd insetInlineStart shadowOffset backgroundColor borderColor borderBlockStartColor borderBlockEndColor borderBlockColor borderBottomColor borderInlineColor borderInlineStartColor borderInlineEndColor borderTopColor borderLeftColor borderRightColor borderEndColor borderStartColor shadowColor outlineColor',
+  process.env.TAMAGUI_TARGET === 'web' ? 'caretColor' : undefined,
+  'borderRadius borderTopLeftRadius borderTopRightRadius borderBottomLeftRadius borderBottomRightRadius borderTopStartRadius borderTopEndRadius borderBottomStartRadius borderBottomEndRadius borderStartStartRadius borderStartEndRadius borderEndStartRadius borderEndEndRadius width height minWidth minHeight maxWidth maxHeight blockSize minBlockSize maxBlockSize inlineSize minInlineSize maxInlineSize shadowRadius',
   stylePropsTransform,
   stylePropsUnitless,
   isAndroid ? { elevationAndroid: true } : undefined,
--- code/core/helpers/src/webOnlyStyleProps.ts
+++ code/core/helpers/src/webOnlyStyleProps.ts
@@ -3,11 +3,7 @@
 import { toStylePropsObject as toObj } from './toStylePropsObject'
 
 export const nonAnimatableWebViewProps = toObj(
-  'backgroundAttachment backgroundBlendMode backgroundClip backgroundOrigin backgroundRepeat ' +
-    'borderBottomStyle borderLeftStyle borderRightStyle borderTopStyle contain containerType ' +
-    'containerName content float maskBorderMode maskBorderRepeat maskClip maskComposite ' +
-    'maskMode maskOrigin maskRepeat maskType objectFit overflowBlock overflowInline ' +
-    'overflowX overflowY scrollbarWidth textWrap touchAction transformStyle willChange'
+  'backgroundAttachment backgroundBlendMode backgroundClip backgroundOrigin backgroundRepeat borderBottomStyle borderLeftStyle borderRightStyle borderTopStyle contain containerType containerName content float maskBorderMode maskBorderRepeat maskClip maskComposite maskMode maskOrigin maskRepeat maskType objectFit overflowBlock overflowInline overflowX overflowY scrollbarWidth textWrap touchAction transformStyle willChange'
 )
 
 export const nonAnimatableWebTextProps = toObj(
@@ -16,11 +12,7 @@ export const nonAnimatableWebTextProps = toObj(
 
 export const webOnlyStylePropsView = toObj(
   nonAnimatableWebViewProps,
-  'transition transitionProperty transitionDuration transitionTimingFunction transitionDelay ' +
-    'transitionBehavior backdropFilter WebkitBackdropFilter borderTop borderRight borderBottom ' +
-    'borderLeft backgroundPosition backgroundSize borderImage caretColor clipPath mask ' +
-    'maskBorder maskBorderOutset maskBorderSlice maskBorderSource maskBorderWidth maskImage ' +
-    'maskPosition maskSize objectPosition textEmphasis userSelect overflowWrap wordWrap resize'
+  'transition transitionProperty transitionDuration transitionTimingFunction transitionDelay transitionBehavior backdropFilter WebkitBackdropFilter borderTop borderRight borderBottom borderLeft backgroundPosition backgroundSize borderImage caretColor clipPath mask maskBorder maskBorderOutset maskBorderSlice maskBorderSource maskBorderWidth maskImage maskPosition maskSize objectPosition textEmphasis userSelect overflowWrap wordWrap resize'
 )
 
 export const webOnlyStylePropsText = toObj(
