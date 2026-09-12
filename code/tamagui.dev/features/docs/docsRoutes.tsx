801478b09b57cbbc55be5954a81016f631e9e6d0
natew <natewienert@gmail.com>
2026-09-11 15:04:44 -1000

    docs: name styling functions with call parentheses
    Team-Machine-Session: r28236

--- code/tamagui.dev/features/docs/docsRoutes.tsx
+++ code/tamagui.dev/features/docs/docsRoutes.tsx
@@ -36,8 +36,8 @@ export const docsRoutes = [
       { title: 'Styling', route: '/docs/intro/styles' },
       { title: 'styled()', route: '/docs/core/styled' },
       { title: 'Variants', route: '/docs/core/variants' },
-      { title: 'Style Pieces', route: '/docs/core/style-pieces' },
-      { title: 'createStyledContext', route: '/docs/core/create-styled-context' },
+      { title: 'style()', route: '/docs/core/style-pieces' },
+      { title: 'createStyledContext()', route: '/docs/core/create-styled-context' },
     ],
   },
 
